b6566d6521e8db3b5ee1b852e6a8656769eb98ce
Samuel Alev <samuel.alev@frontify.com>
2021-07-07 16:16:03 +0200

    refactor: update export of components

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,49 @@
-export * from "./components/Badge/Badge";
-export * from "./components/Breadcrumb/Breadcrumb";
-export * from "./components/Button/Button";
-export * from "./components/Card/Card";
-export * from "./components/Checkbox/Checkbox";
-export * from "./components/Divider/Divider";
-export * from "./components/Form/Label/Label";
-export * from "./components/Form/TextInput/TextInput";
-export * from "./components/Form/Textarea/Textarea";
-export * from "./components/Iframe/Iframe";
-export * from "./components/Image/Image";
-export * from "./components/Image/FrontifyImage";
-export * from "./components/Modal/Modal";
-export * from "./components/Modal/ModalPortal";
-export * from "./components/Sidebar/Sidebar";
-export * from "./components/Sidebar/SidebarItemCheckbox/SidebarItemCheckbox";
-export * from "./components/Sidebar/SidebarItemDefault/SidebarItemDefault";
-export * from "./components/Sidebar/SidebarItemLink/SidebarItemLink";
-export * from "./components/Sidebar/SidebarItemSearch/SidebarItemSearch";
-export * from "./components/Stepper/Stepper";
-export * from "./components/Tabs/Tabs";
-export * from "./components/Tabs/Tab/Tab";
-export * from "./components/Tabs/TabTitle/TabTitle";
+import Badge from "./components/Badge/Badge";
+import Breadcrumb from "./components/Breadcrumb/Breadcrumb";
+import Button from "./components/Button/Button";
+import Card from "./components/Card/Card";
+import Checkbox from "./components/Checkbox/Checkbox";
+import Divider from "./components/Divider/Divider";
+import Label from "./components/Form/Label/Label";
+import TextInput from "./components/Form/TextInput/TextInput";
+import Textarea from "./components/Form/Textarea/Textarea";
+import Iframe from "./components/Iframe/Iframe";
+import Image from "./components/Image/Image";
+import FrontifyImage from "./components/Image/FrontifyImage";
+import Modal from "./components/Modal/Modal";
+import ModalPortal from "./components/Modal/ModalPortal";
+import Sidebar from "./components/Sidebar/Sidebar";
+import SidebarItemCheckbox from "./components/Sidebar/SidebarItemCheckbox/SidebarItemCheckbox";
+import SidebarItemDefault from "./components/Sidebar/SidebarItemDefault/SidebarItemDefault";
+import SidebarItemLink from "./components/Sidebar/SidebarItemLink/SidebarItemLink";
+import SidebarItemSearch from "./components/Sidebar/SidebarItemSearch/SidebarItemSearch";
+import Stepper from "./components/Stepper/Stepper";
+import Tabs from "./components/Tabs/Tabs";
+import Tab from "./components/Tabs/Tab/Tab";
+import TabTitle from "./components/Tabs/TabTitle/TabTitle";
+
+export {
+    Badge,
+    Breadcrumb,
+    Button,
+    Card,
+    Checkbox,
+    Divider,
+    Label,
+    TextInput,
+    Textarea,
+    Iframe,
+    Image,
+    FrontifyImage,
+    Modal,
+    ModalPortal,
+    Sidebar,
+    SidebarItemCheckbox,
+    SidebarItemDefault,
+    SidebarItemLink,
+    SidebarItemSearch,
+    Stepper,
+    Tabs,
+    Tab,
+    TabTitle,
+};
