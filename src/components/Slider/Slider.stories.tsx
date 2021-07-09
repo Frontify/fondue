de7d6b2a31cd54181622411f47d1b9a30c2d2038
Nadia Posch <nadia@smartive.ch>
2021-07-09 10:56:50 +0200

    add icon slider story

--- src/components/Slider/Slider.stories.tsx
+++ src/components/Slider/Slider.stories.tsx
@@ -1,5 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import { ReactComponent as TextAlignCenter } from "@components/Icon/Svg/TextAlignCenter.svg";
 import { Meta, Story } from "@storybook/react";
 import Slider, { SliderProps } from "./Slider";
 
@@ -10,13 +11,22 @@ export default {
 
 const SliderTemplate: Story<SliderProps> = (args) => <Slider {...args} />;
 
-export const Default = SliderTemplate.bind({});
-
-Default.args = {
+export const Text = SliderTemplate.bind({});
+Text.args = {
     items: [
         { id: "a", name: "abc" },
         { id: "b", name: "def" },
         { id: "c", name: "ghi" },
+    ],
+    activeItemId: "a",
+};
+
+export const Icon = SliderTemplate.bind({});
+Icon.args = {
+    items: [
+        { id: "a", icon: <TextAlignCenter /> },
+        { id: "b", icon: <TextAlignCenter /> },
+        { id: "c", icon: <TextAlignCenter /> },
     ],
     activeItemId: "a",
 };
