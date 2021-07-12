dd8f97901a613bfbadfa3e7398ba8adbbfabc635
Nadia Posch <nadia@smartive.ch>
2021-07-12 16:41:39 +0200

    update tests

--- src/components/Dropdown/Dropdown.spec.tsx
+++ src/components/Dropdown/Dropdown.spec.tsx
@@ -1,17 +1,57 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import { mount } from "@cypress/react";
-
+import { useState } from "react";
 import Dropdown from "./Dropdown";
 
-const Dropdown_FOO = "bar";
+const DROPDOWN_ID = "[data-test-id=dropdown]";
+const MENU_ITEM_ID = "[data-test-id=dropdown-menu-item]";
+
+const SMALL_ITEMS = [
+    {
+        id: "1",
+        title: "Small",
+    },
+    {
+        id: "2",
+        title: "Small warning",
+        warning: true,
+    },
+    {
+        id: "3",
+        title: "Small disabled",
+        disabled: true,
+    },
+    {
+        id: "4",
+        title: "Small warning disabled",
+        warning: true,
+        disabled: true,
+    },
+];
+
+const Component = () => {
+    {
+        const [activeItemId, setActiveItemId] = useState("");
+        return (
+            <Dropdown
+                activeItemId={activeItemId}
+                onChange={setActiveItemId}
+                menuItems={SMALL_ITEMS}
+                placeholder="Select item"
+            />
+        );
+    }
+};
 
 describe("Dropdown Component", () => {
-    it("should render foo text correctly", () => {
-        mount(<Dropdown foo={Dropdown_FOO} />);
+    it("renders small items", () => {
+        mount(<Component />);
 
-        cy.get("[data-test-id=Dropdown]").as("Dropdown");
-
-        cy.get("@Dropdown").contains(Dropdown_FOO);
+        cy.get(DROPDOWN_ID).contains("Select item");
+        cy.get(DROPDOWN_ID).click();
+        cy.get(MENU_ITEM_ID).should("have.length", 5);
+        cy.get(MENU_ITEM_ID).eq(2).click();
+        cy.get(DROPDOWN_ID).contains("Small warning");
     });
 });
--- src/components/Dropdown/DropdownMenuItem/DropdownMenuItem.spec.tsx
+++ src/components/Dropdown/DropdownMenuItem/DropdownMenuItem.spec.tsx
@@ -1,17 +1,35 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import { mount } from "@cypress/react";
-
 import DropdownMenuItem from "./DropdownMenuItem";
 
-const DropdownMenuItem_FOO = "bar";
+const MENU_ITEM_ID = "[data-test-id=dropdown-menu-item]";
 
+const ITEMS = [
+    {
+        id: "1",
+        title: "Small",
+    },
+    {
+        id: "2",
+        title: "Small warning",
+        warning: true,
+    },
+    {
+        id: "3",
+        title: "Small disabled",
+        disabled: true,
+    },
+    {
+        id: "4",
+        title: "Small warning disabled",
+        warning: true,
+        disabled: true,
+    },
+];
 describe("DropdownMenuItem Component", () => {
     it("should render foo text correctly", () => {
-        mount(<DropdownMenuItem foo={DropdownMenuItem_FOO} />);
-
-        cy.get("[data-test-id=DropdownMenuItem]").as("DropdownMenuItem");
-
-        cy.get("@DropdownMenuItem").contains(DropdownMenuItem_FOO);
+        mount(<DropdownMenuItem item={ITEMS[0]} />);
+        cy.get(MENU_ITEM_ID).contains("Small");
     });
 });
