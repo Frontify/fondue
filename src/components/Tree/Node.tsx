4034651e8faa4e296990c47e7ca124b1a54c74a5
Marco Bohler <triggertoo@gmail.com>
2021-07-15 16:06:14 +0200

    update styles

--- src/components/Tree/Node.tsx
+++ src/components/Tree/Node.tsx
@@ -1,6 +1,7 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import { IconProps, IconSize } from "@components/Icon/Icon";
+import { ReactComponent as IconCaretDown } from "@components/Icon/Svg/CaretDown.svg";
 import { ReactComponent as IconCaretRight } from "@components/Icon/Svg/CaretRight.svg";
 import { ReactElement, useState } from "react";
 import css from "./Tree.module.css";
@@ -53,16 +54,16 @@ export default function Node({
                         onClick && onClick();
                     }}
                 >
-                    {nodes && (
-                        <IconCaretRight
-                            size={IconSize.Size16}
-                            className={showNodes ? `${css.nodeToggle} ${css.nodeToggleActive}` : css.nodeToggle}
-                        />
-                    )}
+                    {nodes &&
+                        (showNodes ? (
+                            <IconCaretDown size={IconSize.Size16} />
+                        ) : (
+                            <IconCaretRight size={IconSize.Size16} />
+                        ))}
                 </span>
                 {icon && <span className={css.nodeIcon}>{icon}</span>}
                 <span className={css.nodeName}>{name}</span>
-                <span className={css.nodeLabel}>{label}</span>
+                <span className={active ? [css.nodeLabel, css.nodeLabelActive].join(" ") : css.nodeLabel}>{label}</span>
             </a>
             {nodes && showNodes && (
                 <ul className={css.tree}>
