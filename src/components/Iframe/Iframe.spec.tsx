26310a15f15853e01252b51d1e70d44bd4066c38
Robert Vogt <robert@smartive.ch>
2021-07-28 17:36:45 +0200

    fix iframe test

--- src/components/Iframe/Iframe.spec.tsx
+++ src/components/Iframe/Iframe.spec.tsx
@@ -12,21 +12,21 @@ describe("Iframe component", () => {
         mount(<Iframe title={title} src={src} />);
         cy.get("iframe").should("have.class", css.iframe);
         cy.get("iframe").should("have.attr", "src").and("eq", src);
-        cy.get("iframe").should("eq", "attr.title", title);
+        cy.get("iframe").invoke("attr", "title").should("eq", title);
     });
     it("has borders", () => {
         mount(<Iframe title={title} src={src} border={IframeBorder.Default} />);
         cy.get("iframe").should("have.class", css.borderDefault);
-        cy.get("iframe").should("eq", "attr.title", title);
+        cy.get("iframe").invoke("attr", "title").should("eq", title);
     });
     it("has a small height", () => {
         mount(<Iframe title={title} src={src} height={IframeHeight.Small} />);
         cy.get("iframe").should("have.class", css.heightSmall);
-        cy.get("iframe").should("eq", "attr.title", title);
+        cy.get("iframe").invoke("attr", "title").should("eq", title);
     });
     it("has pointerEvents", () => {
         mount(<Iframe title={title} src={src} pointerEvents={IframePointerEvents.None} />);
         cy.get("iframe").should("have.class", css.pointerEventsNone);
-        cy.get("iframe").should("eq", "attr.title", title);
+        cy.get("iframe").invoke("attr", "title").should("eq", title);
     });
 });
