a356f58aeceaab9e6f2417321cabb6f091beb858
Samuel Alev <samuel.alev@frontify.com>
2021-07-30 16:47:35 +0200

    chore: fix issue with types

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,8 +1,12 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { resolve } from "path";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
 import { startDevServer } from "@cypress/vite-dev-server";
 
-export default (on) => {
-    on("dev-server:start", (options) => {
+export default (on: any): any => {
+    on("dev-server:start", (options: any) => {
         return startDevServer({
             options,
             viteConfig: {
