461b5e75aa2a13fabab221ab22c6610ca54b0e0c
Samuel Alev <samuel.alev@protonmail.com>
2021-08-05 13:00:59 +0200

    chore: regenerate icons

--- src/elements/Icon/Generated/IconApprove.tsx
+++ src/elements/Icon/Generated/IconApprove.tsx
@@ -1,19 +1,11 @@
 import * as React from "react";
 import { IconProps } from "@elements/Icon/IconProps";
-import { IconSize } from "@elements/Icon/IconSize";
-const iconSizeMap = {
-    [IconSize.Size8]: "h-2 w-2",
-    [IconSize.Size12]: "h-3 w-3",
-    [IconSize.Size16]: "h-4 w-4",
-    [IconSize.Size20]: "h-5 w-5",
-    [IconSize.Size24]: "h-6 w-6",
-    [IconSize.Size32]: "h-8 w-8",
-};
+import { IconSize, IconSizeMap } from "@elements/Icon/IconSize";
 
 function IconApprove(props: IconProps): React.ReactElement<IconProps> {
     const customClassName = [
         "flex items-center justify-center fill-current",
-        iconSizeMap[props.size || IconSize.Size16],
+        IconSizeMap[props.size || IconSize.Size16],
     ].join(" ");
     return (
         <svg
