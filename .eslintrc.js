c11694b7997d972b7677f15a1f60b4562a20f612
Samuel Alev <samuel.alev@protonmail.com>
2021-08-05 13:10:06 +0200

    chore: add back react version detection

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
     extends: ["@frontify/eslint-config-react", "plugin:jsx-a11y/recommended"],
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
     plugins: ["jsx-a11y"],
 };
