22bad417d35025d306706ecb14a7183522e9daad
Samuel Alev <samuel.alev@protonmail.com>
2021-08-07 21:39:31 +0200

    chore: prettier

--- src/components/RichTextEditor/decorators/index.ts
+++ src/components/RichTextEditor/decorators/index.ts
@@ -1,6 +1,6 @@
-import { CompositeDecorator } from 'draft-js';
-import findLinkEntities from './strategies/findLinkEntities';
-import LinkDecorator from './components/LinkDecorator';
+import { CompositeDecorator } from "draft-js";
+import findLinkEntities from "./strategies/findLinkEntities";
+import LinkDecorator from "./components/LinkDecorator";
 
 export const decorators = new CompositeDecorator([
     {
