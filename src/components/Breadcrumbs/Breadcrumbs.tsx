066b84c80e5046b140da216f7644140ade5bb35a
Nadia Posch <nadia@smartive.ch>
2021-08-13 15:44:20 +0200

    fix(Breadcrumbs): add onClick and fix hover

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,12 +1,14 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import React, { FC, ReactElement } from "react";
-import { Badge, BadgeProps } from "@elements/Badge/Badge";
+import { BadgeProps } from "@elements/Badge/Badge";
 import { IconProps } from "@elements/Icon/IconProps";
+import React, { FC, ReactElement } from "react";
+import { CurrentBreadcrumbItem } from "./CurrentBreadcrumbItem";
 
 export type Breadcrumb = {
-    link: string;
     label: string;
+    link?: string;
+    onClick?: () => void;
     decorator?: ReactElement<IconProps>;
     bold?: boolean;
     badges?: BadgeProps[];
@@ -33,34 +35,32 @@ const Seperator: FC = (props) => (
 export const Breadcrumbs: FC<BreadcrumbsProps> = ({ items }) => (
     <nav className="tw-font-sans" aria-label="Breadcrumb">
         <ol className="tw-list-none tw-flex tw-flex-wrap tw-gap-y-1">
-            {items.map((item, index) =>
+            {items.map(({ label, badges, bold, decorator, link, onClick }, index) =>
                 index < items.length - 1 ? (
                     <li
                         className="tw-flex tw-items-center tw-text-black-80 tw-text-xs dark:tw-text-black-10"
                         key={`breadcrumb-${index}`}
                         data-test-id="breadcrumb-item"
                     >
-                        <a href={item.link}>{item.label}</a>
+                        {link ? (
+                            <a href={link}>{label}</a>
+                        ) : onClick ? (
+                            <button onClick={onClick}>{label}</button>
+                        ) : (
+                            <span>{label}</span>
+                        )}
                         {index < items.length - 2 && <Seperator aria-hidden="true" />}
                     </li>
                 ) : (
-                    <li
-                        className="tw-w-full tw-h-6 tw-flex tw-gap-x-1 tw-items-center tw-text-m tw-text-black dark:tw-text-white"
+                    <CurrentBreadcrumbItem
                         key={`breadcrumb-${index}`}
-                        data-test-id="breadcrumb-item"
-                    >
-                        {item.decorator}
-                        <span className="tw-inline-flex tw-gap-x-2 tw-items-center">
-                            <a href={item.link} aria-current="page" className={item.bold ? "tw-font-bold" : ""}>
-                                {item.label}
-                            </a>
-                            {item.badges?.map(({ children, ...props }, index) => (
-                                <Badge {...props} key={`breadcrumb-badge-${index}`}>
-                                    {children}
-                                </Badge>
-                            ))}
-                        </span>
-                    </li>
+                        label={label}
+                        badges={badges}
+                        bold={bold}
+                        decorator={decorator}
+                        link={link}
+                        onClick={onClick}
+                    />
                 ),
             )}
         </ol>
