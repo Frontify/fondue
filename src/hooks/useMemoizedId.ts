e4973bc015b02a215c53fdd5462ec740e42722de
Nadia Posch <nadia@smartive.ch>
2021-08-19 09:14:12 +0200

    add explanatory comment

--- src/hooks/useMemoizedId.ts
+++ src/hooks/useMemoizedId.ts
@@ -1,4 +1,6 @@
 import generateRandomId from "@utilities/generateRandomId";
 import { useMemo } from "react";
 
+// Used to make sure that ids persist during rerenders
+// framer motion needs persistent ids in order to properly animate elements
 export const useMemoizedId = (id?: string): string => useMemo(() => id || generateRandomId(), [id]);
