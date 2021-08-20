14142f49ee3382710af126f0a1964e55541872ce
Nadia Posch <nadia@smartive.ch>
2021-08-20 14:49:52 +0200

    add tests

---
+++ src/components/Flyout/Flyout.spec.tsx
@@ -0,0 +1,46 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { FIELDSET_HEADER_ID } from "@compositions/FieldsetHeader/FieldsetHeader.spec";
+import { mount } from "@cypress/react";
+import { BADGE_ID } from "@elements/Badge/Badge.spec";
+import { BUTTON_ID } from "@elements/Button/Button.spec";
+import { TextInput } from "@elements/TextInput/TextInput";
+import { TEXT_INPUT_ID } from "@elements/TextInput/TextInput.spec";
+import React from "react";
+import { Flyout } from "./Flyout";
+
+describe("Flyout Component", () => {
+    it("should render with header and badges", () => {
+        const onCloseStub = cy.stub().as("onCloseStub");
+
+        mount(
+            <Flyout
+                title="Header title"
+                badges={[{ children: "Badge 1" }, { children: "Badge 2" }]}
+                onClose={onCloseStub}
+            >
+                <TextInput placeholder="placeholder" />
+            </Flyout>,
+        );
+
+        cy.get(FIELDSET_HEADER_ID).should("contain", "Header title");
+        cy.get(BADGE_ID).should("have.length", 2);
+        cy.get(TEXT_INPUT_ID).should("have.attr", "placeholder").and("eq", "placeholder");
+        cy.get(BUTTON_ID).click();
+        cy.get("@onCloseStub").should("be.calledOnce");
+    });
+    it("should render with onClick action", () => {
+        const onCloseStub = cy.stub().as("onCloseStub");
+        const onClickStub = cy.stub().as("onClickStub");
+
+        mount(
+            <Flyout title="Header title" onClick={onClickStub} onClose={onCloseStub}>
+                <TextInput placeholder="placeholder" />
+            </Flyout>,
+        );
+
+        cy.get(BUTTON_ID).should("have.length", 2);
+        cy.get(BUTTON_ID).eq(1).click();
+        cy.get("@onClickStub").should("be.calledOnce");
+    });
+});
