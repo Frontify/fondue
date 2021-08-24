fb5528fd176eb4df6a7f6a25f8ba6b2c0cf0cca5
thisguyscode <jamio.mitchell@gmail.com>
2021-08-24 17:31:16 +0200

    Adds react-aria approach for pressed state

--- src/elements/AddBlockButton/AddBlockButton.tsx
+++ src/elements/AddBlockButton/AddBlockButton.tsx
@@ -1,13 +1,18 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import IconAddSimple from "@elements/Icon/Generated/IconAddSimple";
+// React
+import React, { FC, useRef } from "react";
+// Icon
 import { IconSize } from "@elements/Icon/IconSize";
+import IconAddSimple from "@elements/Icon/Generated/IconAddSimple";
+// ARIA
 import { useButton } from "@react-aria/button";
+import { useHover } from "@react-aria/interactions";
 import { useFocusRing } from "@react-aria/focus";
 import { mergeProps } from "@react-aria/utils";
+// Utilities
 import { FOCUS_STYLE } from "@utilities/focusStyle";
 import { merge } from "@utilities/merge";
-import React, { FC, useRef } from "react";
 
 export enum AddBlockButtonDirection {
     Horizontal = "Horizontal",
@@ -25,23 +30,29 @@ export const AddBlockButton: FC<AddBlockButtonProps> = ({
     title,
     orientation = AddBlockButtonDirection.Horizontal,
 }) => {
-    const { isFocusVisible, focusProps } = useFocusRing();
     const ref = useRef<HTMLButtonElement | null>(null);
-    const { buttonProps } = useButton({ onPress: () => onClick() }, ref);
+    const { isFocusVisible, focusProps } = useFocusRing();
+    const { buttonProps, isPressed } = useButton({ onPress: () => onClick() }, ref);
+    const { hoverProps, isHovered } = useHover({});
 
     return (
         <button
-            {...mergeProps(buttonProps, focusProps)}
+            {...mergeProps(buttonProps, focusProps, hoverProps)}
             title={title}
             data-test-id="add-block-button"
             className={merge([
-                `tw-group tw-leading-none tw-rounded-sm tw-outline-none`,
+                "tw-leading-none tw-rounded-sm tw-outline-none",
                 isFocusVisible && FOCUS_STYLE,
                 orientation === AddBlockButtonDirection.Vertical ? "tw-rotate-90" : "",
             ])}
         >
             <span
-                className={`tw-text-white tw-bg-violet-60 tw-rounded tw-inline-flex tw-items-center tw-w-7 tw-h-6 tw-relative tw-p-1 tw-transition-colors hover:tw-bg-violet-70 group-active:tw-bg-violet-90`}
+                className={merge([
+                    "tw-text-white tw-bg-violet-60 tw-rounded tw-inline-flex tw-items-center tw-w-7 tw-h-6 tw-relative tw-p-1 tw-transition-color",
+                    isHovered && !isPressed && "tw-bg-violet-70",
+                    isPressed && "tw-bg-violet-90",
+                    orientation === AddBlockButtonDirection.Vertical ? "tw-rotate-90" : "",
+                ])}
                 style={{
                     clipPath: `path(
                         "M27.3333 10.9369L21.7246 2C20.5 0.5 19.5 0 18 0H4C1.79086 0 0 1.79086 0 4V20C0 22.2091 1.79086 24 4 24H18C19.5 24 20.5 23.5 21.7246 22L27.3333 13.0631C27.7412 12.4131 27.7412 11.5869 27.3333 10.9369Z"
