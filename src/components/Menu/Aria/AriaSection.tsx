1f2956db0beca078016404334015fc29f5d7cf10
Marco Bohler <triggertoo@gmail.com>
2021-09-22 14:04:58 +0200

    fix: set correct vertical menu spacing and set menu divider color to black-10 (#180)
    * fix: set correct vertical menu spacing
    
    * fix: use black-10 for menu divider

--- src/components/Menu/Aria/AriaSection.tsx
+++ src/components/Menu/Aria/AriaSection.tsx
@@ -8,7 +8,7 @@ export type AriaSectionProps = {
 export const AriaSection: FC<AriaSectionProps> = ({ sectionProps, groupProps, children }) => (
     <li
         {...sectionProps}
-        className="tw-pb-2 tw-border-b tw-border-b-black-40 tw-mb-2 last:tw-pb-0 last:tw-border-0 last:tw-mb-0"
+        className="tw-border-b tw-border-b-black-10 last:tw-border-0"
         data-test-id="menu-block-divider"
     >
         <ul {...groupProps} className="tw-py-2 tw-px-0 tw-m-0 tw-list-none" data-test-id="menu-item-list">
