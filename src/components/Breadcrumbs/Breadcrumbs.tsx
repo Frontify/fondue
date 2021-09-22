7ffdd39f21e633024cc43a944b056f902fe6245c
Nadia Posch <nadia@smartive.ch>
2021-09-22 13:56:14 +0200

    add aria props to breadcrumbs

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -2,8 +2,18 @@
 
 import { BadgeProps } from "@elements/Badge/Badge";
 import { IconProps } from "@elements/Icon/IconProps";
-import React, { FC, ReactElement } from "react";
+import { useBreadcrumbItem, useBreadcrumbs } from "@react-aria/breadcrumbs";
+import React, { FC, ReactElement, useRef } from "react";
+import { BreadcrumbItem } from "./BreadcrumbItem";
 import { CurrentBreadcrumbItem } from "./CurrentBreadcrumbItem";
+
+export const mapToAriaProps = (items: Breadcrumb[]) => ({
+    children: items.map(({ label }, index) => (
+        <li key={`breadcrumb-${index}`} aria-label={label}>
+            {label}
+        </li>
+    )),
+});
 
 export type Breadcrumb = {
     label: string;
@@ -18,51 +28,55 @@ export type BreadcrumbsProps = {
     items: Breadcrumb[];
 };
 
-const Seperator: FC = (props) => (
-    <svg
-        {...props}
-        width="13"
-        height="17"
-        viewBox="0 0 13 17"
-        fill="none"
-        className="tw-stroke-current tw-text-black-20"
-        xmlns="http://www.w3.org/2000/svg"
-    >
-        <line x1="3.52447" y1="16.0623" x2="8.46874" y2="0.845385" />
-    </svg>
-);
+export const Breadcrumbs: FC<BreadcrumbsProps> = ({ items }) => {
+    const props = mapToAriaProps(items);
+    const { navProps } = useBreadcrumbs(props);
 
-export const Breadcrumbs: FC<BreadcrumbsProps> = ({ items }) => (
-    <nav className="tw-font-sans" aria-label="Breadcrumb">
-        <ol className="tw-list-none tw-flex tw-flex-wrap tw-gap-y-1">
-            {items.map(({ label, badges, bold, decorator, link, onClick }, index) =>
-                index < items.length - 1 ? (
-                    <li
-                        className="tw-flex tw-items-center tw-text-black-80 tw-text-xs dark:tw-text-black-10"
-                        key={`breadcrumb-${index}`}
-                        data-test-id="breadcrumb-item"
-                    >
-                        {link ? (
-                            <a href={link}>{label}</a>
-                        ) : onClick ? (
-                            <button onClick={onClick}>{label}</button>
-                        ) : (
-                            <span>{label}</span>
-                        )}
-                        {index < items.length - 2 && <Seperator aria-hidden="true" />}
-                    </li>
-                ) : (
-                    <CurrentBreadcrumbItem
-                        key={`breadcrumb-${index}`}
-                        label={label}
-                        badges={badges}
-                        bold={bold}
-                        decorator={decorator}
-                        link={link}
-                        onClick={onClick}
-                    />
-                ),
-            )}
-        </ol>
-    </nav>
-);
+    return (
+        <nav {...navProps} className="tw-font-sans" aria-label="Breadcrumb">
+            <ol className="tw-list-none tw-flex tw-flex-wrap tw-gap-y-1">
+                {items.map(({ label, badges, bold, decorator, link, onClick }, index) => {
+                    const isCurrent = index === items.length - 1;
+                    const key = `breadcrumb-${index}`;
+                    const ref = useRef<HTMLAnchorElement | HTMLButtonElement | HTMLSpanElement | null>(null);
+                    const { itemProps } = useBreadcrumbItem(
+                        {
+                            isCurrent,
+                            children: label,
+                            elementType: link ? "a" : onClick ? "button" : "span",
+                        },
+                        ref,
+                    );
+
+                    if (isCurrent) {
+                        return (
+                            <CurrentBreadcrumbItem
+                                key={key}
+                                ref={ref}
+                                ariaProps={itemProps}
+                                label={label}
+                                badges={badges}
+                                bold={bold}
+                                decorator={decorator}
+                                link={link}
+                                onClick={onClick}
+                            />
+                        );
+                    }
+
+                    return (
+                        <BreadcrumbItem
+                            key={key}
+                            ref={ref}
+                            ariaProps={itemProps}
+                            label={label}
+                            link={link}
+                            onClick={onClick}
+                            showSeparator={index < items.length - 2}
+                        />
+                    );
+                })}
+            </ol>
+        </nav>
+    );
+};
