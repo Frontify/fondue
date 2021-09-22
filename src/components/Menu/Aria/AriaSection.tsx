e4934c56c8456a3ea0c4e7c39899a9ac59a885fa
Marco Bohler <triggertoo@gmail.com>
2021-09-22 11:36:23 +0200

    fix: use black-10 for menu divider

--- src/components/Menu/Aria/AriaSection.tsx
+++ src/components/Menu/Aria/AriaSection.tsx
@@ -8,7 +8,7 @@ export type AriaSectionProps = {
 export const AriaSection: FC<AriaSectionProps> = ({ sectionProps, groupProps, children }) => (
     <li
         {...sectionProps}
-        className="tw-border-b tw-border-b-black-40 last:tw-border-0"
+        className="tw-border-b tw-border-b-black-10 last:tw-border-0"
         data-test-id="menu-block-divider"
     >
         <ul {...groupProps} className="tw-py-2 tw-px-0 tw-m-0 tw-list-none" data-test-id="menu-item-list">
