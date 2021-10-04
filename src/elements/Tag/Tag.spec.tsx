0b24464672e8ef219bc6215b8af3ea192feeaac9
CelienB <celien.boillat@frontify.com>
2021-10-04 09:50:12 +0200

    feat: add tests

--- src/elements/Tag/Tag.spec.tsx
+++ src/elements/Tag/Tag.spec.tsx
@@ -13,4 +13,24 @@ describe("Tag Component", () => {
 
         cy.get(TAG_ID).contains(TAG_LABEL);
     });
+    it("should render a suggested tag", () => {
+        mount(<Tag type={TagType.Suggested} label={TAG_LABEL} />);
+
+        cy.get(TAG_ID).should("have.class", "tw-bg-white").and("have.class", "tw-text-black-80");
+    });
+    it("should render a selected tag", () => {
+        mount(<Tag type={TagType.Selected} label={TAG_LABEL} />);
+
+        cy.get(TAG_ID).should("have.class", "tw-bg-black-5");
+    });
+    it("should render a selected with focus tag", () => {
+        mount(<Tag type={TagType.SelectedWithFocus} label={TAG_LABEL} />);
+
+        cy.get(TAG_ID).should("have.class", "tw-bg-violet-60");
+    });
+    it("should render a previously selected tag", () => {
+        mount(<Tag type={TagType.PreviouslySelected} label={TAG_LABEL} />);
+
+        cy.get(TAG_ID).should("have.class", "tw-bg-white").and("have.class", "tw-text-violet-60");
+    });
 });
