0d50e8524247ca3167f3743acb8e4a16c4452af6
CelienB <celien.boillat@frontify.com>
2021-10-04 13:23:29 +0200

    refactor: space tests out for better readability

--- src/elements/Tag/Tag.spec.tsx
+++ src/elements/Tag/Tag.spec.tsx
@@ -14,30 +14,35 @@ describe("Tag Component", () => {
 
         cy.get(TAG_ID).contains(TAG_LABEL);
     });
+
     it("should render a suggested tag", () => {
         mount(<Tag type={TagType.Suggested} label={TAG_LABEL} />);
 
         cy.get(TAG_ID).should("have.class", "tw-bg-white").and("have.class", "tw-text-black-80");
         cy.get(TAG_REJECT_ICON_ATTRIBUTE).should("not.exist");
     });
+
     it("should render a selected tag", () => {
         mount(<Tag type={TagType.Selected} label={TAG_LABEL} />);
 
         cy.get(TAG_ID).should("have.class", "tw-bg-black-5");
         cy.get(TAG_ID).should("be.visible", TAG_REJECT_ICON_ATTRIBUTE);
     });
+
     it("should render a selected with focus tag", () => {
         mount(<Tag type={TagType.SelectedWithFocus} label={TAG_LABEL} />);
 
         cy.get(TAG_ID).should("have.class", "tw-bg-violet-60");
         cy.get(TAG_ID).should("be.visible", TAG_REJECT_ICON_ATTRIBUTE);
     });
+
     it("should render a previously selected tag", () => {
         mount(<Tag type={TagType.PreviouslySelected} label={TAG_LABEL} />);
 
         cy.get(TAG_ID).should("have.class", "tw-bg-white").and("have.class", "tw-text-violet-60");
         cy.get(TAG_REJECT_ICON_ATTRIBUTE).should("not.exist");
     });
+
     it("should trigger a click event on click when type is selected", () => {
         const onClickStub = cy.stub().as("onClickStub");
 
@@ -47,6 +52,7 @@ describe("Tag Component", () => {
         cy.get(TAG_ID).click();
         cy.get("@onClickStub").should("be.calledOnce");
     });
+
     it("should trigger a click event on click when type is selected with focus", () => {
         const onClickStub = cy.stub().as("onClickStub");
 
