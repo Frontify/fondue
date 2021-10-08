62c6e87813cb15fe9e4ff5a68609955ed7f6569b
Samuel Alev <samuel.alev@protonmail.com>
2021-10-08 13:42:08 +0200

    refactor: rename story

--- src/components/RichTextEditor/RichTextEditor.stories.tsx
+++ src/components/RichTextEditor/RichTextEditor.stories.tsx
@@ -2,15 +2,17 @@
 
 import React from "react";
 import { Meta, Story } from "@storybook/react";
-import { RichTextEditor, RichTextEditorProps } from "./RichTextEditor";
+import { RichTextEditor as RichTextEditorComponent, RichTextEditorProps } from "./RichTextEditor";
 
 export default {
     title: "Components/Rich Text Editor",
-    component: RichTextEditor,
+    component: RichTextEditorComponent,
     args: {
         placeholder: "Some placeholder",
         readonly: false,
     },
 } as Meta;
 
-export const Template: Story<RichTextEditorProps> = (args: RichTextEditorProps) => <RichTextEditor {...args} />;
+export const RichTextEditor: Story<RichTextEditorProps> = (args: RichTextEditorProps) => (
+    <RichTextEditorComponent {...args} />
+);
