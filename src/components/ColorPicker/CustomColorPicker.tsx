a14cabb7b20e42b2dc3f1d7ac83384cae2153c15
Kevin Walker <mail@kevinwalker.ch>
2021-10-12 16:27:20 +0200

    Fix of formatting fix

--- src/components/ColorPicker/CustomColorPicker.tsx
+++ src/components/ColorPicker/CustomColorPicker.tsx
@@ -65,7 +65,7 @@ export const CustomColorPicker: FC<Omit<ColorPickerProps, "palette">> = ({
                         )}
                         hsl={hsl}
                         direction="vertical"
-                        onChange={debounce(({ hex }) => onSelect(toColor(currentColor, { hex })))}
+                        onChange={debounce((color) => onSelect(toColor(currentColor, { hex: toState(color).hex })))}
                     />
                 </div>
                 <div className="tw-relative tw-w-6 tw-overflow-hidden tw-rounded">
