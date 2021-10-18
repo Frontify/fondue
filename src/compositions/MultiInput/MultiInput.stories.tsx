76bcd4cee2fa0d29bdd8622b47ba8f240a934492
Kevin Walker <mail@kevinwalker.ch>
2021-10-18 10:45:35 +0200

    Added layout columns & spider

--- src/compositions/MultiInput/MultiInput.stories.tsx
+++ src/compositions/MultiInput/MultiInput.stories.tsx
@@ -4,29 +4,62 @@ import { Meta, Story } from "@storybook/react";
 import React, { useState } from "react";
 import { TextInput } from "@elements/TextInput/TextInput";
 import { MultiInput as MultiInputComponent, MultiInputLayout, MultiInputProps } from "./MultiInput";
+import generateRandomId from "@utilities/generateRandomId";
 
 export default {
     title: "Compositions/Multi Input",
     component: MultiInputComponent,
+    args: {
+        layout: MultiInputLayout.Columns,
+        label: {
+            children: "Input Label",
+            required: false,
+            htmlFor: generateRandomId(),
+            tooltip: { content: "Tooltip Text" },
+        },
+        extra: "Extra Text",
+        helper: "Input the thing to make it gooo.",
+    },
     argTypes: {
         layout: {
             options: Object.values(MultiInputLayout),
             control: { type: "radio" },
         },
     },
-    args: {
-        layout: MultiInputLayout.Spider,
-    },
 } as Meta<MultiInputProps>;
 
-export const MultiInput: Story<MultiInputProps> = (args: MultiInputProps) => {
+export const MultiInputColumns: Story<MultiInputProps> = (args: MultiInputProps) => {
     const [input1, setInput1] = useState("");
     const [input2, setInput2] = useState("");
+    const [input3, setInput3] = useState("");
+    const [input4, setInput4] = useState("");
 
     return (
         <MultiInputComponent {...args}>
             <TextInput value={input1} onChange={(value) => setInput1(value)} />
             <TextInput value={input2} onChange={(value) => setInput2(value)} />
+            <TextInput value={input3} onChange={(value) => setInput3(value)} />
+            <TextInput value={input4} onChange={(value) => setInput4(value)} />
         </MultiInputComponent>
     );
+};
+
+export const MultiInputSpider: Story<MultiInputProps> = (args: MultiInputProps) => {
+    const [input1, setInput1] = useState("");
+    const [input2, setInput2] = useState("");
+    const [input3, setInput3] = useState("");
+    const [input4, setInput4] = useState("");
+
+    return (
+        <MultiInputComponent {...args}>
+            <TextInput value={input1} onChange={(value) => setInput1(value)} />
+            <TextInput value={input2} onChange={(value) => setInput2(value)} />
+            <TextInput value={input3} onChange={(value) => setInput3(value)} />
+            <TextInput value={input4} onChange={(value) => setInput4(value)} />
+        </MultiInputComponent>
+    );
+};
+
+MultiInputSpider.args = {
+    layout: MultiInputLayout.Spider,
 };
--- src/compositions/MultiInput/MultiInput.tsx
+++ src/compositions/MultiInput/MultiInput.tsx
@@ -1,21 +1,45 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import { InputLabel, InputLabelProps } from "@elements/InputLabel/InputLabel";
 import React, { FC } from "react";
 
 export enum MultiInputLayout {
-    Spider = "Spider",
     Columns = "Columns",
+    Spider = "Spider",
 }
 
 export type MultiInputProps = {
     layout: MultiInputLayout;
+    label?: Omit<InputLabelProps, "disabled">;
+    helper?: string;
 };
 
-export const MultiInput: FC<MultiInputProps> = ({ layout, children }) => {
-    console.log({ layout });
+export const MultiInput: FC<MultiInputProps> = ({ layout, label, extra, helper, children }) => {
     return (
-        <div data-test-id="multi-input" className="tw-flex">
-            {children}
+        <div data-test-id="multi-input" className="tw-flex tw-gap-2 tw-w-full tw-flex-col">
+            {(label || extra) && (
+                <div className="tw-flex tw-flew-row tw-items-center tw-justify-between">
+                    {label && <InputLabel {...label} />}
+                    {extra && (
+                        <span
+                            data-test-id="form-control-extra"
+                            className="tw-pl-2 tw-text-black-80 tw-font-sans tw-text-s tw-whitespace-nowrap"
+                        >
+                            {extra}
+                        </span>
+                    )}
+                </div>
+            )}
+            {layout === MultiInputLayout.Columns && <div className="tw-grid tw-grid-cols-2 tw-gap-2">{children}</div>}
+            {layout === MultiInputLayout.Spider && children.length === 4 && (
+                <div className="tw-grid tw-grid-cols-8 tw-gap-2">
+                    <div className="tw-col-start-3 tw-col-span-4">{children[0]}</div>
+                    <div className="tw-col-start-1 tw-col-span-4">{children[1]}</div>
+                    <div className="tw-col-start-5 tw-col-span-4">{children[2]}</div>
+                    <div className="tw-col-start-3 tw-col-span-4">{children[3]}</div>
+                </div>
+            )}
+            {helper && <div className="tw-text-s tw-font-sans tw-text-black-80">{helper}</div>}
         </div>
     );
 };
