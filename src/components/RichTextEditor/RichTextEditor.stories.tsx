ee09274e113fdc19c6f047ba03cf19a386e43ab2
Marco Bohler <triggertoo@gmail.com>
2021-10-21 16:12:18 +0200

    feat(RichTextEditor): Improve stories

--- src/components/RichTextEditor/RichTextEditor.stories.tsx
+++ src/components/RichTextEditor/RichTextEditor.stories.tsx
@@ -13,6 +13,7 @@ export default {
         readonly: false,
     },
     argTypes: {
+        value: { type: "object" },
         onTextChange: { action: "onTextChange" },
     },
 } as Meta;
@@ -24,31 +25,29 @@ export const RichTextEditor: Story<RichTextEditorProps> = (args: RichTextEditorP
 );
 
 export const WithReadonlyState: Story<RichTextEditorProps> = (args: RichTextEditorProps) => (
-    <RichTextEditorComponent
-        {...args}
-        value={convertToRaw(ContentState.createFromText("This is some text that you can not edit"))}
-    />
+    <RichTextEditorComponent {...args} />
 );
 WithReadonlyState.args = {
     readonly: true,
+    value: convertToRaw(ContentState.createFromText("This is some text that you can not edit")),
 };
 
 export const RichTextWithHTML: Story<RichTextEditorProps> = (args: RichTextEditorProps) => (
-    <RichTextEditorComponent
-        {...args}
-        value={{
-            content:
-                '<h4><strong>Export docker binaries</strong></h4><p>In your Bash or Bash-like shell config file (e.g. <strong>~/.bashrc</strong> or <strong>~/.zshrc</strong>), add the following at the end:</p><p><code>export PATH&#61;&#34;$PATH:$HOME/frontify/docker/bin&#34;</code></p><p><br class="softbreak" /></p><p>In case you did not clone the repositories in your home directory, you&#039;ll need to update the export command accordingly.</p><p>For example, if you saved the repos in <code>~/dev/code/frontify/</code> , the export should look like this <code>export PATH&#61;&#34;$PATH:$HOME/dev/code/frontify/docker/bin&#34;</code></p><p><br class="softbreak" /></p><h4>Trust development SSL certificate</h4><p>You&#039;ll find the certificate in <code>frontify/docker/private/local_ca.pem</code></p><p>Double click the file, and add it in the login keychain. After adding it, right click on it, press &#34;Get Info&#34; and make sure it&#039;s always trusted.</p><p><br class="softbreak" /></p><p><strong>\u203c\ufe0f You will now need to restart your computer to get the root certificate applied system wide.</strong></p>',
-        }}
-    />
+    <RichTextEditorComponent {...args} />
 );
+RichTextWithHTML.args = {
+    value: {
+        content:
+            '<h4><strong>Export docker binaries</strong></h4><p>In your Bash or Bash-like shell config file (e.g. <strong>~/.bashrc</strong> or <strong>~/.zshrc</strong>), add the following at the end:</p><p><code>export PATH&#61;&#34;$PATH:$HOME/frontify/docker/bin&#34;</code></p><p><br class="softbreak" /></p><p>In case you did not clone the repositories in your home directory, you&#039;ll need to update the export command accordingly.</p><p>For example, if you saved the repos in <code>~/dev/code/frontify/</code> , the export should look like this <code>export PATH&#61;&#34;$PATH:$HOME/dev/code/frontify/docker/bin&#34;</code></p><p><br class="softbreak" /></p><h4>Trust development SSL certificate</h4><p>You&#039;ll find the certificate in <code>frontify/docker/private/local_ca.pem</code></p><p>Double click the file, and add it in the login keychain. After adding it, right click on it, press &#34;Get Info&#34; and make sure it&#039;s always trusted.</p><p><br class="softbreak" /></p><p><strong>\u203c\ufe0f You will now need to restart your computer to get the root certificate applied system wide.</strong></p>',
+    },
+};
 
 export const RichTextWithHTMLLinks: Story<RichTextEditorProps> = (args: RichTextEditorProps) => (
-    <RichTextEditorComponent
-        {...args}
-        value={{
-            content:
-                '<p>We have a few prerequisite for the host machine:</p><ul><li><a href="https://git-scm.com/downloads" class="link" data-tooltip-link-label="undefined" target="_blank">Git</a> (installed and <a href="https://weare.frontify.com/document/42#/guidelines/commit-signing" class="link" data-tooltip-link-label="undefined" target="_blank">configured</a>)</li><li><a href="https://nodejs.org/en/download/current/" class="link" data-tooltip-link-label="undefined" target="_blank">Node.js / npm</a> (you can also install Node.js with <a href="https://github.com/tj/n" class="link" data-tooltip-link-label="undefined" target="_blank">n</a> or <a href="https://github.com/nvm-sh/nvm" class="link" data-tooltip-link-label="undefined" target="_blank">nvm</a>)</li><li><a href="https://formulae.brew.sh/formula/php" class="link" data-tooltip-link-label="undefined" target="_blank">PHP</a></li><li><a href="https://getcomposer.org/download/" class="link" data-tooltip-link-label="undefined" target="_blank">Composer</a></li><li><a href="https://www.docker.com/products/docker-desktop" class="link" data-tooltip-link-label="undefined" target="_blank">Docker for Desktop</a> ()</li><li><a href="https://app.blissfully.com/ticket?template&#61;dd005773-30e9-5b2e-ae87-91eb2d3d230f" class="link" data-tooltip-link-label="undefined" target="_blank">AWS credentials</a> (Access to: &#34;AWS&#34;)</li></ul>',
-        }}
-    />
+    <RichTextEditorComponent {...args} />
 );
+RichTextWithHTMLLinks.args = {
+    value: {
+        content:
+            '<p>We have a few prerequisite for the host machine:</p><ul><li><a href="https://git-scm.com/downloads" class="link" data-tooltip-link-label="undefined" target="_blank">Git</a> (installed and <a href="https://weare.frontify.com/document/42#/guidelines/commit-signing" class="link" data-tooltip-link-label="undefined" target="_blank">configured</a>)</li><li><a href="https://nodejs.org/en/download/current/" class="link" data-tooltip-link-label="undefined" target="_blank">Node.js / npm</a> (you can also install Node.js with <a href="https://github.com/tj/n" class="link" data-tooltip-link-label="undefined" target="_blank">n</a> or <a href="https://github.com/nvm-sh/nvm" class="link" data-tooltip-link-label="undefined" target="_blank">nvm</a>)</li><li><a href="https://formulae.brew.sh/formula/php" class="link" data-tooltip-link-label="undefined" target="_blank">PHP</a></li><li><a href="https://getcomposer.org/download/" class="link" data-tooltip-link-label="undefined" target="_blank">Composer</a></li><li><a href="https://www.docker.com/products/docker-desktop" class="link" data-tooltip-link-label="undefined" target="_blank">Docker for Desktop</a> ()</li><li><a href="https://app.blissfully.com/ticket?template&#61;dd005773-30e9-5b2e-ae87-91eb2d3d230f" class="link" data-tooltip-link-label="undefined" target="_blank">AWS credentials</a> (Access to: &#34;AWS&#34;)</li></ul>',
+    },
+};
