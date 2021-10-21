ec44fd45f95d4edb963523333818affef2fb0f91
Marco Bohler <triggertoo@gmail.com>
2021-10-21 13:48:24 +0200

    add keyBindingFn and handleKeyCommand

--- src/components/RichTextEditor/RichTextEditor.tsx
+++ src/components/RichTextEditor/RichTextEditor.tsx
@@ -1,7 +1,7 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import { useMachine } from "@xstate/react";
-import { convertFromRaw, Editor, EditorState, RawDraftContentState } from "draft-js";
+import { convertFromRaw, Editor, EditorState, getDefaultKeyBinding, RawDraftContentState, RichUtils } from "draft-js";
 import "draft-js/dist/Draft.css";
 import React, { FC, useRef } from "react";
 import { DoneInvokeEvent, Interpreter } from "xstate";
@@ -44,6 +44,21 @@ export const RichTextEditor: FC<RichTextEditorProps> = ({
                 editorState={context.editorState}
                 placeholder={placeholder}
                 onChange={(editorState) => send({ type: "CHANGE", data: { editorState } })}
+                keyBindingFn={(event) => {
+                    if (event.code === "Tab") {
+                        send({ type: "CHANGE", data: { editorState: RichUtils.onTab(event, context.editorState, 4) } });
+                        return "tab";
+                    }
+                    return getDefaultKeyBinding(event);
+                }}
+                handleKeyCommand={(command: string, editorState: EditorState) => {
+                    const newState = RichUtils.handleKeyCommand(editorState, command);
+                    if (newState) {
+                        send({ type: "CHANGE", data: { editorState: newState } });
+                        return "handled";
+                    }
+                    return "not-handled";
+                }}
                 onBlur={() => editor.current?.blur()}
                 readOnly={readonly}
             />
