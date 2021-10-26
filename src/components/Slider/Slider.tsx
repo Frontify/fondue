128faa292bb4f3619610d131aa5a8dbdb31ae4dc
Samuel Alev <samuel.alev@protonmail.com>
2021-10-26 09:43:46 +0200

    style: remove z-index from Slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -98,7 +98,7 @@ export const Slider: FC<SliderProps> = ({
                                         : "slider-item-number"
                                 }
                                 className={merge([
-                                    "tw-relative tw-w-full tw-z-10 tw-inline-flex tw-justify-center tw-items-center tw-font-sans tw-font-normal tw-h-full tw-text-center",
+                                    "tw-relative tw-w-full tw-inline-flex tw-justify-center tw-items-center tw-font-sans tw-font-normal tw-h-full tw-text-center",
                                     isActive && !disabled ? "tw-text-black" : "tw-text-black-80",
                                     !disabled ? "hover:tw-text-black hover:tw-cursor-pointer" : "",
                                 ])}
