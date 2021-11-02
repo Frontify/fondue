6188dc13b853cf90a5ff36f0cbcf3936be780193
Marco Bohler <triggertoo@gmail.com>
2021-11-02 08:18:26 +0100

    fix inline style highlighting

--- src/components/RichTextEditor/InlineStyleButton.tsx
+++ src/components/RichTextEditor/InlineStyleButton.tsx
@@ -30,8 +30,8 @@ export const InlineStyleButton: FC<InlineStyleButtonProps> = ({ style, children 
         <button
             data-test-id={`inline-style-button-${style}`}
             className={merge([
-                "tw-flex tw-w-6 tw-h-6 tw-items-center tw-justify-center tw-border-0 tw-bg-white tw-rounded tw-text-black-95 tw-cursor-pointer",
-                styleIsActive && "tw-bg-black-10",
+                "tw-flex tw-w-6 tw-h-6 tw-items-center tw-justify-center tw-border-0  tw-rounded tw-text-black-95 tw-cursor-pointer",
+                styleIsActive ? "tw-bg-black-10" : "tw-bg-white",
             ])}
             onClick={(event) => {
                 event.preventDefault();
