7ae27239176e4880578f2b3bf8dfb36e0e28f1e9
Marco Bohler <triggertoo@gmail.com>
2021-11-04 16:53:34 +0100

    move buttons into toolbar

--- src/components/RichTextEditor/styling-controls/StrikethroughButton.tsx
+++
@@ -1,9 +0,0 @@
-/* (c) Copyright Frontify Ltd., all rights reserved. */
-
-import React, { FC } from "react";
-import { InlineStyleButton } from "../InlineStyleButton";
-import { InlineStyles } from "../renderer/renderInlineStyles";
-
-export const StrikethroughButton: FC = () => (
-    <InlineStyleButton style={InlineStyles.Strikethrough}>s</InlineStyleButton>
-);
