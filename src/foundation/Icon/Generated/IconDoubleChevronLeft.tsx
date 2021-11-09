43ec3d34d49035d2753a8db8bbc15e9cd9f8a372
Nadia Posch <nadia@smartive.ch>
2021-11-09 17:07:28 +0100

    feat: add quote icons

---
+++ src/foundation/Icon/Generated/IconDoubleChevronLeft.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { GeneratedIconProps } from "@foundation/Icon/IconProps";
+import { IconSize, IconSizeMap } from "@foundation/Icon/IconSize";
+
+function IconDoubleChevronLeft(props: GeneratedIconProps): React.ReactElement<GeneratedIconProps> {
+    const customClassName = [
+        "tw-flex tw-items-center tw-justify-center tw-fill-current",
+        IconSizeMap[props.size || IconSize.Size16],
+    ].join(" ");
+    return (
+        <svg
+            fill="currentColor"
+            viewBox="0 0 16 12"
+            xmlns="http://www.w3.org/2000/svg"
+            width="100%"
+            height="100%"
+            className={customClassName}
+            name="IconDoubleChevronLeft"
+            {...props}
+        >
+            <path d="M8.487 0v2.273L2.645 5.841v.318l5.842 3.594V12h-.159L.716 7.216V4.784L8.328 0h.159zm7.083 2.273L9.702 5.841v.318l5.868 3.594V12h-.158L7.773 7.216V4.784L15.412 0h.158v2.273z" />
+        </svg>
+    );
+}
+
+const MemoIconDoubleChevronLeft = React.memo(IconDoubleChevronLeft);
+export default MemoIconDoubleChevronLeft;
---
+++ src/foundation/Icon/Generated/IconSingleChevronRight.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { GeneratedIconProps } from "@foundation/Icon/IconProps";
+import { IconSize, IconSizeMap } from "@foundation/Icon/IconSize";
+
+function IconSingleChevronRight(props: GeneratedIconProps): React.ReactElement<GeneratedIconProps> {
+    const customClassName = [
+        "tw-flex tw-items-center tw-justify-center tw-fill-current",
+        IconSizeMap[props.size || IconSize.Size16],
+    ].join(" ");
+    return (
+        <svg
+            fill="currentColor"
+            viewBox="0 0 10 12"
+            xmlns="http://www.w3.org/2000/svg"
+            width="100%"
+            height="100%"
+            className={customClassName}
+            name="IconSingleChevronRight"
+            {...props}
+        >
+            <path d="M.78 0l8.886 4.736v2.501L.779 12H.62V9.765l6.972-3.619v-.319L.619 2.208V0h.16z" />
+        </svg>
+    );
+}
+
+const MemoIconSingleChevronRight = React.memo(IconSingleChevronRight);
+export default MemoIconSingleChevronRight;
