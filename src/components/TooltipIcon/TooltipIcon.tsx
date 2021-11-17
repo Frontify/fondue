6380a89ef1b48d8d7685ae9cfb2c8e37a2e19faf
Margus Lanto <margus.lanto@frontify.com>
2021-11-17 10:55:41 +0100

    Introduce react-aria's useHover and log out the values on state changes

--- src/components/TooltipIcon/TooltipIcon.tsx
+++ src/components/TooltipIcon/TooltipIcon.tsx
@@ -4,14 +4,22 @@ import React, { FC, ReactElement } from "react";
 import IconQuestion from "@foundation/Icon/Generated/IconQuestion";
 import { IconSize } from "@foundation/Icon/IconSize";
 import { TooltipProps } from "@components/Tooltip/Tooltip";
+import { useHover } from "@react-aria/interactions";
 
 export type TooltipIconProps = {
     tooltip: ReactElement<TooltipProps>;
 };
 
 export const TooltipIcon: FC<TooltipIconProps> = ({ tooltip }: TooltipIconProps) => {
+    const { hoverProps, isHovered } = useHover({
+        onHoverStart: () => console.log('hover start'),
+        onHoverEnd: () => console.log('hover end'),
+    });
+
+    console.log('hoverProps:', hoverProps, isHovered);
+    
     return (
-        <div data-test-id="tooltip-icon">
+        <div data-test-id="tooltip-icon" {...hoverProps}>
             <span className="tw-text-black-80">
                 <IconQuestion size={IconSize.Size20} />
                 {tooltip}
