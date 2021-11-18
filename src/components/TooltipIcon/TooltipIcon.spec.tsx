47464ac16d4efd17a72abcf93ca613a5417f0d5a
Margus Lanto <margus.lanto@frontify.com>
2021-11-18 12:21:13 +0100

    Add cypress tests

--- src/components/TooltipIcon/TooltipIcon.spec.tsx
+++ src/components/TooltipIcon/TooltipIcon.spec.tsx
@@ -1,16 +1,34 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-/* import { mount } from "@cypress/react";
 import React from "react";
+import { mount } from "@cypress/react";
 import { TooltipIcon } from "./TooltipIcon";
+import { IconSize } from "@foundation/Icon/IconSize";
 
-const TOOLTIP_COMPONENT = <Tooltip content="Cupcake ipsum dolor sit amet ice cream." />;
+const TOOLTIP_PROPS = {
+    content: "Lorem ipsum dolor sit amet.",
+};
 const TOOLTIP_ICON_ID = "[data-test-id=tooltip-icon]";
+const TOOLTIP_ICON_TRIGGER_ID = "[data-test-id=tooltip-icon-trigger]";
+const TOOLTIP_ID = "[data-test-id=tooltip]";
 
 describe("TooltipIcon Component", () => {
-    it("should render a tooltip icon", () => {
-        mount(<TooltipIcon content={TOOLTIP_COMPONENT} />);
+    it("should render an icon with default size", () => {
+        mount(<TooltipIcon tooltip={TOOLTIP_PROPS} />);
 
-        cy.get(TOOLTIP_ICON_ID).should("contain", TOOLTIP_COMPONENT);
+        cy.get(TOOLTIP_ICON_ID).find(TOOLTIP_ICON_TRIGGER_ID).children().should("have.class", "tw-h-4");
     });
-}); */
+
+    it("should render an icon with custom size", () => {
+        mount(<TooltipIcon tooltip={TOOLTIP_PROPS} iconSize={IconSize.Size20} />);
+
+        cy.get(TOOLTIP_ICON_ID).find(TOOLTIP_ICON_TRIGGER_ID).children().should("have.class", "tw-h-5");
+    });
+
+    it("should render a tooltip when the icon is hovered", () => {
+        mount(<TooltipIcon tooltip={TOOLTIP_PROPS} />);
+
+        cy.get(TOOLTIP_ICON_TRIGGER_ID).realHover({ position: "top" });
+        cy.get(TOOLTIP_ICON_ID).find(TOOLTIP_ID).should("exist");
+    });
+});
