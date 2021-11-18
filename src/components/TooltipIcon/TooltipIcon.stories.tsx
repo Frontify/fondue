224c9a52514e491d74a10303c49a181046353693
Margus Lanto <margus.lanto@frontify.com>
2021-11-18 10:22:09 +0100

    Make iconSize configurable

--- src/components/TooltipIcon/TooltipIcon.stories.tsx
+++ src/components/TooltipIcon/TooltipIcon.stories.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { Meta, Story } from "@storybook/react";
 import { TooltipIcon, TooltipIconProps } from "./TooltipIcon";
+import { IconSize } from "@foundation/Icon/IconSize";
 
 export default {
     title: "Components/TooltipIcon",
@@ -15,11 +16,17 @@ const TooltipIconTemplate: Story<TooltipIconProps> = (args: TooltipIconProps) =>
     return <TooltipIcon {...args} />;
 };
 
-export const WithDefaultContent = TooltipIconTemplate.bind({});
-
-export const WithCustomContent = TooltipIconTemplate.bind({});
-WithCustomContent.args = {
+export const WithDefaultIconSize = TooltipIconTemplate.bind({});
+WithDefaultIconSize.args = {
     tooltip: {
         content: "Lorem ipsum dolor sit amet.",
     },
+};
+
+export const WithCustomIconSize = TooltipIconTemplate.bind({});
+WithCustomIconSize.args = {
+    tooltip: {
+        content: "Lorem ipsum dolor sit amet.",
+    },
+    iconSize: IconSize.Size20,
 };
--- src/components/TooltipIcon/TooltipIcon.tsx
+++ src/components/TooltipIcon/TooltipIcon.tsx
@@ -2,11 +2,23 @@
 
 import React, { FC } from "react";
 import { Tooltip, TooltipProps } from "@components/Tooltip/Tooltip";
+import IconQuestion from "@foundation/Icon/Generated/IconQuestion";
+import { IconSize } from "@foundation/Icon/IconSize";
 
 export type TooltipIconProps = {
     tooltip?: Omit<TooltipProps, "tooltipAriaProps">;
+    iconSize?: IconSize;
 };
 
-export const TooltipIcon: FC<TooltipIconProps> = ({ tooltip }: TooltipIconProps) => {
-    return <div data-test-id="tooltip-icon">{tooltip && <Tooltip {...tooltip} />}</div>;
+export const TooltipIcon: FC<TooltipIconProps> = ({ tooltip, iconSize = IconSize.Size16 }: TooltipIconProps) => {
+    return (
+        <div data-test-id="tooltip-icon">
+            {tooltip && (
+        <>
+            <IconQuestion size={iconSize} />
+            <Tooltip {...tooltip} />
+        </>
+            )}
+        </div>
+    );
 };
