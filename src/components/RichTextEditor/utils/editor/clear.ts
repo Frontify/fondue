f878cd69e0747cf00580851d8a007d3188e0634a
Marco Bohler <triggertoo@gmail.com>
2021-12-01 13:25:31 +0100

    cleanup clear

--- src/components/RichTextEditor/utils/editor/clear.ts
+++ src/components/RichTextEditor/utils/editor/clear.ts
@@ -2,11 +2,5 @@
 
 import { Editor } from "slate";
 
-export const clearEditor = (editor: Editor): void => {
-    const children = [...editor.children];
-
-    for (let i = 0; i < children.length; i++) {
-        const node = children[i];
-        editor.apply({ type: "remove_node", path: [0], node });
-    }
-};
+export const clearEditor = ({ children, apply }: Editor): void =>
+    children.forEach((node) => apply({ type: "remove_node", path: [0], node }));
