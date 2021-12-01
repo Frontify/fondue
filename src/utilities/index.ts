5e57feb49b600e5b9568830f4567e8cd7564b601
Samuel Alev <samuel.alev@frontify.com>
2021-12-01 18:03:54 +0100

    feat: add barrel export to hooks and utilities

---
+++ src/utilities/index.ts
@@ -0,0 +1,10 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+export * from "./colors";
+export * from "./compose";
+export * from "./debounce";
+export * from "./focusStyle";
+export * from "./generateRandomId";
+export * from "./merge";
+export * from "./useDebounce";
+export * from "./useForwardedRef";
