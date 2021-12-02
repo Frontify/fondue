0c6001c8aec480fbd5a557c7269b4d478f1ae73a
Kevin Walker <mail@kevinwalker.ch>
2021-12-02 11:01:11 +0100

    Add width to TextInput

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -149,7 +149,7 @@ export const TextInput: FC<TextInputProps> = ({
                 id={useMemoizedId(propId)}
                 ref={inputElement}
                 className={merge([
-                    "tw-flex-grow tw-border-none tw-outline-none tw-bg-transparent tw-hide-input-arrows",
+                    "tw-w-full tw-flex-grow tw-border-none tw-outline-none tw-bg-transparent tw-hide-input-arrows",
                     disabled
                         ? "tw-text-black-40 tw-placeholder-black-30 dark:tw-text-black-30 dark:tw-placeholder-black-40"
                         : "tw-text-black tw-placeholder-black-60 dark:tw-text-white",
