dfbfe8efd6c74c346bfa695ff4147ea2f9487885
Sam Creasey <sam.creasey@frontify.com>
2021-12-16 15:12:09 +0100

    remove fill colors

--- src/foundation/Icon/Generated/IconStrikethrough.tsx
+++ src/foundation/Icon/Generated/IconStrikethrough.tsx
@@ -10,7 +10,7 @@ function IconStrikethrough(props: GeneratedIconProps): React.ReactElement<Genera
     return (
         <svg
             viewBox="0 0 17 17"
-            fill="none"
+            fill="currentColor"
             xmlns="http://www.w3.org/2000/svg"
             width="100%"
             height="100%"
@@ -18,14 +18,13 @@ function IconStrikethrough(props: GeneratedIconProps): React.ReactElement<Genera
             name="IconStrikethrough"
             {...props}
         >
-            <path d="M11.577 14.929H9.269v-3.941H7.731v3.94H5.423v1.54h6.154v-1.54Z" fill="#2D3232" />
+            <path d="M11.577 14.929H9.269v-3.941H7.731v3.94H5.423v1.54h6.154v-1.54Z" />
             <path
                 fillRule="evenodd"
                 clipRule="evenodd"
                 d="M.75 8.544a.75.75 0 0 1 .75-.75h14a.75.75 0 0 1 0 1.5h-14a.75.75 0 0 1-.75-.75Z"
-                fill="#2D3232"
             />
-            <path d="M7.747 5.93h1.506V2.16h4.742v1.538H15.5V.621h-14v3.077h1.505V2.16h4.742v3.77Z" fill="#2D3232" />
+            <path d="M7.747 5.93h1.506V2.16h4.742v1.538H15.5V.621h-14v3.077h1.505V2.16h4.742v3.77Z" />
         </svg>
     );
 }
