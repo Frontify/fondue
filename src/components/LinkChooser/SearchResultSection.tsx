038ef1c2fbc474b56c2f2afe5ec52ad92144f71f
Sam Creasey <sam.creasey@frontify.com>
2022-01-05 13:51:34 +0100

    cleaned up returns

--- src/components/LinkChooser/SearchResultSection.tsx
+++ src/components/LinkChooser/SearchResultSection.tsx
@@ -10,11 +10,12 @@ import { useActor } from "@xstate/react";
 import React, { FC, useMemo, useRef } from "react";
 import { IconOptions } from "./LinkChooser";
 import { DropdownState, LinkChooserState, SectionState } from "./state/machine";
-import { SearchResultListProps, SearchResultSectionProps, SearchResultOptionProps, TemplateProps } from "./types";
+import { SearchResultListProps, SearchResultSectionProps, SearchResultOptionProps, ImageMenuItemProps } from "./types";
 import NoResultsIcon from "./assets/no-results.svg";
 import BackgroundIcon from "./assets/background.svg";
 import FetchingIcon from "./assets/nook-animated.png";
 import { isFetching, isUnsuccessful, shouldGoBack } from "./utils/state";
+import { getInteractionModality } from "@react-aria/interactions";
 
 export const SearchResultsList: FC<SearchResultListProps> = (props: SearchResultListProps) => {
     const ref = useRef<HTMLUListElement>(null);
@@ -91,25 +92,23 @@ const SearchResultSection = ({ heading, state, keyItemRecord, machineService }: 
     });
 
     return (
-        <>
-            <li {...itemProps} className="tw-border-b tw-border-b-black-10 last:tw-border-0">
-                <ul
-                    {...groupProps}
-                    data-test-id="link-chooser-select-section"
-                    className="tw-py-2 tw-px-0 tw-m-0 tw-list-none"
-                >
-                    {[...heading.childNodes].map((node) => (
-                        <SearchResultOption
-                            key={node.key}
-                            item={node}
-                            state={state}
-                            keyItemRecord={keyItemRecord}
-                            machineService={machineService}
-                        />
-                    ))}
-                </ul>
-            </li>
-        </>
+        <li {...itemProps} className="tw-border-b tw-border-b-black-10 last:tw-border-0">
+            <ul
+                {...groupProps}
+                data-test-id="link-chooser-select-section"
+                className="tw-py-2 tw-px-0 tw-m-0 tw-list-none"
+            >
+                {[...heading.childNodes].map((node) => (
+                    <SearchResultOption
+                        key={node.key}
+                        item={node}
+                        state={state}
+                        keyItemRecord={keyItemRecord}
+                        machineService={machineService}
+                    />
+                ))}
+            </ul>
+        </li>
     );
 };
 
@@ -134,9 +133,11 @@ const SearchResultOption = ({ item, state, keyItemRecord, machineService }: Sear
             matches(`${LinkChooserState.Focused}.${DropdownState.Templates}.${SectionState.Loaded}`) ||
             matches(`${LinkChooserState.Focused}.${DropdownState.Guidelines}.${SectionState.Loaded}`)
         ) {
-            return <TemplateItem {...menuItem} />;
+            return <ImageMenuItem {...menuItem} />;
         }
     };
+
+    const isUsingKeyboard = getInteractionModality() === "keyboard";
 
     return (
         <li
@@ -145,7 +146,7 @@ const SearchResultOption = ({ item, state, keyItemRecord, machineService }: Sear
             className={merge([
                 "tw-relative hover:tw-bg-black-0 tw-list-none tw-outline-none",
                 isDisabled && "tw-pointer-events-none tw-top-px",
-                isFocused && "tw-bg-black-0",
+                isFocused && isUsingKeyboard && "tw-bg-black-0",
             ])}
         >
             {renderOptionItem()}
@@ -153,51 +154,43 @@ const SearchResultOption = ({ item, state, keyItemRecord, machineService }: Sear
     );
 };
 
-const EmptyResults = ({ prompt, icon }: { prompt: string; icon: string }) => {
-    return (
-        <div
-            data-test-id="link-chooser-empty-results"
-            className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-h-[350px]"
-        >
-            <img className="tw-w-[75px] tw-mb-5" src={icon} alt="Icon" />
-            <p className="tw-text-black-60">{prompt}</p>
-        </div>
-    );
-};
+const EmptyResults = ({ prompt, icon }: { prompt: string; icon: string }) => (
+    <div
+        data-test-id="link-chooser-empty-results"
+        className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-h-[350px]"
+    >
+        <img className="tw-w-[75px] tw-mb-5" src={icon} alt="Icon" />
+        <p className="tw-text-black-60">{prompt}</p>
+    </div>
+);
 
-const FetchingError = ({ error = "An error occurred while fetching the results" }: { error?: string }) => {
-    return (
-        <div
-            data-test-id="link-chooser-error"
-            className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-h-[350px]"
-        >
-            <img className="tw-w-[75px] tw-mb-5" src={NoResultsIcon} alt="Error" />
-            <p className="tw-text-black-60">{error}</p>
-        </div>
-    );
-};
+const FetchingError = ({ error = "An error occurred while fetching the results" }: { error?: string }) => (
+    <div
+        data-test-id="link-chooser-error"
+        className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-h-[350px]"
+    >
+        <img className="tw-w-[75px] tw-mb-5" src={NoResultsIcon} alt="Error" />
+        <p className="tw-text-black-60">{error}</p>
+    </div>
+);
 
-const FetchingAnimation = () => {
-    return (
-        <div
-            data-test-id="link-chooser-loader"
-            className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-h-[350px]"
-        >
-            <img className="tw-w-[50px]" src={FetchingIcon} alt="Fetching" />
-        </div>
-    );
-};
+const FetchingAnimation = () => (
+    <div
+        data-test-id="link-chooser-loader"
+        className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-h-[350px]"
+    >
+        <img className="tw-w-[50px]" src={FetchingIcon} alt="Fetching" />
+    </div>
+);
 
-const TemplateItem = ({ title, subtitle, preview }: TemplateProps) => {
-    return (
-        <div className="tw-flex tw-px-5 tw-py-1.5 tw-cursor-pointer">
-            <div className="tw-flex tw-flex-shrink-0 tw-w-[75px] tw-h-[75px] tw-max-w-xs tw-bg-black-0 tw-border-black-10 tw-border tw-rounded">
-                {preview && <img className="tw-w-full tw-object-contain" src={preview} alt={title as string} />}
-            </div>
-            <div className="tw-flex tw-flex-col tw-justify-center tw-ml-4">
-                <p className="tw-block tw-text-lg tw-leading-tight tw-hover:underline tw-text-black-80">{title}</p>
-                {subtitle && <p className="tw-text-[12px] tw-text-black-80">{subtitle}</p>}
-            </div>
+const ImageMenuItem = ({ title, subtitle, preview }: ImageMenuItemProps) => (
+    <div className="tw-flex tw-px-5 tw-py-1.5 tw-cursor-pointer">
+        <div className="tw-flex tw-flex-shrink-0 tw-w-[75px] tw-h-[75px] tw-max-w-xs tw-bg-black-0 tw-border-black-10 tw-border tw-rounded">
+            {preview && <img className="tw-w-full tw-object-contain" src={preview} alt={title as string} />}
         </div>
-    );
-};
+        <div className="tw-flex tw-flex-col tw-justify-center tw-ml-4">
+            <p className="tw-block tw-text-lg tw-leading-tight tw-hover:underline tw-text-black-80">{title}</p>
+            {subtitle && <p className="tw-text-[12px] tw-text-black-80">{subtitle}</p>}
+        </div>
+    </div>
+);
