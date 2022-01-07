0a85450c0d2441e9d33e0ee91b9d8de31fcd282a
Sam Creasey <sam.creasey@frontify.com>
2022-01-07 13:14:26 +0100

    added dark mode default label color

--- src/components/ColorPicker/ColorPicker.tsx
+++ src/components/ColorPicker/ColorPicker.tsx
@@ -2,7 +2,6 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Slider } from "@components/Slider/Slider";
 import { getBackgroundColor, getColorDisplayValue } from "@utilities/colors";
-import { merge } from "@utilities/merge";
 import React, { FC, useEffect, useMemo, useState } from "react";
 // @ts-ignore
 import { getContrastingColor } from "react-color/lib/helpers/color";
@@ -73,13 +72,13 @@ const ColorPreview: FC<{ color: Color; format: ColorFormat }> = ({ color, format
     const backgroundColor = getBackgroundColor(color);
     const displayValue = getColorDisplayValue(color, format);
     const labelColor = useMemo(() => {
-        return alpha && alpha < 0.3 ? "#000000" : getContrastingColor(hex);
+        return alpha && alpha >= 0.3 ? getContrastingColor(hex) : null;
     }, [hex, rgba, alpha]);
 
     return (
         <div className="tw-sticky tw-top-0 tw-bg-white tw-z-20 dark:tw-bg-black-95">
             <div
-                className="tw-flex tw-justify-center tw-p-7 tw-text-m tw-gap-2"
+                className={"tw-flex tw-justify-center tw-p-7 tw-text-m tw-text-black dark:tw-text-white tw-gap-2"}
                 style={{ background: backgroundColor, color: labelColor }}
                 data-test-id="color-preview"
             >
