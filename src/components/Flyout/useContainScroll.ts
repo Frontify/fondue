a9b03506d25ac7d79962db49a096a0f675b5557f
Sam Creasey <sam.creasey@frontify.com>
2022-01-17 16:22:42 +0100

    added review suggestions

--- src/components/Flyout/useContainScroll.ts
+++ src/components/Flyout/useContainScroll.ts
@@ -2,37 +2,45 @@ import { MutableRefObject, useEffect } from "react";
 
 export const useContainScroll = (overlayRef: MutableRefObject<HTMLDivElement | null>, { isDisabled = false }) => {
     useEffect(() => {
+        const overlay = overlayRef.current;
+
         const scrollHandler = (event: WheelEvent) => {
-            const prevent = function () {
+            const prevent = () => {
                 event.stopPropagation();
                 event.preventDefault();
-                return false;
             };
-            const overlay = overlayRef.current as HTMLDivElement;
-            const deltaY = event.deltaY;
-            const up = deltaY < 0;
-            const scrollTop = overlay.scrollTop;
-            const scrollHeight = overlay.scrollHeight;
-            const height = overlay.clientHeight;
 
-            if (!up && deltaY > scrollHeight - height - scrollTop) {
-                // Scrolling down, but this will take us past the bottom.
-                overlay.scrollTop = scrollHeight;
-                return prevent();
-            } else if (up && -deltaY > scrollTop) {
-                // Scrolling up, but this will take us past the top.
-                overlay.scrollTop = 0;
-                return prevent();
+            if (overlay !== null) {
+                const deltaY = event.deltaY;
+                const up = deltaY < 0;
+                const scrollTop = overlay.scrollTop;
+                const scrollHeight = overlay.scrollHeight;
+                const height = overlay.clientHeight;
+
+                if (!up && deltaY > scrollHeight - height - scrollTop) {
+                    // Scrolling down, but this will take us past the bottom.
+                    overlay.scrollTop = scrollHeight;
+                    prevent();
+                    return false;
+                } else if (up && -deltaY > scrollTop) {
+                    // Scrolling up, but this will take us past the top.
+                    overlay.scrollTop = 0;
+                    prevent();
+                    return false;
+                }
             }
         };
+
         const eventProps = { passive: false, capture: true };
-        if (!isDisabled && overlayRef.current !== null) {
-            overlayRef.current.addEventListener("wheel", scrollHandler, eventProps);
+
+        if (!isDisabled && overlay !== null) {
+            overlay.addEventListener("wheel", scrollHandler, eventProps);
         }
+
         return () => {
-            if (!isDisabled && overlayRef.current !== null) {
-                overlayRef.current?.removeEventListener("wheel", scrollHandler, eventProps);
+            if (!isDisabled && overlay !== null) {
+                overlay.removeEventListener("wheel", scrollHandler, eventProps);
             }
         };
-    }, [isDisabled, overlayRef.current]);
+    }, [isDisabled, overlayRef]);
 };
