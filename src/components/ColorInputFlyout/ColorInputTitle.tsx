5970c4e579d77ac9911d61dced8b13e61b1f872b
Kevin Walker <kevin@smartive.ch>
2022-01-18 16:03:28 +0100

    feat: change color type and start using tinycolor

--- src/components/ColorInputFlyout/ColorInputTitle.tsx
+++ src/components/ColorInputFlyout/ColorInputTitle.tsx
@@ -1,5 +1,3 @@
-/* (c) Copyright Frontify Ltd., all rights reserved. */
-
 import { getColorDisplayValue } from "@utilities/colors";
 import React, { FC } from "react";
 import tinycolor from "tinycolor2";
