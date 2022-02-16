183a6d4372ed187f90a9696465e3a5698f10abe2
imoutaharik <ilham.moutaharik@frontify.com>
2022-02-16 18:07:49 +0100

    feat(tabs): Create different stories - label, badge, icon.

--- src/components/Tabs/Tabs.spec.tsx
+++ src/components/Tabs/Tabs.spec.tsx
@@ -3,11 +3,56 @@
 import { mount } from "@cypress/react";
 import React from "react";
 import { Tabs, TabSize, TabsPaddingX } from "@components/Tabs/Tabs";
-import { data } from "./Tabs.stories";
+import { IconIcons, IconSize } from "@foundation/Icon";
+import { Card } from "@components/Card";
+import { BadgeStyle } from "@components/Badge";
+import { Button } from "@components/Button";
+
+const content = [
+    {
+        label: "A tab",
+        content: "This is content for label 1",
+    },
+    {
+        label: "Disabled",
+        content: "This is content for label 2",
+        disabled: true,
+    },
+    {
+        label: "A long tab name",
+        content: "This is content for label 3",
+        decorator: <IconIcons size={IconSize.Size12} />,
+    },
+    {
+        label: "An even longer tab name",
+        content: (
+            <div>
+                <h2>Hello</h2>
+                <Card hoverable={false}>
+                    <p>This is content for tab 4</p>
+                </Card>
+            </div>
+        ),
+        badge: { style: BadgeStyle.Positive, children: "Badge 1" },
+    },
+    {
+        label: "All disabled",
+        content: <Button>This is content for label 5</Button>,
+        decorator: <IconIcons size={IconSize.Size12} />,
+        badge: { style: BadgeStyle.Danger, children: "Badge 2" },
+        disabled: true,
+    },
+    {
+        label: "Last tab",
+        content: <Button>This is content for label 5</Button>,
+        decorator: <IconIcons size={IconSize.Size12} />,
+        badge: { style: BadgeStyle.Danger, children: "Badge 2" },
+    },
+];
 
 describe("Tabs Component", () => {
     it("should render correctly", () => {
-        mount(<Tabs content={data} paddingX={TabsPaddingX.Small} size={TabSize.Small} />);
+        mount(<Tabs content={content} paddingX={TabsPaddingX.Small} size={TabSize.Small} />);
 
         cy.get("[data-test-id=tabs]").as("Tabs");
         cy.get("@Tabs").should("be.visible");
--- src/components/Tabs/Tabs.stories.tsx
+++ src/components/Tabs/Tabs.stories.tsx
@@ -4,49 +4,62 @@ import React from "react";
 import { Meta, Story } from "@storybook/react";
 import { Tabs, TabSize, TabsPaddingX, TabsProps } from "./Tabs";
 import { Button } from "@components/Button";
-import { Card } from "@components/Card";
 import { IconIcons, IconSize } from "@foundation/Icon";
 import { BadgeStyle } from "@components/Badge";
+import { Text } from "../../typography/Text";
+import { Divider } from "@components/Divider";
 
-export const data = [
+const data = [
     {
         label: "A tab",
-        content: "This is content for label 1",
+        content: (
+            <Text>
+                Bacon ipsum dolor amet spare ribs swine meatloaf, pastrami cupim tail leberkas frankfurter jowl chislic
+                shoulder. Frankfurter drumstick t-bone bacon ground round. Cupim pork loin shank kielbasa. Short loin
+                shank meatloaf tongue. Chicken sirloin swine ball tip. Turkey pork kevin burgdoggen meatball t-bone.
+            </Text>
+        ),
     },
     {
         label: "Disabled",
-        content: "This is content for label 2",
+        content: "Rib-eye",
         disabled: true,
     },
     {
         label: "A long tab name",
-        content: "This is content for label 3",
-        decorator: <IconIcons size={IconSize.Size12} />,
+        content: (
+            <Text>
+                Sirloin prosciutto cow short loin, andouille bresaola doner fatback pork loin. Tongue ribeye kielbasa
+                pastrami. Meatball tail hamburger beef ribs beef ball tip pork chop buffalo short loin boudin chicken
+                pastrami ribeye picanha flank. Shoulder chicken pancetta shankle biltong rump hamburger kielbasa tri-tip
+                ham hock frankfurter.
+            </Text>
+        ),
     },
     {
         label: "An even longer tab name",
         content: (
             <div>
-                <h2>Hello</h2>
-                <Card hoverable={false}>
-                    <p>This is content for tab 4</p>
-                </Card>
+                <Text>
+                    Porchetta burgdoggen kielbasa tenderloin boudin, ham cow pork loin salami. Pork chop swine meatloaf,
+                    sausage ground round tenderloin beef pork chislic chicken short loin landjaeger. Pig pork chop
+                    meatball picanha short ribs. Meatloaf buffalo shoulder, tail beef ribs corned beef short ribs ground
+                    round andouille pork belly landjaeger swine hamburger meatball. Ham hock turducken tri-tip picanha,
+                    buffalo leberkas pork loin chuck ground round.
+                    <Divider />
+                    <Button>Some button</Button>
+                </Text>
             </div>
         ),
-        badge: { style: BadgeStyle.Positive, children: "Badge 1" },
     },
     {
-        label: "All disabled",
-        content: <Button>This is content for label 5</Button>,
-        decorator: <IconIcons size={IconSize.Size12} />,
-        badge: { style: BadgeStyle.Danger, children: "Badge 2" },
+        label: "Disabled",
+        content: <Button>Sirloin</Button>,
         disabled: true,
     },
     {
         label: "Last tab",
-        content: <Button>This is content for label 5</Button>,
-        decorator: <IconIcons size={IconSize.Size12} />,
-        badge: { style: BadgeStyle.Danger, children: "Badge 2" },
+        content: <Text>Meat.</Text>,
     },
 ];
 
@@ -64,13 +77,31 @@ export default {
             control: { type: "select" },
         },
     },
+    args: {
+        paddingX: TabsPaddingX.Small,
+        size: TabSize.Small,
+    },
 } as Meta<TabsProps>;
 
-const Template: Story<TabsProps> = (args) => <Tabs {...args} content={data} />;
+const TabTemplate: Story<TabsProps> = (args) => <Tabs {...args} content={data} />;
+export const Default = TabTemplate.bind({});
+Default.storyName = "Label Only";
 
-export const Default = Template.bind({});
+const dataWithIcon = data.map((item) => Object.assign({}, item, { decorator: <IconIcons size={IconSize.Size16} /> }));
+const TabWithIconTemplate: Story<TabsProps> = (args) => <Tabs {...args} content={dataWithIcon} />;
+export const withIcon = TabWithIconTemplate.bind({});
+withIcon.storyName = "Icon and Label";
 
-Default.args = {
-    paddingX: TabsPaddingX.Small,
-    size: TabSize.Small,
-};
+const dataWithBadge = data.map((item) =>
+    Object.assign({}, item, { badge: { style: BadgeStyle.Positive, children: "Badge" } }),
+);
+const TabWithBadgeTemplate: Story<TabsProps> = (args) => <Tabs {...args} content={dataWithBadge} />;
+export const withBadge = TabWithBadgeTemplate.bind({});
+withBadge.storyName = "Label and Badge";
+
+const dataWithBadgeAndIcon = dataWithBadge.map((item) =>
+    Object.assign({}, item, { decorator: <IconIcons size={IconSize.Size16} /> }),
+);
+const TabWithBadgeAndIconTemplate: Story<TabsProps> = (args) => <Tabs {...args} content={dataWithBadgeAndIcon} />;
+export const withBadgeAndIcon = TabWithBadgeAndIconTemplate.bind({});
+withBadgeAndIcon.storyName = "Label, Badge and Icon";
