b8855bb7725cd3d00a6fcbee02268791d191a90b
Sam Creasey <sam.creasey@frontify.com>
2022-02-16 15:47:40 +0100

    updated card component

--- src/components/Card/Card.spec.tsx
+++ src/components/Card/Card.spec.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { mount } from "@cypress/react";
 import { Card } from "./Card";
+import { FOCUS_STYLE } from "@utilities/focusStyle";
 
 const CARD_ID = "[data-test-id=card]";
 
@@ -20,6 +21,24 @@ describe("Card Component", () => {
         mount(<Card onClick={stubbedOnClick}>{CARD_CHILDREN}</Card>);
 
         cy.get(CARD_ID).click();
+        cy.get(CARD_ID).invoke("attr", "tabIndex").should("equal", "0");
         cy.wrap(stubbedOnClick).should("have.been.called");
+    });
+
+    it("should be accessible", () => {
+        const stubbedOnClick = cy.stub();
+        mount(<Card onClick={stubbedOnClick}>{CARD_CHILDREN}</Card>);
+
+        cy.window().focus();
+        cy.get("body").realPress("Tab");
+        cy.get(CARD_ID).should("have.class", FOCUS_STYLE);
+        cy.get(CARD_ID).realPress("Enter");
+        cy.wrap(stubbedOnClick).should("have.been.called");
+    });
+
+    it("should not be focusable/tabbable if no onClick supplied", () => {
+        mount(<Card>{CARD_CHILDREN}</Card>);
+
+        cy.get(CARD_ID).invoke("attr", "tabIndex").should("be.undefined");
     });
 });
