b71c649a69bb133d83c57150ad463f988c9ea1a5
Sam Creasey <sam.creasey@frontify.com>
2022-02-16 09:40:28 +0100

    removed scroll container from color picker

--- src/components/ColorInputFlyout/ColorPickerFlyout.stories.tsx
+++ src/components/ColorInputFlyout/ColorPickerFlyout.stories.tsx
@@ -22,16 +22,14 @@ export const Flyout: Story<ColorPickerFlyoutProps> = ({ disabled, currentColor, 
     const [selectedColor, setSelectedColor] = useState<Color | null>(currentColor);
 
     return (
-        <div className="tw-h-[1000px] tw-pt-[200px]">
-            <ColorPickerFlyoutComponent
-                disabled={disabled}
-                currentColor={temporaryColor ?? selectedColor}
-                onClick={() => setSelectedColor(temporaryColor)}
-                onClose={() => setTemporaryColor(null)}
-                onSelect={(color) => setTemporaryColor(color)}
-                palettes={EXAMPLE_PALETTES}
-                verticalMargin={verticalMargin}
-            />
-        </div>
+        <ColorPickerFlyoutComponent
+            disabled={disabled}
+            currentColor={temporaryColor ?? selectedColor}
+            onClick={() => setSelectedColor(temporaryColor)}
+            onClose={() => setTemporaryColor(null)}
+            onSelect={(color) => setTemporaryColor(color)}
+            palettes={EXAMPLE_PALETTES}
+            verticalMargin={verticalMargin}
+        />
     );
 };
