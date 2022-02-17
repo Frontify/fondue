55093cf698aaf77266160e2923a2f830aae52775
Sam Creasey <93908356+SamCreasey@users.noreply.github.com>
2022-02-17 16:13:57 +0100

    fix(color-picker-flyout): wrap palettes and add padding option (#594)
    * Bump to version 0.16.1
    
    * bump lock to version 0.16.1
    
    * re-added wrapping for color palattes
    
    * calculate flyout position externally
    
    * refactored spacing function
    
    * simplified overlayHeight
    
    * removed offsetBottom
    
    * renamed overlaypadding to verticalmargin
    
    * added vertical subtraction
    
    * fixed prettier erro
    
    * add nullish coalescing margin
    
    * removed scroll container from color picker
    
    * remove useMemo
    
    * update flyout with new function
    
    * remove vertical margin and abstract into hook
    
    * removed vertical margin from color picker
    
    * fixed prettier error
    
    * refactored hook
    
    * added hooks folder
    
    * refactored function
    
    * renamed function

--- src/components/Flyout/Flyout.tsx
+++ src/components/Flyout/Flyout.tsx
@@ -4,15 +4,16 @@ import { BadgeProps } from "@components/Badge/Badge";
 import { watchModals } from "@react-aria/aria-modal-polyfill";
 import { useButton } from "@react-aria/button";
 import { FocusScope, useFocusRing } from "@react-aria/focus";
-import { OverlayContainer, OverlayProvider, useOverlayPosition, useOverlayTrigger } from "@react-aria/overlays";
+import { OverlayContainer, OverlayProvider, useOverlayTrigger } from "@react-aria/overlays";
 import { mergeProps } from "@react-aria/utils";
 import { useOverlayTriggerState } from "@react-stately/overlays";
 import { FOCUS_STYLE } from "@utilities/focusStyle";
 import { merge } from "@utilities/merge";
-import React, { FC, MouseEvent, PropsWithChildren, ReactNode, RefObject, useEffect, useMemo, useRef } from "react";
+import React, { FC, MouseEvent, PropsWithChildren, ReactNode, useEffect, useRef } from "react";
 import { LegacyFlyoutFooter } from ".";
 import { Overlay } from "./Overlay";
-import { useContainScroll } from "./useContainScroll";
+import { useContainScroll } from "./hooks/useContainScroll";
+import { useOverlayPositionWithBottomMargin } from "./hooks/useOverlayPositionWithBottomMargin";
 
 export const FLYOUT_DIVIDER_COLOR = "#eaebeb";
 export const FLYOUT_DIVIDER_HEIGHT = "10px";
@@ -57,7 +58,6 @@ export const Flyout: FC<FlyoutProps> = ({
     const { toggle, close } = state;
     const triggerRef = useRef<HTMLDivElement | null>(null);
     const overlayRef = useRef<HTMLDivElement | null>(null);
-    const innerOverlayRef = useRef<HTMLDivElement | null>(null);
     const scrollRef = useRef<HTMLDivElement | null>(null);
     const { isFocusVisible, focusProps } = useFocusRing();
 
@@ -67,36 +67,15 @@ export const Flyout: FC<FlyoutProps> = ({
         triggerRef,
     );
 
-    const ariaScrollCalculationRef = useMemo(() => {
-        if (scrollRef.current && innerOverlayRef.current) {
-            const outerScrollHeight = innerOverlayRef.current.scrollHeight;
-            const { scrollHeight: innerScrollHeight, clientHeight: innerClientHeight } = scrollRef.current;
-            /* The scrollRef passed to useOverlayPosition is used by react-aria to determine if the height should flip.
-            Since the only properties it needs are the 4 below, and since it expects the scrollable content to be the
-            outermost container, we need to combine the scrollHeights of the innerOverlay and the scrollRef so that it
-            can properly calculate if it must flip the position */
-
-            return {
-                current: {
-                    ...scrollRef.current,
-                    scrollHeight: outerScrollHeight + (innerScrollHeight - innerClientHeight),
-                },
-            } as RefObject<HTMLDivElement>;
-        }
-
-        return scrollRef;
-    }, [innerOverlayRef.current, scrollRef.current?.scrollHeight, scrollRef.current?.clientHeight]);
-
-    const { overlayProps: positionProps } = useOverlayPosition({
-        targetRef: triggerRef,
+    const { positionProps } = useOverlayPositionWithBottomMargin({
+        triggerRef,
         overlayRef,
-        placement: "bottom left",
-        offset: 5,
-        scrollRef: ariaScrollCalculationRef,
+        scrollRef,
         isOpen,
     });
 
     const { buttonProps } = useButton({ onPress: () => toggle() }, triggerRef);
+
     useEffect(() => {
         const revert = watchModals();
 
@@ -140,7 +119,6 @@ export const Flyout: FC<FlyoutProps> = ({
                             }
                             ref={overlayRef}
                             scrollRef={scrollRef}
-                            innerOverlayRef={innerOverlayRef}
                             fitContent={fitContent}
                         >
                             {overlayRef?.current && children}
---
+++ src/components/Flyout/hooks/useOverlayPositionWithBottomMargin.ts
@@ -0,0 +1,46 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { useOverlayPosition } from "@react-aria/overlays";
+import { MutableRefObject } from "react";
+import { getTotalOverlayHeight } from "../helpers/getTotalOverlayHeight";
+import { shouldDisplayAbove } from "../helpers/shouldDisplayAbove";
+
+const FLYOUT_OVERLAY_OFFSET = 5;
+const DEFAULT_OVERLAY_PADDING = 12;
+const INTERCOM_BUTTON_HEIGHT = 70;
+
+type UseOverlayPositionWithBottomMarginProps = {
+    triggerRef: MutableRefObject<HTMLElement | null>;
+    overlayRef: MutableRefObject<HTMLElement | null>;
+    scrollRef: MutableRefObject<HTMLElement | null>;
+    isOpen: boolean;
+};
+
+export const useOverlayPositionWithBottomMargin = ({
+    triggerRef,
+    overlayRef,
+    scrollRef,
+    isOpen,
+}: UseOverlayPositionWithBottomMarginProps) => {
+    const overlayHeight = getTotalOverlayHeight(overlayRef, scrollRef);
+
+    const isFlipped = shouldDisplayAbove(triggerRef, overlayHeight, FLYOUT_OVERLAY_OFFSET, INTERCOM_BUTTON_HEIGHT);
+
+    const { overlayProps: positionProps } = useOverlayPosition({
+        targetRef: triggerRef,
+        overlayRef,
+        shouldFlip: false,
+        placement: isFlipped ? "top left" : "bottom left",
+        containerPadding: DEFAULT_OVERLAY_PADDING,
+        offset: FLYOUT_OVERLAY_OFFSET,
+        scrollRef,
+        isOpen,
+    });
+
+    if (typeof positionProps?.style?.maxHeight === "number" && !isFlipped) {
+        const heightToSubtract = INTERCOM_BUTTON_HEIGHT - DEFAULT_OVERLAY_PADDING;
+        positionProps.style.maxHeight -= heightToSubtract;
+    }
+
+    return { positionProps };
+};
