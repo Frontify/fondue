45bbf708009372d0dfd2598fdbbd8e4f11d19f45
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2022-02-24 13:53:01 +0100

    Making some adjustments to drop SVG

--- src/foundation/Icon/Generated/IconDrop.tsx
+++ src/foundation/Icon/Generated/IconDrop.tsx
@@ -9,8 +9,8 @@ function IconDrop(props: GeneratedIconProps): React.ReactElement<GeneratedIconPr
     ].join(" ");
     return (
         <svg
+            fill="currentColor"
             viewBox="0 0 33 33"
-            fill="none"
             xmlns="http://www.w3.org/2000/svg"
             width="100%"
             height="100%"
