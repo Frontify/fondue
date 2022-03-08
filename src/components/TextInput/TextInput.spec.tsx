ce596e63672d3bd1d80ea0ce44f323f47d574117
Sam Creasey <sam.creasey@frontify.com>
2022-03-08 11:02:21 +0100

    add new readonly test

--- src/components/TextInput/TextInput.spec.tsx
+++ src/components/TextInput/TextInput.spec.tsx
@@ -150,6 +150,20 @@ describe("Text Input component", () => {
         cy.get(TEXT_INPUT_ID).should("have.attr", "readOnly", "readonly");
     });
 
+    it("still allows buttons to be pressed if readonly", () => {
+        mount(<StatefulInput readonly obfuscated copyable type={TextInputType.Password} value={PASSWORD} />);
+        cy.get(TEXT_INPUT_ID).should("have.attr", "readOnly", "readonly");
+        cy.get(COPY_ICON_ID).realClick();
+        cy.window().then((win) => {
+            win.navigator.clipboard.readText().then((text) => {
+                expect(text).to.equal(INPUT_TEXT);
+            });
+        });
+        cy.get(VISIBILITY_ICON_ID).click();
+        cy.get(TEXT_INPUT_ID).should("have.value", PASSWORD);
+        cy.get(TEXT_INPUT_ID).should("have.attr", "type", "text");
+    });
+
     it("calls the copy event", () => {
         mount(<StatefulInput copyable={true} value={INPUT_TEXT} />);
         cy.get(COPY_ICON_ID).find("svg").should("have.attr", "name", "IconCopyToClipboard");
