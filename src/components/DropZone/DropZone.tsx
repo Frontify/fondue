c9c47e791df2746c5e9cdc3f4492f5c944ac19c8
Jiri Matyas <jiri.matyas@frontify.com>
2022-03-10 15:16:09 +0100

    refator: DropZone classNames

--- src/components/DropZone/DropZone.tsx
+++ src/components/DropZone/DropZone.tsx
@@ -43,6 +43,9 @@ export const DropZone = <T extends object>({ data, onDrop, children, listId }: D
     });
 
     const isActive = isOver && canDrop;
+    const outerDropZoneClassNames = "tw-my-[-4px] tw-h-[10px] tw-py-1 tw-outline-none tw-relative tw-z-20";
+    const activeOuterDropZoneClassNames = "tw-border-violet-60 tw-border-2 tw-h-7 tw-bg-clip-content";
+    const bgColorClassName = "tw-bg-violet-20";
 
     return (
         <div
@@ -51,13 +54,9 @@ export const DropZone = <T extends object>({ data, onDrop, children, listId }: D
             data-test-id="drop-zone"
             className={merge([
                 "tw-w-full tw-transition-height",
-                data.position !== DropZonePosition.Within
-                    ? "tw-my-[-4px] tw-h-[10px] tw-py-1 tw-outline-none tw-relative tw-z-20"
-                    : "tw-h-auto",
-                isActive && data.position !== DropZonePosition.Within
-                    ? "tw-border-violet-60 tw-border-2 tw-h-7 tw-bg-clip-content"
-                    : "",
-                isActive && data.position === DropZonePosition.Within ? "tw-bg-violet-20" : "",
+                data.position !== DropZonePosition.Within ? outerDropZoneClassNames : "tw-h-auto",
+                isActive && data.position !== DropZonePosition.Within ? activeOuterDropZoneClassNames : "",
+                isActive && data.position === DropZonePosition.Within ? bgColorClassName : "",
             ])}
             ref={drop}
         >
