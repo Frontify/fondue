ef7824c37ae8da99bba4917a803bc50cc1a2ca22
CelienB <celien.boillat@frontify.com>
2022-03-11 10:42:47 +0100

    feat: allow for story customization

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -5,43 +5,107 @@ import React, { useState } from "react";
 import { ModalBody } from "./ModalBody";
 import { ModalHeader } from "./ModalHeader";
 import { ModalFooter } from "./ModalFooter";
-import { IconIcons } from "@foundation/Icon";
+import { ModalHeaderProps, ModalHeaderVariant, ModalProps, ModalVisualProps } from "./types";
 import { Story, Meta } from "@storybook/react";
 import { TextInput } from "@components/TextInput";
-import { ModalHeaderVariant, ModalProps } from "./types";
 import { generateRandomId } from "@utilities/generateRandomId";
 import { PatternDesign, PatternTheme } from "@foundation/Pattern";
+import { IconAcademy, IconAudio, IconIcons } from "@foundation/Icon";
 import { ScrollWrapperDirection } from "@components/ScrollWrapper/types";
 import { FormControl, FormControlDirection, FormControlStyle } from "@components/FormControl";
+import { action } from "@storybook/addon-actions";
 
 // eslint-disable-next-line import/no-default-export
 export default {
     title: "Components/Modal",
     component: Modal,
-} as Meta<ModalProps>;
+    argTypes: {
+        pattern: {
+            table: {
+                category: "Visual",
+            },
+            name: "Pattern",
+            options: ["None", ...Object.values(PatternDesign)],
+            mapping: { None: null, ...Object.values(PatternDesign) },
+            defaultValue: PatternDesign.DigitalAssets,
+            control: { type: "select" },
+        },
+        foregroundColor: {
+            table: {
+                category: "Visual",
+            },
+            name: "Theme",
+            options: ["None", ...Object.values(PatternTheme)],
+            mapping: { None: null, ...Object.values(PatternDesign) },
+            defaultValue: PatternTheme.Black,
+            control: { type: "select" },
+        },
+        title: {
+            table: {
+                category: "Header",
+            },
+            name: "Title",
+            defaultValue: "Awesome modal",
+            control: { type: "text" },
+        },
+        leadText: {
+            table: {
+                category: "Header",
+            },
+            name: "Lead Text",
+            defaultValue: "The Home for Brand Management",
+            control: { type: "text" },
+        },
+        decorator: {
+            table: {
+                category: "Header",
+            },
+            name: "Decorator",
+            options: ["None", "Icons", "Academy", "Audio"],
+            mapping: {
+                None: null,
+                Icons: <IconIcons key="iconIcons" />,
+                Academy: <IconAcademy key="iconAcademy" />,
+                Audio: <IconAudio key="iconAudio" />,
+            },
+            defaultValue: "Icons",
+            control: { type: "select" },
+        },
+        variant: {
+            table: {
+                category: "Header",
+            },
+            name: "Variant",
+            options: ["None", ...Object.values(ModalHeaderVariant)],
+            mapping: { None: null, ...Object.values(ModalHeaderVariant) },
+            defaultValue: ModalHeaderVariant.Informative,
+            control: { type: "select" },
+        },
+    },
+} as Meta;
 
-export const Default: Story<ModalProps> = () => {
+export const Default: Story<ModalProps & ModalVisualProps & ModalHeaderProps> = (args) => {
     const [input, setInput] = useState("");
 
     return (
         <Modal
             visual={{
-                pattern: PatternDesign.DigitalAssets,
-                foregroundColor: PatternTheme.Violet,
+                pattern: args.pattern,
+                foregroundColor: args.foregroundColor,
             }}
             header={
                 <ModalHeader
-                    title="Modal header"
-                    leadText="The Home for Brand Management"
-                    decorator={<IconIcons />}
-                    variant={ModalHeaderVariant.Informative}
+                    title={args.title}
+                    leadText={args.leadText}
+                    decorator={args.decorator}
+                    variant={args.variant}
                 />
             }
             footer={
                 <ModalFooter
                     buttons={[
-                        { label: "Okay", action: () => console.log("okay") },
-                        { label: "Cancel", action: () => console.log("cancel") },
+                        { label: "Okay", action: () => action("click") },
+                        { label: "Cancel", action: () => action("click") },
                     ]}
                 />
             }
