f3c5ab6c9e8e28c108cba3be2d9a0ce72cb01cdb
Céline <42832501+celine-s@users.noreply.github.com>
2022-03-15 13:42:47 +0100

    feat: multi asset preview component (#636)
    * feat: multi asset preview component and stories
    
    * add tests and a story
    
    * fix: changes according to PR
    
    * fix: add tests for asset section
    
    * install
    
    * package-lock update
    
    * fix: add Multi Asset Preview to Asset Input
    
    * fix: update import
    
    * fix: add Multi Asset Preview to Asset Input
    
    * add tests
    
    * fix: package-lock.json
    
    * fix: changes according to PR
    
    * fix: refactor argTypes to const
    
    * fix: changes according to PR & refactoring
    
    * fix: changes according to PR
    
    * adapt design
    
    * fix: margin & padding
    
    * fix: changes according to PR
    
    * fix: default value

---
+++ src/components/AssetInput/asset-input-actions.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { ActionMenuBlock, MenuItemStyle } from "..";
+import { IconCrop, IconExternalLink, IconImageLibrary, IconReject, IconUploadAlternative } from "../..";
+
+export const assetInputActions = [
+    {
+        id: "block1",
+        ariaLabel: "First section",
+        menuItems: [
+            {
+                id: "1",
+                title: "Replace with Asset",
+                decorator: <IconImageLibrary />,
+            },
+            {
+                id: "2",
+                title: "Replace with Upload",
+                decorator: <IconUploadAlternative />,
+            },
+        ],
+    },
+    {
+        id: "block2",
+        ariaLabel: "Second section",
+        menuItems: [
+            {
+                id: "10",
+                title: "Crop / Resize",
+                decorator: <IconCrop />,
+            },
+            {
+                id: "11",
+                title: "Small warning",
+                decorator: <IconExternalLink />,
+            },
+        ],
+    },
+    {
+        id: "block3",
+        ariaLabel: "Third section",
+        menuItems: [
+            {
+                id: "20",
+                title: "Remove",
+                style: MenuItemStyle.Danger,
+                decorator: <IconReject />,
+            },
+        ],
+    },
+] as ActionMenuBlock[];
