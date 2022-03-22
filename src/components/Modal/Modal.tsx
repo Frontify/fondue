6b42c0c39f736dbdc0096e98d515779b9fe6c9e1
Sam Creasey <sam.creasey@frontify.com>
2022-03-22 12:28:22 +0100

    added data attribute to underlay

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -57,6 +57,7 @@ const ModalComponent: FC<ModalProps> = memo((props) => {
             exit="exit"
             style={{ background: "rgba(0, 0, 0, .5)" }}
             onPointerDown={onPointerDown}
+            data-is-underlay={true}
             className="tw-fixed tw-top-0 tw-left-0 tw-bottom-0 tw-right-0 tw-z-50 tw-flex tw-justify-center tw-items-center tw-p-3"
         >
             {/* eslint-disable-next-line jsx-a11y/no-autofocus */}
