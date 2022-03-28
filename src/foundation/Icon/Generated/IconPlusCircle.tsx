d5f4b35b08f586de54ad52081ea21d51d1287626
imoutaharik <ilham.moutaharik@frontify.com>
2022-03-28 14:55:40 +0200

    feat: Add Generated Plus Circle Icon

---
+++ src/foundation/Icon/Generated/IconPlusCircle.tsx
@@ -0,0 +1,31 @@
+import React, { memo } from "react";
+import { GeneratedIconProps } from "@foundation/Icon/IconProps";
+import { IconSize, IconSizeMap } from "@foundation/Icon/IconSize";
+
+function IconPlusCircle(props: GeneratedIconProps): React.ReactElement<GeneratedIconProps> {
+    const customClassName = [
+        "tw-flex tw-items-center tw-justify-center tw-fill-current",
+        IconSizeMap[props.size || IconSize.Size16],
+    ].join(" ");
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="100%"
+            height="100%"
+            className={customClassName}
+            name="IconPlusCircle"
+            {...props}
+        >
+            <path
+                fill="currentColor"
+                fillRule="evenodd"
+                d="M12 20.5a8.5 8.5 0 1 0 0-17 8.5 8.5 0 0 0 0 17Zm0 1.5c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10ZM7 12a.75.75 0 0 1 .75-.75h3.5v-3.5a.75.75 0 0 1 1.5 0v3.5h3.5a.75.75 0 0 1 0 1.5h-3.5v3.5a.75.75 0 0 1-1.5 0v-3.5h-3.5A.75.75 0 0 1 7 12Z"
+                clipRule="evenodd"
+            />
+        </svg>
+    );
+}
+
+const Memo = memo(IconPlusCircle);
+export default Memo;
