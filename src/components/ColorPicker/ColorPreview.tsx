145adc664d35d875cbe73afe2c9310afe02f8df5
Kevin Walker <kevin@smartive.ch>
2022-03-30 10:46:09 +0200

    add border to swatches and color preview

--- src/components/ColorPicker/ColorPreview.tsx
+++ src/components/ColorPicker/ColorPreview.tsx
@@ -18,7 +18,7 @@ export const ColorPreview: FC<ColorPreviewProps> = ({ color, format }) => {
     const displayValue = getColorDisplayValue(color, format);
 
     return (
-        <div className="tw-sticky tw-top-0 tw-bg-white tw-z-20 dark:tw-bg-black-95">
+        <div className="tw-sticky tw-top-0 tw-bg-white tw-z-20 dark:tw-bg-black-95 tw-border-b tw-border-b-black-10">
             <div
                 className={merge([
                     "tw-flex tw-justify-center tw-p-7 tw-text-m tw-gap-2",
