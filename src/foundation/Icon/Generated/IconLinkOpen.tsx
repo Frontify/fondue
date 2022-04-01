4f5bfa50d46cc4f476c5a6f0ff8110bb241c1ba6
imoutaharik <ilham.moutaharik@frontify.com>
2022-04-01 11:22:40 +0200

    feat(Icons): Add Linl and LinkOpen icons

--- src/foundation/Icon/Generated/IconLinkOpen.tsx
+++ src/foundation/Icon/Generated/IconLinkOpen.tsx
@@ -2,7 +2,7 @@ import React, { memo } from "react";
 import { GeneratedIconProps } from "@foundation/Icon/IconProps";
 import { IconSize, IconSizeMap } from "@foundation/Icon/IconSize";
 
-function IconLink(props: GeneratedIconProps): React.ReactElement<GeneratedIconProps> {
+function IconLinkOpen(props: GeneratedIconProps): React.ReactElement<GeneratedIconProps> {
     const customClassName = [
         "tw-flex tw-items-center tw-justify-center tw-fill-current",
         IconSizeMap[props.size || IconSize.Size16],
@@ -14,18 +14,18 @@ function IconLink(props: GeneratedIconProps): React.ReactElement<GeneratedIconPr
             width="100%"
             height="100%"
             className={customClassName}
-            name="IconLink"
+            name="IconLinkOpen"
             {...props}
         >
             <path
                 fill="currentColor"
                 fillRule="evenodd"
-                d="M2 12a4.75 4.75 0 0 1 4.75-4.75h2a.75.75 0 0 1 0 1.5h-2a3.25 3.25 0 0 0 0 6.5h2a.75.75 0 0 1 0 1.5h-2A4.75 4.75 0 0 1 2 12Zm20 0a4.75 4.75 0 0 1-4.75 4.75h-2a.75.75 0 0 1 0-1.5h2a3.25 3.25 0 0 0 0-6.5h-2a.75.75 0 0 1 0-1.5h2A4.75 4.75 0 0 1 22 12Zm-13.75-.75a.75.75 0 0 0 0 1.5h7.5a.75.75 0 0 0 0-1.5h-7.5Z"
+                d="M6.75 7.25a4.75 4.75 0 0 0 0 9.5h2a.75.75 0 0 0 0-1.5h-2a3.25 3.25 0 0 1 0-6.5h2a.75.75 0 0 0 0-1.5h-2Zm10.5 9.5a4.75 4.75 0 1 0 0-9.5h-2a.75.75 0 0 0 0 1.5h2a3.25 3.25 0 0 1 0 6.5h-2a.75.75 0 0 0 0 1.5h2Z"
                 clipRule="evenodd"
             />
         </svg>
     );
 }
 
-const Memo = memo(IconLink);
+const Memo = memo(IconLinkOpen);
 export default Memo;
