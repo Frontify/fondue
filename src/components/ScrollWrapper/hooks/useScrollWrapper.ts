28971d9edfdf3503558f371bd794d5cc62be0cf0
Samuel Alev <samuel.alev@frontify.com>
2022-04-12 15:57:34 +0200

    fix: disconnect `ResizeObserver` in any case (#720)

--- src/components/ScrollWrapper/hooks/useScrollWrapper.ts
+++ src/components/ScrollWrapper/hooks/useScrollWrapper.ts
@@ -19,7 +19,6 @@ export const useScrollWrapper = (scrollingContainer: RefObject<HTMLElement>) => 
     };
 
     useEffect(() => {
-        let resizeObserver: ResizeObserver;
         const updateDimensionsFromRef = () => {
             if (scrollingContainer.current) {
                 const dimensions = getScrollDimensions(scrollingContainer.current);
@@ -27,18 +26,17 @@ export const useScrollWrapper = (scrollingContainer: RefObject<HTMLElement>) => 
             }
         };
 
+        updateDimensionsFromRef();
+
+        const resizeObserver = new ResizeObserver(updateDimensionsFromRef);
         if (scrollingContainer.current) {
-            updateDimensionsFromRef();
-            resizeObserver = new ResizeObserver(updateDimensionsFromRef);
             resizeObserver.observe(scrollingContainer.current);
-            window.addEventListener("resize", updateDimensionsFromRef);
         }
+        window.addEventListener("resize", updateDimensionsFromRef);
 
         return () => {
-            if (scrollingContainer.current) {
-                resizeObserver.disconnect();
-                window.removeEventListener("resize", updateDimensionsFromRef);
-            }
+            resizeObserver.disconnect();
+            window.removeEventListener("resize", updateDimensionsFromRef);
         };
     }, []);
 
