3df1d0d5540fd19a21db539d23df1698634d02b2
imoutaharik <ilham.moutaharik@frontify.com>
2022-04-13 15:08:23 +0200

    BREAKING CHANGE: Refactor Tooltip component

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -4,31 +4,30 @@ import { Button, ButtonSize, ButtonStyle } from "@components/Button/Button";
 import { IconSize } from "@foundation/Icon/IconSize";
 import { useFocusRing } from "@react-aria/focus";
 import { useLink } from "@react-aria/link";
-import { useTooltip } from "@react-aria/tooltip";
 import { mergeProps } from "@react-aria/utils";
-import { AriaTooltipProps } from "@react-types/tooltip";
 import { FOCUS_STYLE } from "@utilities/focusStyle";
 import { merge } from "@utilities/merge";
 import React, {
     cloneElement,
-    CSSProperties,
-    forwardRef,
-    ForwardRefRenderFunction,
+    FC,
+    MutableRefObject,
     ReactChild,
     ReactElement,
     ReactNode,
+    useEffect,
     useRef,
+    useState,
 } from "react";
 import { BrightHeader, BrightHeaderStyle } from "./BrightHeader";
+import { usePopper } from "react-popper";
 
 export type TooltipButton = {
     label: string;
     action: () => void;
 };
 
-type PopperAttributes = { [key: string]: string };
-
 export type TooltipProps = {
+    triggerRefElement: MutableRefObject<any>;
     content: ReactNode;
     tooltipIcon?: ReactElement;
     heading?: ReactNode;
@@ -37,10 +36,11 @@ export type TooltipProps = {
     linkLabel?: string;
     brightHeader?: BrightHeaderStyle;
     buttons?: [TooltipButton, TooltipButton] | [TooltipButton];
-    tooltipAriaProps?: AriaTooltipProps;
-    style?: CSSProperties;
     children?: ReactChild;
-    popperAttributes?: PopperAttributes;
+    position?: TooltipPosition;
+    alignment?: TooltipAlignment;
+    flip?: boolean;
+    withArrow?: boolean;
 };
 
 /**
@@ -56,113 +56,228 @@ const paddingsBottom = {
     large: "tw-pb-4",
 };
 
-const TooltipComponent: ForwardRefRenderFunction<HTMLDivElement, TooltipProps> = (
-    {
-        content,
-        tooltipIcon,
-        heading,
-        headingIcon,
-        linkUrl,
-        linkLabel,
-        brightHeader,
-        buttons,
-        tooltipAriaProps = {},
-        style,
-        children,
-        popperAttributes,
-    },
-    tooltipContainerRef,
-) => {
-    const linkRef = useRef<HTMLAnchorElement | null>(null);
-    const { linkProps } = useLink({}, linkRef);
+export enum TooltipPosition {
+    Top = "top",
+    Right = "right",
+    Bottom = "bottom",
+    Left = "left",
+}
 
-    const { isFocusVisible, focusProps } = useFocusRing();
+export enum TooltipAlignment {
+    Start = "Start",
+    Middle = "Middle",
+    End = "End",
+}
 
-    const { tooltipProps } = useTooltip(tooltipAriaProps);
-
-    const hasLargePaddingTop = linkUrl || brightHeader || buttons || heading || headingIcon;
-
-    return (
-        <>
-            <div
-                ref={tooltipContainerRef}
-                className="tw-popper-container tw-inline-block tw-max-w-[200px] tw-bg-black-100 dark:tw-bg-white tw-rounded-md tw-shadow-mid tw-text-white dark:tw-text-black-100 tw-z-20"
-                style={style}
-                data-test-id="tooltip"
-                {...popperAttributes}
-                {...tooltipProps}
-            >
-                {brightHeader && <BrightHeader headerStyle={brightHeader} />}
-                <div
-                    className={merge([
-                        "tw-px-4",
-                        hasLargePaddingTop ? paddingsTop.small : paddingsTop.large,
-                        linkUrl ? paddingsBottom.small : paddingsBottom.large,
-                    ])}
-                >
-                    {heading && (
-                        <h4 className="tw-flex tw-text-m tw-font-bold tw-mb-1">
-                            {headingIcon && (
-                                <span className="tw-mr-1.5">
-                                    {cloneElement(headingIcon, { size: IconSize.Size20 })}
-                                </span>
-                            )}
-                            {heading}
-                        </h4>
-                    )}
-                    <div className="tw-flex">
-                        {tooltipIcon && (
-                            <span className="tw-shrink-0 tw-mr-1">
-                                {cloneElement(tooltipIcon, { size: IconSize.Size16 })}
-                            </span>
-                        )}
-                        <p className="tw-text-s">{content}</p>
-                    </div>
-                    {linkUrl && (
-                        <a
-                            {...mergeProps(linkProps, focusProps)}
-                            data-test-id="tooltip-link"
-                            ref={linkRef}
-                            href={linkUrl}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className={merge([
-                                "tw-text-xs tw-text-black-40 dark:tw-text-black-80 tw-underline tw-mt-1",
-                                isFocusVisible && FOCUS_STYLE,
-                            ])}
-                        >
-                            {linkLabel ?? "Click here to learn more."}
-                        </a>
-                    )}
-                    {buttons && (
-                        <div className="tw-flex tw-flex-row-reverse tw-gap-x-1 tw-mt-4">
-                            {buttons.length > 0 && (
-                                <Button
-                                    style={ButtonStyle.Primary}
-                                    size={ButtonSize.Small}
-                                    inverted
-                                    onClick={buttons[0].action}
-                                >
-                                    {buttons[0].label}
-                                </Button>
-                            )}
-                            {buttons.length === 2 && (
-                                <Button
-                                    style={ButtonStyle.Secondary}
-                                    size={ButtonSize.Small}
-                                    inverted
-                                    onClick={buttons[1].action}
-                                >
-                                    {buttons[1].label}
-                                </Button>
-                            )}
-                        </div>
-                    )}
-                </div>
-                {children}
-            </div>
-        </>
-    );
+const alignementSuffix: Record<TooltipAlignment, string> = {
+    [TooltipAlignment.Start]: "-start",
+    [TooltipAlignment.Middle]: "",
+    [TooltipAlignment.End]: "-end",
 };
 
-export const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(TooltipComponent);
+const brightHeaderBackgrounds: Record<BrightHeaderStyle, string> = {
+    [BrightHeaderStyle.Information]: "before:tw-bg-violet-60",
+    [BrightHeaderStyle.Warning]: "before:tw-bg-red-60",
+    [BrightHeaderStyle.Tip]: "before:tw-bg-green-60",
+    [BrightHeaderStyle.Note]: "before:tw-bg-yellow-60",
+};
+
+export const Tooltip: FC<TooltipProps> = ({
+    content,
+    tooltipIcon,
+    heading,
+    headingIcon,
+    linkUrl,
+    linkLabel,
+    brightHeader,
+    buttons,
+    children,
+    position = TooltipPosition.Bottom,
+    alignment = TooltipAlignment.Middle,
+    withArrow,
+    flip = true,
+    triggerRefElement,
+}) => {
+    const linkRef = useRef<HTMLAnchorElement | null>(null);
+    const { linkProps } = useLink({}, linkRef);
+    const { isFocusVisible, focusProps } = useFocusRing();
+    const hasLargePaddingTop = linkUrl || brightHeader || buttons || heading || headingIcon;
+
+    const placement = position + alignementSuffix[alignment];
+    const tooltipContainerRef = useRef<HTMLDivElement | null>(null);
+    const [arrowElement, setArrowElement] = useState<HTMLDivElement | null>(null);
+
+    const setArrowClasses = () => {
+        const currentPlacement = popperInstance.state?.placement ?? position;
+        switch (true) {
+            case currentPlacement.toString().includes(TooltipPosition.Top):
+                return "before:tw-border-t-0 before:tw-border-l-0 tw-bottom-[-6px] before:tw-bg-black-100 before:dark:tw-bg-white";
+            case currentPlacement.toString().includes(TooltipPosition.Right):
+                return merge([
+                    "before:tw-border-t-0 before:tw-border-r-0 tw-left-[-6px]",
+                    brightHeader && alignment === TooltipAlignment.Start
+                        ? brightHeaderBackgrounds[brightHeader]
+                        : "before:tw-bg-black-100 before:dark:tw-bg-white",
+                ]);
+            case currentPlacement.toString().includes(TooltipPosition.Bottom):
+                return merge([
+                    "before:tw-border-b-0 before:tw-border-r-0 tw-top-[-6px]",
+                    brightHeader
+                        ? brightHeaderBackgrounds[brightHeader]
+                        : "before:tw-bg-black-100 before:dark:tw-bg-white",
+                ]);
+            case currentPlacement.toString().includes(TooltipPosition.Left):
+                return merge([
+                    "before:tw-border-b-0 before:tw-border-l-0 tw-right-[-6px]",
+                    brightHeader && alignment === TooltipAlignment.Start
+                        ? brightHeaderBackgrounds[brightHeader]
+                        : "before:tw-bg-black-100 before:dark:tw-bg-white",
+                ]);
+            default:
+                return "before:tw-border-b-0 before:tw-border-r-0 tw-top-[-6px] before:tw-bg-black-100 before:dark:tw-bg-white";
+        }
+    };
+
+    const tooltipOffset = withArrow ? 10 : 5;
+    const popperInstance = usePopper(triggerRefElement?.current, tooltipContainerRef.current, {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        placement: placement,
+        modifiers: [
+            {
+                name: "arrow",
+                options: {
+                    element: arrowElement,
+                },
+            },
+            {
+                name: "offset",
+                options: {
+                    offset: [0, tooltipOffset],
+                },
+            },
+            {
+                name: "flip",
+                enabled: flip,
+            },
+        ],
+    });
+
+    const arrowStyling = setArrowClasses();
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        if (triggerRefElement) {
+            triggerRefElement.current.addEventListener("mouseover", () => setIsOpen(true));
+            triggerRefElement.current.addEventListener("mouseleave", () => setTimeout(() => setIsOpen(false), 2000));
+            triggerRefElement.current.addEventListener("focus", () => setIsOpen(true));
+        }
+
+        return () => {
+            if (triggerRefElement) {
+                triggerRefElement.current.removeEventListener("mouseover", () => setIsOpen(true));
+                triggerRefElement.current.removeEventListener("mouseleave", () =>
+                    setTimeout(() => setIsOpen(false), 2000),
+                );
+                triggerRefElement.current.removeEventListener("focus", () => setIsOpen(true));
+            }
+        };
+    }, []);
+
+    return (
+        <div>
+            {isOpen && (
+                <div
+                    ref={tooltipContainerRef}
+                    className="tw-popper-container tw-inline-block tw-max-w-[200px] tw-bg-black-100 dark:tw-bg-white tw-rounded-md tw-shadow-mid tw-text-white dark:tw-text-black-100 tw-z-20"
+                    data-test-id="tooltip"
+                    role="tooltip"
+                    aria-hidden={!isOpen}
+                    tabIndex={isOpen ? 0 : -1}
+                    style={popperInstance.styles.popper}
+                    {...popperInstance.attributes.popper}
+                >
+                    {brightHeader && <BrightHeader headerStyle={brightHeader} />}
+                    <div
+                        className={merge([
+                            "tw-px-4",
+                            hasLargePaddingTop ? paddingsTop.small : paddingsTop.large,
+                            linkUrl ? paddingsBottom.small : paddingsBottom.large,
+                        ])}
+                    >
+                        {heading && (
+                            <h4 className="tw-flex tw-text-m tw-font-bold tw-mb-1">
+                                {headingIcon && (
+                                    <span className="tw-mr-1.5">
+                                        {cloneElement(headingIcon, { size: IconSize.Size20 })}
+                                    </span>
+                                )}
+                                {heading}
+                            </h4>
+                        )}
+                        <div className="tw-flex">
+                            {tooltipIcon && (
+                                <span className="tw-shrink-0 tw-mr-1">
+                                    {cloneElement(tooltipIcon, { size: IconSize.Size16 })}
+                                </span>
+                            )}
+                            <p className="tw-text-s">{content}</p>
+                        </div>
+                        {linkUrl && (
+                            <a
+                                {...mergeProps(linkProps, focusProps)}
+                                data-test-id="tooltip-link"
+                                ref={linkRef}
+                                href={linkUrl}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className={merge([
+                                    "tw-text-xs tw-text-black-40 dark:tw-text-black-80 tw-underline tw-mt-1",
+                                    isFocusVisible && FOCUS_STYLE,
+                                ])}
+                            >
+                                {linkLabel ?? "Click here to learn more."}
+                            </a>
+                        )}
+                        {buttons && (
+                            <div className="tw-flex tw-flex-row-reverse tw-gap-x-1 tw-mt-4">
+                                {buttons.length > 0 && (
+                                    <Button
+                                        style={ButtonStyle.Primary}
+                                        size={ButtonSize.Small}
+                                        inverted
+                                        onClick={buttons[0].action}
+                                    >
+                                        {buttons[0].label}
+                                    </Button>
+                                )}
+                                {buttons.length === 2 && (
+                                    <Button
+                                        style={ButtonStyle.Secondary}
+                                        size={ButtonSize.Small}
+                                        inverted
+                                        onClick={buttons[1].action}
+                                    >
+                                        {buttons[1].label}
+                                    </Button>
+                                )}
+                            </div>
+                        )}
+                        <div
+                            data-test-id="popover-arrow"
+                            data-popper-arrow
+                            ref={setArrowElement}
+                            style={popperInstance.styles.arrow}
+                            className={merge([
+                                withArrow &&
+                                    "tw-popper-arrow tw-absolute tw-w-3 tw-h-3 tw-pointer-events-none before:tw-absolute before:tw-w-3 before:tw-h-3 before:tw-rotate-45 before:tw-border before:tw-border-line",
+                                withArrow && arrowStyling,
+                            ])}
+                        />
+                    </div>
+                    {children}
+                </div>
+            )}
+        </div>
+    );
+};
--- src/components/TooltipIcon/TooltipIcon.tsx
+++ src/components/TooltipIcon/TooltipIcon.tsx
@@ -1,14 +1,10 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import React, { FC, useRef, useState } from "react";
-import { usePopper } from "react-popper";
-import { useFocusVisible, useHover } from "@react-aria/interactions";
-import { useTooltipTrigger } from "@react-aria/tooltip";
-import { useTooltipTriggerState } from "@react-stately/tooltip";
+import React, { FC, useRef } from "react";
+import { useFocusVisible } from "@react-aria/interactions";
 import IconQuestion from "@foundation/Icon/Generated/IconQuestion";
 import { IconSize } from "@foundation/Icon/IconSize";
 import { Tooltip, TooltipProps } from "@components/Tooltip/Tooltip";
-import { TooltipArrow } from "@components/Tooltip/TooltipArrow";
 import { FOCUS_STYLE } from "@utilities/focusStyle";
 import { merge } from "@utilities/merge";
 
@@ -17,61 +13,29 @@ export type TooltipIconProps = {
     iconSize?: IconSize;
 };
 
-const TOOLTIP_DISTANCE = 15;
-const TOOLTIP_SKIDDING = 0;
-const TOOLTIP_PADDING = 15;
-
 export const TooltipIcon: FC<TooltipIconProps> = ({ tooltip, iconSize = IconSize.Size16 }: TooltipIconProps) => {
-    const [tooltipArrowElement, setTooltipArrowElement] = useState<HTMLElement | null>(null);
     const tooltipTriggerElement = useRef<HTMLButtonElement | null>(null);
-    const tooltipElement = useRef<HTMLDivElement | null>(null);
-    const state = useTooltipTriggerState();
-    const { triggerProps, tooltipProps } = useTooltipTrigger({}, state, tooltipTriggerElement);
-    const { isOpen } = state;
     const { isFocusVisible } = useFocusVisible();
-    const { styles, attributes } = usePopper(tooltipTriggerElement.current, tooltipElement.current, {
-        placement: "auto",
-        modifiers: [
-            { name: "offset", options: { offset: [TOOLTIP_SKIDDING, TOOLTIP_DISTANCE] } },
-            { name: "arrow", options: { element: tooltipArrowElement, padding: TOOLTIP_PADDING } },
-        ],
-    });
-    const { hoverProps } = useHover({
-        onHoverStart: () => state.open(),
-        onHoverEnd: () => state.close(),
-    });
 
     return (
         <div data-test-id="tooltip-icon">
             {tooltip && (
-                <div {...hoverProps}>
+                <div>
                     <button
                         data-test-id="tooltip-icon-trigger"
                         ref={tooltipTriggerElement}
                         className={merge([
                             "tw-inline-flex tw-justify-center tw-items-center tw-text-black-60 hover:tw-text-black-60 dark:tw-text-black-40 dark:hover:tw-text-white tw-cursor-default tw-outline-none tw-rounded-full",
-                            isOpen && isFocusVisible && FOCUS_STYLE,
+                            isFocusVisible && FOCUS_STYLE,
                         ])}
-                        {...triggerProps}
                     >
                         <IconQuestion size={iconSize} />
                     </button>
-                    {isOpen && (
-                        <Tooltip
-                            {...tooltip}
-                            popperAttributes={attributes.popper}
-                            ref={tooltipElement}
-                            style={styles.popper}
-                            tooltipAriaProps={tooltipProps}
-                        >
-                            <TooltipArrow
-                                ref={setTooltipArrowElement}
-                                style={styles.arrow}
-                                placement={attributes.popper?.["data-popper-placement"]}
-                                headerColor={tooltip.brightHeader}
-                            />
-                        </Tooltip>
-                    )}
+                    <Tooltip
+                        triggerRefElement={tooltipTriggerElement}
+                        withArrow={true}
+                        content="Lorem ipsum dolor sit amet."
+                    />
                 </div>
             )}
         </div>
