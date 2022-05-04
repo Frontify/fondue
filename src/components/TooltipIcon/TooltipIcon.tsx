2325a1c24627c0640e688a53b198b6ee1bfcd1dc
imoutaharik <ilham.moutaharik@frontify.com>
2022-05-04 13:48:26 +0200

    fix: Remove ref from TooltipIcon

--- src/components/TooltipIcon/TooltipIcon.tsx
+++ src/components/TooltipIcon/TooltipIcon.tsx
@@ -1,6 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import React, { FC, useRef } from "react";
+import React, { FC } from "react";
 import { useFocusVisible } from "@react-aria/interactions";
 import IconQuestion from "@foundation/Icon/Generated/IconQuestion";
 import { IconSize } from "@foundation/Icon/IconSize";
@@ -14,24 +14,27 @@ export type TooltipIconProps = {
 };
 
 export const TooltipIcon: FC<TooltipIconProps> = ({ tooltip, iconSize = IconSize.Size16 }: TooltipIconProps) => {
-    const tooltipTriggerElement = useRef<HTMLButtonElement | null>(null);
     const { isFocusVisible } = useFocusVisible();
 
     return (
         <div data-test-id="tooltip-icon">
             {tooltip && (
                 <div>
-                    <button
-                        data-test-id="tooltip-icon-trigger"
-                        ref={tooltipTriggerElement}
-                        className={merge([
-                            "tw-inline-flex tw-justify-center tw-items-center tw-text-black-60 hover:tw-text-black-60 dark:tw-text-black-40 dark:hover:tw-text-white tw-cursor-default tw-outline-none tw-rounded-full",
-                            isFocusVisible && FOCUS_STYLE,
-                        ])}
-                    >
-                        <IconQuestion size={iconSize} />
-                    </button>
-                    <Tooltip triggerRefElement={tooltipTriggerElement} withArrow={true} {...tooltip} />
+                    <Tooltip
+                        triggerElement={
+                            <button
+                                data-test-id="tooltip-icon-trigger"
+                                className={merge([
+                                    "tw-inline-flex tw-justify-center tw-items-center tw-text-black-60 hover:tw-text-black-60 dark:tw-text-black-40 dark:hover:tw-text-white tw-cursor-default tw-outline-none tw-rounded-full",
+                                    isFocusVisible && FOCUS_STYLE,
+                                ])}
+                            >
+                                <IconQuestion size={iconSize} />
+                            </button>
+                        }
+                        withArrow={true}
+                        {...tooltip}
+                    />
                 </div>
             )}
         </div>
