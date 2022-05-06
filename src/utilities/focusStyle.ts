9b97c5c8fd0d5dec2ba0f6d2cf8de5cb537d67ea
imoutaharik <ilham.moutaharik@frontify.com>
2022-05-06 16:03:46 +0200

    fix: Small styling improvements for focus ring

--- src/utilities/focusStyle.ts
+++ src/utilities/focusStyle.ts
@@ -2,5 +2,5 @@
 
 import { merge } from "./merge";
 
-export const FOCUS_STYLE = "tw-ring-4 tw-ring-blue tw-ring-offset-2 dark:tw-ring-offset-black tw-outline-none";
+export const FOCUS_STYLE = "tw-ring-2 tw-ring-blue tw-ring-offset-1 dark:tw-ring-offset-black tw-outline-none";
 export const FOCUS_STYLE_INSET = merge([FOCUS_STYLE, "tw-ring-inset"]);
