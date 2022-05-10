8d57cda3ea7d8b7ce3aba173b1170cc033fcb9a2
imoutaharik <ilham.moutaharik@frontify.com>
2022-05-10 16:25:15 +0200

    fix: Fix create new component script template - remove FC

--- scripts/templates/component.ts
+++ scripts/templates/component.ts
@@ -1,18 +1,21 @@
 import ComponentFileBuilderResponse from "./ComponentFileBuilderResponse";
 import { toKebabCase } from "./transforms";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars,import/no-default-export
 export default (componentName: string, componentType: string): ComponentFileBuilderResponse => ({
     content: `/* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import React, { FC } from "react";
+import React, { PropsWithChildren } from "react";
 
-export type ${componentName}Props = {
-    foo: string;
-};
+export type ${componentName}Props = PropsWithChildren<{ foo: string }>;
 
-export const ${componentName}: FC<${componentName}Props> = ({ foo }) => {
-    return <div data-test-id="${toKebabCase(componentName)}">{foo}</div>;
+export const ${componentName} = ({ foo, children }: ${componentName}Props) => {
+    return (
+        <div data-test-id="${toKebabCase(componentName)}">
+            <div>{foo}</div>
+            {children}
+        </div>
+    );
 };
 `,
     extension: `.tsx`,
