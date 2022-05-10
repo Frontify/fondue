492608b7a7c91fbd5a128587ceb83b655bfa0567
imoutaharik <ilham.moutaharik@frontify.com>
2022-05-10 13:20:03 +0200

    fix: Fix hover in firefox

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -189,7 +189,7 @@ export const Tooltip = ({
 
     const checkIfHovered = useCallback(
         (event) => {
-            const hoveredElement = event.path;
+            const hoveredElement = event.path || (event.composedPath && event.composedPath());
             if (hoveredElement && hoveredElement.includes(triggerRefElement?.current)) {
                 handleShowTooltipOnHover();
             }
