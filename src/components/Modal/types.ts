a6954813438ddf54b5ce78607d3281a4246d4fbd
Michael Ray-Von <michaelrayvon@gmail.com>
2022-05-11 15:24:36 +0200

    add zIndex prop to the Modal

--- src/components/Modal/types.ts
+++ src/components/Modal/types.ts
@@ -58,5 +58,6 @@ export type ModalProps = {
     width?: ModalWidth;
     children?: ModalBodyChildren;
     isOpen: boolean;
+    zIndex?: number;
 } & Omit<OverlayProps, "isOpen"> &
     AriaDialogProps;
