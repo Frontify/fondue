9acc3083989240a47705de5416cfd7f5f574b745
celine <celine@smartive.ch>
2022-05-11 11:30:43 +0200

    fix styling

--- src/components/RichTextEditor/components/code.tsx
+++ src/components/RichTextEditor/components/code.tsx
@@ -3,10 +3,12 @@
 import { PlateRenderLeafProps } from '@udecode/plate';
 import React, { FC } from 'react';
 
+//TODO pass a prop classNames
+//    classNames = "tw-table-cell tw-rounded tw-bg-base-alt tw-text-text-interactive tw-m-0 tw-px-2 tw-py-0.5",
 export const CodeMark: FC<PlateRenderLeafProps> = ({ attributes, children }) => (
     <span
         {...attributes}
-        className="tw-table-cell tw-rounded tw-text-xs tw-bg-black-5 tw-text-violet-90 tw-m-0 tw-px-2 tw-py-0.5"
+        className="tw-table-cell tw-rounded tw-bg-base-alt tw-text-text-interactive tw-m-0 tw-px-2 tw-py-0.5"
     >
         {children}
     </span>
