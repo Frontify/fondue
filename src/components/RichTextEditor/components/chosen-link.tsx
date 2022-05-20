430379a166c0f647c464f84800b5df8c9d332e45
Raphi <raphael@smartive.ch>
2022-05-20 18:29:53 +0200

    fix(RichTextEditor): positioning of preview flyout

--- src/components/RichTextEditor/components/chosen-link.tsx
+++ src/components/RichTextEditor/components/chosen-link.tsx
@@ -2,8 +2,8 @@
 
 import { Flyout } from "@components/Flyout";
 import { PlateRenderElementProps, useEditableProps } from "@udecode/plate";
-import React, { FC, useState } from "react";
-import { IconPen, IconReject } from "../../..";
+import React, { FC, MutableRefObject, useState } from "react";
+import { IconPen, IconTrash } from "../../..";
 import { EditLinkChooserButton } from "../plugins/linkChooserPlugin/ui/EditLinkChooserButton";
 import { RemoveLinkChooserButton } from "../plugins/linkChooserPlugin/ui/RemoveLinkChooserButton copy";
 
@@ -28,39 +28,48 @@ export const ChosenLinkElement: FC<PlateRenderElementProps> = (props) => {
 
     return (
         <>
-            <a
-                onMouseDown={() => {
-                    if (isReadOnly) {
-                        return;
-                    }
-                    setTimeout(() => {
-                        setIsPreviewFlyoutOpen(true);
-                    }, 100);
-                }}
-                data-chosen-link={JSON.stringify(chosenLink)}
-                href={getHref()}
-                target={getTarget()}
-                rel="noreferrer"
-                className={"tw-text-violet-70 tw-underline tw-cursor-pointer"}
-                {...attributes}
-            >
-                {children}
-            </a>
-            {!isReadOnly && (
+            {isReadOnly ? (
+                <a
+                    data-chosen-link={JSON.stringify(chosenLink)}
+                    href={getHref()}
+                    target={getTarget()}
+                    rel="noreferrer"
+                    className={"tw-text-violet-70 tw-underline tw-cursor-pointer"}
+                >
+                    {children}
+                </a>
+            ) : (
                 <Flyout
                     isOpen={isPreviewFlyoutOpen}
                     onOpenChange={setIsPreviewFlyoutOpen}
                     onCancel={() => setIsPreviewFlyoutOpen(false)}
-                    trigger={undefined}
+                    trigger={({ "aria-label": ariaLabel }, ref: MutableRefObject<HTMLAnchorElement>) => (
+                        <a
+                            ref={ref}
+                            aria-label={ariaLabel}
+                            onClick={() => {
+                                setTimeout(() => {
+                                    setIsPreviewFlyoutOpen(true);
+                                }, 100);
+                            }}
+                            data-chosen-link={JSON.stringify(chosenLink)}
+                            href={getHref()}
+                            target={getTarget()}
+                            rel="noreferrer"
+                            className={"tw-text-violet-70 tw-underline tw-cursor-pointer"}
+                        >
+                            {children}
+                        </a>
+                    )}
                     legacyFooter={false}
                 >
-                    <div className="tw-flex tw-justify-between tw-gap-y-8 tw-p-8">
-                        <div className="tw-text-violet-70">{getHref()}</div>
-                        <div className="tw-flex">
+                    <span className="tw-flex tw-justify-between tw-gap-y-8 tw-p-8">
+                        <span className="tw-text-violet-70">{getHref()}</span>
+                        <span className="tw-flex">
                             <EditLinkChooserButton icon={<IconPen />} />
-                            <RemoveLinkChooserButton editor={editor} icon={<IconReject />} />
-                        </div>
-                    </div>
+                            <RemoveLinkChooserButton editor={editor} icon={<IconTrash />} />
+                        </span>
+                    </span>
                 </Flyout>
             )}
         </>
