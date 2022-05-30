e5e45e4d9a66a958ce79006606e4be0feb19a22a
celine <celine@smartive.ch>
2022-05-30 16:56:24 +0200

    ticket: RichTextEditor: show correct selected heading in the dropdownTrigger

--- src/components/RichTextEditor/TextStyleDropdown/DropdownTrigger.tsx
+++ src/components/RichTextEditor/TextStyleDropdown/DropdownTrigger.tsx
@@ -1,33 +1,68 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { IconCaretDown } from '@foundation/Icon';
-import { getParent, getPreventDefaultHandler, usePlateEditorState } from '@udecode/plate';
-import { merge } from '@utilities/merge';
-import React from 'react';
-import { TextStyles, textStyleTitles } from '../utils/getTextStyles';
+import { IconCaretDown } from "@foundation/Icon";
+import {
+    getNodes,
+    getPreventDefaultHandler,
+    isElement,
+    isType,
+    PlateEditor,
+    usePlateEditorState,
+} from "@udecode/plate";
+import { merge } from "@utilities/merge";
+import React from "react";
+import { TextStyles, textStyleTitles } from "../utils/getTextStyles";
 
 type DropdownTriggerProps = {
     editorId?: string;
     open: boolean;
 };
-const DEFAULT_TEXTSTYLE_VALUE = 'Mixed';
+const DEFAULT_TEXTSTYLE_VALUE = "Mixed";
+
+enum ListType {
+    UL = "ul",
+    OL = "ol",
+    CHECKLIST_ITEM = "checkbox_item",
+}
+
+const listTitle: Record<ListType, string> = {
+    [ListType.UL]: "Unordered List",
+    [ListType.OL]: "Ordered List",
+    [ListType.CHECKLIST_ITEM]: "Checklist",
+};
+
+type AvailableTextStyles = ListType & TextStyles;
+
+const availableTextStyleTitles: Record<AvailableTextStyles, string> = { ...listTitle, ...textStyleTitles };
+
+const getSelectedTextStyles: (editor: PlateEditor) => AvailableTextStyles[] = (editor) => {
+    if (editor.selection) {
+        return Array.from(
+            getNodes(editor, {
+                unhang: true,
+                at: editor.selection,
+                match: (node) => isElement(node) && isType(editor, node, Object.values({ ...TextStyles, ...ListType })),
+            }),
+        ).reduce<AvailableTextStyles[]>((types, current) => {
+            const type = current[0].type as AvailableTextStyles;
+            if (!types.includes(type)) {
+                types.push(type);
+            }
+
+            return types;
+        }, []);
+    }
+
+    return [];
+};
 
 export const DropdownTrigger = ({ editorId, open }: DropdownTriggerProps) => {
     const editor = usePlateEditorState(editorId);
 
-    const getStyleInSelection = (): TextStyles | undefined => {
-        if (editor.selection) {
-            const parentEntry = getParent(editor, editor.selection);
-            if (!parentEntry) {
-                return;
-            }
-            const [node] = parentEntry;
-            return node.type;
-        }
-    };
+    const selectedTextStyles = getSelectedTextStyles(editor);
 
-    const activeStyleInSelection = getStyleInSelection();
-    const label = activeStyleInSelection ? textStyleTitles[activeStyleInSelection] : DEFAULT_TEXTSTYLE_VALUE;
+    const label =
+        selectedTextStyles.length === 1 ? availableTextStyleTitles[selectedTextStyles[0]] : DEFAULT_TEXTSTYLE_VALUE;
 
     return (
         <button
@@ -38,12 +73,12 @@ export const DropdownTrigger = ({ editorId, open }: DropdownTriggerProps) => {
         >
             <div
                 className={merge([
-                    'tw-relative tw-inline-flex tw-flex-row tw-items-center tw-justify-between tw-overflow-hidden tw-text-text tw-rounded tw-gap-1 tw-p-2 hover:tw-text-text hover:tw-bg-box-neutral tw-h-8',
-                    open && 'tw-bg-box-neutral',
+                    "tw-relative tw-inline-flex tw-flex-row tw-items-center tw-justify-between tw-overflow-hidden tw-text-text tw-rounded tw-gap-1 tw-p-2 hover:tw-text-text hover:tw-bg-box-neutral tw-h-8",
+                    open && "tw-bg-box-neutral",
                 ])}
             >
-                <span className="tw-text-s">{label || DEFAULT_TEXTSTYLE_VALUE}</span>
-                <div className={merge(['tw-transition-transform', open && 'tw-rotate-180'])}>
+                <span className="tw-text-s">{label}</span>
+                <div className={merge(["tw-transition-transform", open && "tw-rotate-180"])}>
                     <IconCaretDown />
                 </div>
             </div>
