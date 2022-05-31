728abf7815a8c2fbd8e02773403b3cff090431b7
imoutaharik <ilham.moutaharik@frontify.com>
2022-05-31 12:35:25 +0200

    fix: remove left and right shadow

--- src/components/ScrollWrapper/ScrollWrapper.tsx
+++ src/components/ScrollWrapper/ScrollWrapper.tsx
@@ -7,8 +7,6 @@ import { useScrollWrapper } from './hooks/useScrollWrapper';
 import { ScrollWrapperDirection, ScrollWrapperProps, scrollWrapperDirections } from './types';
 
 const GRADIENTS = {
-    right: 'linear-gradient(90deg, rgba(232, 233, 233, 0) 0%, #E8E9E9 100%)',
-    left: 'linear-gradient(270deg, rgba(232, 233, 233, 0) 0%, #E8E9E9 100%)',
     bottom: 'linear-gradient(180deg, rgba(232, 233, 233, 0) 0%, #E8E9E9 100%)',
     top: 'linear-gradient(0deg, rgba(232, 233, 233, 0) 0%, #E8E9E9 100%)',
 };
@@ -16,15 +14,12 @@ const GRADIENTS = {
 export const ScrollWrapper: FC<ScrollWrapperProps> = ({ direction = ScrollWrapperDirection.Vertical, children }) => {
     const scrollingContainer = useRef<HTMLDivElement>(null);
 
-    const [{ showTopShadow, showBottomShadow, showLeftShadow, showRightShadow }, scrollDivProps] =
-        useScrollWrapper(scrollingContainer);
+    const [{ showTopShadow, showBottomShadow }, scrollDivProps] = useScrollWrapper(scrollingContainer);
 
     const { isFocusVisible, focusProps } = useFocusRing();
 
     const directionVertical =
         direction === ScrollWrapperDirection.Vertical || direction === ScrollWrapperDirection.Both;
-    const directionHorizontal =
-        direction === ScrollWrapperDirection.Horizontal || direction === ScrollWrapperDirection.Both;
 
     return (
         <div
@@ -37,12 +32,6 @@ export const ScrollWrapper: FC<ScrollWrapperProps> = ({ direction = ScrollWrappe
                     style={{ background: GRADIENTS.top }}
                 />
             )}
-            {directionHorizontal && showLeftShadow && (
-                <div
-                    className="tw-w-3 tw-top-0 tw-absolute tw-left-0 tw-bottom-0 tw-h-full tw-z-10 tw-mix-blend-darken tw-border-l tw-border-line"
-                    style={{ background: GRADIENTS.left }}
-                />
-            )}
             <div
                 ref={scrollingContainer}
                 // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
@@ -51,8 +40,7 @@ export const ScrollWrapper: FC<ScrollWrapperProps> = ({ direction = ScrollWrappe
                 aria-label="Scrollable dialogue content"
                 className={merge([
                     scrollWrapperDirections[direction],
-                    'tw-flex-auto tw-min-h-0 tw-outline-none tw-pb-2',
-                    showLeftShadow || isFocusVisible ? 'tw-pr-2' : 'tw-px-2 tw--ml-2',
+                    'tw-flex-auto tw-min-h-0 tw-outline-none tw-pb-2 tw-px-2 tw--ml-2',
                     isFocusVisible && FOCUS_STYLE,
                 ])}
                 {...scrollDivProps}
@@ -64,12 +52,6 @@ export const ScrollWrapper: FC<ScrollWrapperProps> = ({ direction = ScrollWrappe
                 <div
                     className="tw-h-3 tw-w-full tw-absolute tw-z-10 tw-bottom-0 tw-left-0 tw-mix-blend-darken tw-border-b tw-border-line"
                     style={{ background: GRADIENTS.bottom }}
-                />
-            )}
-            {directionHorizontal && showRightShadow && (
-                <div
-                    className="tw-w-3 tw-top-0 tw-absolute tw-right-0 tw-bottom-0 tw-h-full tw-z-10 tw-mix-blend-darken tw-border-r tw-border-line"
-                    style={{ background: GRADIENTS.right }}
                 />
             )}
         </div>
