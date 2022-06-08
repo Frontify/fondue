10b495a3fc929b2c09462c750f6edb1cd232d938
imoutaharik <ilham.moutaharik@frontify.com>
2022-06-08 15:29:05 +0200

    fix: Input label story to use new icons

--- src/components/InputLabel/InputLabel.stories.tsx
+++ src/components/InputLabel/InputLabel.stories.tsx
@@ -2,9 +2,10 @@
 
 import { Meta, Story } from '@storybook/react';
 import React from 'react';
-import { IconAttentionFilled, IconInfo, IconQuestion } from '@foundation/Icon/Generated';
+import { IconExclamationMarkCircle, IconInfo, IconQuestionMarkCircle } from '@foundation/Icon/Generated';
 import { InputLabel as InputLabelComponent, InputLabelProps } from './InputLabel';
 import { TooltipIconTriggerStyle } from '@components/TooltipIcon';
+import { IconSize } from '@foundation/Icon';
 
 export default {
     title: 'Components/Input Label',
@@ -26,8 +27,20 @@ export const InputLabelWithMultipleTooltips = InputLabel.bind({});
 InputLabelWithMultipleTooltips.args = {
     ...InputLabel.args,
     tooltip: [
-        { triggerIcon: <IconInfo />, triggerStyle: TooltipIconTriggerStyle.Danger, content: 'Tooltip 1' },
-        { triggerIcon: <IconQuestion />, triggerStyle: TooltipIconTriggerStyle.Primary, content: 'Tooltip 2' },
-        { triggerIcon: <IconAttentionFilled />, triggerStyle: TooltipIconTriggerStyle.Warning, content: 'Tooltip 3' },
+        {
+            triggerIcon: <IconInfo size={IconSize.Size16} />,
+            triggerStyle: TooltipIconTriggerStyle.Danger,
+            content: 'Tooltip 1',
+        },
+        {
+            triggerIcon: <IconQuestionMarkCircle size={IconSize.Size16} />,
+            triggerStyle: TooltipIconTriggerStyle.Primary,
+            content: 'Tooltip 2',
+        },
+        {
+            triggerIcon: <IconExclamationMarkCircle size={IconSize.Size16} filled />,
+            triggerStyle: TooltipIconTriggerStyle.Warning,
+            content: 'Tooltip 3',
+        },
     ],
 };
