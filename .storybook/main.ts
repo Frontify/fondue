22bb64571421e8f59c18db7e38c2188e0789d82b
julianiff <julian.iff@frontify.com>
2022-06-30 09:29:06 +0200

    chore: add feature storyStoreV7 to enable code-splitting in storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,6 +12,9 @@ module.exports = {
     stories: ['../src/**/*.stories.tsx'],
     addons: ['storybook-dark-mode', '@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
     staticDirs: ['assets'],
+    features: {
+        storyStoreV7: true,
+    },
     async viteFinal(config: any) {
         config.resolve.alias = {
             ...config.resolve.alias,
