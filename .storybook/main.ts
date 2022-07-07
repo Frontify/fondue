a006d03c0c0147fb94ff0ce330e6cf99df74bb28
Julian Iff <julian.iff@frontify.com>
2022-07-07 16:22:14 +0200

    vite(storyStore): Build Stories on Demand, improve Storybook build time (#862)
    * chore: add feature storyStoreV7 to enable code-splitting in storybook
    
    * chore: add feature flag buildStoriesJson to increase individual story interaction speed

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,6 +12,10 @@ module.exports = {
     stories: ['../src/**/*.stories.tsx'],
     addons: ['storybook-dark-mode', '@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
     staticDirs: ['assets'],
+    features: {
+        storyStoreV7: true,
+        buildStoriesJson: true,
+    },
     async viteFinal(config: any) {
         config.resolve.alias = {
             ...config.resolve.alias,
