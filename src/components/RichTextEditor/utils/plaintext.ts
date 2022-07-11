c42482106e2da7ad45c638b977673b94cb318088
Marco Bohler <triggertoo@gmail.com>
2022-07-11 15:52:52 +0200

    add checks

--- src/components/RichTextEditor/utils/plaintext.ts
+++ src/components/RichTextEditor/utils/plaintext.ts
@@ -1,5 +1,13 @@
-import { TDescendant } from '@udecode/plate';
+import { isDescendant, TDescendant } from '@udecode/plate';
 import { Node } from 'slate';
 
 export const toPlaintext = (nodes: TDescendant[]): string =>
-    nodes.map((node) => (node.children ? toPlaintext(node.children) : Node.string(node))).join('\n');
+    nodes
+        .map((node) => {
+            if (!isDescendant(node)) {
+                return '';
+            }
+
+            return Array.isArray(node.children) ? toPlaintext(node.children) : Node.string(node);
+        })
+        .join('\n');
