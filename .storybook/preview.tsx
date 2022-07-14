7e3648ccfc3328bcb33226841e3ac3b653a42603
julianiff <julian.iff@frontify.com>
2022-07-14 09:01:10 +0200

    feat(theme-switcher): Remove legacy-tokens from tailwind.config and add dark theme

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,15 +1,15 @@
-import "../src/styles.css";
-import { dark, light } from "./frontifyTheme";
+import '../src/styles.css';
+import { dark, light } from './frontifyTheme';
 
 export const parameters = {
     options: {
         storySort: {
-            order: ["Tokens", "Layout", "Typography", "Components"],
+            order: ['Tokens', 'Layout', 'Typography', 'Components'],
         },
     },
     darkMode: {
-        darkClass: "tw-dark",
-        classTarget: "html",
+        darkClass: 'tw-dark',
+        classTarget: 'html',
         stylePreview: true,
         dark: { ...dark },
         light: { ...light },
