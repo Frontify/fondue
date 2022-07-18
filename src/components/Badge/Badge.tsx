46e3e6bd1b50fe5bab662b7c98206517dbcafbd5
Sam Creasey <sam.creasey@frontify.com>
2022-07-18 14:14:50 +0200

    removed exterior component

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -27,60 +27,56 @@ export const Badge: FC<BadgeProps> = ({
     const Container = onClick ? 'a' : 'span';
 
     return (
-        // This `tw-inline-block` and the `tw-float-left` on the first child was added, to remove
-        // a 1px space before this component when using without the `icon`-prop
-        <span className="tw-inline-flex tw-flex-initial tw-min-w-0">
-            <Container
-                onClick={() => onClick && onClick()}
-                className={merge([
-                    'tw-float-left tw-inline-flex tw-items-center tw-justify-center tw-rounded-full tw-transition-color tw-select-none tw-flex-initial tw-min-w-0',
-                    disabled
-                        ? 'tw-bg-box-disabled tw-text-box-disabled-inverse'
-                        : getStyleClasses(style, !!onClick, emphasis === BadgeEmphasis.Strong),
-                    onClick && !disabled ? 'hover:tw-cursor-pointer' : 'tw-cursor-default',
-                    getSizeClasses(children, Boolean(status || icon), size === 's'),
-                ])}
-                data-test-id="badge"
-            >
-                {status && (
-                    <span
-                        data-test-id="badge-status"
-                        className={merge([
-                            'tw-w-2 tw-h-2 tw-rounded-full tw-flex-none',
-                            disabled && 'tw-opacity-30',
-                            isBadgeStatus(status) && badgeStatusClasses[status],
-                        ])}
-                        style={
-                            isBadgeStatus(status)
-                                ? {}
-                                : {
-                                      backgroundColor:
-                                          typeof status === 'string'
-                                              ? status
-                                              : getColorDisplayValue(status, ColorFormat.Rgba, true),
-                                  }
-                        }
-                    />
-                )}
-                {icon && (
-                    <span data-test-id="badge-icon" className={merge([disabled && 'tw-opacity-30', 'tw-flex-none'])}>
-                        {cloneElement(icon, { size: IconSize.Size16 })}
-                    </span>
-                )}
-                <span className="tw-text-center tw-text-xxs tw-font-sans tw-font-normal tw-truncate">{children}</span>
-                {onDismiss && (
-                    <button
-                        type="button"
-                        data-test-id="badge-dismiss"
-                        onClick={(event) => {
-                            event.stopPropagation();
-                            onDismiss();
-                        }}
-                    >
-                        <RejectIcon size={IconSize.Size12} />
-                    </button>
-                )}
-            </Container>
-        </span>
+        <Container
+            onClick={() => onClick && onClick()}
+            className={merge([
+                'tw-inline-flex tw-items-center tw-justify-center tw-self-start tw-rounded-full tw-transition-color tw-select-none tw-flex-initial tw-min-w-0 tw-align-top',
+                disabled
+                    ? 'tw-bg-box-disabled tw-text-box-disabled-inverse'
+                    : getStyleClasses(style, !!onClick, emphasis === BadgeEmphasis.Strong),
+                onClick && !disabled ? 'hover:tw-cursor-pointer' : 'tw-cursor-default',
+                getSizeClasses(children, Boolean(status || icon), size === 's'),
+            ])}
+            data-test-id="badge"
+        >
+            {status && (
+                <span
+                    data-test-id="badge-status"
+                    className={merge([
+                        'tw-w-2 tw-h-2 tw-rounded-full tw-flex-none',
+                        disabled && 'tw-opacity-30',
+                        isBadgeStatus(status) && badgeStatusClasses[status],
+                    ])}
+                    style={
+                        isBadgeStatus(status)
+                            ? {}
+                            : {
+                                  backgroundColor:
+                                      typeof status === 'string'
+                                          ? status
+                                          : getColorDisplayValue(status, ColorFormat.Rgba, true),
+                              }
+                    }
+                />
+            )}
+            {icon && (
+                <span data-test-id="badge-icon" className={merge([disabled && 'tw-opacity-30', 'tw-flex-none'])}>
+                    {cloneElement(icon, { size: IconSize.Size16 })}
+                </span>
+            )}
+            <span className="tw-text-center tw-text-xxs tw-font-sans tw-font-normal tw-truncate">{children}</span>
+            {onDismiss && (
+                <button
+                    type="button"
+                    data-test-id="badge-dismiss"
+                    onClick={(event) => {
+                        event.stopPropagation();
+                        onDismiss();
+                    }}
+                >
+                    <RejectIcon size={IconSize.Size12} />
+                </button>
+            )}
+        </Container>
     );
 };
