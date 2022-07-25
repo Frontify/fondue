049e8e15f69bc0598fc2d93a0e54a2845cd88cf2
Céline <42832501+celine-s@users.noreply.github.com>
2022-07-25 12:00:13 +0200

    chore: update cypress v10 (#881)

---
+++ cypress/support/component.ts
@@ -0,0 +1,20 @@
+import './commands';
+
+import { MountOptions, MountReturn } from 'cypress/react';
+
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace Cypress {
+        interface Chainable {
+            /**
+             * Mounts a React node
+             * @param component React Node to mount
+             * @param options Additional options to pass into mount
+             */
+            mount(component: React.ReactNode, options?: MountOptions): Cypress.Chainable<MountReturn>;
+        }
+    }
+}
+
+import 'cypress-real-events/support';
+import '../../src/styles.css';
---
+++ src/components/MultiSelect/MultiSelect.cy.tsx
@@ -0,0 +1,78 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import React, { FC, useState } from 'react';
+import { MultiSelect } from './MultiSelect';
+
+const TRIGGER_ID = '[data-test-id=trigger]';
+const CHECKBOX_ID = '[data-test-id=checkbox]';
+const CHECKBOX_INPUT_ID = '[data-test-id=checkbox-input]';
+const TAG_ID = '[data-test-id=tag]';
+const CHECKLIST_ID = '[data-test-id=checklist]';
+
+const ITEMS = {
+    activeItemKeys: ['Short tag', 'Tag 74'],
+    items: [
+        {
+            value: 'Checkbox label 1',
+        },
+        {
+            value: 'Short tag',
+        },
+        {
+            value: 'Checkbox label 2',
+        },
+        {
+            value: 'Checkbox label 3',
+        },
+        {
+            value: 'Tag 74',
+        },
+        {
+            value: 'This is a long tag',
+        },
+    ],
+};
+
+const Component: FC = () => {
+    const [activeItems, setActiveItems] = useState<(string | number)[]>(ITEMS.activeItemKeys);
+    return (
+        <MultiSelect
+            items={ITEMS.items}
+            activeItemKeys={activeItems}
+            onSelectionChange={(keys) => setActiveItems(keys)}
+        />
+    );
+};
+
+describe('MultiSelect Component', () => {
+    it('renders with initial active items', () => {
+        cy.mount(<Component />);
+
+        cy.get(TAG_ID).should('have.length', 2);
+        cy.get(TAG_ID).should('contain', 'Short tag').and('contain', 'Tag 74');
+    });
+    it('opens dropdown select list on click', () => {
+        cy.mount(<Component />);
+
+        cy.get(TRIGGER_ID).click();
+        cy.get(CHECKLIST_ID).should('be.visible');
+    });
+    it('changes selection on click', () => {
+        cy.mount(<Component />);
+        cy.get(TRIGGER_ID).click();
+
+        cy.get(CHECKBOX_ID).first().click();
+        cy.get(CHECKBOX_INPUT_ID).first().should('have.attr', 'aria-checked', 'true');
+        cy.get(TAG_ID).contains('Checkbox label 1');
+
+        cy.get(CHECKBOX_ID).eq(1).click();
+        cy.get(CHECKBOX_INPUT_ID).eq(1).should('have.attr', 'aria-checked', 'false');
+        cy.get(TRIGGER_ID).contains('Short tag').should('not.exist');
+        cy.get(TAG_ID).first().click();
+        cy.get(CHECKBOX_INPUT_ID).eq(1).should('have.attr', 'aria-checked', 'false');
+
+        cy.get(CHECKBOX_ID).eq(2).click();
+        cy.get(CHECKBOX_INPUT_ID).eq(2).should('have.attr', 'aria-checked', 'true');
+        cy.get(TAG_ID).contains('Checkbox label 2');
+    });
+});
