dda886b29922c417c4d18267d04a50ef305bc233
Jonas B??chel <jonas@frontify.com>
2022-07-29 08:34:38 +0200

    Remove not inverted, because it's not supported anymore

--- src/components/ButtonGroup/ButtonGroup.stories.tsx
+++ src/components/ButtonGroup/ButtonGroup.stories.tsx
@@ -1,7 +1,7 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { Button, ButtonSize, ButtonStyle, ButtonEmphasis } from '@components/Button/Button';
-import { IconAddSimple, IconReject, IconCheck } from '@foundation/Icon/Generated';
+import { Button, ButtonEmphasis, ButtonSize, ButtonStyle } from '@components/Button/Button';
+import { IconAddSimple, IconCheck, IconReject } from '@foundation/Icon/Generated';
 import { action } from '@storybook/addon-actions';
 import { Meta } from '@storybook/react';
 import React from 'react';
@@ -28,37 +28,18 @@ export default {
         size: ButtonSize.Medium,
         style: ButtonStyle.Default,
         emphasis: ButtonEmphasis.Default,
-        inverted: false,
     },
 } as Meta<ButtonGroupProps>;
 
 export const ButtonGroup = ({ ...args }) => (
     <ButtonGroupComponent size={args.size}>
-        <Button
-            style={args.style}
-            emphasis={args.emphasis}
-            inverted={args.inverted}
-            icon={<IconAddSimple />}
-            onClick={action('onClick')}
-        >
+        <Button style={args.style} emphasis={args.emphasis} icon={<IconAddSimple />} onClick={action('onClick')}>
             Button Text
         </Button>
-        <Button
-            style={args.style}
-            emphasis={args.emphasis}
-            inverted={args.inverted}
-            icon={<IconCheck />}
-            onClick={action('onClick')}
-        >
+        <Button style={args.style} emphasis={args.emphasis} icon={<IconCheck />} onClick={action('onClick')}>
             The Second
         </Button>
-        <Button
-            style={args.style}
-            emphasis={args.emphasis}
-            inverted={args.inverted}
-            icon={<IconReject />}
-            onClick={action('onClick')}
-        >
+        <Button style={args.style} emphasis={args.emphasis} icon={<IconReject />} onClick={action('onClick')}>
             The Third
         </Button>
     </ButtonGroupComponent>
