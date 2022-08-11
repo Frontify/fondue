fbff8b4975817af693814331e81fac0ad2c3ba4b
Sam Creasey <sam.creasey@frontify.com>
2022-08-11 16:22:52 +0200

    reduce padding on x axis of small multiselect

--- src/components/MultiSelect/helpers.ts
+++ src/components/MultiSelect/helpers.ts
@@ -1,12 +1,10 @@
+import { merge } from '@utilities/merge';
 import { MultiSelectSize } from './MultiSelect';
 
-export const getSizeClasses = (size: MultiSelectSize, hasActiveKeys: boolean) => {
-    if (size === MultiSelectSize.Small) {
-        if (hasActiveKeys) {
-            return 'tw-py-1';
-        } else {
-            return 'tw-py-2';
-        }
-    }
-    return 'tw-py-[11px]';
-};
+export const getPaddingClasses = (size: MultiSelectSize, hasActiveKeys: boolean) =>
+    merge([
+        'tw-pr-9',
+        size === MultiSelectSize.Small
+            ? `tw-pl-3 ${hasActiveKeys ? 'tw-py-1' : 'tw-py-2'}`
+            : 'tw-pl-[19px] tw-py-[11px]',
+    ]);
