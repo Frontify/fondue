29aad0ab3b44b8445f477f06f78c1b1cdbbded12
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-08-19 11:11:16 +0200

    fix design

--- src/components/RichTextEditor/Abstract/ToolbarPositioningBottom.tsx
+++ src/components/RichTextEditor/Abstract/ToolbarPositioningBottom.tsx
@@ -4,7 +4,11 @@ import React, { forwardRef } from 'react';
 import { ToolbarWrapperProps, WrapperProps } from './types';
 
 export const PlateWrapper = forwardRef<HTMLDivElement, WrapperProps>(({ children }, ref) => (
-    <div data-test-id="rich-text-editor" className="tw-relative tw-w-full tw-flex tw-flex-col-reverse" ref={ref}>
+    <div
+        data-test-id="rich-text-editor"
+        className="tw-relative tw-rounded tw-border tw-border-line tw-w-full tw-flex tw-flex-col-reverse tw-px-1"
+        ref={ref}
+    >
         {children}
     </div>
 ));
@@ -15,7 +19,7 @@ export const ToolbarWrapper = ({ children }: ToolbarWrapperProps) => {
     return (
         <div
             data-test-id="toolbar-bottom"
-            className="tw-relative tw-rounded tw-min-h-12 tw-border tw-border-line tw-shadow-lg tw-bg-base tw-divide-y tw-divide-line tw-flex tw-flex-wrap"
+            className="tw-relative tw-rounded tw-min-h-12 tw-border-t tw-border-line tw-bg-base tw-divide-y tw-divide-line tw-flex tw-flex-wrap"
         >
             {children}
         </div>
--- src/components/RichTextEditor/Abstract/ToolbarPositioningTop.tsx
+++ src/components/RichTextEditor/Abstract/ToolbarPositioningTop.tsx
@@ -4,7 +4,11 @@ import React, { forwardRef } from 'react';
 import { ToolbarWrapperProps, WrapperProps } from './types';
 
 export const PlateWrapper = forwardRef<HTMLDivElement, WrapperProps>(({ children }, ref) => (
-    <div data-test-id="rich-text-editor" className="tw-relative tw-w-full tw-flex tw-flex-col" ref={ref}>
+    <div
+        data-test-id="rich-text-editor"
+        className="tw-relative tw-rounded tw-border tw-border-line tw-w-full tw-flex tw-flex-col tw-px-1"
+        ref={ref}
+    >
         {children}
     </div>
 ));
@@ -15,7 +19,7 @@ export const ToolbarWrapper = ({ children }: ToolbarWrapperProps) => {
     return (
         <div
             data-test-id="toolbar-top"
-            className="tw-relative tw-rounded tw-min-h-12 tw-border tw-border-line tw-shadow-lg tw-bg-base tw-divide-y tw-divide-line tw-flex tw-flex-wrap"
+            className="tw-relative tw-min-h-12 tw-border-b tw-border-line tw-bg-base tw-divide-y tw-divide-line tw-flex tw-flex-wrap"
         >
             {children}
         </div>
