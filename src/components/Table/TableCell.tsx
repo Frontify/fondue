dbe42bc650c9431e64d889bc89e99241480b1306
joseazevedo88 <josemendesazevedo@gmail.com>
2022-08-29 09:27:09 +0100

    Use alias token instead of palette color code

--- src/components/Table/TableCell.tsx
+++ src/components/Table/TableCell.tsx
@@ -52,7 +52,7 @@ export const TableCell = ({
                 ref={ref}
                 className={merge([
                     'tw-pl-8 tw-py-4 tw-pr-4 tw-relative after:tw-absolute after:tw-left-0 after:tw-top-[-1px] after:tw-bottom-[-1px] after:tw-w-1',
-                    isChecked ? 'after:tw-bg-violet-60' : 'after:tw-bg-transparent',
+                    isChecked ? 'after:tw-bg-box-selected-strong' : 'after:tw-bg-transparent',
                 ])}
                 data-test-id="table-select-cell"
             >
