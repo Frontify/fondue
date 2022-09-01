fab8513aeb5ce838ff5d837123caec0e39a1bc97
Fabricio Mendonca <fabricio.mendonca@frontify.com>
2022-09-01 15:06:56 +0200

    feat(OverflowMenu): use popper js for repositioning

--- src/components/OverflowMenu/OverflowMenu.tsx
+++ src/components/OverflowMenu/OverflowMenu.tsx
@@ -1,6 +1,7 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import React, { useState } from 'react';
+import { usePopper } from 'react-popper';
 import { IconDotsHorizontal } from '@foundation/Icon';
 import { Button, ButtonRounding, ButtonSize, ButtonStyle, ButtonType } from '..';
 import { OverflowMenuItem, OverflowMenuItemProps } from './OverflowMenuItem';
@@ -11,10 +12,30 @@ export interface OverflowMenuProps {
 
 export const OverflowMenu = ({ items }: OverflowMenuProps) => {
     const [isMenuOpened, setIsMenuOpened] = useState(false);
+    const [menuContainerRef, setMenuContainerRef] = useState<HTMLElement | null>(null);
+    const [menuOpenerRef, setMenuOpenerRef] = useState<HTMLButtonElement | null>(null);
+
+    const popperInstance = usePopper(menuOpenerRef, menuContainerRef, {
+        placement: 'bottom-start',
+        strategy: 'fixed',
+        modifiers: [
+            {
+                name: 'flip',
+                enabled: true,
+            },
+            {
+                name: 'offset',
+                options: {
+                    offset: [0, 8],
+                },
+            },
+        ],
+    });
 
     return (
         <div data-test-id="overflow-menu" className="tw-relative tw-bottom-0 tw-top-0 tw-flex">
             <Button
+                ref={setMenuOpenerRef}
                 icon={<IconDotsHorizontal />}
                 solid={false}
                 rounding={ButtonRounding.Medium}
@@ -29,6 +50,9 @@ export const OverflowMenu = ({ items }: OverflowMenuProps) => {
                 <nav
                     className="tw-bg-base tw-border tw-border-line-strong tw-rounded-lg tw-absolute tw-left-0 tw-top-7 tw-py-1.5 tw-shadow tw-w-max"
                     role="dialog"
+                    ref={setMenuContainerRef}
+                    style={popperInstance.styles.popper}
+                    {...popperInstance.attributes.popper}
                 >
                     <ol className="tw-list-none tw-flex-wrap tw-gap-y-1">
                         {items.map((item, index) => (
