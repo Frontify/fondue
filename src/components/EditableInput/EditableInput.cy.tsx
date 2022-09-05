1f0e5866a937f0e175513084943b2d011d8645ba
julianiff <julian.iff@frontify.com>
2022-09-05 14:37:45 +0200

    chore: cleanup test and move component mount to beforeEach

--- src/components/EditableInput/EditableInput.cy.tsx
+++ src/components/EditableInput/EditableInput.cy.tsx
@@ -6,33 +6,8 @@ const EDITABLE_LINK_NAME = '[data-test-id=node-link-name]';
 const EDITABLE_NODE = '[data-test-id=node-editable]';
 
 describe('EditableInput', () => {
-    it('renders without error', () => {
-        cy.mount(
-            <EditableInput
-                name={'This Input'}
-                targetItemId={'id'}
-                onEditableSave={() => console.log('edit')}
-            ></EditableInput>,
-        );
-
-        cy.get(EDITABLE_CONTAINER).should('exist');
-    });
-    it('on double click changes to input', () => {
-        cy.mount(
-            <EditableInput
-                name={'This Input'}
-                targetItemId={'id'}
-                onEditableSave={() => console.log('edit')}
-            ></EditableInput>,
-        );
-        cy.get(EDITABLE_LINK_NAME).should('exist');
-        cy.get(EDITABLE_LINK_NAME).dblclick();
-
-        cy.get(EDITABLE_NODE).should('exist');
-    });
-
-    it('Event gets triggered when writing Hey in input form that nothing is sent and when clicking away that it sent an event', () => {
-        const onEditableSaveSpy = cy.spy().as('onChangeSpy');
+    beforeEach('Setup component', () => {
+        const onEditableSaveSpy = cy.spy().as('onEditableSaveSpy');
         cy.mount(
             <EditableInput
                 name={'This Input'}
@@ -40,28 +15,32 @@ describe('EditableInput', () => {
                 onEditableSave={(one: string, two: string) => onEditableSaveSpy(two)}
             ></EditableInput>,
         );
+    });
+    it('renders without error', () => {
+        cy.get(EDITABLE_CONTAINER).should('exist');
+    });
+    it('on double click changes to input', () => {
+        cy.get(EDITABLE_LINK_NAME).should('exist');
+        cy.get(EDITABLE_LINK_NAME).dblclick();
+
+        cy.get(EDITABLE_NODE).should('exist');
+    });
+
+    it('Event gets triggered when writing Hey in input form that nothing is sent and when clicking away that it sent an event', () => {
         cy.get(EDITABLE_LINK_NAME).dblclick();
 
         cy.get(EDITABLE_NODE).type('Hey');
 
-        cy.get('@onChangeSpy').should('have.not.been.called');
+        cy.get('@onEditableSaveSpy').should('have.not.been.called');
         cy.get(EDITABLE_CONTAINER).click();
-        cy.get('@onChangeSpy').should('have.been.calledWith', 'This InputHey');
+        cy.get('@onEditableSaveSpy').should('have.been.calledWith', 'This InputHey');
     });
 
     it('Event Triggers when hitting enter', () => {
-        const onChangeSpy = cy.spy().as('onChangeSpy');
-        cy.mount(
-            <EditableInput
-                name={'This Input'}
-                targetItemId={'id'}
-                onEditableSave={(one: string, two: string) => onChangeSpy(two)}
-            ></EditableInput>,
-        );
         cy.get(EDITABLE_LINK_NAME).dblclick();
 
         cy.get(EDITABLE_NODE).type('Hey');
         cy.get(EDITABLE_NODE).type('{enter}');
-        cy.get('@onChangeSpy').should('have.been.called');
+        cy.get('@onEditableSaveSpy').should('have.been.called');
     });
 });
