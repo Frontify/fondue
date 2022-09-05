b7c146d506ce235b3c983f6fc2297ac006657af2
julianiff <julian.iff@frontify.com>
2022-09-05 15:29:20 +0200

    chore: readd linting rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     },
     plugins: ['jsx-a11y'],
     rules: {
+        'unicorn/no-nested-ternary': 'off' /* disabled to align with prettier */,
         'react-hooks/rules-of-hooks': 'warn' /* degraded to warning to do a partial release */,
     },
 };
