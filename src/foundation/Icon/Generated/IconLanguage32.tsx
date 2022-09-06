b698e975c70f8f4c47d4754274ac643c41491e0b
imoutaharik <ilham.moutaharik@frontify.com>
2022-09-06 16:23:40 +0200

    feat: add new layersSingle, language and country icons

--- src/foundation/Icon/Generated/IconLanguage32.tsx
+++ src/foundation/Icon/Generated/IconLanguage32.tsx
@@ -1,7 +1,7 @@
 import React, { memo } from 'react';
 import { GeneratedIconProps } from '@foundation/Icon/IconProps';
 
-function IconLayersSingle32(props: GeneratedIconProps): React.ReactElement<GeneratedIconProps> {
+function IconLanguage32(props: GeneratedIconProps): React.ReactElement<GeneratedIconProps> {
     const customClassName = 'tw-flex tw-items-center tw-justify-center tw-fill-current';
     return (
         <svg
@@ -9,17 +9,17 @@ function IconLayersSingle32(props: GeneratedIconProps): React.ReactElement<Gener
             width={32}
             height={32}
             className={customClassName}
-            name="IconLayersSingle32"
+            name="IconLanguage32"
             {...props}
         >
             <path
                 fill="currentColor"
                 fillRule="evenodd"
-                d="M25.988 16 16 10.184 6.012 16 16 21.817 25.988 16Zm-8.982-7.545a2 2 0 0 0-2.013 0l-11.472 6.68a1 1 0 0 0 0 1.73l11.472 6.68a2 2 0 0 0 2.013 0l11.472-6.68a1 1 0 0 0 0-1.73l-11.472-6.68Z"
+                d="M3 6h16v3.4a1 1 0 1 1-2 0V8h-5v13h2a1 1 0 1 1 0 2H8a1 1 0 1 1 0-2h2V8H5v1.4a1 1 0 1 1-2 0V6Zm19.071 14.818.702-.468A5 5 0 0 0 25 16.19V14H15v-2h6v-2a1 1 0 1 1 2 0v2h6v2h-2v2.19a7 7 0 0 1-2.875 5.655l3.322 1.661a1 1 0 1 1-.894 1.789l-4.338-2.17-4.66 3.107-1.11-1.664 3.75-2.5A7 7 0 0 1 17 16.192V15.4h2v.792a5 5 0 0 0 2.764 4.472l.307.154Z"
                 clipRule="evenodd"
             />
         </svg>
     );
 }
 
-export default memo(IconLayersSingle32);
+export default memo(IconLanguage32);
