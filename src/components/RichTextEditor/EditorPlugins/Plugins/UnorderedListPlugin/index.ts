4cb04fd213e8219c9b91e686f9ba46d24270b630
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-09-07 09:12:04 +0200

    updated

--- src/components/RichTextEditor/EditorPlugins/Plugins/UnorderedListPlugin/index.ts
+++ src/components/RichTextEditor/EditorPlugins/Plugins/UnorderedListPlugin/index.ts
@@ -4,15 +4,6 @@ import { ELEMENT_UL, createListPlugin } from '@udecode/plate';
 import { UnorderedListElement } from './UnorderedListElement';
 import { UnorderedListButton } from './UnorderedListButton';
 import { Plugin } from '../types';
-import { ToolbarStorage } from '../../ToolbarStorage';
-
-export const initListPlugin = () => {
-    const toolbarStorage = ToolbarStorage.getInstance();
-
-    toolbarStorage.setComponent(ELEMENT_UL, UnorderedListElement);
-    toolbarStorage.setPlugins([createListPlugin()]);
-    toolbarStorage.setButton(UnorderedListButton);
-};
 
 export class UnorderedListPlugin implements Plugin {
     public id = ELEMENT_UL;
