ef8346631989d2f4deae1ff0b2b0a592b71e78f6
Raphi <raphael@smartive.ch>
2022-09-07 10:43:07 +0200

    feat(plate): upgrade plate version

--- src/components/RichTextEditor/EditorPositioningWrapper/ToolbarWrapper/ToolbarWrapperPositioningFloating.tsx
+++ src/components/RichTextEditor/EditorPositioningWrapper/ToolbarWrapper/ToolbarWrapperPositioningFloating.tsx
@@ -2,7 +2,7 @@
 
 import { OFFSET_IN_PX } from '@components/RichTextEditor/utils';
 import { calculateToolbarWidth } from '@components/RichTextEditor/utils/toolbarCalc';
-import { BalloonToolbar } from '@udecode/plate';
+import { autoPlacement, BalloonToolbar } from '@udecode/plate';
 import React, { useEffect, useState } from 'react';
 import { ToolbarWrapperProps } from './types';
 
@@ -21,7 +21,12 @@ export const ToolbarWrapperPositioningFloating = ({
     }, [editorWidth, toolbarButtonGroups]);
 
     return (
-        <BalloonToolbar styles={{ root: { border: 'none', background: '#ffffff', width } }}>
+        <BalloonToolbar
+            floatingOptions={{
+                middleware: [autoPlacement()],
+            }}
+            styles={{ root: { border: 'none', background: '#ffffff', width } }}
+        >
             <div
                 data-test-id="toolbar-floating"
                 className="tw-rounded tw-min-h-12 tw-border tw-border-line tw-shadow-lg tw-bg-base tw-divide-y tw-divide-line tw-flex tw-flex-wrap"
