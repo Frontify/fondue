8b381bda929a07f09c9020772396d3673ab40e6d
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-09-08 17:00:43 +0200

    added checkbox plugin

--- src/components/RichTextEditor/EditorActions/Plugins/index.ts
+++ src/components/RichTextEditor/EditorActions/Plugins/index.ts
@@ -2,6 +2,7 @@
 
 import { PluginComposer } from './PluginComposer';
 import { OrderedListPlugin, UnorderedListPlugin } from './ListPlugin';
+import { CheckboxListPlugin } from './CheckboxListPlugin';
 import { BoldPlugin } from './BoldPlugin';
 import { AlignCenterPlugin, AlignJustifyPlugin, AlignLeftPlugin, AlignRightPlugin } from './AlignPlugin';
 
@@ -9,7 +10,7 @@ export const defaultPlugins = new PluginComposer();
 defaultPlugins
     .setPlugin([new BoldPlugin()])
     .setPlugin([[new AlignLeftPlugin(), new AlignCenterPlugin(), new AlignRightPlugin(), new AlignJustifyPlugin()]])
-    .setPlugin([[new UnorderedListPlugin(), new OrderedListPlugin()]]);
+    .setPlugin([[new UnorderedListPlugin(), new CheckboxListPlugin(), new OrderedListPlugin()]]);
 
 export * from './PluginComposer';
 export * from './ListPlugin';
