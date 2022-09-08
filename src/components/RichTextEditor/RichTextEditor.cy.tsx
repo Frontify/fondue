abb51dee751b70cd06542d09eed36a60ffeb775e
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-09-08 18:15:57 +0200

    fixed test

--- src/components/RichTextEditor/RichTextEditor.cy.tsx
+++ src/components/RichTextEditor/RichTextEditor.cy.tsx
@@ -535,7 +535,7 @@ describe('RichTextEditor Component', () => {
 
         cy.viewport(1200, 1200);
         cy.get(TOOLBAR_FLOATING).children().should('have.length', 1);
-        cy.viewport(550, 750);
+        cy.viewport(500, 750);
         cy.get(TOOLBAR_FLOATING).children().should('have.length', 2);
         cy.viewport(320, 480);
         cy.get(TOOLBAR_FLOATING).children().should('have.length', 3);
