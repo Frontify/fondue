67025cef9a505b73340632fdb0addf4f3fd497d3
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-09-12 19:44:55 +0200

    fixed

--- src/components/RichTextEditor/EditorActions/Plugins/MentionPlugin/index.ts
+++ src/components/RichTextEditor/EditorActions/Plugins/MentionPlugin/index.ts
@@ -3,15 +3,19 @@
 import { createComboboxPlugin } from '@udecode/plate';
 import { MENTION_PLUGIN } from './id';
 import { MentionMarkupElement } from './MentionMarkupElement';
-import { Plugin } from '../Plugin';
+import { Plugin, PluginProps } from '../Plugin';
 import { MentionInline } from './MentionInline';
 import { createMentionPlugin } from './createMentionPlugin';
 
-import { mentionable } from '@components/RichTextEditor/utils/exampleValues';
+import { mentionable } from '../../../utils/exampleValues';
 
 export class MentionPlugin extends Plugin {
-    constructor(id = MENTION_PLUGIN, button = undefined, markupElement = new MentionMarkupElement()) {
-        super(id, button, markupElement);
+    constructor(props?: PluginProps) {
+        super({
+            id: MENTION_PLUGIN,
+            markupElement: new MentionMarkupElement(),
+            ...props,
+        });
     }
 
     inline() {
