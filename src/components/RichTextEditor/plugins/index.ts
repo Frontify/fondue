23b0c3847029c30470384419e0c2be3322ca2492
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-09-14 13:23:38 +0200

    removed unsed file

--- src/components/RichTextEditor/plugins/index.ts
+++
@@ -1,4 +0,0 @@
-/* (c) Copyright Frontify Ltd., all rights reserved. */
-
-export * from './checkboxListPlugin/createCheckboxListPlugin';
-export * from './linkChooserPlugin/createLinkChooserPlugin';
