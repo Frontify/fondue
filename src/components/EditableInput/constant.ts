0a908d0b0186a2e605a339cdad548b057a051a52
julianiff <julian.iff@frontify.com>
2022-09-15 17:26:46 +0200

    fix: add style copy helper

--- src/components/EditableInput/constant.ts
+++
@@ -1,4 +0,0 @@
-export const DEFAULT_CONTAINER_CLASS =
-    'tw-flex tw-items-center tw-flex tw-items-center tw-h-6 tw-gap-2 tw-px-3 tw-border tw-rounded tw-text-s tw-font-sans tw-relative tw-bg-base';
-export const DEFAULT_INPUT_TEXT_CLASS =
-    'tw-w-full tw-grow tw-border-none tw-outline-none tw-bg-base tw-hide-input-arrows tw-text-text tw-placeholder-text';
--- src/components/EditableInput/lib/helper.ts
+++ src/components/EditableInput/lib/helper.ts
@@ -5,7 +5,6 @@ export const EditableInputHelper = {
      * Recursively iterators through Children
      * returns all instances of string amended together
      *
-     * @param children
      */
     getLabel: (children?: ReactNode | string) => {
         let label = '';
@@ -21,5 +20,19 @@ export const EditableInputHelper = {
         iteratorStep(children);
 
         return label;
+    },
+
+    /**
+     * Accesses the window object and copies the computed style
+     * of the element
+     *
+     */
+    copyStyles: (current: Element) => {
+        return {
+            fontSize: window.getComputedStyle(current).getPropertyValue('font-size'),
+            lineHeight: window.getComputedStyle(current).getPropertyValue('line-height'),
+            letterSpacing: window.getComputedStyle(current).getPropertyValue('letter-spacing'),
+            minWidth: window.getComputedStyle(current).getPropertyValue('width'),
+        };
     },
 };
