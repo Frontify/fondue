82ffcc0671cde14b993c13a4803721e4a4a5df43
Zeid Selimovic <zeid.selimovic@frontify.com>
2022-10-13 09:05:12 +0200

    feat (icons) SidebarRight and SidebarLeft icons (#993)

--- src/foundation/Icon/Generated/IconPlusCircle12.tsx
+++ src/foundation/Icon/Generated/IconPlusCircle12.tsx
@@ -12,12 +12,21 @@ function IconPlusCircle12(props: GeneratedIconProps): React.ReactElement<Generat
             name="IconPlusCircle12"
             {...props}
         >
-            <path
-                fill="currentColor"
-                fillRule="evenodd"
-                d="M6 11A5 5 0 1 0 6 1a5 5 0 0 0 0 10Zm0 1A6 6 0 1 0 6 0a6 6 0 0 0 0 12ZM3.25 6.5a.5.5 0 0 1 0-1H5.5V3.25a.5.5 0 0 1 1 0V5.5h2.25a.5.5 0 0 1 0 1H6.5v2.25a.5.5 0 0 1-1 0V6.5H3.25Z"
-                clipRule="evenodd"
-            />
+            <g fill="none">
+                <g clipPath="url(#prefix__a)">
+                    <path
+                        fill="currentColor"
+                        fillRule="evenodd"
+                        d="M6 11A5 5 0 1 0 6 1a5 5 0 0 0 0 10Zm0 1A6 6 0 1 0 6 0a6 6 0 0 0 0 12ZM3.25 6.5a.5.5 0 0 1 0-1H5.5V3.25a.5.5 0 0 1 1 0V5.5h2.25a.5.5 0 0 1 0 1H6.5v2.25a.5.5 0 0 1-1 0V6.5H3.25Z"
+                        clipRule="evenodd"
+                    />
+                </g>
+                <defs>
+                    <clipPath id="prefix__a">
+                        <path fill="#fff" d="M0 0h12v12H0z" />
+                    </clipPath>
+                </defs>
+            </g>
         </svg>
     );
 }
