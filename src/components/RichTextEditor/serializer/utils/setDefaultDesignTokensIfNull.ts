4c68af18cc0392f2c01c6e56cd3307b89df2e299
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-10-14 10:08:29 +0200

    added new method to merge objects

--- src/components/RichTextEditor/serializer/utils/setDefaultDesignTokensIfNull.ts
+++ src/components/RichTextEditor/serializer/utils/setDefaultDesignTokensIfNull.ts
@@ -1,8 +1,7 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import { DesignTokens } from '@components/RichTextEditor/types';
+import { deepMerge } from '@components/RichTextEditor/utils';
 
-export const setDefaultDesignTokensIfNull = (
-    defaultTokens: DesignTokens,
-    customTokens: DesignTokens,
-): DesignTokens => ({ ...defaultTokens, ...customTokens });
+export const setDefaultDesignTokensIfNull = (defaultTokens: DesignTokens, customTokens: DesignTokens): DesignTokens =>
+    deepMerge<DesignTokens>(defaultTokens, customTokens);
