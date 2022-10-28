9d6b6c5324238176185cbc169a1bb58ef9204faa
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-10-28 11:38:08 +0200

    adjusted types

--- src/components/RichTextEditor/serializer/md/MdToSlate.ts
+++ src/components/RichTextEditor/serializer/md/MdToSlate.ts
@@ -5,9 +5,10 @@ import slate from 'remark-slate';
 import parse from 'remark-parse';
 import { options } from './options';
 import { MdTransformer } from './MdTransformer';
+import { MdToSlateReturn } from './types';
 
-export class MdToSlate extends MdTransformer<string> {
-    process(data: string) {
-        return unified().use(parse).use(slate, options(this.editor)).processSync(data).result;
+export class MdToSlate extends MdTransformer<string, MdToSlateReturn> {
+    process(value: string) {
+        return unified().use(parse).use(slate, options(this.editor)).processSync(value).result as MdToSlateReturn;
     }
 }
--- src/components/RichTextEditor/serializer/types.ts
+++ src/components/RichTextEditor/serializer/types.ts
@@ -1,7 +1,5 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-export type Unknown = unknown;
-
-export interface Transformer<T extends Unknown = Unknown> {
-    process(value: T): unknown;
+export interface Transformer<T, R> {
+    process(value: T): R;
 }
