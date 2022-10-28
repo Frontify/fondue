7b4116b7d322a1ba1274a543d8244d1c281a9d28
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-10-28 14:09:06 +0200

    udpated type

--- src/components/RichTextEditor/serializer/md/MdToSlate.ts
+++ src/components/RichTextEditor/serializer/md/MdToSlate.ts
@@ -5,10 +5,10 @@ import slate from 'remark-slate';
 import parse from 'remark-parse';
 import { options } from './options';
 import { MdTransformer } from './MdTransformer';
-import { MdToSlateReturn } from './types';
+import { SlateObject } from './types';
 
-export class MdToSlate extends MdTransformer<string, MdToSlateReturn> {
+export class MdToSlate extends MdTransformer<string, SlateObject> {
     process(value: string) {
-        return unified().use(parse).use(slate, options(this.editor)).processSync(value).result as MdToSlateReturn;
+        return unified().use(parse).use(slate, options(this.editor)).processSync(value).result as SlateObject;
     }
 }
