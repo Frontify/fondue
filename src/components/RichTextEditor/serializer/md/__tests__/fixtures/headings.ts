5cb482c4b1c90da862691fa8cf8fdefebb1ecc3d
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-10-28 15:45:58 +0200

    updated fixtures

--- src/components/RichTextEditor/serializer/md/__tests__/fixtures/headings.ts
+++ src/components/RichTextEditor/serializer/md/__tests__/fixtures/headings.ts
@@ -1,10 +1,15 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 export const headingsMD = `# heading 1
+
 ## heading 2
+
 ### heading 3
+
 #### heading 4
+
 ##### heading 5
+
 ###### heading 6
 `;
 
