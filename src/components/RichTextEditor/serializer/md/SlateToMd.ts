13753e7f1fde1ec38a957281df1996c68d92a228
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-10-28 14:09:49 +0200

    type adjusted

--- src/components/RichTextEditor/serializer/md/SlateToMd.ts
+++ src/components/RichTextEditor/serializer/md/SlateToMd.ts
@@ -4,8 +4,9 @@ import { BlockType, LeafType } from 'remark-slate';
 import { options } from './options';
 import { serialize as remarkSerialize } from 'remark-slate';
 import { MdTransformer } from './MdTransformer';
+import { SlateObject } from './types';
 
-export class SlateToMd extends MdTransformer<(BlockType | LeafType)[]> {
+export class SlateToMd extends MdTransformer<SlateObject, string> {
     process(value: (BlockType | LeafType)[]) {
         return value.map((node) => remarkSerialize(node, options(this.editor))).join('\n');
     }
