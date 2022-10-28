001a61764bd1e6f5006c3c97f06fa136fbc4c76a
Sam Creasey <sam.creasey@frontify.com>
2022-10-28 16:28:21 +0200

    Feat: loading bar (#1018)
    * update loading bar
    
    * add spec file
    
    * fix spacing
    
    * move react to TOF

---
+++ src/components/LoadingBar/LoadingBar.stories.tsx
@@ -0,0 +1,28 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { LoadingBar as LoadingBarComponent, LoadingBarProps, LoadingBarSize, LoadingBarState } from './LoadingBar';
+
+export default {
+    title: 'Components/LoadingBar',
+    component: LoadingBarComponent,
+    argTypes: {
+        state: {
+            options: Object.values(LoadingBarState),
+            control: { type: 'select' },
+        },
+        size: {
+            options: Object.values(LoadingBarSize),
+            control: { type: 'select' },
+        },
+    },
+    args: {
+        state: LoadingBarState.Progress,
+        size: LoadingBarSize.Medium,
+        rounded: false,
+        percentage: 40,
+    },
+} as Meta<LoadingBarProps>;
+
+export const LoadingBar: Story<LoadingBarProps> = (args: LoadingBarProps) => <LoadingBarComponent {...args} />;
