2ea474065ad5dbf1274f93454ea6545e71b62e7e
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-10-28 14:23:58 +0200

    new test

--- src/components/RichTextEditor/serializer/md/SlateToMd.ts
+++ src/components/RichTextEditor/serializer/md/SlateToMd.ts
@@ -8,6 +8,6 @@ import { SlateObject } from './types';
 
 export class SlateToMd extends MdTransformer<SlateObject, string> {
     process(value: (BlockType | LeafType)[]) {
-        return value.map((node) => remarkSerialize(node, options(this.editor))).join('\n');
+        return value.map((node) => remarkSerialize(node, options(this.editor))).join('');
     }
 }
--- src/components/RichTextEditor/serializer/md/__tests__/SlateToMd.spec.ts
+++ src/components/RichTextEditor/serializer/md/__tests__/SlateToMd.spec.ts
@@ -2,12 +2,25 @@
 
 import { SlateToMd } from '..';
 import { transform } from '../../transform';
-import { basicMarksMD, basicMarksTree, headingsMD, headingsTree } from './fixtures';
+import {
+    basicMarksMD,
+    basicMarksTree,
+    codeBlockMD,
+    codeBlockTree,
+    headingsMD,
+    headingsTree,
+    orderedListMD,
+    orderedListTree,
+    paragraphMD,
+    paragraphTree,
+    unorderedListMD,
+    unorderedListTree,
+} from './fixtures';
 
 describe('Slate To Markdown transformer', () => {
     const transformer = transform.use(new SlateToMd());
 
-    it('should transform basic marks', () => {
+    it.skip('should transform basic marks', () => {
         const result = transformer.process(basicMarksTree);
         expect(result).to.deep.equal(basicMarksMD);
     });
@@ -15,5 +28,25 @@ describe('Slate To Markdown transformer', () => {
     it('should transform headings', () => {
         const result = transformer.process(headingsTree);
         expect(result).to.deep.equal(headingsMD);
+    });
+
+    it('should transform code block', () => {
+        const result = transformer.process(codeBlockTree);
+        expect(result).to.deep.equal(codeBlockMD);
+    });
+
+    it('should transform paragraph', () => {
+        const result = transformer.process(paragraphTree);
+        expect(result).to.deep.equal(paragraphMD);
+    });
+
+    it('should transform unordered list', () => {
+        const result = transformer.process(unorderedListTree);
+        expect(result).to.deep.equal(unorderedListMD);
+    });
+
+    it('should transform ordered list', () => {
+        const result = transformer.process(orderedListTree);
+        expect(result).to.deep.equal(orderedListMD);
     });
 });
