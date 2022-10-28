1ca3d814e706affc58c215c0eeba6e321d10fd89
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-10-28 14:14:17 +0200

    test

--- src/components/RichTextEditor/serializer/md/__tests__/SlateToMd.spec.ts
+++ src/components/RichTextEditor/serializer/md/__tests__/SlateToMd.spec.ts
@@ -2,13 +2,18 @@
 
 import { SlateToMd } from '..';
 import { transform } from '../../transform';
-import { basicMarksMD, basicMarksTree } from './fixtures';
+import { basicMarksMD, basicMarksTree, headingsMD, headingsTree } from './fixtures';
 
 describe('Slate To Markdown transformer', () => {
     const transformer = transform.use(new SlateToMd());
 
-    it('should serialize basic marks', () => {
+    it('should transform basic marks', () => {
         const result = transformer.process(basicMarksTree);
         expect(result).to.deep.equal(basicMarksMD);
+    });
+
+    it('should transform headings', () => {
+        const result = transformer.process(headingsTree);
+        expect(result).to.deep.equal(headingsMD);
     });
 });
