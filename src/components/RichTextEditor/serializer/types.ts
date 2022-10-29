cfc3561d10fd7a3b9311c1b706b58ed26dfcc6c2
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-10-29 12:01:33 +0200

    updated type

--- src/components/RichTextEditor/serializer/types.ts
+++ src/components/RichTextEditor/serializer/types.ts
@@ -1,5 +1,5 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-export interface Transformer<T, R> {
+export interface Transformer<T = unknown, R = unknown> {
     process(value: T): R;
 }
