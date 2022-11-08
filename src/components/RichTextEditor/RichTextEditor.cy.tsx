077b808b2b3820c21430d366c97f1780e23a65e6
Mike Schmid <mikeschmid89@gmail.com>
2022-11-08 15:47:48 +0100

    fix: existing cypress test

--- src/components/RichTextEditor/RichTextEditor.cy.tsx
+++ src/components/RichTextEditor/RichTextEditor.cy.tsx
@@ -229,7 +229,7 @@ describe('RichTextEditor Component', () => {
 
         insertTextAndOpenToolbar();
         cy.get(TOOLBAR_FLOATING).should('be.visible');
-        cy.get(TOOLBAR_GROUP_1).children().eq(5).click();
+        cy.get(TOOLBAR_GROUP_1).children().eq(6).click();
         cy.get('[contenteditable=true]').should(
             'include.html',
             'tw-table-cell tw-rounded tw-bg-box-neutral tw-text-box-neutral-inverse tw-m-0 tw-px-2 tw-py-0.5',
