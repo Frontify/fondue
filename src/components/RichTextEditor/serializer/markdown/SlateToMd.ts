32164d4a0a4013dc43679174aecd15da6e5f62e0
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-11-09 13:37:50 +0100

    updated imports

--- src/components/RichTextEditor/serializer/markdown/SlateToMd.ts
+++ src/components/RichTextEditor/serializer/markdown/SlateToMd.ts
@@ -1,8 +1,9 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { NodeType, serialize } from './RemarkPlate';
+import { serialize } from './serializer';
 import { options } from './options';
 import { MdTransformer } from './MdTransformer';
+import { NodeType } from './types';
 
 export class SlateToMd extends MdTransformer<NodeType[], string> {
     process(value: NodeType[]) {
