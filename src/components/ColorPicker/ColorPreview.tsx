d21b2f673d90f628ab24d18a8fd6753414922b63
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2022-11-14 16:32:42 +0100

    Replacing inline border radius CSS with tw-rounded-t in color preview

--- src/components/ColorPicker/ColorPreview.tsx
+++ src/components/ColorPicker/ColorPreview.tsx
@@ -15,8 +15,8 @@ export const ColorPreview: FC<ColorPreviewProps> = ({ color }) => {
 
     return (
         <div
-            className="tw-sticky tw-box-content tw-shrink-0 tw-h-2 tw-top-0 tw-bg-white tw-z-20 dark:tw-bg-black-95 tw-shadow-inner tw-border-l tw-border-t tw-border-r tw-border-line"
-            style={{ backgroundColor, borderTopLeftRadius: '3px', borderTopRightRadius: '3px' }}
+            className="tw-sticky tw-box-content tw-shrink-0 tw-h-2 tw-top-0 tw-bg-white tw-z-20 dark:tw-bg-black-95 tw-shadow-inner tw-border-l tw-border-t tw-border-r tw-border-line tw-rounded-t"
+            style={{ backgroundColor }}
             data-test-id="color-preview"
         ></div>
     );
