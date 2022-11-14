3487fa0c31207203f7a140e70786388d0795ab46
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2022-11-14 13:06:10 +0100

    fix(Color Picker): only the top corners of the color preview should be rounded

--- src/components/ColorPicker/ColorPreview.tsx
+++ src/components/ColorPicker/ColorPreview.tsx
@@ -15,7 +15,7 @@ export const ColorPreview: FC<ColorPreviewProps> = ({ color }) => {
 
     return (
         <div
-            className="tw-sticky tw-box-content tw-shrink-0 tw-h-2 tw-top-0 tw-bg-white tw-z-20 dark:tw-bg-black-95 tw-rounded"
+            className="tw-sticky tw-box-content tw-shrink-0 tw-h-2 tw-top-0 tw-bg-white tw-z-20 dark:tw-bg-black-95 tw-rounded-t"
             style={{ backgroundColor }}
             data-test-id="color-preview"
         ></div>
