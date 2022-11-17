601dfb00a58c48508701f84be1e15fb909c7e9f2
Mike Schmid <mikeschmid89@gmail.com>
2022-11-17 09:23:46 +0100

    feat(RichTextEditor): add reset formatting button (#1055)

---
+++ src/components/RichTextEditor/Plugins/ResetFormattingPlugin/id.ts
@@ -0,0 +1,4 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+export const RESET_FORMATTING_PLUGIN = 'reset-formatting-plugin';
+export const ELEMENT_RESET_FORMATTING = 'reset-formatting';
