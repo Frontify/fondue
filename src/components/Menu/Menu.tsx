c8a02077bdcf5c01de3d37f536730bd38a3db819
Fabricio Mendonca <fabricio.mendonca@frontify.com>
2022-11-18 11:33:55 +0100

    fix(Menu): disable inset border pseudo element pointer events

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -18,7 +18,7 @@ export type MenuProps = PropsWithChildren<Props>;
 const CONTAINER_BASE_CLASSES =
     'tw-relative tw-bg-base tw-rounded tw-absolute tw-left-0 tw-top-7 tw-py-2 tw-shadow-mid tw-w-max';
 const INSET_BORDER =
-    'before:tw-block before:tw-rounded before:tw-top-0 before:tw-left-0 before:tw-bg-transparent before:tw-absolute before:tw-shadow-inner-line before:tw-w-full before:tw-h-full';
+    'before:tw-pointer-events-none before:tw-block before:tw-rounded before:tw-top-0 before:tw-left-0 before:tw-bg-transparent before:tw-absolute before:tw-shadow-inner-line before:tw-w-full before:tw-h-full';
 const CONTAINER_CLASSES = merge([CONTAINER_BASE_CLASSES, INSET_BORDER]);
 
 export const Menu = ({ triggerRef, children, open = false, onClose }: MenuProps) => {
