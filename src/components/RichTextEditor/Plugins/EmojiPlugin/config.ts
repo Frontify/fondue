264a49e3ea7778a176efa6c476196e6359e74b01
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-11-23 23:27:48 +0100

    ts ignore, type update

--- src/components/RichTextEditor/Plugins/EmojiPlugin/config.ts
+++ src/components/RichTextEditor/Plugins/EmojiPlugin/config.ts
@@ -4,5 +4,8 @@ import { EmojiPlugin, PlatePlugin } from '@udecode/plate';
 import { EmojiCombobox } from './EmojiCombobox';
 
 export const emojiPlugin: Partial<PlatePlugin<EmojiPlugin>> = {
+    // needed Plate type troubles
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     renderAfterEditable: EmojiCombobox,
 };
