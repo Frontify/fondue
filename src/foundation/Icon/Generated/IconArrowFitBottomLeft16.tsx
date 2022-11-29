be3872173fec28f2c8b0cf0ebefa2e7c06597460
Ruslan Sazonov <ruslan.sazonov@frontify.com>
2022-11-29 16:17:19 +0200

    New icons generated (#1081)

---
+++ src/foundation/Icon/Generated/IconArrowFitBottomLeft16.tsx
@@ -0,0 +1,23 @@
+import React, { memo } from 'react';
+import { GeneratedIconProps } from '@foundation/Icon/IconProps';
+function IconArrowFitBottomLeft16(props: GeneratedIconProps): React.ReactElement<GeneratedIconProps> {
+    const customClassName = 'tw-flex tw-items-center tw-justify-center tw-fill-current';
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={16}
+            height={16}
+            className={customClassName}
+            name="IconArrowFitBottomLeft16"
+            {...props}
+        >
+            <path
+                fill="currentColor"
+                fillRule="evenodd"
+                d="M7.69 2.501a.5.5 0 0 1 .5-.5H14V7.81h-1V3.708l-9.292 9.29h6.757a.5.5 0 0 1 0 1H3.501a1.5 1.5 0 0 1-1.5-1.5V5.535a.5.5 0 1 1 1 0v6.758L12.293 3H8.19a.5.5 0 0 1-.5-.5Zm5.81 5.31h.5a.5.5 0 1 1-1 0h.5Z"
+                clipRule="evenodd"
+            />
+        </svg>
+    );
+}
+export default memo(IconArrowFitBottomLeft16);
---
+++ src/foundation/Icon/Generated/IconArrowFitBottomLeft24.tsx
@@ -0,0 +1,23 @@
+import React, { memo } from 'react';
+import { GeneratedIconProps } from '@foundation/Icon/IconProps';
+function IconArrowFitBottomLeft24(props: GeneratedIconProps): React.ReactElement<GeneratedIconProps> {
+    const customClassName = 'tw-flex tw-items-center tw-justify-center tw-fill-current';
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            className={customClassName}
+            name="IconArrowFitBottomLeft24"
+            {...props}
+        >
+            <path
+                fill="currentColor"
+                fillRule="evenodd"
+                d="M5.748 21.001A2.748 2.748 0 0 1 3 18.251V8.298a.75.75 0 0 1 1.5 0v9.953c0 .06.004.12.012.177L18.442 4.5h-6.158a.75.75 0 0 1 0-1.5h8.718v8.716h-.75.75a.75.75 0 1 1-1.5 0h.75-.75V5.56L5.573 19.489c.057.008.116.012.175.012h9.955a.75.75 0 0 1 0 1.5H5.748Z"
+                clipRule="evenodd"
+            />
+        </svg>
+    );
+}
+export default memo(IconArrowFitBottomLeft24);
