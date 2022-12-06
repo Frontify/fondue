6c207d91851fb7ef36a08dabf8e742dbcee769dd
Nikola Milenković <nikola.milenkovic@frontify.com>
2022-12-06 09:35:39 +0100

    refactor(Tree)!: change to component based structure (#1056)
    * WIP: create new tree components
    
    * feat: remove unneeded markup, create context, update types
    
    * feat: add multiselect support and cleanup
    
    * feat: multiselect mode and improvements
    
    * remove old Tree
    
    * rename NewTree, add tests and stories
    
    * fix: linting
    
    * pr comment fixes
    
    * fix: caret styles
    
    * create default content component
    
    * make label optional and export component
    
    * rename to legacy component
    
    * update tests and stories
    
    * update children type
    
    * add keys to array items
    
    * move toggle click and increase click area
    
    * fix: add temporary eslint disable
    
    * fix: pr comment fixes
    
    * add label prop and force children type
    
    * fix: remove code duplication
    
    * fix: remove force children type
    
    * fix: add keys
    
    * fix: remove unused constant
    
    * fix: rename type
    
    * fix: PR comment fixes
    
    * fix: PR comment fix
    
    * feat: add activeIds prop
    
    * chore: move constants outside of component

--- src/components/Tree/utils/index.ts
+++ src/components/Tree/utils/index.ts
@@ -1,6 +1,3 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-export * from './ActionsFlyoutComponent';
-export * from './getReorderedNodes';
-export * from './listToTree';
 export * from './mocks';
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,7 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+type Nullable<T> = T | null;
+
 type NullableString = string | null;
 type NullableNumber = number | null;
 type NullableBoolean = boolean | null;
