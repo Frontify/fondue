c65919907066442d7743facb69a41eb3432019c7
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-12-19 09:38:06 +0100

    changed emoji plugin key

--- src/components/RichTextEditor/Plugins/EmojiPlugin/EmojiCombobox/EmojiCombobox.tsx
+++ src/components/RichTextEditor/Plugins/EmojiPlugin/EmojiCombobox/EmojiCombobox.tsx
@@ -1,11 +1,11 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 import React from 'react';
-import { Combobox, ELEMENT_EMOJI, EmojiItemData, TEmojiCombobox, useEmojiCombobox } from '@udecode/plate';
+import { Combobox, EmojiItemData, KEY_EMOJI, TEmojiCombobox, useEmojiCombobox } from '@udecode/plate';
 import { EmojiComboboxItem, EmojiComboboxStyles } from './';
 
 export const EmojiCombobox = <TData extends EmojiItemData = EmojiItemData>({
-    pluginKey = ELEMENT_EMOJI,
+    pluginKey = KEY_EMOJI,
     id = pluginKey,
 }: TEmojiCombobox<TData>) => {
     const { trigger, onSelectItem } = useEmojiCombobox(pluginKey);
