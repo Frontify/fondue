b1d7964358bc3d8118d7de635ff2b0b1fea8153a
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-12-21 19:20:00 +0100

    remark mentions

---
+++ src/components/RichTextEditor/serializer/markdown/remarkMention/index.ts
@@ -0,0 +1,68 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { VisitorResult, visit } from 'unist-util-visit';
+import { NodeChild, ParagraphNode, RegExpMatchArray, Transformer } from './types';
+
+function is<T extends NodeChild, K extends keyof T>(node: T, type: K): boolean {
+    return node[type] !== '' && !!node.value;
+}
+
+function transformer(tree: ParagraphNode) {
+    visit(tree, 'paragraph', visitor);
+
+    function visitor(node: ParagraphNode): VisitorResult {
+        const { children } = node;
+        node.children = [];
+
+        for (const child of children) {
+            if (!is(child, 'text') || !child.value) {
+                node.children.push(child);
+                continue;
+            }
+
+            const matches = [...child.value.matchAll(/@\[[a-z]+:\s\d+]/gi)] as RegExpMatchArray[];
+            console.log('matches', matches);
+
+            if (matches === null || matches.length === 0) {
+                node.children.push(child);
+                true;
+                continue;
+            }
+
+            if (matches[0].index > 0) {
+                node.children.push({
+                    type: 'text',
+                    value: child.value.substr(0, matches[0].index),
+                });
+            }
+
+            for (const [index, match] of matches.entries()) {
+                node.children.push({
+                    type: 'mention',
+                    children: [{ type: 'text', value: match[0] }],
+                });
+
+                if (matches.length > index + 1 && child.value) {
+                    const startAt = match.index + match[0].length;
+                    node.children.push({
+                        type: 'text',
+                        value: child.value.slice(startAt, matches[index + 1].index - startAt),
+                    });
+                }
+            }
+
+            const lastMatch = matches[matches.length - 1];
+
+            if (lastMatch.index + lastMatch[0].length < child.value.length) {
+                node.children.push({
+                    type: 'text',
+                    value: child.value.slice(lastMatch.index + lastMatch[0].length),
+                });
+            }
+        }
+    }
+}
+
+export default function plugin(): Transformer {
+    return transformer;
+}
---
+++ src/components/RichTextEditor/serializer/markdown/remarkMention/types.ts
@@ -0,0 +1,21 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+export type NodeChild = {
+    type: string;
+    value?: string;
+    text?: string;
+    children?: NodeChild[];
+};
+
+export type ParagraphNode = Record<string, string | [] | undefined> & {
+    type: string;
+    children: NodeChild[];
+};
+
+export type Transformer = (tree: ParagraphNode) => void;
+
+export type RegExpMatchArray = [string] &
+    string[] & {
+        index: number;
+        input: string;
+    };
