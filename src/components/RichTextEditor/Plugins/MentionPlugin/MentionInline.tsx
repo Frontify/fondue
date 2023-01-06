1d7648882ef65a8ed5bf797fa2a5cf6b599548b3
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2023-01-06 16:49:58 +0100

    fix: custom filtering mention items

--- src/components/RichTextEditor/Plugins/MentionPlugin/MentionInline.tsx
+++ src/components/RichTextEditor/Plugins/MentionPlugin/MentionInline.tsx
@@ -3,8 +3,9 @@
 import React from 'react';
 import { MentionCombobox } from '@udecode/plate';
 import { MentionableItems } from './types';
+import { filterItems } from './helpers';
 
 // eslint-disable-next-line react/display-name
 export const MentionInline = (items: MentionableItems) => () => {
-    return <MentionCombobox items={items} />;
+    return <MentionCombobox items={items} filter={filterItems} />;
 };
