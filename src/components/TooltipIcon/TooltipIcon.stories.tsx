e25c49cb3acf8aced0464e40a70b8f81c5343252
imoutaharik <ilham.moutaharik@frontify.com>
2023-02-06 16:46:19 +0100

    chore: Add Deprecation category in Storybook

--- src/components/TooltipIcon/TooltipIcon.stories.tsx
+++ src/components/TooltipIcon/TooltipIcon.stories.tsx
@@ -7,7 +7,7 @@ import { IconSize } from '@foundation/Icon/IconSize';
 import { IconExclamationMarkCircle } from '@foundation/Icon/Generated';
 
 export default {
-    title: 'Components/TooltipIcon',
+    title: 'Deprecating/TooltipIcon',
     component: TooltipIcon,
     tags: ['autodocs'],
     argTypes: {
