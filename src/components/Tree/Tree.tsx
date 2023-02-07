d0ab30b5129cf960ce5b59018fcc1440f76a5a86
Samuel Alev <samuel.alev@frontify.com>
2023-02-07 17:57:25 +0100

    refactor!: register tree items (#1211)

--- src/components/Tree/Tree.tsx
+++ src/components/Tree/Tree.tsx
@@ -1,50 +1,210 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import React, { ReactNode, useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useReducer } from 'react';
 
-import type { TreeProps } from '@components/Tree/types';
+import type {
+    RegisterTreeItemPayload,
+    TreeItemState,
+    TreeProps,
+    TreeState,
+    TreeStateAction,
+} from '@components/Tree/types';
 import { TreeContext } from '@components/Tree/TreeContext';
 import { DndWrapper, DraggableItem, DropZonePosition } from '@utilities/dnd';
 
 import { useDraggableEnhancedChildren } from './hooks/useDraggableEnhancedChildren';
+import { cloneThroughFragments } from './utils';
 
 const noop = () => undefined;
+export const ROOT_ID = '__ROOT__';
 
-export const Tree = ({ id, activeIds, draggable = false, onDrop, children }: TreeProps) => {
-    const [selectedIds, setSelectedIds] = useState<string[]>(activeIds || []);
-    const [multiselect, setMultiselect] = useState<boolean>(false);
+const reducer = (state: TreeState, action: TreeStateAction): TreeState => {
+    switch (action.type) {
+        case 'SET_SELECT': {
+            const { id, isSelected } = action.payload;
 
-    const downKeyHandler = (event: KeyboardEvent) => {
-        setMultiselect(event.key === 'Meta' || event.ctrlKey);
-    };
+            if (!state.items.has(id)) {
+                throw new Error(`No tree item registered with id ${id}.`);
+            }
 
-    const upKeyHandler = (event: KeyboardEvent) => {
-        setMultiselect(!(event.key === 'Meta' || event.ctrlKey));
-    };
+            if (isSelected) {
+                if (state.selectionMode === 'single') {
+                    state.selectedIds.clear();
+                }
+
+                return {
+                    ...state,
+                    selectedIds: state.selectedIds.add(id),
+                };
+            }
+
+            state.selectedIds.delete(id);
+            return {
+                ...state,
+            };
+        }
+
+        case 'SET_EXPAND': {
+            const { id, isExpanded } = action.payload;
+
+            if (!state.items.has(id)) {
+                throw new Error(`No tree item registered with id ${id}.`);
+            }
+
+            if (isExpanded) {
+                return {
+                    ...state,
+                    expandedIds: state.expandedIds.add(id),
+                };
+            }
+
+            state.expandedIds.delete(id);
+            return {
+                ...state,
+            };
+        }
+
+        case 'SET_SELECTION_MODE':
+            return {
+                ...state,
+                selectionMode: action.payload.selectionMode,
+            };
+
+        case 'REGISTER_TREE_ITEM': {
+            const { id, childrenIds, level, parentId, domElement: ref } = action.payload;
+
+            if (state.items.has(id)) {
+                console.warn('Tree item already registered.');
+                return state;
+            }
+
+            if (parentId) {
+                const parentState = state.items.get(parentId);
+                if (parentState) {
+                    const newChildrenIds = [...(parentState?.childrenIds ?? []), id];
+                    state.items.set(parentId, { ...parentState, childrenIds: newChildrenIds });
+                }
+            }
+
+            return {
+                ...state,
+                items: state.items.set(id, {
+                    parentId,
+                    childrenIds,
+                    level,
+                    domElement: ref,
+                }),
+            };
+        }
+
+        case 'UNREGISTER_TREE_ITEM':
+            const { id } = action.payload;
+            const currentItem = state.items.get(id);
+
+            if (currentItem?.parentId) {
+                const parentState = state.items.get(currentItem.parentId);
+                if (parentState) {
+                    const newChildrenIds = (parentState?.childrenIds ?? []).filter((childId) => childId !== id);
+                    state.items.set(currentItem.parentId, { ...parentState, childrenIds: newChildrenIds });
+                }
+            }
+
+            state.items.delete(id);
+
+            return {
+                ...state,
+                items: state.items,
+            };
+
+        case 'REPLACE_STATE':
+            return {
+                ...state,
+                selectedIds: action.payload.selectedIds,
+                expandedIds: action.payload.expandedIds,
+                selectionMode: action.payload.selectionMode,
+            };
+
+        default:
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            //@ts-ignore
+            console.warn(`Updated tree with action "${action.type}" but it has not effect.`);
+            return state;
+    }
+};
+
+export const Tree = ({
+    id,
+    selectedIds,
+    expandedIds,
+    draggable = false,
+    onSelect,
+    onExpand,
+    onDrop,
+    children,
+}: TreeProps) => {
+    const initialState: TreeState = useMemo(
+        () => ({
+            items: new Map<string, TreeItemState>().set(ROOT_ID, { level: -1 }),
+            selectedIds: new Set(selectedIds ?? []),
+            expandedIds: new Set(expandedIds ?? []),
+            selectionMode: 'single',
+        }),
+        [expandedIds, selectedIds],
+    );
+
+    const [treeState, updateTreeState] = useReducer(reducer, initialState);
+
+    const keyDownHandler = useCallback((event: KeyboardEvent) => {
+        if (event.key === 'Meta' || event.ctrlKey) {
+            updateTreeState({
+                type: 'SET_SELECTION_MODE',
+                payload: { selectionMode: 'multiselect' },
+            });
+        }
+    }, []);
+
+    const keyUpHandler = useCallback((event: KeyboardEvent) => {
+        if (event.key === 'Meta' || event.ctrlKey) {
+            updateTreeState({
+                type: 'SET_SELECTION_MODE',
+                payload: { selectionMode: 'single' },
+            });
+        }
+    }, []);
 
     useEffect(() => {
-        window.addEventListener('keydown', downKeyHandler);
-        window.addEventListener('keyup', upKeyHandler);
+        window.addEventListener('keydown', keyDownHandler);
+        window.addEventListener('keyup', keyUpHandler);
 
         return () => {
-            window.removeEventListener('keydown', downKeyHandler);
-            window.removeEventListener('keyup', upKeyHandler);
+            window.removeEventListener('keydown', keyDownHandler);
+            window.removeEventListener('keyup', keyUpHandler);
         };
-    }, []);
+    }, [keyDownHandler, keyUpHandler]);
+
+    useEffect(() => {
+        updateTreeState({
+            type: 'REPLACE_STATE',
+            payload: initialState,
+        });
+    }, [initialState]);
 
     const handleSelect = useCallback(
         (id: string) => {
-            setSelectedIds((prevState) => {
-                if (!multiselect) {
-                    return [id];
-                }
-                return prevState.includes(id)
-                    ? prevState.filter((selectedId) => selectedId !== id)
-                    : [...prevState, id];
+            updateTreeState({
+                type: 'SET_SELECT',
+                payload: { id, isSelected: !treeState.selectedIds.has(id) },
             });
         },
-        [multiselect],
+        [treeState.selectedIds],
     );
+
+    const handleExpand = useCallback((id: string, isExpanded: boolean) => {
+        updateTreeState({
+            type: 'SET_EXPAND',
+            payload: { id, isExpanded },
+        });
+    }, []);
 
     const handleDrop = useCallback(
         (
@@ -52,42 +212,47 @@ export const Tree = ({ id, activeIds, draggable = false, onDrop, children }: Tre
             sourceItem: DraggableItem<{ id: string; sort: Nullable<number> }>,
             position: DropZonePosition,
         ) => {
-            if (onDrop) {
-                onDrop(targetItem, sourceItem, position);
-            } else {
-                return null;
-            }
+            onDrop?.(targetItem, sourceItem, position);
         },
         [onDrop],
     );
 
-    const { draggableEnhancedChildren } = useDraggableEnhancedChildren({ accept: id, onDrop: handleDrop, children });
-
-    const memoizedTreeContextValue = useMemo(
-        () => ({
-            treeId: id,
-            selectedIds,
-            onSelect: handleSelect,
-            draggable,
-            onDrop: handleDrop ?? noop,
-        }),
-        [id, selectedIds, handleSelect, draggable, handleDrop],
+    const unregisterTreeItem = useCallback(
+        (id: string) => updateTreeState({ type: 'UNREGISTER_TREE_ITEM', payload: { id } }),
+        [],
     );
 
-    const childrenArray = React.Children.toArray(children);
+    const registerTreeItem = useCallback(
+        (payload: RegisterTreeItemPayload) => updateTreeState({ type: 'REGISTER_TREE_ITEM', payload }),
+        [],
+    );
 
-    let enhancedChildren: ReactNode = childrenArray;
-
-    if (draggable) {
-        enhancedChildren = draggableEnhancedChildren;
-    }
+    const childrenArray = useMemo(() => cloneThroughFragments(children, { level: 0, parentId: ROOT_ID }), [children]);
+    const { draggableEnhancedChildren } = useDraggableEnhancedChildren({
+        accept: id,
+        onDrop: handleDrop,
+        children: childrenArray,
+    });
+    const enhancedChildren = draggable ? draggableEnhancedChildren : childrenArray;
 
     return (
-        <TreeContext.Provider value={memoizedTreeContextValue}>
+        <TreeContext.Provider
+            value={{
+                treeId: id,
+                treeState,
+                draggable,
+                onSelect: onSelect ?? handleSelect,
+                onExpand: onExpand ?? handleExpand,
+                onDrop: handleDrop ?? noop,
+                unregisterTreeItem,
+                registerTreeItem,
+            }}
+        >
             <ul
                 id={id}
                 data-test-id="tree"
                 className="tw-p-0 tw-m-0 tw-font-sans tw-font-normal tw-list-none tw-text-left tw-text-sm tw-select-none"
+                role="tree"
             >
                 <DndWrapper id={id}>{enhancedChildren}</DndWrapper>
             </ul>
--- src/components/Tree/TreeItem.tsx
+++ src/components/Tree/TreeItem.tsx
@@ -1,6 +1,16 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import React, { ReactNode, useState } from 'react';
+import React, {
+    Children,
+    KeyboardEvent,
+    MouseEvent,
+    ReactNode,
+    useCallback,
+    useEffect,
+    useMemo,
+    useRef,
+    useState,
+} from 'react';
 import { useDrag } from 'react-dnd';
 
 import { merge } from '@utilities/merge';
@@ -9,33 +19,66 @@ import { DraggableItem, DropZonePosition } from '@utilities/dnd';
 import { DropZone } from '@components/DropZone';
 import type { TreeItemProps } from '@components/Tree/types';
 import { useTreeContext } from '@components/Tree/TreeContext';
+import { FOCUS_VISIBLE_STYLE } from '@utilities/focusStyle';
 
-import { useDraggableEnhancedChildren } from './hooks/useDraggableEnhancedChildren';
+import { useDraggableEnhancedChildren } from './hooks';
+import { getAcceptTypes, getItemPositionInParent, getNextItemToFocus, getPreviousItemToFocus } from './helpers';
+import { cloneThroughFragments, flattenChildren } from './utils';
 
 const DRAGGING_OPACITY = 0.4;
 const DEFAULT_OPACITY = 1;
 
 export const TreeItem = ({
     id,
-    sort,
+    parentId,
     label,
     contentComponent,
-    onSelect,
     onDrop,
     type,
     accepts,
     children,
+    level,
 }: TreeItemProps) => {
-    const { treeId, selectedIds, onSelect: onItemSelect, draggable, onDrop: onTreeDrop } = useTreeContext();
+    const {
+        treeId,
+        draggable,
+        onSelect,
+        onExpand,
+        onDrop: onTreeDrop,
+        registerTreeItem,
+        unregisterTreeItem,
+        treeState,
+    } = useTreeContext();
 
-    const [expanded, setExpanded] = useState<boolean>(false);
+    const treeItemState = treeState.items.get(id);
+    const itemRef = useRef<HTMLDivElement>(null);
+
     const [hovered, setHovered] = useState<boolean>(false);
+    const handleMouseEnter = useCallback(() => setHovered(true), []);
+    const handleMouseLeave = useCallback(() => setHovered(false), []);
 
-    const handleMouseEnter = () => setHovered(true);
-    const handleMouseLeave = () => setHovered(false);
+    const sort = treeItemState?.parentId ? getItemPositionInParent(id, treeItemState.parentId, treeState) : -1;
+
+    useEffect(() => {
+        if (itemRef.current) {
+            const childrenIds = flattenChildren(children).map((child) => child.props.id);
+
+            registerTreeItem({
+                id,
+                parentId,
+                childrenIds: childrenIds.length > 0 ? childrenIds : undefined,
+                level: level ?? 0,
+                domElement: itemRef.current,
+            });
+
+            return () => {
+                unregisterTreeItem(id);
+            };
+        }
+    }, [id, level, parentId, registerTreeItem, unregisterTreeItem, itemRef, children]);
 
     const [{ opacity }, drag] = useDrag({
-        item: { id, sort, type },
+        item: { id, type, sort },
         collect: (monitor) => ({
             opacity: monitor.isDragging() ? DRAGGING_OPACITY : DEFAULT_OPACITY,
             isDragging: monitor.isDragging(),
@@ -44,50 +87,122 @@ export const TreeItem = ({
         canDrag: draggable,
     });
 
-    const handleDrop = (
-        targetItem: DraggableItem<{ id: string; sort: Nullable<number> }>,
-        sourceItem: DraggableItem<{ id: string; sort: Nullable<number> }>,
-        position: DropZonePosition,
-    ) => {
-        (onDrop ?? onTreeDrop)?.(targetItem, sourceItem, position);
-    };
+    const handleDrop = useCallback(
+        (
+            targetItem: DraggableItem<{ id: string; sort: Nullable<number> }>,
+            sourceItem: DraggableItem<{ id: string; sort: Nullable<number> }>,
+            position: DropZonePosition,
+        ) => {
+            (onDrop ?? onTreeDrop)?.(targetItem, sourceItem, position);
+        },
+        [onDrop, onTreeDrop],
+    );
+
+    const handleSelect = useCallback(
+        (event: MouseEvent) => {
+            event.stopPropagation();
+            onSelect(id);
+        },
+        [id, onSelect],
+    );
+
+    const handleExpandClick = useCallback(
+        (event: MouseEvent) => {
+            event.stopPropagation();
+            onExpand(id, !treeState.expandedIds.has(id));
+        },
+        [id, onExpand, treeState.expandedIds],
+    );
+
+    const handleExpandKeyDown = useCallback(
+        (event: KeyboardEvent) => {
+            if (event.key === 'Space' || event.key === 'Enter') {
+                event.stopPropagation();
+                onExpand(id, !treeState.expandedIds.has(id));
+            }
+        },
+        [id, onExpand, treeState.expandedIds],
+    );
+
+    const handleKeyDown = useCallback(
+        (event: KeyboardEvent) => {
+            switch (event.code) {
+                case 'Enter':
+                    event.preventDefault();
+                    onSelect(id);
+                    break;
+
+                case 'Space':
+                    event.preventDefault();
+                    if (treeItemState?.childrenIds) {
+                        onExpand(id, !treeState.expandedIds.has(id));
+                    } else {
+                        onSelect(id);
+                    }
+                    break;
+
+                case 'ArrowRight':
+                    event.preventDefault();
+                    if (treeItemState?.childrenIds && !treeState.expandedIds.has(id)) {
+                        onExpand(id, true);
+                    } else if (treeItemState?.childrenIds?.[0]) {
+                        const firstChildId = treeItemState.childrenIds[0];
+                        const firstChildState = treeState.items.get(firstChildId);
+                        firstChildState?.domElement?.focus();
+                    }
+                    break;
+
+                case 'ArrowLeft':
+                    event.preventDefault();
+                    if (treeItemState?.childrenIds && treeState.expandedIds.has(id)) {
+                        onExpand(id, false);
+                    } else if (treeItemState?.parentId) {
+                        const parentId = treeItemState.parentId;
+                        const parentState = treeState.items.get(parentId);
+                        parentState?.domElement?.focus();
+                    }
+                    break;
+
+                case 'ArrowUp':
+                    event.preventDefault();
+                    if (treeItemState) {
+                        const previousItemToFocus = getPreviousItemToFocus(id, treeItemState, treeState);
+                        previousItemToFocus?.domElement?.focus();
+                    }
+                    break;
+
+                case 'ArrowDown':
+                    event.preventDefault();
+                    if (treeItemState) {
+                        const nextItemToFocus = getNextItemToFocus(id, treeItemState, treeState);
+                        nextItemToFocus?.domElement?.focus();
+                    }
+                    break;
+            }
+        },
+        [id, onExpand, onSelect, treeItemState, treeState],
+    );
+
+    const childrenArray = useMemo(
+        () =>
+            children
+                ? cloneThroughFragments(children, {
+                      parentId: id,
+                      level: (level || 0) + 1,
+                  })
+                : [],
+        [children, id, level],
+    );
 
     const { draggableEnhancedChildren } = useDraggableEnhancedChildren({
         accept: getAcceptTypes(accepts ?? treeId, 'before'),
         onDrop: handleDrop,
-        children,
+        children: childrenArray,
+        sort,
     });
+    const enhancedChildren: ReactNode = draggable ? draggableEnhancedChildren : childrenArray;
 
-    const handleSelect = () => {
-        onItemSelect(id);
-        onSelect?.(id);
-    };
-
-    const toggleExpand = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        event.stopPropagation();
-        setExpanded((previousExpanded) => !previousExpanded);
-    };
-
-    const childrenArray = React.Children.toArray(children);
-
-    let enhancedChildren: ReactNode = childrenArray;
-
-    if (draggable) {
-        enhancedChildren = draggableEnhancedChildren;
-    }
-
-    const caretComponent =
-        childrenArray.length > 0 ? (
-            <div
-                className={merge([
-                    'tw-transition-transform tw-w-0 tw-h-0 tw-text-black-100 tw-text-opacity-40 tw-font-normal tw-border-t-4 tw-border-t-transparent tw-border-b-4 tw-border-b-transparent tw-border-l-4 tw-border-l-x-strong',
-                    expanded ? 'tw-rotate-90' : '',
-                    selectedIds.includes(id) && 'tw-text-box-selected-strong-inverse',
-                ])}
-            />
-        ) : (
-            <></>
-        );
+    const hasChildren = Children.count(enhancedChildren) > 0;
 
     return (
         <li data-test-id="tree-item" ref={drag} style={{ opacity }}>
@@ -100,46 +215,63 @@ export const TreeItem = ({
                 onDrop={handleDrop}
                 accept={getAcceptTypes(accepts ?? treeId, 'within')}
             >
-                {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
                 <div
+                    ref={itemRef}
                     className={merge([
                         'tw-flex tw-py-2 tw-px-2.5 tw-no-underline tw-leading-5 tw-h-10',
-                        selectedIds.includes(id)
+                        FOCUS_VISIBLE_STYLE,
+                        treeState.selectedIds.has(id)
                             ? 'tw-font-medium tw-bg-box-selected-strong tw-text-box-selected-strong-inverse hover:tw-bg-box-selected-strong-hover hover:tw-text-box-selected-strong-inverse-hover'
                             : 'tw-text-text hover:tw-bg-box-neutral-hover hover:tw-text-box-neutral-inverse-hover',
                     ])}
+                    role="treeitem"
+                    aria-label={label}
+                    aria-level={treeItemState?.level}
+                    aria-selected={treeState.selectedIds.has(id)}
+                    aria-expanded={treeState.expandedIds.has(id)}
+                    tabIndex={0}
                     onClick={handleSelect}
+                    onKeyDown={handleKeyDown}
                     onMouseEnter={handleMouseEnter}
                     onMouseLeave={handleMouseLeave}
                 >
                     <div className="tw-flex tw-flex-1 tw-space-x-1 tw-items-center tw-h-6">
                         {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
-                        <span
-                            data-test-id="tree-item-toggle"
-                            className={merge([
-                                'tw-w-2 tw-h-3 tw-flex tw-items-center tw-justify-center tw-py-3',
-                                childrenArray.length > 0 && 'tw-cursor-pointer',
-                            ])}
-                            onClick={toggleExpand}
-                        >
-                            {caretComponent}
-                        </span>
+                        <div className="tw-w-2 tw-h-3 tw-flex tw-items-center tw-justify-center">
+                            {hasChildren && (
+                                <button
+                                    data-test-id="tree-item-toggle"
+                                    className="tw-flex tw-items-center tw-justify-center tw-py-3 tw-cursor-pointer"
+                                    onClick={handleExpandClick}
+                                    onKeyDown={handleExpandKeyDown}
+                                    tabIndex={0}
+                                >
+                                    <div
+                                        className={merge([
+                                            'tw-transition-transform tw-w-0 tw-h-0 tw-text-black-100 tw-text-opacity-40 tw-font-normal tw-border-t-4 tw-border-t-transparent tw-border-b-4 tw-border-b-transparent tw-border-l-4 tw-border-l-x-strong',
+                                            treeState.selectedIds.has(id) && 'tw-text-box-selected-strong-inverse',
+                                            treeState.expandedIds.has(id) && 'tw-rotate-90',
+                                        ])}
+                                    />
+                                </button>
+                            )}
+                        </div>
 
-                        {label && <span>{label}</span>}
+                        {label ? <span>{label}</span> : null}
 
-                        {contentComponent?.({ selected: selectedIds.includes(id), hovered })}
+                        {contentComponent?.({ selected: treeState.selectedIds.has(id) ?? false, hovered })}
                     </div>
                 </div>
             </DropZone>
 
-            {expanded && (
+            {treeState.expandedIds.has(id) ? (
                 <ul
                     className="tw-p-0 tw-m-0 tw-list-none tw-font-sans tw-font-normal tw-text-left [&>li]:tw-pl-4"
                     data-test-id="sub-tree-items"
                 >
                     {enhancedChildren}
                 </ul>
-            )}
+            ) : null}
 
             <DropZone
                 data-position={DropZonePosition.After}
@@ -153,13 +285,3 @@ export const TreeItem = ({
         </li>
     );
 };
-
-function getAcceptTypes<
-    T extends { within: string | string[]; after: string | string[]; before: string | string[] } | string | string[],
->(accepts: T, position: 'within' | 'after' | 'before') {
-    if (typeof accepts === 'object' && !(accepts instanceof Array)) {
-        return accepts[position];
-    } else {
-        return accepts as string | string[];
-    }
-}
