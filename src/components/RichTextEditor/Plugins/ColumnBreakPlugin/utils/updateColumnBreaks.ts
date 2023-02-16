70ff63d6ff75e9f8ca59e4380ce63bf98e4982b9
triggertoo <triggertoo@gmail.com>
2023-02-16 14:40:29 +0100

    remove console.log

--- src/components/RichTextEditor/Plugins/ColumnBreakPlugin/utils/updateColumnBreaks.ts
+++ src/components/RichTextEditor/Plugins/ColumnBreakPlugin/utils/updateColumnBreaks.ts
@@ -10,7 +10,6 @@ export const updateColumnBreak = (editor: PlateEditor<Value>, at: Location, valu
 
 export const updateColumnBreaks = (editor: PlateEditor<Value>, columns: number) => {
     const nodeEntries = Array.from(getNodeEntries(editor, { at: [], match: (node) => !!node.breakAfterColumn }));
-    console.log('update breaks');
     for (const [index, [, path]] of nodeEntries.entries()) {
         updateColumnBreak(editor, path, index < columns - 1 ? 'active' : 'inactive');
     }
