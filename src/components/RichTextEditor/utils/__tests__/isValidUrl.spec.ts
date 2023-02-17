6aa336ea22d510788625c56cf8391292c9d81fb6
Kevin Walker <kevin@smartive.ch>
2023-02-17 08:20:41 +0100

    Revert "Update isValidUrl.spec.ts"
    This reverts commit 0439a0c25c8728a42ed76afc7c5d0d4596b3ef0c.

--- src/components/RichTextEditor/utils/__tests__/isValidUrl.spec.ts
+++ src/components/RichTextEditor/utils/__tests__/isValidUrl.spec.ts
@@ -7,28 +7,32 @@ describe('Validate urls correctly', () => {
         expect(isValidUrl('https://frontify.com')).to.be.true;
     });
 
-    it('//this-could-be-a-long-domain.com', () => {
-        expect(isValidUrl('//this-could-be-a-long-domain.com')).to.be.false;
-    });
-
     it('https://frontify.com/something', () => {
         expect(isValidUrl('https://frontify.com/something')).to.be.true;
-    });
-
-    it('https://', () => {
-        expect(isValidUrl('https://')).to.be.false;
     });
 
     it('https://frontify.com/something?q=query#hash', () => {
         expect(isValidUrl('https://frontify.com/something?q=query#hash')).to.be.true;
     });
 
+    it('mailto:hello@frontify.com', () => {
+        expect(isValidUrl('mailto:hello@frontify.com')).to.be.true;
+    });
+
+    it('//this-could-be-a-long-domain.com', () => {
+        expect(isValidUrl('//this-could-be-a-long-domain.com')).to.be.false;
+    });
+
+    it('https://', () => {
+        expect(isValidUrl('https://')).to.be.false;
+    });
+
     it('loremipsumdolorsitamet', () => {
         expect(isValidUrl('loremipsumdolorsitamet')).to.be.false;
     });
 
-    it('mailto:hello@frontify.com', () => {
-        expect(isValidUrl('mailto:hello@frontify.com')).to.be.true;
+    it('anotherlongbranddomaintoinordertonetgetflaggedforcodeduplication.com', () => {
+        expect(isValidUrl('anotherlongbranddomaintoinordertonetgetflaggedforcodeduplication.com')).to.be.false;
     });
 
     it('empty', () => {
