6295ac7a3d283dadcd3e384a466734006e34cd95
triggertoo <triggertoo@gmail.com>
2023-02-17 09:45:30 +0100

    apply column breaks ath the end of the selection

--- src/components/RichTextEditor/Plugins/ColumnBreakPlugin/onKeyDownColumnBreak.ts
+++ src/components/RichTextEditor/Plugins/ColumnBreakPlugin/onKeyDownColumnBreak.ts
@@ -1,7 +1,15 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { KeyboardHandlerReturnType, PlateEditor, getPreventDefaultHandler, someNode } from '@udecode/plate';
+import {
+    KeyboardHandlerReturnType,
+    PlateEditor,
+    getNodeEntries,
+    getPreventDefaultHandler,
+    isBlock,
+    someNode,
+} from '@udecode/plate';
 import isHotkey from 'is-hotkey';
+import { Location } from 'slate';
 import { KEY_ELEMENT_BREAK_AFTER } from './createColumnBreakPlugin';
 import { getColumnBreakCount } from './utils/getColumnBreakCount';
 import { setColumnBreaks } from './utils/setColumnBreaks';
@@ -9,16 +17,31 @@ import { updateColumnBreaks } from './utils/updateColumnBreaks';
 
 export const toggleColumnBreak = (editor: PlateEditor, columns: number, event: React.BaseSyntheticEvent) => {
     const isActive = !!editor?.selection && someNode(editor, { match: (node) => !!node.breakAfterColumn });
-    const isInsert = !isActive;
 
-    if (isInsert && getColumnBreakCount(editor) + 1 >= columns) {
-        return;
+    if (isActive) {
+        setColumnBreaks(editor, { value: undefined });
+    } else {
+        const columnBreakCount = getColumnBreakCount(editor);
+        const toggleRange = Math.max(columns - columnBreakCount - 1, 0);
+
+        if (toggleRange === 0) {
+            return;
+        }
+
+        const elementsToToggle = Array.from(
+            getNodeEntries(editor, {
+                at: editor.selection as Location,
+                match: (node) => isBlock(editor, node),
+                mode: 'lowest',
+            }),
+        ).slice(-toggleRange); // apply column breaks ath the end of the selection
+
+        getPreventDefaultHandler(() => {
+            for (const element of elementsToToggle) {
+                setColumnBreaks(editor, { value: 'active', at: element[1] });
+            }
+        })(event);
     }
-
-    getPreventDefaultHandler(setColumnBreaks, editor, {
-        value: isActive ? undefined : 'active',
-        key: KEY_ELEMENT_BREAK_AFTER,
-    })(event);
 
     updateColumnBreaks(editor, columns);
 };
