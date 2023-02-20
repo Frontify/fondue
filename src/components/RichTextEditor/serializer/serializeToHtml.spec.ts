d80d77d3d682c82b27fe8ff6a9dc28a56f833386
triggertoo <triggertoo@gmail.com>
2023-02-21 00:15:26 +0100

    add more tests

--- src/components/RichTextEditor/serializer/serializeToHtml.spec.ts
+++ src/components/RichTextEditor/serializer/serializeToHtml.spec.ts
@@ -1,21 +1,36 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import {
-    customDesignTokens,
-    nodesToSerialize,
-    serializedHtml,
-    serializedHtmlWithCustomDesignTokens,
-} from '../helpers/exampleValues';
+import { nodesToSerialize } from '../helpers/exampleValues';
 import { serializeNodesToHtml } from './serializeToHtml';
 
 describe('serializeNodesToHtml()', () => {
-    it('should serialize nodes to html', () => {
+    it('should serialize nodes to html string', () => {
         const serialized = serializeNodesToHtml(nodesToSerialize);
-        expect(serialized).to.equal(serializedHtml);
+        expect(serialized).to.be.a('string');
     });
 
-    it('should serialize nodes to html with custom design tokens', () => {
-        const serialized = serializeNodesToHtml(nodesToSerialize, { designTokens: customDesignTokens });
-        expect(serialized).to.equal(serializedHtmlWithCustomDesignTokens);
+    it('should render columns if there is more than one given', () => {
+        const serialized = serializeNodesToHtml(nodesToSerialize, { columns: 2 });
+        expect(serialized).to.contain('columns:2;');
+    });
+
+    it('should not render columns if there is only one given', () => {
+        const serialized = serializeNodesToHtml(nodesToSerialize, { columns: 1 });
+        expect(serialized).not.to.contain('columns:1;');
+    });
+
+    it('should have a default column count of 1', () => {
+        const serialized = serializeNodesToHtml(nodesToSerialize);
+        expect(serialized).not.to.contain('columns:1;');
+    });
+
+    it('should render column gap if there is more than one column', () => {
+        const serialized = serializeNodesToHtml(nodesToSerialize, { columns: 2, columnGap: '10px' });
+        expect(serialized).to.contain('column-gap:10px;');
+    });
+
+    it('should have a default column gap of normal', () => {
+        const serialized = serializeNodesToHtml(nodesToSerialize, { columns: 2 });
+        expect(serialized).to.contain('column-gap:normal');
     });
 });
