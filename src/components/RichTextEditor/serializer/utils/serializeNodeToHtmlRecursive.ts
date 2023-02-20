0b642b21e0c8bcf11f7b4e0c94c74f9fb575428f
triggertoo <triggertoo@gmail.com>
2023-02-20 22:13:10 +0100

    disable checkboxes

--- src/components/RichTextEditor/serializer/utils/serializeNodeToHtmlRecursive.ts
+++ src/components/RichTextEditor/serializer/utils/serializeNodeToHtmlRecursive.ts
@@ -153,9 +153,11 @@ export const serializeNodeToHtmlRecursive = (
                 ((node.indent as number) ?? 0) * 24
             }px">
                 <div class="tw-flex tw-items-center tw-justify-center tw-select-none tw-mr-1.5">
-                    <input class="${defaultClassNames} tw-w-4 tw-h-4 tw-m-0" type="checkbox" ${
-                node.checked ? 'checked' : ''
-            }/>
+                    <input 
+                        class="${defaultClassNames} tw-w-4 tw-h-4 tw-m-0"
+                        type="checkbox"
+                        ${node.checked ? 'checked' : ''}
+                        onclick="return false;" />
                 </div>
                 <span class="${merge([
                     'tw-flex-1 tw-focus:outline-none',
