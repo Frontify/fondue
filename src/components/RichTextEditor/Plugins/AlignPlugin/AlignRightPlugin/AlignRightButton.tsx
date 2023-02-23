4340df9da571f1c53f6851f36524f33526afe714
triggertoo <triggertoo@gmail.com>
2023-02-23 15:26:17 +0100

    feat(RichTextEditor): Add tooltips

--- src/components/RichTextEditor/Plugins/AlignPlugin/AlignRightPlugin/AlignRightButton.tsx
+++ src/components/RichTextEditor/Plugins/AlignPlugin/AlignRightPlugin/AlignRightButton.tsx
@@ -1,14 +1,16 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import React from 'react';
-import { AlignToolbarButton } from '@udecode/plate';
+import { getTooltip } from '@components/RichTextEditor/helpers/getTooltip';
 import { IconSize, IconTextAlignmentRight } from '@foundation/Icon';
-import { PluginButtonProps } from '../../types';
+import { AlignToolbarButton } from '@udecode/plate';
+import React from 'react';
 import { ButtonWrapper, IconStylingWrapper, buttonClassNames, buttonStyles } from '../../helper';
+import { PluginButtonProps } from '../../types';
 
 export const AlignRightButton = ({ id }: PluginButtonProps) => (
     <ButtonWrapper id={id}>
         <AlignToolbarButton
+            tooltip={getTooltip('Align right')}
             value="right"
             icon={<IconStylingWrapper icon={<IconTextAlignmentRight size={IconSize.Size16} />} />}
             classNames={buttonClassNames}
--- src/components/RichTextEditor/Plugins/UnderlinePlugin/UnderlineButton.tsx
+++ src/components/RichTextEditor/Plugins/UnderlinePlugin/UnderlineButton.tsx
@@ -1,14 +1,17 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import React from 'react';
-import { MarkToolbarButton, getPluginType } from '@udecode/plate';
+import { getHotkeyByPlatform } from '@components/RichTextEditor/helpers/getHotkeyByPlatform';
+import { getTooltip } from '@components/RichTextEditor/helpers/getTooltip';
 import { IconSize, IconTextFormatUnderline } from '@foundation/Icon';
+import { MarkToolbarButton, getPluginType } from '@udecode/plate';
+import React from 'react';
 import { ButtonWrapper, IconStylingWrapper, buttonClassNames, buttonStyles } from '../helper';
 import { PluginButtonProps } from '../types';
 
 export const UnderlineButton = ({ editor, id }: PluginButtonProps) => (
     <ButtonWrapper id={id}>
         <MarkToolbarButton
+            tooltip={getTooltip(`Underline\n${getHotkeyByPlatform('Ctrl+U')}`)}
             type={getPluginType(editor, id)}
             icon={<IconStylingWrapper icon={<IconTextFormatUnderline size={IconSize.Size16} />} />}
             classNames={buttonClassNames}
