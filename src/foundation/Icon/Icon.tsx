84c10f0f28d3902efc2dd463d12737775713bf26
imoutaharik <ilham.moutaharik@frontify.com>
2023-02-28 14:29:31 +0100

    chore: test other path

--- src/foundation/Icon/Icon.tsx
+++ src/foundation/Icon/Icon.tsx
@@ -5,7 +5,7 @@ import { IconProps } from './IconProps';
 import { IconIcon } from '@foundation/Icon/Generated';
 
 export const Icon: FC<IconProps> = ({ size, icon }) => {
-    const path = `../../foundation/Icon/Generated/Icon${icon}`;
+    const path = `./Generated/Icon${icon}`;
     const IconComponent = lazy(() => import(`${path}`));
     return (
         <Suspense fallback={<IconIcon size={size} />}>
