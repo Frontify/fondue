3f6f1a0ddd85e8cf33e8cf19eb7672f52405b7c9
Samuel Alev <samuel.alev@frontify.com>
2023-03-01 12:29:44 +0100

    refactor!: various changes related to icons Removed `<Icon />`, `iconsMap`& `AllIcons` Changed script to generate only the enum Move lint of generated file closer

--- src/foundation/Icon/index.ts
+++ src/foundation/Icon/index.ts
@@ -1,11 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 export * from './Generated';
-export * from './Icon';
 export * from './IconEnum';
 export * from './IconProps';
 export * from './IconSize';
-export * from './IconsMap';
-
-import * as AllIcons from './Generated';
-export { AllIcons };
