13e54200c1e382720441ff5366ad467c2d19e0eb
Sam Creasey <sam.creasey@frontify.com>
2023-03-06 11:05:57 +0100

    apply alternative classname to treeitem

--- src/components/Tree/TreeItem.tsx
+++ src/components/Tree/TreeItem.tsx
@@ -230,7 +230,7 @@ export const TreeItem = ({
                     className={merge([
                         treeState.selectedIds.has(id)
                             ? 'tw-font-medium tw-bg-box-selected-strong tw-text-box-selected-strong-inverse hover:tw-bg-box-selected-strong-hover hover:tw-text-box-selected-strong-inverse-hover'
-                            : 'tw-text-text hover:tw-bg-box-neutral-hover hover:tw-text-box-neutral-inverse-hover',
+                            : 'tw-text-text hover:tw-bg-base-alt hover:tw-text-box-neutral-inverse-hover',
                         FOCUS_VISIBLE_STYLE,
                         'tw-leading-5 tw-no-underline tw-flex',
                     ])}
