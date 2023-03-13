2b7ebef797ee0c5814eccd8bc8c80c18c52c57e3
Celine <celine@smartive.ch>
2023-03-13 21:53:08 +0100

    feat(RTE): checklist with textstyles

--- src/components/RichTextEditor/Plugins/CheckboxListPlugin/CheckboxListElement.tsx
+++ src/components/RichTextEditor/Plugins/CheckboxListPlugin/CheckboxListElement.tsx
@@ -6,13 +6,18 @@ import { PlateRenderElementProps, TTodoListItemElement, getRootProps, setNodes }
 import { merge } from '@utilities/merge';
 import { MarkupElement } from '../MarkupElement';
 import { ELEMENT_CHECK_ITEM } from './id';
+import { useRichTextEditorContext } from '@components/RichTextEditor/context/RichTextEditorContext';
+import { getTextStyle } from '../ListPlugin/ListItemContentMarkupElement';
 
 export const CheckboxListElementNode = (props: PlateRenderElementProps) => {
     const { attributes, children, nodeProps, element, editor } = props;
     const rootProps = getRootProps(props);
     const checked = element.checked as boolean;
+    const { designTokens } = useRichTextEditorContext();
+    const tokenStyles = designTokens[getTextStyle(element.children[0])];
+
     return (
-        <div {...attributes} {...rootProps} className="tw-flex tw-flex-row tw-pb-2">
+        <div {...attributes} {...rootProps} style={tokenStyles} className="tw-flex tw-flex-row tw-pb-2">
             <div contentEditable={false} className="tw-flex tw-items-center tw-justify-center tw-select-none tw-mr-1.5">
                 <input
                     data-test-id="checkbox-input"
--- src/components/RichTextEditor/Plugins/TextStylePlugin/TextStyleDropdown/useSelectedTextStyles.ts
+++ src/components/RichTextEditor/Plugins/TextStylePlugin/TextStyleDropdown/useSelectedTextStyles.ts
@@ -1,28 +1,7 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import {
-    ELEMENT_LIC,
-    ENode,
-    PlateEditor,
-    TElement,
-    Value,
-    getNodeEntries,
-    isElement,
-    isNode,
-    isText,
-    isType,
-} from '@udecode/plate';
-import { MARK_TEXT_STYLE } from '../../ListPlugin/ListPlugin';
-import { AVAILABLE_STYLE, AvailableStyles, TextStyles } from '../TextStyles';
-
-const getLicStyle = (node: ENode<Value>): AvailableStyles => {
-    if (Array.isArray(node.children)) {
-        const textNode = node.children.find((node) => isText(node)) ?? {};
-        return textNode[MARK_TEXT_STYLE] ?? TextStyles.ELEMENT_PARAGRAPH;
-    }
-
-    return TextStyles.ELEMENT_PARAGRAPH;
-};
+import { PlateEditor, getNodeEntries, isNode } from '@udecode/plate';
+import { AvailableStyles, TextStyles } from '../TextStyles';
 
 export const useSelectedTextStyles = (editor: PlateEditor): AvailableStyles[] => {
     if (!editor || !editor.selection) {
@@ -35,7 +14,6 @@ export const useSelectedTextStyles = (editor: PlateEditor): AvailableStyles[] =>
             mode: 'lowest',
             at: editor.selection,
             reverse: true,
-            match: (node: TElement) => isElement(node) && isType(editor, node, AVAILABLE_STYLE),
         }),
     );
 
@@ -43,13 +21,12 @@ export const useSelectedTextStyles = (editor: PlateEditor): AvailableStyles[] =>
         if (!isNode(node)) {
             return styles;
         }
-
-        const styleToAdd = node.type === ELEMENT_LIC ? getLicStyle(node) : (node.type as AvailableStyles);
+        const styleToAdd = node?.textStyle as AvailableStyles;
 
         if (styleToAdd && !styles.includes(styleToAdd)) {
             styles.push(styleToAdd);
         }
 
-        return styles;
+        return styles.length > 0 ? styles : [TextStyles.ELEMENT_PARAGRAPH];
     }, []);
 };
