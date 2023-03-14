700ba4a237c2c6a9321555ccf8144debf8cdffb2
Goran Ribic <goran.ribic@frontify.com>
2023-03-14 16:18:20 +0100

    fix: export error

--- src/components/Tree/utils/index.ts
+++ src/components/Tree/utils/index.ts
@@ -4,6 +4,5 @@ export * from './cloneThroughFragment';
 export * from './collisionDetection';
 export * from './flattenChildren';
 export * from './isInsideOfElement';
-export * from './keyboardCoordinates';
 export * from './mocks';
 export * from './modifiers';
