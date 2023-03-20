369ecc80c0968b0dabe0742e60799d3411c5edac
Celine <celine@smartive.ch>
2023-03-20 11:51:41 +0100

    fix: css warning

--- src/components/RichTextEditor/Plugins/ListPlugin/ListItemMarkupElement.tsx
+++ src/components/RichTextEditor/Plugins/ListPlugin/ListItemMarkupElement.tsx
@@ -7,7 +7,8 @@ import React from 'react';
 import { MarkupElement } from '../MarkupElement';
 import { getTextStyle } from './ListItemContentMarkupElement';
 
-export const LI_CLASSNAMES = '[&>]:tw-flex [&>]:tw-justify-end [&>]:tw-w-[1.2em] !tw-no-underline';
+export const LI_CLASSNAMES =
+    '[&>p]:before:tw-flex [&>p]:before:tw-justify-end [&>p]:before:tw-w-[1.2em] !tw-no-underline';
 export const getLiStyles = (designTokens: DesignTokens, element: TElement) => {
     const licElement = (element.children[0]?.children as TDescendant[])?.[0];
     const tokenStyles = designTokens[getTextStyle(licElement)];
