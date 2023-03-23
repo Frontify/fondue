48e18f193bcba1fcb58aeb5a97820436ee1e089c
Nikola Milenković <nikola.milenkovic@frontify.com>
2023-03-23 10:47:16 +0100

    feat(TreeItem): Add new focus styles (#1329)

--- src/utilities/focusStyle.ts
+++ src/utilities/focusStyle.ts
@@ -6,3 +6,4 @@ export const FOCUS_STYLE = 'tw-ring-4 tw-ring-blue tw-ring-offset-2 dark:tw-ring
 export const FOCUS_VISIBLE_STYLE =
     'focus-visible:tw-ring-4 focus-visible:tw-ring-blue focus-visible:tw-ring-offset-2 focus-visible:dark:tw-ring-offset-black focus-visible:tw-outline-none';
 export const FOCUS_STYLE_INSET = merge([FOCUS_STYLE, 'tw-ring-inset']);
+export const FOCUS_VISIBLE_STYLE_INSET = merge([FOCUS_VISIBLE_STYLE, 'tw-ring-inset']);
