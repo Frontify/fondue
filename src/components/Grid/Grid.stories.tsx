3fbab23dd7f7b4922a79efa105c87c33c6fa478c
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2023-03-28 13:48:06 +0200

    Saving progress

---
+++ src/components/Grid/Grid.stories.tsx
@@ -0,0 +1,79 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { Meta, StoryFn } from '@storybook/react';
+import React, { useEffect, useState } from 'react';
+import { IconExclamationMarkCircle } from '@foundation/Icon/Generated';
+import { Switch, SwitchLabelStyle, SwitchProps, SwitchSize } from './Switch';
+import { TooltipIconTriggerStyle } from '@components/TooltipIcon';
+
+export default {
+    title: 'Components/Switch',
+    component: Switch,
+    tags: ['autodocs'],
+    argTypes: {
+        size: {
+            options: Object.values(SwitchSize),
+            control: { type: 'radio' },
+        },
+        labelStyle: {
+            options: Object.values(SwitchLabelStyle),
+            control: { type: 'radio' },
+        },
+        label: {
+            placeholder: 'Switch label',
+            type: { name: 'string', required: false },
+            control: {
+                type: 'text',
+            },
+        },
+    },
+    args: {
+        on: true,
+        disabled: false,
+        labelStyle: SwitchLabelStyle.Default,
+        hug: false,
+        name: 'switch-name',
+        size: SwitchSize.Small,
+    },
+} as Meta<SwitchProps>;
+
+type Props = SwitchProps & { hug?: boolean };
+
+const Default: StoryFn<Props> = (args: Props) => {
+    const [on, setOn] = useState(args.on);
+    const toggle = () => setOn(!on);
+
+    useEffect(() => {
+        setOn(args.on);
+    }, [args.on]);
+
+    return <Switch {...args} onChange={toggle} on={on} />;
+};
+
+export const Small: StoryFn<Props> = Default.bind({});
+Small.args = { size: SwitchSize.Small };
+
+export const Medium: StoryFn<Props> = Default.bind({});
+Medium.args = { size: SwitchSize.Medium };
+
+export const WithLabel: StoryFn<Props> = Default.bind({});
+WithLabel.args = { label: 'Switch label' };
+
+export const WithLabelAndTooltip: StoryFn<Props> = Default.bind({});
+WithLabelAndTooltip.args = { label: 'Switch label', tooltip: { content: 'Switch tooltip' } };
+
+export const WithLabelAndMultipleTooltips: StoryFn<Props> = Default.bind({});
+WithLabelAndMultipleTooltips.args = {
+    label: 'Switch label',
+    tooltip: [
+        { content: 'First tooltip' },
+        {
+            content: 'Second Tooltip',
+            triggerIcon: <IconExclamationMarkCircle />,
+            triggerStyle: TooltipIconTriggerStyle.Warning,
+        },
+    ],
+};
+
+export const HugLabel: StoryFn<Props> = Default.bind({});
+HugLabel.args = { hug: true, label: 'Switch label' };
---
+++ src/components/Grid/Grid.tsx
@@ -0,0 +1,142 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import React, { FC, MouseEvent, useMemo } from 'react';
+import { merge } from '@utilities/merge';
+import { useFocusRing } from '@react-aria/focus';
+import { FOCUS_STYLE } from '@utilities/focusStyle';
+import { useMemoizedId } from '@hooks/useMemoizedId';
+import { InputLabel, InputLabelTooltipProps } from '@components/InputLabel/InputLabel';
+
+export const SWITCH_ID = 'switch-container';
+
+export enum SwitchSize {
+    Small = 'Small',
+    Medium = 'Medium',
+}
+
+export enum SwitchLabelStyle {
+    Default = 'Default',
+    Heading = 'Heading',
+}
+
+const trackSizeClasses: Record<SwitchSize, string> = {
+    [SwitchSize.Small]: 'tw-w-[22px] tw-h-3',
+    [SwitchSize.Medium]: 'tw-w-[30px] tw-h-4',
+};
+
+const dotSizeClasses: Record<SwitchSize, Record<'dimensions' | 'activeWidth' | 'activeTranslation', string>> = {
+    [SwitchSize.Small]: {
+        dimensions: 'tw-w-3 tw-h-3',
+        activeWidth: 'group-active:tw-w-[14px]',
+        activeTranslation: 'tw-translate-x-[9px]',
+    },
+    [SwitchSize.Medium]: {
+        dimensions: 'tw-w-4 tw-h-4',
+        activeWidth: 'group-active:tw-w-[18px]',
+        activeTranslation: 'tw-translate-x-[13px]',
+    },
+};
+
+export type SwitchProps = {
+    id?: string;
+    on?: boolean;
+    disabled?: boolean;
+    label?: string;
+    labelStyle?: string;
+    name?: string;
+    size?: SwitchSize;
+    hug?: boolean;
+    tooltip?: InputLabelTooltipProps;
+    onChange?: (e: MouseEvent) => void;
+};
+
+export const Switch: FC<SwitchProps> = ({
+    id: propId,
+    name,
+    label,
+    disabled,
+    onChange,
+    size = SwitchSize.Medium,
+    labelStyle = SwitchLabelStyle.Default,
+    on = false,
+    hug = false,
+    tooltip,
+}) => {
+    const id = useMemoizedId(propId);
+    const { isFocusVisible, focusProps } = useFocusRing();
+
+    // The track on which the dot moves
+    const trackClasses = useMemo(() => {
+        const baseClasses = 'tw-group tw-border tw-inline-flex tw-rounded-full tw-shrink-0 tw-p-0 tw-transition-colors';
+
+        const valueClasses = on
+            ? 'tw-bg-text-weak tw-border-line-xx-strong hover:tw-bg-text'
+            : 'tw-bg-box-neutral tw-border-line-x-strong hover:tw-bg-box-neutral-hover';
+
+        const disabledClasses = disabled ? 'tw-bg-box-disabled tw-border-line tw-pointer-events-none' : valueClasses;
+
+        return merge([baseClasses, disabledClasses, trackSizeClasses[size], isFocusVisible && FOCUS_STYLE]);
+    }, [on, disabled, size, isFocusVisible]);
+
+    // Responsible for the left-right translation
+    const dotWrapperClasses = useMemo(() => {
+        const baseClasses = 'tw-relative tw-self-center tw-transition-transform';
+
+        const valueClasses = on && dotSizeClasses[size].activeTranslation;
+
+        return merge([baseClasses, dotSizeClasses[size].dimensions, valueClasses]);
+    }, [on, size]);
+
+    // Responsible for dot styling and width animation on hover
+    const dotClasses = useMemo(() => {
+        const baseClasses =
+            'tw-border tw-bg-base tw-rounded-full tw-absolute tw-block tw-self-center tw-transition-width tw-duration-75';
+
+        const valueClasses = on ? 'tw-right-0' : 'tw-left-0 -tw-translate-x-px';
+
+        const disabledStateClasses = disabled
+            ? 'tw-border-line-strong'
+            : merge(['tw-bg-base tw-border-line-xx-strong', dotSizeClasses[size].activeWidth]);
+
+        return merge([baseClasses, dotSizeClasses[size].dimensions, valueClasses, disabledStateClasses]);
+    }, [on, disabled, size]);
+
+    // Wraps the InputLabel instance and switch element
+    const containerClasses = useMemo(() => {
+        return merge(['tw-gap-2 tw-items-center tw-justify-between', hug ? 'tw-inline-flex' : 'tw-flex']);
+    }, [hug]);
+
+    return (
+        <div className={containerClasses} data-test-id={SWITCH_ID}>
+            {label && (
+                <InputLabel clickable={true} htmlFor={id} disabled={disabled} tooltip={tooltip}>
+                    {labelStyle === SwitchLabelStyle.Default ? (
+                        <span data-test-id="switch-label-wrapper" className="tw-font-medium tw-text-text-weak">
+                            {label}
+                        </span>
+                    ) : (
+                        <span data-test-id="switch-label-wrapper" className="tw-font-bold tw-text-text">
+                            {label}
+                        </span>
+                    )}
+                </InputLabel>
+            )}
+            <button
+                {...focusProps}
+                id={id}
+                disabled={disabled}
+                name={name}
+                data-test-id="switch"
+                className={trackClasses}
+                value={on.toString()}
+                onClick={onChange}
+                type="button"
+            >
+                <div className={dotWrapperClasses}>
+                    <div className={dotClasses} />
+                </div>
+            </button>
+        </div>
+    );
+};
+Switch.displayName = 'FondueSwitch';
