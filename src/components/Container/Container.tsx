1d7b8472232a5a3a01755f69768ead5ed6a8e276
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2023-03-28 17:02:41 +0200

    Adding padding class map

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -13,14 +13,29 @@ export type ContainerProps = {
 
 export const Container = ({ children, minWidth, maxWidth, padding = 0, ...props }: ContainerProps) => {
     const acceptablePaddingInput: PaddingType[] = [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40];
+    const paddingMap = {
+        0: 'tw-p-0',
+        4: 'tw-p-1',
+        8: 'tw-p-2',
+        12: 'tw-p-3',
+        16: 'tw-p-4',
+        20: 'tw-p-5',
+        24: 'tw-p-6',
+        28: 'tw-p-7',
+        32: 'tw-p-8',
+        36: 'tw-p-9',
+        40: 'tw-p-10',
+    };
+
+    const paddingClassName = acceptablePaddingInput.includes(padding) ? paddingMap[padding] : paddingMap[0];
 
     return (
         <div
             data-test-id={props['data-test-id'] ? props['data-test-id'] : 'fondue-container'}
+            className={paddingClassName}
             style={{
                 maxWidth,
                 minWidth,
-                padding: acceptablePaddingInput.includes(padding) ? padding : 0,
             }}
         >
             {children}
