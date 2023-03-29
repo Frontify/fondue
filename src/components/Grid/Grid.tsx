5db3908e6891d30d5bcd77654bae7911688c8b49
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2023-03-29 12:25:33 +0200

    Saving progress

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -7,136 +7,30 @@ import { FOCUS_STYLE } from '@utilities/focusStyle';
 import { useMemoizedId } from '@hooks/useMemoizedId';
 import { InputLabel, InputLabelTooltipProps } from '@components/InputLabel/InputLabel';
 
-export const SWITCH_ID = 'switch-container';
+export type GridSpacingX = 0 | 4 | 8 | 12 | 16 | 20 | 24 | 28 | 32 | 36 | 40;
+export type GridSpacingY = 0 | 4 | 8 | 12 | 16 | 20 | 24 | 28 | 32 | 36 | 40;
 
-export enum SwitchSize {
-    Small = 'Small',
-    Medium = 'Medium',
-}
-
-export enum SwitchLabelStyle {
-    Default = 'Default',
-    Heading = 'Heading',
-}
-
-const trackSizeClasses: Record<SwitchSize, string> = {
-    [SwitchSize.Small]: 'tw-w-[22px] tw-h-3',
-    [SwitchSize.Medium]: 'tw-w-[30px] tw-h-4',
+export type GridProps = {
+    column?: number;
+    spacingX?: GridSpacingX;
+    spacingY?: GridSpacingY;
 };
 
-const dotSizeClasses: Record<SwitchSize, Record<'dimensions' | 'activeWidth' | 'activeTranslation', string>> = {
-    [SwitchSize.Small]: {
-        dimensions: 'tw-w-3 tw-h-3',
-        activeWidth: 'group-active:tw-w-[14px]',
-        activeTranslation: 'tw-translate-x-[9px]',
-    },
-    [SwitchSize.Medium]: {
-        dimensions: 'tw-w-4 tw-h-4',
-        activeWidth: 'group-active:tw-w-[18px]',
-        activeTranslation: 'tw-translate-x-[13px]',
-    },
-};
+export const Grid = ({ column = 2, spacingX = 0, spacingY = 0 }: GridProps) => {
+    const columnMapping = {
+        1: 'tw-basis',
+        2: 'tw-basis-1/2',
+        3: 'tw-basis-1/3',
+        4: 'tw-basis-1/4',
+        5: 'tw-basis-1/5',
+        6: 'tw-basis-1/6',
+    };
 
-export type SwitchProps = {
-    id?: string;
-    on?: boolean;
-    disabled?: boolean;
-    label?: string;
-    labelStyle?: string;
-    name?: string;
-    size?: SwitchSize;
-    hug?: boolean;
-    tooltip?: InputLabelTooltipProps;
-    onChange?: (e: MouseEvent) => void;
-};
-
-export const Grid: FC<GridProps> = ({
-    id: propId,
-    name,
-    label,
-    disabled,
-    onChange,
-    size = SwitchSize.Medium,
-    labelStyle = SwitchLabelStyle.Default,
-    on = false,
-    hug = false,
-    tooltip,
-}) => {
-    const id = useMemoizedId(propId);
-    const { isFocusVisible, focusProps } = useFocusRing();
-
-    // The track on which the dot moves
-    const trackClasses = useMemo(() => {
-        const baseClasses = 'tw-group tw-border tw-inline-flex tw-rounded-full tw-shrink-0 tw-p-0 tw-transition-colors';
-
-        const valueClasses = on
-            ? 'tw-bg-text-weak tw-border-line-xx-strong hover:tw-bg-text'
-            : 'tw-bg-box-neutral tw-border-line-x-strong hover:tw-bg-box-neutral-hover';
-
-        const disabledClasses = disabled ? 'tw-bg-box-disabled tw-border-line tw-pointer-events-none' : valueClasses;
-
-        return merge([baseClasses, disabledClasses, trackSizeClasses[size], isFocusVisible && FOCUS_STYLE]);
-    }, [on, disabled, size, isFocusVisible]);
-
-    // Responsible for the left-right translation
-    const dotWrapperClasses = useMemo(() => {
-        const baseClasses = 'tw-relative tw-self-center tw-transition-transform';
-
-        const valueClasses = on && dotSizeClasses[size].activeTranslation;
-
-        return merge([baseClasses, dotSizeClasses[size].dimensions, valueClasses]);
-    }, [on, size]);
-
-    // Responsible for dot styling and width animation on hover
-    const dotClasses = useMemo(() => {
-        const baseClasses =
-            'tw-border tw-bg-base tw-rounded-full tw-absolute tw-block tw-self-center tw-transition-width tw-duration-75';
-
-        const valueClasses = on ? 'tw-right-0' : 'tw-left-0 -tw-translate-x-px';
-
-        const disabledStateClasses = disabled
-            ? 'tw-border-line-strong'
-            : merge(['tw-bg-base tw-border-line-xx-strong', dotSizeClasses[size].activeWidth]);
-
-        return merge([baseClasses, dotSizeClasses[size].dimensions, valueClasses, disabledStateClasses]);
-    }, [on, disabled, size]);
-
-    // Wraps the InputLabel instance and switch element
-    const containerClasses = useMemo(() => {
-        return merge(['tw-gap-2 tw-items-center tw-justify-between', hug ? 'tw-inline-flex' : 'tw-flex']);
-    }, [hug]);
 
     return (
-        <div className={containerClasses} data-test-id={SWITCH_ID}>
-            {label && (
-                <InputLabel clickable={true} htmlFor={id} disabled={disabled} tooltip={tooltip}>
-                    {labelStyle === SwitchLabelStyle.Default ? (
-                        <span data-test-id="switch-label-wrapper" className="tw-font-medium tw-text-text-weak">
-                            {label}
-                        </span>
-                    ) : (
-                        <span data-test-id="switch-label-wrapper" className="tw-font-bold tw-text-text">
-                            {label}
-                        </span>
-                    )}
-                </InputLabel>
-            )}
-            <button
-                {...focusProps}
-                id={id}
-                disabled={disabled}
-                name={name}
-                data-test-id="switch"
-                className={trackClasses}
-                value={on.toString()}
-                onClick={onChange}
-                type="button"
-            >
-                <div className={dotWrapperClasses}>
-                    <div className={dotClasses} />
-                </div>
-            </button>
-        </div>
+        <div className={
+            merge([columnMapping[column], ])
+        }
     );
 };
 Grid.displayName = 'FondueGrid';
