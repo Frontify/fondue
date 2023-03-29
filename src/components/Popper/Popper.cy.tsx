703262e8875f259db6951c6ca4c1ca0f9ed43c22
imoutaharik <ilham.moutaharik@frontify.com>
2023-03-29 18:12:29 +0200

    feat(overlays): add Popper component

---
+++ src/components/Popper/Popper.cy.tsx
@@ -0,0 +1,32 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import React, { useState } from 'react';
+import { Popper, PopperProps } from './Popper';
+import { Button } from '@components/Button';
+
+const popper_SELECTOR = '[data-test-id=fondue-popper]';
+
+const PopperComponent = ({ placement, offset, flip }: PopperProps) => {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <Popper open={open} placement={placement} offset={offset} flip={flip}>
+            <Popper.Reference>
+                <Button onClick={() => setOpen(!open)}>Hello</Button>
+            </Popper.Reference>
+            <Popper.Content>
+                <div className="tw-border tw-border-black">
+                    <p>Some content</p>
+                </div>
+            </Popper.Content>
+        </Popper>
+    );
+};
+
+describe('InlineDialog Component', () => {
+    it('should render', () => {
+        cy.mount(<PopperComponent placement="bottom" flip={false} offset={[0, 10]} />);
+
+        cy.get(popper_SELECTOR).should('exist');
+    });
+});
---
+++ src/components/Popper/Popper.tsx
@@ -0,0 +1,115 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import React, {
+    Children,
+    ReactElement,
+    ReactNode,
+    cloneElement,
+    createContext,
+    isValidElement,
+    useContext,
+    useEffect,
+    useMemo,
+    useState,
+} from 'react';
+import { usePopper } from 'react-popper';
+import { Portal } from '@components/Portal';
+import { PopperPlacement, PopperProps } from '@components/Popper/types';
+
+const PopperContext = createContext<PopperProps>({});
+
+const Reference = ({ children }: { children: ReactElement }) => {
+    return children;
+};
+
+const Content = ({ children }: { children?: ReactNode }) => {
+    const { open } = usePopperContext();
+    return open ? <>{children}</> : null;
+};
+const usePopperContext = () => {
+    const context = useContext(PopperContext);
+    if (!context) {
+        throw new Error('Popper compound components cannot be rendered outside the Popper component');
+    }
+    return context;
+};
+
+export const Popper = ({
+    children,
+    open,
+    placement = PopperPlacement.BottomStart,
+    offset = [0, 8],
+    flip = false,
+    enablePortal = false,
+}: PopperProps) => {
+    const [referenceElement, setReferenceElement] = useState<HTMLDivElement | null>(null);
+    const [popperElement, setPopperElement] = useState<HTMLElement | null>(null);
+    const popperInstance = usePopper(referenceElement, popperElement, {
+        placement,
+        modifiers: [
+            { name: 'offset', options: { offset } },
+            {
+                name: 'flip',
+                enabled: flip,
+            },
+        ],
+    });
+
+    useEffect(() => {
+        if (popperInstance.update) {
+            popperInstance.update();
+        }
+    }, [flip, placement, offset, open]);
+
+    const value = useMemo(() => ({ open }), [open]);
+    return (
+        <PopperContext.Provider value={value}>
+            {Children.map(children, (child) => {
+                if (isValidElement(child) && typeof child.type === 'function') {
+                    const { name } = child.type;
+
+                    if (name === 'Reference') {
+                        return (
+                            <div className="tw-w-fit" ref={setReferenceElement}>
+                                {cloneElement(child, { ...child.props })}
+                            </div>
+                        );
+                    }
+
+                    if (name === 'Content') {
+                        return (
+                            <>
+                                {enablePortal ? (
+                                    <Portal>
+                                        <div
+                                            ref={setPopperElement}
+                                            style={popperInstance.styles.popper}
+                                            {...popperInstance.attributes.popper}
+                                        >
+                                            {cloneElement(child, {
+                                                ...child.props,
+                                            })}
+                                        </div>
+                                    </Portal>
+                                ) : (
+                                    <div
+                                        ref={setPopperElement}
+                                        style={popperInstance.styles.popper}
+                                        {...popperInstance.attributes.popper}
+                                    >
+                                        {cloneElement(child, {
+                                            ...child.props,
+                                        })}
+                                    </div>
+                                )}
+                            </>
+                        );
+                    }
+                }
+            })}
+        </PopperContext.Provider>
+    );
+};
+Popper.displayName = 'FonduePopper';
+Popper.Reference = Reference;
+Popper.Content = Content;
