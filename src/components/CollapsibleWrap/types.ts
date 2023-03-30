15e7d68745e95069b1aaa8ff78e5e05cbc012f6c
Jonas Büchel <jonas@frontify.com>
2023-03-30 11:18:57 +0200

    Replace React.FC by direct type definitions

--- src/components/CollapsibleWrap/types.ts
+++ src/components/CollapsibleWrap/types.ts
@@ -1,7 +1,10 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import { ReactNode } from 'react';
+
 export type CollapsibleWrapProps = {
     isOpen: boolean;
     preventInitialAnimation?: boolean;
     animateOpacity?: boolean;
+    children?: ReactNode;
 };
