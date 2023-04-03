917f2af4f9be5dc57fbc91b923a5964f18d46ade
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2023-04-03 17:35:43 +0200

    Adding props

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,7 +1,15 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import React, { useEffect } from 'react';
 import { merge } from '@utilities/merge';
-import React from 'react';
+import {
+    DimensionUnities,
+    MARGIN_VALUES_MAP,
+    PADDING_VALUES_MAP,
+    SPACING_VALUES,
+    SpacingValues,
+} from '@utilities/dimensions';
+import { BOX_ALIAS_TOKENS_PREFIX } from '@utilities/tokens';
 
 export type GridSpacingX = 0 | 4 | 8 | 12 | 16 | 20 | 24 | 28 | 32 | 36 | 40;
 export type GridSpacingY = 0 | 4 | 8 | 12 | 16 | 20 | 24 | 28 | 32 | 36 | 40;
@@ -11,11 +19,37 @@ export type GridProps = {
     spacingX?: GridSpacingX;
     spacingY?: GridSpacingY;
     children?: React.ReactNode;
+    minWidth?: `${number}${DimensionUnities}`;
+    maxWidth?: `${number}${DimensionUnities}`;
+    minHeight?: `${number}${DimensionUnities}`;
+    maxHeight?: `${number}${DimensionUnities}`;
+    padding?: SpacingValues;
+    margin?: SpacingValues;
+    boxColorToken?: string;
+    'data-test-id'?: string;
 };
 
-export const Grid = ({ column = 2, spacingX = 4, spacingY = 4, children }: GridProps) => {
+export const CONTAINER_TEST_ID = 'fondue-grid';
+
+export const Grid = ({
+    column = 2,
+    spacingX = 4,
+    spacingY = 4,
+    children,
+    'data-test-id': dataTestId = CONTAINER_TEST_ID,
+    minWidth,
+    maxWidth,
+    maxHeight,
+    minHeight,
+    boxColorToken,
+    margin = 0,
+    padding = 0,
+}: GridProps) => {
     const acceptableSpacingXInput: GridSpacingX[] = [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40];
     const acceptableSpacingYInput: GridSpacingY[] = [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40];
+
+    const paddingClassName = SPACING_VALUES.includes(padding) ? PADDING_VALUES_MAP[padding] : PADDING_VALUES_MAP[0];
+    const marginClassName = SPACING_VALUES.includes(padding) ? MARGIN_VALUES_MAP[margin] : MARGIN_VALUES_MAP[0];
 
     const gridSpacingXMappings = {
         0: 'tw-gap-0',
@@ -52,11 +86,37 @@ export const Grid = ({ column = 2, spacingX = 4, spacingY = 4, children }: GridP
         ? gridSpacingYMappings[spacingY]
         : gridSpacingYMappings[4];
 
+    const colorClasses = !boxColorToken ? '' : `tw-bg-${boxColorToken} tw-text-${boxColorToken}-inverse`;
+
+    useEffect(() => {
+        if (!boxColorToken) {
+            return;
+        }
+
+        if (!BOX_ALIAS_TOKENS_PREFIX.includes(boxColorToken)) {
+            throw new Error(
+                `boxColorToken should be one of the following values \n${BOX_ALIAS_TOKENS_PREFIX.join('\n')}`,
+            );
+        }
+    }, [boxColorToken]);
+
     return (
         <div
-            className={merge(['tw-flex', gridSpacingXClass, gridSpacingYClass])}
+            data-test-id={dataTestId}
+            className={merge([
+                'tw-flex',
+                gridSpacingXClass,
+                gridSpacingYClass,
+                paddingClassName,
+                marginClassName,
+                colorClasses,
+            ])}
             style={{
                 flexBasis: `${(1 / column) * 100}`,
+                maxWidth,
+                minWidth,
+                maxHeight,
+                minHeight,
             }}
         >
             {children}
