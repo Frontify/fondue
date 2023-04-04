3efc86e08970f9429d04a5ad93c8e2e8eb1f16fa
imoutaharik <ilham.moutaharik@frontify.com>
2023-04-04 12:30:29 +0200

    fix: Testing silencing the ResizeObserver loop limit error

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -3,3 +3,11 @@
 import { mount } from 'cypress/react';
 
 Cypress.Commands.add('mount', mount);
+
+//https://github.com/cypress-io/cypress/issues/8418#issuecomment-992564877
+const resizeObserverLoopErrRe = /^[^ ()ORb-eilmopr-tvxz]/;
+Cypress.on('uncaught:exception', (err) => {
+    if (resizeObserverLoopErrRe.test(err.message)) {
+        return false;
+    }
+});
