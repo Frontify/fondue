10350221c1e40b520d5a0161233b8a8fce8fde38
imoutaharik <ilham.moutaharik@frontify.com>
2023-04-24 17:13:46 +0200

    feat: Add popover cypress tests

--- src/components/Popover/Popover.cy.tsx
+++ src/components/Popover/Popover.cy.tsx
@@ -5,19 +5,24 @@ import { Button } from '@components/Button';
 import { PopperPlacement } from '@components/Popper/types';
 import { Popover, PopoverProps } from '@components/Popover/Popover';
 
-const popover_SELECTOR = '[data-test-id=fondue-popover]';
+const POPOVER_SELECTOR = '[data-test-id=fondue-popover]';
+const POPOVER_TRIGGER = '[data-test-id=popover-trigger]';
+const POPOVER_INTERACTIVE_ELEMENT = '[data-test-id=popover-content-button]';
 
 const PopoverComponent = ({ placement, offset, flip }: PopoverProps) => {
-    const [open, setOpen] = useState(true);
+    const [open, setOpen] = useState(false);
 
     return (
         <Popover open={open} placement={placement} offset={offset} flip={flip}>
             <Popover.Trigger>
-                <Button onClick={() => setOpen(!open)}>Hello</Button>
+                <Button data-test-id="popover-trigger" onClick={() => setOpen(!open)}>
+                    Hello
+                </Button>
             </Popover.Trigger>
             <Popover.Content>
-                <div className="tw-border tw-border-black">
+                <div className="tw-p-3">
                     <p>Some content</p>
+                    <Button data-test-id="popover-content-button">Confirm</Button>
                 </div>
             </Popover.Content>
         </Popover>
@@ -25,9 +30,66 @@ const PopoverComponent = ({ placement, offset, flip }: PopoverProps) => {
 };
 
 describe('Popover Component', () => {
-    it('should render', () => {
-        cy.mount(<PopoverComponent placement={PopperPlacement.Bottom} flip={false} offset={[0, 10]} />);
+    it('should render the trigger', () => {
+        cy.mount(<PopoverComponent placement={PopperPlacement.Bottom} />);
+        cy.get(POPOVER_TRIGGER).should('exist');
+    });
 
-        cy.get(popover_SELECTOR).should('exist');
+    it('should render the popover', () => {
+        cy.mount(<PopoverComponent placement={PopperPlacement.Bottom} />);
+        cy.get(POPOVER_TRIGGER).click();
+        cy.get(POPOVER_SELECTOR).should('exist');
+    });
+
+    it('should render the popover in its initial Top placement', () => {
+        cy.mount(<PopoverComponent placement={PopperPlacement.Top} />);
+        cy.get(POPOVER_TRIGGER).click();
+        cy.get(POPOVER_SELECTOR).parent().should('have.attr', 'data-popper-placement', 'top');
+    });
+
+    it('should render the popover with start alignment', () => {
+        cy.mount(<PopoverComponent placement={PopperPlacement.BottomStart} />);
+        cy.get(POPOVER_TRIGGER).click();
+        cy.get(POPOVER_SELECTOR).parent().should('have.attr', 'data-popper-placement', 'bottom-start');
+    });
+
+    it('should render the popover with middle alignment', () => {
+        cy.mount(<PopoverComponent placement={PopperPlacement.Bottom} />);
+        cy.get(POPOVER_TRIGGER).click();
+        cy.get(POPOVER_SELECTOR).parent().should('have.attr', 'data-popper-placement', 'bottom');
+    });
+
+    it('should render the popover with end alignment', () => {
+        cy.mount(<PopoverComponent placement={PopperPlacement.BottomEnd} />);
+        cy.get(POPOVER_TRIGGER).click();
+        cy.get(POPOVER_SELECTOR).parent().should('have.attr', 'data-popper-placement', 'bottom-end');
+    });
+
+    it('should render the popover in its initial Right placement', () => {
+        cy.mount(<PopoverComponent placement={PopperPlacement.Right} />);
+        cy.get(POPOVER_TRIGGER).click();
+        cy.get(POPOVER_SELECTOR).parent().should('have.attr', 'data-popper-placement', 'right');
+    });
+
+    it('should flip if enabled', () => {
+        cy.mount(<PopoverComponent placement={PopperPlacement.Top} flip />);
+        cy.get(POPOVER_TRIGGER).click();
+        cy.get(POPOVER_SELECTOR).parent().should('have.attr', 'data-popper-placement', 'bottom');
+    });
+
+    it('should not flip if disabled', () => {
+        cy.mount(<PopoverComponent placement={PopperPlacement.Top} flip={false} />);
+        cy.get(POPOVER_TRIGGER).click();
+        cy.get(POPOVER_SELECTOR).parent().should('have.attr', 'data-popper-placement', 'top');
+    });
+
+    it('should access interactive element with keyboard', () => {
+        cy.mount(<PopoverComponent placement={PopperPlacement.Bottom} />);
+        cy.get('body').realPress('Tab');
+        cy.get(POPOVER_TRIGGER).should('be.focused');
+        cy.get('body').realPress('Enter');
+        cy.get(POPOVER_SELECTOR).should('exist');
+        cy.get('body').realPress('Tab');
+        cy.get(POPOVER_INTERACTIVE_ELEMENT).should('be.focused');
     });
 });
