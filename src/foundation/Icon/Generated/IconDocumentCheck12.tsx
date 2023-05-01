ba23e4df69956d1f2f0dc4dbfe7bb949ec6428e5
Samuel Alev <samuel.alev@frontify.com>
2023-05-01 22:09:16 +0200

    chore: add missing generated files

--- src/foundation/Icon/Generated/IconDocumentCheck12.tsx
+++ src/foundation/Icon/Generated/IconDocumentCheck12.tsx
@@ -11,21 +11,12 @@ function IconDocumentCheck12(props: GeneratedIconProps): React.ReactElement<Gene
             name="IconDocumentCheck12"
             {...props}
         >
-            <g fill="none">
-                <g clipPath="url(#prefix__a)">
-                    <path
-                        fill="currentColor"
-                        fillRule="evenodd"
-                        d="M2.5 1h3.417v4.083H10V10.5a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5Zm7.178 2.833a1 1 0 0 1 .197.25H6.917v-2.8l2.761 2.55ZM9.5 12h-7A1.5 1.5 0 0 1 1 10.5v-9A1.5 1.5 0 0 1 2.5 0h3.718a2 2 0 0 1 1.357.53l2.782 2.568A2 2 0 0 1 11 4.568V10.5A1.5 1.5 0 0 1 9.5 12Zm-.988-5.076a.5.5 0 0 0-.707-.707L5.197 8.825 4.195 7.823a.5.5 0 1 0-.707.707l1.355 1.355.354.354.354-.354 2.961-2.961Z"
-                        clipRule="evenodd"
-                    />
-                </g>
-                <defs>
-                    <clipPath id="prefix__a">
-                        <path fill="#fff" d="M0 0h12v12H0z" />
-                    </clipPath>
-                </defs>
-            </g>
+            <path
+                fill="currentColor"
+                fillRule="evenodd"
+                d="M2.5 1h3.417v4.083H10V10.5a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5Zm7.178 2.833a1 1 0 0 1 .197.25H6.917v-2.8l2.761 2.55ZM9.5 12h-7A1.5 1.5 0 0 1 1 10.5v-9A1.5 1.5 0 0 1 2.5 0h3.718a2 2 0 0 1 1.357.53l2.782 2.568A2 2 0 0 1 11 4.568V10.5A1.5 1.5 0 0 1 9.5 12Zm-.988-5.076a.5.5 0 0 0-.707-.707L5.197 8.825 4.195 7.823a.5.5 0 1 0-.707.707l1.355 1.355.354.354.354-.354 2.961-2.961Z"
+                clipRule="evenodd"
+            />
         </svg>
     );
 }
