2834a137a289e7aa4f5a39de05809d6b5124197d
leko91 <nikola.milenkovic@frontify.com>
2023-05-05 12:06:48 +0200

    chore: add correct test case descriptions

--- src/components/Tree/Tree.cy.tsx
+++ src/components/Tree/Tree.cy.tsx
@@ -54,79 +54,54 @@ const TREE_ID = '[data-test-id=fondue-tree]';
 const TREE_ITEM_ID = '[data-test-id=fondue-tree-item]';
 const TREE_ITEM_TOGGLE_ID = '[data-test-id=tree-item-toggle]';
 
-// describe('Tree component with uncontrolled onSelect', () => {
-//     beforeEach(() => {
-//         const onSelectTreeStub = cy.stub().as('onSelectTreeStub');
-//         const onSelectTreeItemStub = cy.stub().as('onSelectTreeItemStub');
+describe('Tree component with uncontrolled onSelect', () => {
+    beforeEach(() => {
+        cy.mount(
+            <Tree key="tree-id" id="tree-id">
+                <TreeItem key="item-id" id="item-id" label="Tree Item" />
+            </Tree>,
+        );
+    });
 
-//         cy.mount(
-//             <Tree key="tree-id" id="tree-id">
-//                 <TreeItem key="item-id" id="item-id" label="Tree Item" onSelect={onSelectTreeItemStub} />
-//             </Tree>,
-//         );
-//     });
+    it('renders the Tree with TreeItem correctly', () => {
+        cy.get(TREE_ID).should('be.visible');
+        cy.get(TREE_ITEM_ID).should('be.visible').should('have.length', 1);
+        cy.get(TREE_ITEM_TOGGLE_ID).should('not.be.visible');
+        cy.get(TREE_ITEM_ID).should('have.attr', 'aria-selected', 'false');
+    });
 
-//     it('renders the Tree with TreeItem correctly', () => {
-//         cy.get(TREE_ID).should('be.visible');
-//         cy.get(TREE_ITEM_ID).should('be.visible').should('have.length', 1);
-//         cy.get(TREE_ITEM_TOGGLE_ID).should('not.be.visible');
-//         cy.get(TREE_ITEM_ID).should('have.attr', 'aria-selected', 'false');
-//     });
+    it('selects the correct items', () => {
+        cy.get(TREE_ITEM_ID).click();
 
-//     it('calls the onSelect of the TreeItem', () => {
-//         cy.get(TREE_ITEM_ID).click();
+        cy.get(TREE_ITEM_ID).should('have.attr', 'aria-selected', 'true');
+    });
+});
 
-//         // Figure why this doesn't work
-//         // cy.get('@onSelectTreeItemStub').should('have.been.called');
+describe('Tree component with uncontrolled onExpand and onShrink', () => {
+    beforeEach(() => {
+        cy.mount(
+            <Tree id="tree-id">
+                <TreeItem key="item-id" id="item-id" label="Tree Item">
+                    <TreeItem key="child-item-id" id="child-item-id" label="Child Tree Item" />
+                </TreeItem>
+            </Tree>,
+        );
+    });
 
-//         cy.get(TREE_ITEM_ID).should('have.attr', 'aria-selected', 'true');
-//     });
-// });
+    it('renders the Tree with TreeItem correctly', () => {
+        cy.get(TREE_ID).should('be.visible');
+        cy.get(TREE_ITEM_ID).should('be.visible').should('have.length', 1);
+        cy.get(TREE_ITEM_TOGGLE_ID).should('be.visible');
+    });
 
-// describe('Tree component with uncontrolled onExpand and onShrink', () => {
-//     beforeEach(() => {
-//         const onExpandTreeStub = cy.stub().as('onExpandTreeStub');
-//         const onExpandTreeItemStub = cy.stub().as('onExpandTreeItemStub');
-//         const onShrinkTreeStub = cy.stub().as('onShrinkTreeStub');
-//         const onShrinkTreeItemStub = cy.stub().as('onShrinkTreeItemStub');
+    it('expands and shrinks the correct items', () => {
+        cy.get(TREE_ITEM_TOGGLE_ID).click();
+        cy.get(TREE_ITEM_ID).should('have.length', 2);
 
-//         cy.mount(
-//             <Tree id="tree-id" onExpand={onExpandTreeStub} onShrink={onShrinkTreeStub}>
-//                 <TreeItem
-//                     key="item-id"
-//                     id="item-id"
-//                     label="Tree Item"
-//                     // onExpand={onExpandTreeItemStub}
-//                     // onShrink={onShrinkTreeItemStub}
-//                 >
-//                     <TreeItem key="child-item-id" id="child-item-id" label="Child Tree Item" />
-//                 </TreeItem>
-//             </Tree>,
-//         );
-//     });
-
-//     it('renders the Tree with TreeItem correctly', () => {
-//         cy.get(TREE_ID).should('be.visible');
-//         cy.get(TREE_ITEM_ID).should('be.visible').should('have.length', 1);
-//         cy.get(TREE_ITEM_TOGGLE_ID).should('be.visible');
-//     });
-
-//     it('calls the onExpand and onShrink of the TreeItem', () => {
-//         cy.get(TREE_ITEM_TOGGLE_ID).click();
-
-//         // Figure why this doesn't work
-//         // cy.get('@onExpandTreeItemStub').should('have.been.called');
-
-//         cy.get(TREE_ITEM_ID).should('have.length', 2);
-
-//         cy.get(TREE_ITEM_TOGGLE_ID).first().click();
-
-//         // Figure why this doesn't work
-//         // cy.get('@onShrinkTreeItemStub').should('have.been.called');
-
-//         cy.get(TREE_ITEM_ID).should('have.length', 1);
-//     });
-// });
+        cy.get(TREE_ITEM_TOGGLE_ID).first().click();
+        cy.get(TREE_ITEM_ID).should('have.length', 1);
+    });
+});
 
 describe('Tree component with controlled onSelect', () => {
     beforeEach(() => {
@@ -135,19 +110,19 @@ describe('Tree component with controlled onSelect', () => {
         cy.mount(<TreeComponent onSelect={onSelectStub} selectedIds={['2']} />);
     });
 
-    it('selects the correct items', () => {
+    it('calls onSelect on the Tree when selecting', () => {
         cy.get(TREE_ITEM_ID).eq(0).click();
         cy.get('@onSelectStub').should('have.been.calledOnce');
     });
 
-    it('deselects the correct items', () => {
+    it('calls onSelect on the Tree when deselecting', () => {
         cy.get(TREE_ITEM_ID).eq(1).click();
         cy.get('@onSelectStub').should('have.been.calledOnce');
     });
 });
 
 describe('Tree component with controlled onExpand and onShrink', () => {
-    it('expands the correct items', () => {
+    it('calls onExpand on the Tree', () => {
         const onExpandStub = cy.stub().as('onExpandStub');
 
         cy.mount(<TreeComponent onExpand={onExpandStub} />);
@@ -156,7 +131,27 @@ describe('Tree component with controlled onExpand and onShrink', () => {
         cy.get('@onExpandStub').should('have.been.calledOnce');
     });
 
-    it('shrinks the correct items', () => {
+    it('calls onShrink the Tree', () => {
+        const onShrinkStub = cy.stub().as('onShrinkStub');
+
+        cy.mount(<TreeComponent onShrink={onShrinkStub} expandedIds={['1']} />);
+
+        cy.get(TREE_ITEM_TOGGLE_ID).eq(0).click();
+        cy.get('@onShrinkStub').should('have.been.calledOnce');
+    });
+});
+
+describe('Tree component with controlled onDrop', () => {
+    it('calls onExpand on the Tree', () => {
+        const onExpandStub = cy.stub().as('onExpandStub');
+
+        cy.mount(<TreeComponent onExpand={onExpandStub} />);
+
+        cy.get(TREE_ITEM_TOGGLE_ID).eq(0).click();
+        cy.get('@onExpandStub').should('have.been.calledOnce');
+    });
+
+    it('calls onShrink the Tree', () => {
         const onShrinkStub = cy.stub().as('onShrinkStub');
 
         cy.mount(<TreeComponent onShrink={onShrinkStub} expandedIds={['1']} />);
