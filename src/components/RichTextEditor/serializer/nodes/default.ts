04f913f0ce34f56e26a5fc165b4723136449da43
Kevin Walker <kevin@smartive.ch>
2023-05-31 09:54:51 +0200

    fix(RTE): revert serializer (#1448)

--- src/components/RichTextEditor/serializer/nodes/default.ts
+++ src/components/RichTextEditor/serializer/nodes/default.ts
@@ -1,33 +1,23 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { reactCssPropsToCss } from '../utils/reactCssPropsToCss';
-import { TElement } from '@udecode/plate';
 import { TextStyles } from '@components/RichTextEditor/Plugins/TextStylePlugin/types';
+import { TElement } from '@udecode/plate';
 import { CSSProperties } from 'react';
+import { reactCssPropsToCss } from '../utils/reactCssPropsToCss';
 
 export const defaultNode = (node: TElement, children: string, styles: CSSProperties, defaultClassNames: string) => {
-    const defaultStyles = getDefaultStyles(styles, node.type);
-
+    const defaultStyles = reactCssPropsToCss(styles);
     if (node.type === TextStyles.heading1) {
-        return `<h1 class="${defaultClassNames}">${getStyledChild(children, defaultStyles)}</h1>`;
+        return `<h1 class="${defaultClassNames}" style="${defaultStyles}">${children}</h1>`;
     }
     if (node.type === TextStyles.heading2) {
-        return `<h2 class="${defaultClassNames}">${getStyledChild(children, defaultStyles)}</h2>`;
+        return `<h2 class="${defaultClassNames}" style="${defaultStyles}">${children}</h2>`;
     }
     if (node.type === TextStyles.heading3) {
-        return `<h3 class="${defaultClassNames}">${getStyledChild(children, defaultStyles)}</h3>`;
+        return `<h3 class="${defaultClassNames}" style="${defaultStyles}">${children}</h3>`;
     }
     if (node.type === TextStyles.heading4) {
-        return `<h4 class="${defaultClassNames}">${getStyledChild(children, defaultStyles)}</h4>`;
+        return `<h4 class="${defaultClassNames}" style="${defaultStyles}">${children}</h4>`;
     }
-    return `<p class="${defaultClassNames}">${getStyledChild(children, defaultStyles)}</p>`;
-};
-
-const getStyledChild = (children: string, styles: string) => `<span style="${styles}">${children}</span>`;
-
-const getDefaultStyles = (styles: CSSProperties, type: string) => {
-    if (type === TextStyles.heading2 || type === TextStyles.heading3 || type === TextStyles.heading4) {
-        return reactCssPropsToCss(styles);
-    }
-    return reactCssPropsToCss({ display: 'inline-block', ...styles });
+    return `<p class="${defaultClassNames}" style="${defaultStyles}">${children}</p>`;
 };
