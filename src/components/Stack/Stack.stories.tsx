c5236b6c96e52483ffbad73031d23a54751faaa0
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2023-06-01 10:10:59 +0200

    Removing unnecessary story

--- src/components/Stack/Stack.stories.tsx
+++ src/components/Stack/Stack.stories.tsx
@@ -1,11 +1,10 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import React, { ReactElement, ReactNode } from 'react';
+import React from 'react';
 import { Meta, StoryFn } from '@storybook/react';
 import { STACK_PROPS, Stack as StackComponent } from './Stack';
 import { SPACING_VALUES } from '@utilities/dimensions';
 import { BOX_BG_ALIAS_TOKENS_CLASSES, BOX_TEXT_ALIAS_TOKENS_CLASSES } from '@utilities/tokens';
-import { Box } from '@components/Box';
 import { Divider } from '@components/Divider';
 
 const STACK_DIRECTIONS = ['row', 'column', 'row-reverse', 'column-reverse'];
@@ -76,10 +75,6 @@ export default {
     },
 } as Meta<STACK_PROPS>;
 
-const Code = ({ children }: { children: ReactNode }): ReactElement => (
-    <code className="tw-bg-box-neutral tw-rounded tw-px-2 tw-text-box-neutral-inverse tw-text-s">{children}</code>
-);
-
 export const Stack: StoryFn<STACK_PROPS> = (args: STACK_PROPS) => {
     const STACK_ITEM_CLASS_NAMES =
         'tw-bg-box-positive-strong tw-text-box-positive-strong-inverse tw-rounded-md tw-px-3 tw-py-1';
@@ -132,43 +127,4 @@ export const StackWithBoxAliasToken: StoryFn<STACK_PROPS> = (args: STACK_PROPS) 
 );
 StackWithBoxAliasToken.args = {
     bg: 'tw-bg-box-neutral',
-};
-
-export const StackAsSpanElement: StoryFn<STACK_PROPS> = (args: STACK_PROPS) => (
-    <Box as="span" className="tw-flex">
-        <StackComponent {...args} bg="tw-bg-box-positive" color="tw-text-box-positive-inverse">
-            <Box as="span">
-                To use <Code>Container</Code> as a <Code>span</Code> element and apply top and bottom{' '}
-                <Code>margin</Code> and <Code>padding</Code> wrap it into a flex or grid container.
-            </Box>
-        </StackComponent>
-        <StackComponent {...args} bg="tw-bg-box-warning" color="tw-text-box-warning-inverse">
-            <Box as="span">
-                Use <Code>span</Code> element when there is a parent element that does not accept <Code>div</Code> as a
-                child element.
-            </Box>
-        </StackComponent>
-        <StackComponent {...args} bg="tw-bg-box-negative" color="tw-text-box-negative-inverse">
-            <Box as="span">
-                It is not possible to define flex items CSS like <Code>flex: 1;</Code> in the Container component. Use a
-                combination of <Code>Flex</Code> and <Code>Box</Code> when that is needed.
-            </Box>
-        </StackComponent>
-    </Box>
-);
-StackAsSpanElement.args = {
-    as: 'span',
-    margin: 8,
-};
-StackAsSpanElement.argTypes = {
-    bg: {
-        control: {
-            disable: true,
-        },
-    },
-    color: {
-        control: {
-            disable: true,
-        },
-    },
 };
