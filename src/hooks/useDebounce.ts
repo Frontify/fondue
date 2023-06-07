10f7dc251271335321098369d692cc2ece675d08
Sam Creasey <sam.creasey@frontify.com>
2023-06-07 10:15:30 +0200

    remove unneccessary useCallback

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,16 +1,14 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { useCallback, useRef } from 'react';
+import { useRef } from 'react';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const useDebounce = <T extends (...args: any[]) => void>(func: T, timeout = 10) => {
+export const useDebounce = <T extends unknown[]>(func: (...args: T) => void, timeout = 10) => {
     const timer = useRef<number | undefined>();
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const debouncedFunc = useCallback((...args: any[]) => {
+    const debouncedFunc = (...args: T) => {
         clearTimeout(timer.current);
         timer.current = window.setTimeout(() => func.apply(this, args), timeout);
-    }, []);
+    };
 
     return debouncedFunc;
 };
