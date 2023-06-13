27a87efeea270e4a28bd2b2c77f1efea1e146c76
Dominique Moreno-Baltierra <conejoplata@gmail.com>
2023-06-13 16:54:37 +0200

    Adding tests for columns

--- src/components/Checklist/Checklist.cy.tsx
+++ src/components/Checklist/Checklist.cy.tsx
@@ -58,9 +58,27 @@ describe('Checklist Component', () => {
         cy.get(CHECKLIST_ID).should('have.class', 'tw-grid');
     });
 
-    it('should render the Checklist in two columns', () => {
+    it('should render the Checklist with one column by default', () => {
+        cy.mount(<Component direction={ChecklistDirection.Vertical} />);
+
+        cy.get(CHECKLIST_ID).should('have.class', 'tw-grid').and('have.class', 'tw-grid-cols-1');
+    });
+
+    it('should render the Checklist with two columns', () => {
         cy.mount(<Component direction={ChecklistDirection.Vertical} columns={2} />);
 
         cy.get(CHECKLIST_ID).should('have.class', 'tw-grid').and('have.class', 'tw-grid-cols-2');
+    });
+
+    it('should render the Checklist with three columns', () => {
+        cy.mount(<Component direction={ChecklistDirection.Vertical} columns={3} />);
+
+        cy.get(CHECKLIST_ID).should('have.class', 'tw-grid').and('have.class', 'tw-grid-cols-3');
+    });
+
+    it('should render the Checklist with four columns', () => {
+        cy.mount(<Component direction={ChecklistDirection.Vertical} columns={4} />);
+
+        cy.get(CHECKLIST_ID).should('have.class', 'tw-grid').and('have.class', 'tw-grid-cols-4');
     });
 });
