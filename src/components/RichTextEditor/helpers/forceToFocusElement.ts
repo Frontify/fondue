893bb5ff4f08960951085116c9db296e3640ba44
fulopdaniel <daniel.fulop@frontify.com>
2023-06-29 12:17:16 +0200

    extend tabbable elements

--- src/components/RichTextEditor/helpers/forceToFocusElement.ts
+++ src/components/RichTextEditor/helpers/forceToFocusElement.ts
@@ -1,13 +1,28 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+const TABBABLE_ELEMENTS = [
+    'input:not([disabled]):not([type=hidden])',
+    'select:not([disabled])',
+    'textarea:not([disabled])',
+    'button:not([disabled])',
+    'a[href]',
+    'area[href]',
+    'summary',
+    'iframe',
+    'object',
+    'embed',
+    'audio[controls]',
+    'video[controls]',
+    '[contenteditable]',
+    '[tabindex]:not([tabindex="-1"]):not([disabled])',
+].join(':not([hidden]):not([tabindex="-1"]),');
+
 export const forceToFocusNextElement = (event: React.KeyboardEvent, forwards: boolean) => {
     if (!document.activeElement) {
         return;
     }
     event.preventDefault();
-    const focusableElementSelectors =
-        'button:not([disabled]), [href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [contenteditable], iframe, object, embed, [tabindex]:not([tabindex="-1"])';
-    const focusableElements = Array.from(document.querySelectorAll(focusableElementSelectors));
+    const focusableElements = Array.from(document.querySelectorAll(TABBABLE_ELEMENTS));
     const currentIndex = focusableElements.indexOf(document.activeElement);
     const indexOfNextFocus =
         (forwards ? currentIndex + 1 : currentIndex - 1 + focusableElements.length) % focusableElements.length;
