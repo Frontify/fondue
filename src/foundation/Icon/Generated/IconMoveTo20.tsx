3453e1acf4f34a94f21fc0963a8b289093ba3985
Raphael Wirth <37732078+rawi96@users.noreply.github.com>
2023-07-07 09:53:46 +0200

    feat(icons): generate new icons (#1506)

---
+++ src/foundation/Icon/Generated/IconMoveTo20.tsx
@@ -0,0 +1,23 @@
+import React, { memo } from 'react';
+import { GeneratedIconProps } from '@foundation/Icon/IconProps';
+function IconMoveTo20(props: GeneratedIconProps): React.ReactElement<GeneratedIconProps> {
+    const customClassName = 'tw-flex tw-items-center tw-justify-center tw-fill-current';
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={20}
+            height={20}
+            className={customClassName}
+            name="IconMoveTo20"
+            {...props}
+        >
+            <path
+                fill="currentColor"
+                fillRule="evenodd"
+                d="M4 3a1.5 1.5 0 0 1 1.5-1.5H8A1.5 1.5 0 0 1 9.5 3v1.5A1.5 1.5 0 0 1 8 6H5.5A1.5 1.5 0 0 1 4 4.5V3Zm4 0H5.5v1.5H8V3ZM5.5 7.75A1.5 1.5 0 0 0 4 9.25v1.5a1.5 1.5 0 0 0 1.5 1.5H8a1.5 1.5 0 0 0 1.5-1.5v-1.5A1.5 1.5 0 0 0 8 7.75H5.5Zm0 1.5H8v1.5H5.5v-1.5Zm0 4.75A1.5 1.5 0 0 0 4 15.5V17a1.5 1.5 0 0 0 1.5 1.5H8A1.5 1.5 0 0 0 9.5 17v-1.5A1.5 1.5 0 0 0 8 14H5.5Zm0 1.5H8V17H5.5v-1.5ZM11 7.75a.75.75 0 0 0 1.5 0V6c1.214.912 2 2.364 2 4a4.992 4.992 0 0 1-2 4v-1.75a.75.75 0 0 0-1.5 0v4.25h4.25a.75.75 0 0 0 0-1.5h-1.596A6.487 6.487 0 0 0 16 10c0-2.01-.913-3.808-2.346-5h1.596a.75.75 0 0 0 0-1.5H11v4.25Z"
+                clipRule="evenodd"
+            />
+        </svg>
+    );
+}
+export default memo(IconMoveTo20);
---
+++ src/foundation/Icon/Generated/IconMoveTo24.tsx
@@ -0,0 +1,23 @@
+import React, { memo } from 'react';
+import { GeneratedIconProps } from '@foundation/Icon/IconProps';
+function IconMoveTo24(props: GeneratedIconProps): React.ReactElement<GeneratedIconProps> {
+    const customClassName = 'tw-flex tw-items-center tw-justify-center tw-fill-current';
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            className={customClassName}
+            name="IconMoveTo24"
+            {...props}
+        >
+            <path
+                fill="currentColor"
+                fillRule="evenodd"
+                d="M6 2.5A1.5 1.5 0 0 0 4.5 4v2A1.5 1.5 0 0 0 6 7.5h3.25a1.5 1.5 0 0 0 1.5-1.5V4a1.5 1.5 0 0 0-1.5-1.5H6ZM6 4h3.25v2H6V4Zm0 5.5A1.5 1.5 0 0 0 4.5 11v2A1.5 1.5 0 0 0 6 14.5h3.25a1.5 1.5 0 0 0 1.5-1.5v-2a1.5 1.5 0 0 0-1.5-1.5H6ZM6 11h3.25v2H6v-2Zm-1.5 7A1.5 1.5 0 0 1 6 16.5h3.25a1.5 1.5 0 0 1 1.5 1.5v2a1.5 1.5 0 0 1-1.5 1.5H6A1.5 1.5 0 0 1 4.5 20v-2Zm4.75 0H6v2h3.25v-2ZM13 9.25a.75.75 0 0 0 1.5 0V6.956A5.995 5.995 0 0 1 17.25 12a5.995 5.995 0 0 1-2.75 5.045V14.75a.75.75 0 0 0-1.5 0v4.75h4.75a.75.75 0 0 0 0-1.5h-1.999a7.489 7.489 0 0 0 2.999-6 7.489 7.489 0 0 0-3-6h2a.75.75 0 0 0 0-1.5H13v4.75Z"
+                clipRule="evenodd"
+            />
+        </svg>
+    );
+}
+export default memo(IconMoveTo24);
