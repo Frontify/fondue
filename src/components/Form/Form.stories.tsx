9db05d670129ae106bea26ac6a85cf083d1c1c62
imoutaharik <ilham.moutaharik@frontify.com>
2023-08-29 16:33:00 +0200

    Add args to story

--- src/components/Form/Form.stories.tsx
+++ src/components/Form/Form.stories.tsx
@@ -1,9 +1,9 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import React, { useState } from 'react';
+import React from 'react';
 import { Meta, StoryFn } from '@storybook/react';
 import { Form, FormProps } from './Form';
-import { FormControl, FormControlProps } from '@components/FormControl';
+import { FormControl } from '@components/FormControl';
 import { TextInput } from '@components/TextInput';
 import { Button } from '@components/Button';
 import { action } from '@storybook/addon-actions';
@@ -13,27 +13,34 @@ export default {
     title: 'Components/Form',
     component: Form,
     tags: ['autodocs'],
+    args: {
+        method: 'post',
+        autocomplete: true,
+        'data-test-id': 'fondue-form',
+        acceptCharset: '',
+        action: '',
+        encType: '',
+        name: '',
+        noValidate: false,
+        target: '',
+        rel: '',
+    },
 } as Meta<FormProps>;
 
-export const Default: StoryFn<FormControlProps> = () => {
-    const [input, setInput] = useState('');
-
-    return (
-        <Form method="post" autoComplete="true">
-            <FormControl
-                clickable={true}
-                label={{
-                    children: 'Input Label',
-                    required: false,
-                    htmlFor: 'label',
-                    tooltip: { content: 'Tooltip Text' },
-                }}
-            >
-                <TextInput id="label" value={input} onChange={setInput} />
-            </FormControl>
-            <Box className="tw-my-2">
-                <Button onClick={action('click')}>Save</Button>
-            </Box>
-        </Form>
-    );
-};
+export const Default: StoryFn<FormProps> = (args: FormProps) => (
+    <Form {...args}>
+        <FormControl
+            clickable={true}
+            label={{
+                children: 'Input Label',
+                required: false,
+                htmlFor: 'label',
+            }}
+        >
+            <TextInput id="label" value="some value" />
+        </FormControl>
+        <Box className="tw-my-2">
+            <Button onClick={action('click')}>Save</Button>
+        </Box>
+    </Form>
+);
