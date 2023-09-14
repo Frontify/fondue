783152cc36d1d662a41c865699b9b4830ea9bff6
angel botana <angel.botana@frontify.com>
2023-09-14 17:37:50 +0200

    some updates and tests

--- src/components/OrderableList/types.ts
+++ src/components/OrderableList/types.ts
@@ -2,35 +2,23 @@
 
 import { ReactElement } from 'react';
 import { DraggableItem } from '@utilities/dnd';
-import { TreeItemColors, TreeItemPropsSizing } from '..';
+import { TreeItemStyling } from '..';
 
 export type RenderListItem<T> = (items: OrderableListItem<T>) => ReactElement;
-
-export type CollectionItemProps<T> = {
-    item: OrderableListItem<T>;
-    dragDisabled: boolean;
-    renderContent: RenderListItem<T>;
-    listId: string;
-};
 
 export type OrderableListItem<T = Record<string, unknown>> = DraggableItem<T> & {
     alt: string;
 };
 
+export type OrderableListItemStyle = TreeItemStyling;
+
 export type OrderableListProps<T> = {
     items: OrderableListItem<T>[];
     dragDisabled: boolean;
     dragHandlerPosition?: 'left' | 'right' | 'none';
-    spacingY?: TreeItemPropsSizing;
-    activeColorStyle?: TreeItemColors;
+    itemStyle?: OrderableListItemStyle;
     selectedId?: string;
     onMove: (modifiedItems: DraggableItem<T>[]) => void;
     renderContent: RenderListItem<T>;
     'data-test-id'?: string;
 };
-
-export enum ItemDragState {
-    Dragging = 'Dragging',
-    Idle = 'Idle',
-    Preview = 'Preview',
-}
