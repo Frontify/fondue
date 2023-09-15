7f9b95c179b50a0d2f93e3fa88fa3c3834807689
Thomas <thomas.tosch@frontify.com>
2023-09-15 14:17:40 +0200

    misc: switched the undefined check to optional chaining
    Signed-off-by: Thomas <thomas.tosch@frontify.com>

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -64,7 +64,7 @@ export const Textarea = ({
 
     const onKeyDown = (event: KeyboardEvent<HTMLTextAreaElement>) => {
         if (event.key === 'Enter') {
-            onEnterPressed && onEnterPressed(event);
+            onEnterPressed?.(event);
         }
     };
 
