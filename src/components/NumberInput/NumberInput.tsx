bc200a0eee7fc328f65e70057916e2fe2bbcf068
Ryan Carville <ryan.carville@frontify.com>
2023-10-03 14:38:39 +0200

    chore: remove focus state & add aria-label to clear button

--- src/components/NumberInput/NumberInput.tsx
+++ src/components/NumberInput/NumberInput.tsx
@@ -1,6 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import { NumberInputIncrement, NumberInputProps } from './types';
 import { merge } from '@utilities/merge';
 import { IconCross16, IconMinus16, IconPlus16 } from '@foundation/Icon';
@@ -8,15 +8,10 @@ import { useMemoizedId } from '@hooks/useMemoizedId';
 import { Validation, validationClassMap } from '@utilities/validation';
 import { KeyboardEvent } from '@react-types/shared';
 import { GetStatusIcon } from '@utilities/input';
-import { FOCUS_WITHIN_STYLE } from '@utilities/focusStyle';
 
 const INCREMENT_KEYS = ['ArrowUp', 'ArrowRight'];
 const DECREMENT_KEYS = ['ArrowDown', 'ArrowLeft'];
 const SPECIAL_KEYS = ['Backspace', 'Tab'];
-type FocusedState = {
-    isFocused: boolean;
-    isClick: boolean;
-};
 
 export const NumberInput = ({
     id: propId,
@@ -44,7 +39,6 @@ export const NumberInput = ({
     const isShift = useRef<boolean>(false);
     const isMouseHold = useRef<boolean>(false);
     const inputElement = useRef<HTMLInputElement | null>(null);
-    const [focusState, setFocusState] = useState<FocusedState>({ isFocused: false, isClick: false });
 
     const getCurrentValueWithoutSuffix = useCallback(() => {
         if (suffix) {
@@ -158,18 +152,12 @@ export const NumberInput = ({
     };
 
     const handleFocus = (event: React.FocusEvent<HTMLInputElement, Element>) => {
-        if (!focusState.isClick && !focusState.isFocused) {
-            setFocusState({ isFocused: true, isClick: false });
-        }
         if (onFocus) {
             onFocus(event);
         }
     };
 
     const handleBlur = (event: React.FocusEvent<HTMLInputElement, Element>) => {
-        if (focusState.isClick || focusState.isFocused) {
-            setFocusState({ isFocused: false, isClick: false });
-        }
         if (onBlur) {
             onBlur(event);
         }
@@ -183,7 +171,6 @@ export const NumberInput = ({
         <div
             className={merge([
                 'tw-w-full tw-flex tw-items-center tw-justify-between tw-h-9 tw-gap-2 tw-px-3 tw-transition tw-text-s tw-font-sans tw-relative tw-bg-white dark:tw-bg-transparent tw-border tw-rounded tw-line-strong hover:tw-border-line-x-strong focus-within:tw-border-line-xx-strong',
-                focusState.isFocused ? FOCUS_WITHIN_STYLE : '',
                 status ? validationClassMap[status] : '',
             ])}
             data-test-id={dataTestId}
@@ -212,7 +199,6 @@ export const NumberInput = ({
                         ? 'tw-text-black-40 tw-placeholder-black-30 dark:tw-text-black-30 dark:tw-placeholder-black-40'
                         : 'tw-text-black tw-placeholder-black-60 dark:tw-text-white',
                 ])}
-                onMouseDown={() => setFocusState({ isFocused: false, isClick: true })}
                 onChange={() => setValue(Number(getCurrentValueWithoutSuffix()))}
                 onKeyDown={handleKeyDown}
                 onKeyUp={handleKeyUp}
@@ -292,6 +278,7 @@ export const NumberInput = ({
                         className={
                             'tw-p-1 hover:tw-bg-box-neutral hover:tw-text-box-selected-inverse focus:tw-ring-line-xx-strong focus:tw-bg-box-neutral'
                         }
+                        aria-label="Clear value"
                     >
                         <IconCross16 />
                     </button>
