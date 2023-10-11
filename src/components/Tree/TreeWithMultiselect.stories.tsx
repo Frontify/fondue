57f3e7a3273d0d116b753525ebe843529ea60640
Anxo Botana <angel.botana@frontify.com>
2023-10-11 16:00:22 +0200

    feat: MultiSelect TreeItem (#1604)
    Co-authored-by: Fabricio Mendonca <fabricio.mendonca@frontify.com>

---
+++ src/components/Tree/TreeWithMultiselect.stories.tsx
@@ -0,0 +1,192 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { useEffect, useState } from 'react';
+import { Meta } from '@storybook/react';
+
+import { IconDocument } from '@foundation/Icon';
+
+import type { TreeItemStyling, TreeNodeWithoutElements, TreeProps } from '@components/Tree/types';
+import {
+    TreeItemBorderClassMap,
+    TreeItemBorderRadiusClassMap,
+    TreeItemBorderStyleClassMap,
+    TreeItemColorsClassMap,
+    TreeItemShadowClassMap,
+    TreeItemSpacingClassMap,
+    Tree as TreeView,
+} from '@components/Tree';
+import { TreeItemMockMultiselect, treeItemsMockMultiseclect } from '@components/Tree/utils';
+import { Container } from '@components/Container';
+import { TreeItemMultiselect } from './TreeItem/TreeItemMultiselect';
+import { getNewSelectedItems } from './helpers';
+
+export default {
+    title: 'Components/Tree/Multiselect',
+    component: TreeView,
+    tags: ['autodocs'],
+    parameters: {
+        controls: { expanded: true },
+    },
+    args: {
+        id: 'storybook-tree',
+        multiselect: true,
+        draggable: true,
+        selectedIds: ['__ROOT__/2'],
+        dragHandlerPosition: 'left',
+        spacingY: 'none',
+        contentHight: 'single-line',
+        shadow: 'none',
+        borderRadius: 'small',
+        borderWidth: 'none',
+        borderStyle: 'none',
+        activeColorStyle: 'neutral',
+    },
+    argTypes: {
+        multiselect: {
+            control: { type: 'boolean' },
+        },
+        draggable: {
+            control: { type: 'boolean' },
+        },
+        dragHandlerPosition: {
+            table: { category: 'Item Options' },
+            options: ['left', 'right', 'none'],
+            control: { type: 'inline-radio' },
+        },
+        spacingY: {
+            table: { category: 'Item Style' },
+            name: 'itemStyle.spacingY',
+            options: [...Object.keys(TreeItemSpacingClassMap)],
+            mapping: [...Object.values(TreeItemSpacingClassMap)],
+            control: { type: 'inline-radio' },
+        },
+        contentHight: {
+            table: { category: 'Item Style' },
+            name: 'itemStyle.contentHight',
+            options: ['content-fit', 'single-line'],
+            control: { type: 'inline-radio' },
+        },
+        shadow: {
+            table: { category: 'Item Style' },
+            name: 'itemStyle.shadow',
+            options: [...Object.keys(TreeItemShadowClassMap)],
+            mapping: [...Object.values(TreeItemShadowClassMap)],
+            control: { type: 'select' },
+        },
+        borderRadius: {
+            table: { category: 'Item Style' },
+            name: 'itemStyle.borderRadius',
+            options: [...Object.keys(TreeItemBorderRadiusClassMap)],
+            mapping: [...Object.values(TreeItemBorderRadiusClassMap)],
+            control: { type: 'select' },
+        },
+        borderWidth: {
+            table: { category: 'Item Style' },
+            name: 'itemStyle.borderWidth',
+            options: [...Object.keys(TreeItemBorderClassMap)],
+            mapping: [...Object.values(TreeItemBorderClassMap)],
+            control: { type: 'select' },
+        },
+        borderStyle: {
+            table: { category: 'Item Style' },
+            name: 'itemStyle.borderStyle',
+            options: [...Object.keys(TreeItemBorderStyleClassMap)],
+            mapping: [...Object.values(TreeItemBorderStyleClassMap)],
+            control: { type: 'select' },
+        },
+        activeColorStyle: {
+            table: { category: 'Item Style' },
+            name: 'itemStyle.activeColorStyle',
+            options: [...Object.keys(TreeItemColorsClassMap)],
+            mapping: [...Object.values(TreeItemColorsClassMap)],
+            control: { type: 'inline-radio' },
+        },
+        selectedIds: {
+            control: { type: 'object' },
+            description:
+                'Pre-selected Ids must be in the format `<parent_id>/<selected_id>` (`__ROOT__` is the root id)',
+        },
+        expandedIds: {
+            control: { type: 'object' },
+        },
+    },
+} as Meta<TreeProps & TreeItemStyling>;
+
+const TreeItemContentComponent = ({ title }: { title: string }) => {
+    return (
+        <div className="tw-flex tw-space-x-1.5 tw-w-full">
+            <span className="tw-flex tw-justify-center tw-items-center tw-w-5">{<IconDocument />}</span>
+            <span>{title}</span>
+        </div>
+    );
+};
+
+const renderTreeItemComponent = (
+    { nodes, label, numChildNodes, onSelect, ...treeItem }: TreeItemMockMultiselect,
+    selectedIds: string[],
+) => {
+    const nodesLength = nodes?.length ?? 0;
+    const showCaret = numChildNodes !== undefined ? !!numChildNodes : nodesLength > 0;
+
+    return (
+        <TreeItemMultiselect
+            {...treeItem}
+            key={treeItem.id}
+            contentComponent={<TreeItemContentComponent title={label || 'NO TITLE'} />}
+            onSelect={onSelect}
+            showCaret={showCaret}
+        >
+            {nodes?.map((node) => renderTreeItemComponent(node, selectedIds))}
+        </TreeItemMultiselect>
+    );
+};
+
+export const MultiselectWithBasicItem = ({ ...args }: TreeProps) => {
+    const [selectedIds, setSelectedIds] = useState<string[]>(args.selectedIds ?? []);
+    const [treeItems, setTreeItems] = useState<TreeItemMockMultiselect[]>([]);
+
+    useEffect(() => {
+        setTreeItems(treeItemsMockMultiseclect);
+    }, []);
+
+    const handleItemSelected = (id: string, ignoreRemoveSelected = false, nodes: TreeNodeWithoutElements[] = []) => {
+        if (nodes.length === 0) {
+            return;
+        }
+
+        setSelectedIds((currentSelectedIds) =>
+            getNewSelectedItems(id, currentSelectedIds, nodes, ignoreRemoveSelected),
+        );
+    };
+
+    return (
+        <Container maxWidth="400px">
+            <TreeView id={args.id} {...cleanProps(args)} selectedIds={selectedIds} onSelect={handleItemSelected}>
+                {treeItems.map((treeItem) => renderTreeItemComponent(treeItem, selectedIds))}
+            </TreeView>
+        </Container>
+    );
+};
+
+const cleanProps = ({ ...args }) => {
+    const itemStyle = {
+        spacingY: args.spacingY,
+        contentHight: args.contentHight,
+        shadow: args.shadow,
+        borderRadius: args.borderRadius,
+        borderWidth: args.borderWidth,
+        borderStyle: args.borderStyle,
+        activeColorStyle: args.activeColorStyle,
+        ...args.itemStyle,
+    };
+
+    delete args.spacingY;
+    delete args.contentHight;
+    delete args.shadow;
+    delete args.borderRadius;
+    delete args.borderWidth;
+    delete args.borderStyle;
+    delete args.activeColorStyle;
+
+    return { ...args, itemStyle };
+};
---
+++ src/components/Tree/helpers/multiselect.spec.cy.tsx
@@ -0,0 +1,359 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import {
+    TreeItemMultiselectWithNodes,
+    addSelectedIds,
+    addSelectedItemsFromSelection,
+    cleanOrphanSelectedIds,
+    convertToPartialSelectedId,
+    fixParentSelectionState,
+    getExtendedId,
+    getMultiselectCheckBoxState,
+    getNewSelectedItems,
+    getParentSelectedTreeItem,
+    getSelectedChildrenItems,
+    getSelectedTreeItem,
+    removePartialFlagSelectedId,
+    removeSelectedIds,
+} from './multiselect';
+import { CheckboxState } from '@components/Checkbox';
+import { ROOT_ID } from './constants';
+
+const treeMultiselect: TreeItemMultiselectWithNodes[] = [
+    {
+        id: 'item-1',
+        parentId: ROOT_ID,
+        extendedId: `${ROOT_ID}/item-1`,
+        nodes: [],
+    },
+    {
+        id: 'item-22',
+        parentId: ROOT_ID,
+        extendedId: `${ROOT_ID}/item-22`,
+        nodes: [
+            {
+                id: 'item-74',
+                parentId: 'item-22',
+                extendedId: 'item-22/item-74',
+                nodes: [],
+            },
+            {
+                id: 'item-265',
+                parentId: 'item-22',
+                extendedId: 'item-22/item-265',
+                nodes: [
+                    {
+                        id: 'item-leaf-709',
+                        parentId: 'item-265',
+                        extendedId: 'item-265/item-leaf-709',
+                        nodes: [],
+                    },
+                ],
+            },
+            {
+                id: 'item-88',
+                parentId: 'item-22',
+                extendedId: 'item-22/item-88',
+                nodes: [],
+            },
+            {
+                id: 'item-48',
+                parentId: 'item-22',
+                extendedId: 'item-22/item-48',
+                nodes: [],
+            },
+        ],
+    },
+    {
+        id: 'item-122',
+        parentId: ROOT_ID,
+        extendedId: `${ROOT_ID}/item-122`,
+        nodes: [],
+    },
+];
+
+const selectedIds = [
+    `${ROOT_ID}/item-1`,
+    `*${ROOT_ID}/item-22`,
+    'item-22/item-88',
+    'item-22/item-48',
+    'item-122/orphan-id',
+];
+
+describe('multiselect', () => {
+    describe('getMultiselectCheckBoxState', () => {
+        it('returns correct checkboxes states according to the state', () => {
+            cy.wrap(getMultiselectCheckBoxState(true, true)).should('be.equal', CheckboxState.Checked);
+            cy.wrap(getMultiselectCheckBoxState(true, false)).should('be.equal', CheckboxState.Checked);
+            cy.wrap(getMultiselectCheckBoxState(false, false)).should('be.equal', CheckboxState.Unchecked);
+            cy.wrap(getMultiselectCheckBoxState(false, true)).should('be.equal', CheckboxState.Mixed);
+        });
+    });
+
+    describe('getSelectedChildrenItems', () => {
+        it('returns selected items without partials', () => {
+            cy.wrap(getSelectedChildrenItems(treeMultiselect, selectedIds, false)).should('deep.equal', [
+                'item-1',
+                'item-22',
+            ]);
+        });
+        it('returns only partially selected items', () => {
+            cy.wrap(getSelectedChildrenItems(treeMultiselect, selectedIds, true)).should('deep.equal', ['item-22']);
+        });
+        it('returns empty if no selectedId', () => {
+            cy.wrap(getSelectedChildrenItems(treeMultiselect, [], false)).should('be.have.length', 0);
+        });
+    });
+
+    describe('getSelectedTreeItem', () => {
+        it('returns item from the tree given an item id', () => {
+            cy.wrap(getSelectedTreeItem(treeMultiselect, 'item-leaf-709')).should('deep.equal', {
+                id: 'item-leaf-709',
+                parentId: 'item-265',
+                extendedId: 'item-265/item-leaf-709',
+                nodes: [],
+            });
+        });
+    });
+
+    describe('getParentSelectedTreeItem', () => {
+        it('returns parent item from the tree given an item id', () => {
+            cy.wrap(getParentSelectedTreeItem(treeMultiselect, 'item-leaf-709', null)).should('deep.equal', {
+                id: 'item-265',
+                parentId: 'item-22',
+                extendedId: 'item-22/item-265',
+                nodes: [
+                    {
+                        id: 'item-leaf-709',
+                        parentId: 'item-265',
+                        extendedId: 'item-265/item-leaf-709',
+                        nodes: [],
+                    },
+                ],
+            });
+        });
+    });
+
+    describe('addSelectedItemsFromSelection', () => {
+        it('should flag parents as partial if siblings in the way up are not selected and fully selected if they are', () => {
+            const result = addSelectedItemsFromSelection(treeMultiselect, 'item-leaf-709', ['item-265/item-leaf-709']);
+            console.log(result);
+            cy.wrap(addSelectedItemsFromSelection(treeMultiselect, 'item-leaf-709', ['item-265/item-leaf-709'])).should(
+                'deep.equal',
+                ['item-265/item-leaf-709', 'item-22/item-265', `*${ROOT_ID}/item-22`],
+            );
+        });
+        it('should get the children selected if a parent is selected', () => {
+            cy.wrap(addSelectedItemsFromSelection(treeMultiselect, 'item-265', ['item-22/item-265'])).should(
+                'deep.equal',
+                ['item-22/item-265', `*${ROOT_ID}/item-22`, 'item-265/item-leaf-709'],
+            );
+        });
+        it('returns unchanged selectedIds array if non-exitent tree item is sent', () => {
+            cy.wrap(addSelectedItemsFromSelection(treeMultiselect, 'item-leaf-700000', [`${ROOT_ID}/item-22`])).should(
+                'deep.equal',
+                [`${ROOT_ID}/item-22`],
+            );
+        });
+    });
+
+    describe('fixParentSelectionState', () => {
+        const parent = {
+            id: 'item-22',
+            parentId: ROOT_ID,
+            extendedId: `${ROOT_ID}/item-22`,
+            nodes: [
+                {
+                    id: 'item-74',
+                    parentId: 'item-22',
+                    extendedId: 'item-22/item-74',
+                    nodes: [],
+                },
+                {
+                    id: 'item-265',
+                    parentId: 'item-22',
+                    extendedId: 'item-22/item-265',
+                    nodes: [
+                        {
+                            id: 'item-leaf-709',
+                            parentId: 'item-265',
+                            extendedId: 'item-265/item-leaf-709',
+                            nodes: [],
+                        },
+                    ],
+                },
+                {
+                    id: 'item-88',
+                    parentId: 'item-22',
+                    extendedId: 'item-22/item-88',
+                    nodes: [],
+                },
+                {
+                    id: 'item-48',
+                    parentId: 'item-22',
+                    extendedId: 'item-22/item-48',
+                    nodes: [],
+                },
+            ],
+        };
+
+        it('should flag parent as fully selected if ther no siblings', () => {
+            const singleChild = {
+                id: 'item-265',
+                parentId: 'item-22',
+                extendedId: 'item-22/item-265',
+                nodes: [
+                    {
+                        id: 'item-leaf-709',
+                        parentId: 'item-265',
+                        extendedId: 'item-265/item-leaf-709',
+                        nodes: [],
+                    },
+                ],
+            };
+            cy.wrap(fixParentSelectionState(singleChild, ['item-265/item-leaf-709'])).should('deep.equal', [
+                'item-265/item-leaf-709',
+                'item-22/item-265',
+            ]);
+        });
+        it('should flag parent as partial if siblings in the way up are not selected and fully selected if they are', () => {
+            cy.wrap(fixParentSelectionState(parent, ['item-22/item-265'])).should('deep.equal', [
+                'item-22/item-265',
+                `*${ROOT_ID}/item-22`,
+            ]);
+        });
+        it('should flag parent as selected if all siblings are selected', () => {
+            cy.wrap(
+                fixParentSelectionState(parent, [
+                    'item-22/item-265',
+                    'item-22/item-74',
+                    'item-22/item-88',
+                    'item-22/item-48',
+                ]),
+            ).should('deep.equal', [
+                'item-22/item-265',
+                'item-22/item-74',
+                'item-22/item-88',
+                'item-22/item-48',
+                `${ROOT_ID}/item-22`,
+            ]);
+        });
+
+        it('should flag parent from partial to selected if all siblings are selected', () => {
+            cy.wrap(
+                fixParentSelectionState(parent, [
+                    'item-22/item-265',
+                    'item-22/item-74',
+                    'item-22/item-88',
+                    'item-22/item-48',
+                    `*${ROOT_ID}/item-22`,
+                ]),
+            ).should('deep.equal', [
+                'item-22/item-265',
+                'item-22/item-74',
+                'item-22/item-88',
+                'item-22/item-48',
+                `${ROOT_ID}/item-22`,
+            ]);
+        });
+    });
+
+    describe('cleanOrphanSelectedIds', () => {
+        it('returns clean slectedIds without orphan items', () => {
+            const result = cleanOrphanSelectedIds(selectedIds);
+            console.log(result);
+            cy.wrap(cleanOrphanSelectedIds(selectedIds)).should('deep.equal', [
+                `${ROOT_ID}/item-1`,
+                `*${ROOT_ID}/item-22`,
+                'item-22/item-88',
+                'item-22/item-48',
+            ]);
+        });
+    });
+
+    describe('convertToPartialSelectedId', () => {
+        it('returns clean slectedIds without orphan items', () => {
+            cy.wrap(convertToPartialSelectedId(['item-22/item-74', 'item-007'])).should('deep.equal', [
+                '*item-22/item-74',
+                '*item-007',
+            ]);
+        });
+    });
+    describe('removePartialFlagSelectedId', () => {
+        it('returns clean slectedIds without orphan items', () => {
+            cy.wrap(removePartialFlagSelectedId(['*item-22/item-74', 'item-007'])).should('deep.equal', [
+                'item-22/item-74',
+                'item-007',
+            ]);
+        });
+    });
+    describe('getExtendedId', () => {
+        const itemWithExtendedId = {
+            id: 'item-74',
+            parentId: 'item-22',
+            extendedId: 'item-22=item-74', // this not-standard format is to ensure the function returns this field.
+            nodes: [],
+        };
+        const itemWithoutExtendedId = {
+            id: 'item-74',
+            parentId: 'item-22',
+            nodes: [],
+        };
+        it('returns the already present extended id in the item', () => {
+            cy.wrap(getExtendedId(itemWithExtendedId)).should('be.equal', 'item-22=item-74');
+        });
+        it('returns the extended id present in the item built as <parentId>/<itemId>', () => {
+            cy.wrap(getExtendedId(itemWithoutExtendedId)).should('be.equal', 'item-22/item-74');
+        });
+    });
+    describe('removeSelectedIds', () => {
+        it('removes the passed Ids from the selected array if it is selected', () => {
+            cy.wrap(removeSelectedIds(selectedIds, ['item-22/item-48'], false)).should('deep.equal', [
+                `${ROOT_ID}/item-1`,
+                `*${ROOT_ID}/item-22`,
+                'item-22/item-88',
+                'item-122/orphan-id',
+            ]);
+        });
+        it('removes the passed Ids from the selected array if it is partially selected', () => {
+            cy.wrap(removeSelectedIds(selectedIds, [`${ROOT_ID}/item-22`], true)).should('deep.equal', [
+                `${ROOT_ID}/item-1`,
+                'item-22/item-88',
+                'item-22/item-48',
+                'item-122/orphan-id',
+            ]);
+        });
+    });
+    describe('addSelectedIds', () => {
+        it('adds the id to the selected ids', () => {
+            cy.wrap(addSelectedIds(selectedIds, [`${ROOT_ID}/item-1111`], false)).should('deep.equal', [
+                `${ROOT_ID}/item-1`,
+                `*${ROOT_ID}/item-22`,
+                'item-22/item-88',
+                'item-22/item-48',
+                'item-122/orphan-id',
+                `${ROOT_ID}/item-1111`,
+            ]);
+        });
+        it('adds the id as partially selected', () => {
+            cy.wrap(addSelectedIds(selectedIds, [`${ROOT_ID}/item-1111`], true)).should('deep.equal', [
+                `${ROOT_ID}/item-1`,
+                `*${ROOT_ID}/item-22`,
+                'item-22/item-88',
+                'item-22/item-48',
+                'item-122/orphan-id',
+                `*${ROOT_ID}/item-1111`,
+            ]);
+        });
+    });
+
+    describe('getNewSelectedItems', () => {
+        const selectedIds = ['item-265/item-leaf-709', 'item-22/item-265', `*${ROOT_ID}/item-22`];
+        it('returns the clean selected ids array with the item included', () => {
+            cy.wrap(getNewSelectedItems('item-leaf-709', [], treeMultiselect, false)).should('deep.equal', selectedIds);
+        });
+        it('returns the clean selected ids array with the item removed', () => {
+            cy.wrap(getNewSelectedItems('item-leaf-709', selectedIds, treeMultiselect, false)).should('deep.equal', []);
+        });
+    });
+});
