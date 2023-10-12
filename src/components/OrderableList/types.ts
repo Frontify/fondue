2c317b066df136449875583b5357080c410c8764
Ryan Carville <ryan.carville@frontify.com>
2023-10-12 16:31:03 +0200

    feat: extend Tree prop to Orderable List

--- src/components/OrderableList/types.ts
+++ src/components/OrderableList/types.ts
@@ -2,7 +2,7 @@
 
 import { ReactElement } from 'react';
 import { DraggableItem } from '@utilities/dnd';
-import { DragHandlerPosition, TreeItemStyling } from '..';
+import { DragHandlerPosition, TreeItemStyling, TreeProps } from '..';
 
 export type RenderListItem<T> = (items: OrderableListItem<T>) => ReactElement;
 
@@ -16,11 +16,9 @@ export type OrderableListProps<T> = {
     items: OrderableListItem<T>[];
     dragDisabled: boolean;
     dragHandlerPosition?: DragHandlerPosition;
-    /**@description Flag to disable the default drag sensor delay */
-    disableDragDelay?: boolean;
     itemStyle?: OrderableListItemStyle;
     selectedId?: string;
     onMove: (modifiedItems: DraggableItem<T>[]) => void;
     renderContent: RenderListItem<T>;
     'data-test-id'?: string;
-};
+} & Pick<TreeProps, 'disableDragDelay'>;
