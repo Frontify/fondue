317002a9256c3e8137213a1e7b18fa7f90c34e44
Ryan Carville <ryan.carville@frontify.com>
2023-10-31 17:48:46 +0100

    feat: decouple format breadcrumbs component

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -4,10 +4,8 @@ import { BadgeProps } from '@components/Badge';
 import { IconProps } from '@foundation/Icon/IconProps';
 import { AriaBreadcrumbsProps, useBreadcrumbs } from '@react-aria/breadcrumbs';
 import { MouseEvent, ReactElement } from 'react';
-import { BreadcrumbItem } from './BreadcrumbItem';
-import { CurrentBreadcrumbItem } from './CurrentBreadcrumbItem';
 import { merge } from '@utilities/merge';
-import { IconDotsHorizontal16 } from '@foundation/Icon';
+import { FormattedBreadcrumbs } from './FormattedBreadcrumbs';
 
 const mapBreadcrumbsToAriaProps = (items: Breadcrumb[]) => ({
     children: items.map(({ label }, index) => (
@@ -46,75 +44,6 @@ export const verticalGapClassMap: Record<BreadcrumbGap, string> = {
     [BreadcrumbGap.None]: 'tw-gap-y-0',
     [BreadcrumbGap.Small]: 'tw-gap-y-0.5',
     [BreadcrumbGap.Medium]: 'tw-gap-y-1',
-};
-
-const FormattedBreadcrumbs = ({
-    items,
-    keepRoot,
-    includesCurrent = false,
-    truncate = false,
-    'data-test-id': dataTestId,
-}: BreadcrumbsProps): ReactElement[] => {
-    let renderTruncation = true;
-    const elements = items.map(({ label, badges, bold, decorator, link, onClick }, index) => {
-        const key = `breadcrumb-${index}`;
-        const isRootKept = keepRoot && index === 0;
-        const isCurrent = index === items.length - 1;
-        const isLastItems = index >= items.length - 2;
-        const isTruncatedItem = truncate && !isRootKept && !isLastItems;
-        const isLastItemsToRender = keepRoot ? isLastItems : index >= items.length - 3;
-        const showSeparator = index < items.length - 2 || (includesCurrent && index === items.length - 2);
-
-        switch (true) {
-            case isCurrent:
-                return (
-                    <CurrentBreadcrumbItem
-                        key={key}
-                        label={label}
-                        badges={badges}
-                        bold={bold}
-                        decorator={decorator}
-                        link={link}
-                        onClick={onClick}
-                        includesCurrent={includesCurrent}
-                        data-test-id={dataTestId}
-                    />
-                );
-
-            case !truncate:
-            case isRootKept:
-            case isLastItemsToRender:
-                return (
-                    <BreadcrumbItem
-                        key={key}
-                        label={label}
-                        decorator={decorator}
-                        link={link}
-                        onClick={onClick}
-                        showSeparator={showSeparator}
-                        data-test-id={dataTestId}
-                    />
-                );
-
-            case isTruncatedItem && renderTruncation:
-                renderTruncation = false;
-                return (
-                    <BreadcrumbItem
-                        key={key}
-                        label={''}
-                        decorator={<IconDotsHorizontal16 />}
-                        link={link}
-                        onClick={onClick}
-                        showSeparator={showSeparator}
-                        data-test-id={`${dataTestId}-truncation`}
-                    />
-                );
-
-            default:
-                return <span key={key} />;
-        }
-    });
-    return [...elements];
 };
 
 export const Breadcrumbs = ({
