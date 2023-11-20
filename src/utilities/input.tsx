b8e3a7bd075355646a83b5b065d112f8efcdf80e
Ryan Carville <ryan.carville@frontify.com>
2023-11-20 14:23:32 +0100

    feat(new): combine theme styles

--- src/utilities/input.tsx
+++ src/utilities/input.tsx
@@ -12,9 +12,8 @@ import {
     LoadingCircle,
     LoadingCircleSize,
     Tooltip,
-    TooltipProps,
 } from '@components/index';
-import { InputActionsProps } from 'src/types/input';
+import { InputActionsProps, InputExtraActionsProps } from 'src/types/input';
 
 type HelperTextProps = {
     text: string;
@@ -34,33 +33,20 @@ type InputStyleGroup =
     | 'disabled'
     | 'readOnly';
 
-export const InputStylesLightTheme: Record<InputStyleGroup, string> = {
-    base: 'tw-flex tw-items-center tw-justify-between tw-gap-2 tw-px-3 tw-transition tw-text-sm tw-font-sans tw-relative tw-bg-base tw-border tw-rounded tw-line-strong',
-    width: 'tw-w-full',
-    height: 'tw-h-9',
-    element: 'tw-border-line-strong tw-text-text tw-placeholder-text-x-weak tw-outline-none tw-p-2',
-    focus: 'focus:tw-border-line-xx-strong',
-    focusWithin: 'focus-within:tw-border-line-xx-strong focus-within:hover:tw-border-line-xx-strong',
-    hover: 'hover:tw-border-line-x-strong',
-    disabled:
-        'disabled:tw-bg-box-disabled disabled:tw-border-line-weak disabled:tw-text-text-disabled disabled:hover:tw-cursor-not-allowed disabled:hover:tw-border-line-weak',
-    readOnly:
-        'read-only:tw-bg-box-disabled read-only:tw-border-none read-only:tw-text-text-weak read-only:hover:tw-cursor-not-allowed read-only:hover:tw-border-line-weak',
-};
-
-export const InputStylesDarkTheme: Record<InputStyleGroup, string> = {
-    base: 'dark:tw-w-full dark:tw-flex dark:tw-items-center dark:tw-justify-between dark:tw-gap-2 dark:tw-px-3 dark:tw-transition dark:tw-text-sm dark:tw-font-sans dark:tw-relative dark:tw-bg-base dark:tw-border dark:tw-rounded dark:tw-line-strong',
-    width: 'dark:tw-w-full',
-    height: 'dark:tw-h-9',
+export const InputStyles: Record<InputStyleGroup, string> = {
+    base: 'tw-flex tw-items-center tw-justify-between tw-gap-2 tw-px-3 tw-transition tw-text-sm tw-font-sans tw-relative tw-bg-base tw-border tw-rounded tw-line-strong dark:tw-w-full dark:tw-flex dark:tw-items-center dark:tw-justify-between dark:tw-gap-2 dark:tw-px-3 dark:tw-transition dark:tw-text-sm dark:tw-font-sans dark:tw-relative dark:tw-bg-base dark:tw-border dark:tw-rounded dark:tw-line-strong',
+    width: 'tw-w-full dark:tw-w-full',
+    height: 'tw-h-9 dark:tw-h-9',
     element:
-        'dark:tw-border-line-strong dark:tw-text-weak dark:tw-placeholder-text-x-weak dark:tw-outline-none dark:tw-p-2',
-    focus: 'dark:focus:tw-border-line-xx-strong',
-    focusWithin: 'dark:focus-within:tw-border-line-xx-strong dark:focus-within:hover:tw-border-line-xx-strong',
-    hover: 'dark:hover:tw-border-line-x-strong',
+        'tw-border-line-strong tw-text-text tw-placeholder-text-x-weak tw-outline-none tw-p-2 dark:tw-border-line-strong dark:tw-text-weak dark:tw-placeholder-text-x-weak dark:tw-outline-none dark:tw-p-2',
+    focus: 'focus:tw-border-line-xx-strong dark:focus:tw-border-line-xx-strong',
+    focusWithin:
+        'focus-within:tw-border-line-xx-strong focus-within:hover:tw-border-line-xx-strong dark:focus-within:tw-border-line-xx-strong dark:focus-within:hover:tw-border-line-xx-strong',
+    hover: 'hover:tw-border-line-x-strong dark:hover:tw-border-line-x-strong',
     disabled:
-        'dark:disabled:tw-bg-box-disabled dark:disabled:tw-border-line-weak dark:disabled:tw-text-text-disabled dark:disabled:hover:tw-cursor-not-allowed dark:disabled:hover:tw-border-line-weak',
+        'disabled:tw-bg-box-disabled disabled:tw-border-line-weak disabled:tw-text-text-disabled disabled:hover:tw-cursor-not-allowed disabled:hover:tw-border-line-weak dark:disabled:tw-bg-box-disabled dark:disabled:tw-border-line-weak dark:disabled:tw-text-text-disabled dark:disabled:hover:tw-cursor-not-allowed dark:disabled:hover:tw-border-line-weak',
     readOnly:
-        'dark:read-only:tw-bg-box-disabled dark:read-only:tw-border-none dark:read-only:tw-text-text-weak dark:read-only:hover:tw-cursor-not-allowed',
+        'read-only:tw-bg-box-disabled read-only:tw-border-none read-only:tw-text-text-weak read-only:hover:tw-cursor-not-allowed read-only:hover:tw-border-line-weak dark:read-only:tw-bg-box-disabled dark:read-only:tw-border-none dark:read-only:tw-text-text-weak dark:read-only:hover:tw-cursor-not-allowed',
 };
 
 export const HelperText = ({
@@ -116,7 +102,6 @@ export const InputActions = ({
     disabled,
     obfuscated,
     isObfuscated,
-    status,
     callbacks,
     dataTestId = 'fondue-input-actions',
 }: InputActionsProps): ReactElement => {
@@ -146,23 +131,11 @@ export const InputActions = ({
                     data-test-id={`${dataTestId}-clear`}
                 />
             )}
-            {status && GetStatusIcon(status, dataTestId)}
         </span>
     );
 };
 
 InputActions.displayName = 'FondueInputActionButtons';
-
-export type ExtraAction = {
-    icon: ReactElement;
-    tooltip: TooltipProps;
-    callback: () => void;
-};
-
-type InputExtraActionsProps = {
-    actions: ExtraAction[];
-    dataTestId: string;
-};
 
 export const InputExtraActions = ({ actions, dataTestId }: InputExtraActionsProps) => {
     return actions.map((action, i) => {
