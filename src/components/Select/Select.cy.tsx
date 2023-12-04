c7094cdb2b2d09a2fd744914843c09de1c4bc560
Ryan Carville <ryan.carville@frontify.com>
2023-12-04 10:59:34 +0100

    feat(new): basic Select tests

--- src/components/Select/Select.cy.tsx
+++ src/components/Select/Select.cy.tsx
@@ -1,15 +1,39 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { Select } from "./Select";
+import { SelectGroupItem } from '@components/SelectGroupItem/SelectGroupItem';
+import { Select } from './Select';
+import { SelectItem, SelectItemProps } from '@components/SelectItem/SelectItem';
 
-const Select_FOO = "bar";
+const SELECT_ID = '[data-test-id=fondue-select]';
+// const SELECT_GROUP_ID = '[data-test-id=fondue-select-group]';
+// const SELECT_ITEM_ID = '[data-test-id=fondue-select-item]';
+const ITEMS: SelectItemProps[] = [
+    { id: '1', value: 'value1' },
+    { id: '2', value: 'value2' },
+    { id: '3', value: 'value3' },
+    { id: '4', value: 'value4' },
+];
 
-describe("Select Component", () => {
-    it("should render foo text correctly", () => {
-        cy.mount(<Select foo={Select_FOO} />);
+const BASE_COMPONENT = (
+    <Select>
+        <SelectGroupItem>
+            {ITEMS.map((item) => (
+                <SelectItem key={`${item.id}-key`} id={item.id} value={item.value} />
+            ))}
+        </SelectGroupItem>
+    </Select>
+);
 
-        cy.get("[data-test-id=select]").as("Select");
+describe('Select Component', () => {
+    it('should render component', () => {
+        cy.mount(BASE_COMPONENT);
+        cy.get(SELECT_ID).as('Select');
+        cy.get('@Select').should('exist');
+    });
 
-        cy.get("@Select").contains(Select_FOO);
+    it('should be visible', () => {
+        cy.mount(BASE_COMPONENT);
+        cy.get(SELECT_ID).as('Select');
+        cy.get('@Select').should('be.visible');
     });
 });
