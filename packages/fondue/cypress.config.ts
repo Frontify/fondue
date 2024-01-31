f7c9001281a44b12488a4e917350720721c2a407
Samuel Alev <samuel.alev@frontify.com>
2024-01-31 18:03:49 +0100

    fix

--- packages/fondue/cypress.config.ts
+++ packages/fondue/cypress.config.ts
@@ -8,9 +8,6 @@ export default defineConfig({
         runMode: 2,
     },
     component: {
-        setupNodeEvents() {
-            return require('./cypress/plugins/index.ts');
-        },
         viewportHeight: 1280,
         viewportWidth: 800,
         specPattern: './src/**/*.{spec,cy}.{ts,tsx}',
