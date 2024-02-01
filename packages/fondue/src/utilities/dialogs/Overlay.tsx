ea44b9e104f3f2f729728d3dbe952871ebfcc516
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2024-02-01 13:02:14 +0100

    fix: stop scrolling only on non modal popups

--- packages/fondue/src/utilities/dialogs/Overlay.tsx
+++ packages/fondue/src/utilities/dialogs/Overlay.tsx
@@ -80,15 +80,17 @@ export const Overlay = ({
     });
 
     useEffect(() => {
-        if (open && modality !== Modality.NonModal) {
-            document.body.style.pointerEvents = 'none';
-            if (strategy === 'fixed') {
-                document.body.style.overflow = 'hidden';
+        if (modality !== Modality.NonModal) {
+            if (open) {
+                document.body.style.pointerEvents = 'none';
+                if (strategy === 'fixed') {
+                    document.body.style.overflow = 'hidden';
+                }
             }
-        }
-        if (!open) {
-            document.body.style.pointerEvents = 'auto';
-            document.body.style.overflow = 'auto';
+            if (!open) {
+                document.body.style.pointerEvents = 'auto';
+                document.body.style.overflow = 'auto';
+            }
         }
     }, [open, modality, strategy]);
 
