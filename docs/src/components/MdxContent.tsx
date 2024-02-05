c15287cef4bdcc82655f692dbf4812fb460df5d2
Noah Waldner <noah.waldner@frontify.com>
2024-02-05 17:57:28 +0100

    update lockfile

--- docs/src/components/MdxContent.tsx
+++ docs/src/components/MdxContent.tsx
@@ -2,7 +2,7 @@
 
 import { useMemo } from 'react';
 import { getMDXComponent } from 'mdx-bundler/client';
-import * as customComponents from 'components';
+import * as customComponents from '@frontify/fondue-components';
 
 export const MdxContent = ({ data }: { data: string }) => {
     const Component = useMemo(() => getMDXComponent(data), [data]);
