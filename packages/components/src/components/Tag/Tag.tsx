51916dda3e8b11fc0dde7b451608f3c0a57f2f81
Noah Waldner <noah.waldner@frontify.com>
2024-02-05 21:05:36 +0100

    add eslint

--- packages/components/src/components/Tag/Tag.tsx
+++ packages/components/src/components/Tag/Tag.tsx
@@ -1,7 +1,7 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 type TagProps = {
-    children: any;
+    children: React.ReactNode;
 };
 export const Tag = ({ children }: TagProps) => {
     return <div className="bg-dark-red tw-bg-black-5 tw-bg-box-disabled">{children}</div>;
