3937f9cef3294a6d477ac2ffc4783724565945c6
fulopdaniel <daniel.fulop@frontify.com>
2024-02-06 15:24:26 +0100

    Add mentions plugin

---
+++ packages/fondue/src/components/RichTextEditor/Plugins/MentionPlugin/MentionInputMarkupElement/MentionInputElement.tsx
@@ -0,0 +1,29 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { cn, withRef } from '@udecode/cn';
+import { PlateElement, getHandler } from '@udecode/plate';
+
+export const MentionInputElement = withRef<
+    typeof PlateElement,
+    {
+        onClick?: (mentionNode: unknown) => void;
+    }
+>(({ className, onClick, ...props }, ref) => {
+    const { children, element } = props;
+
+    return (
+        <PlateElement
+            ref={ref}
+            asChild
+            data-slate-value={element.value}
+            className={cn(
+                'tw-inline-block tw-rounded-sm tw-shadow-none tw-font-bold tw-leading-[10px] tw-m-0 tw-py-0.5 tw-px-1.5 tw-text-[rgb(130,95,255)] tw-bg-[rgb(227,232,246)]',
+                className,
+            )}
+            onClick={getHandler(onClick, element)}
+            {...props}
+        >
+            <span>{children}</span>
+        </PlateElement>
+    );
+});
