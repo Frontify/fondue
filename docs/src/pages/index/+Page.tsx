f71b8b6c1853d2019d776c54f8af53a8a65649c9
Samuel Alev <samuel.alev@frontify.com>
2024-02-07 18:50:06 +0100

    update

--- docs/src/pages/index/+Page.tsx
+++ docs/src/pages/index/+Page.tsx
@@ -1,9 +1,5 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 export const Page = () => {
-    return (
-        <>
-            <h1 className="tw-font-bold">Welcome to Fondue</h1>
-        </>
-    );
+    return <h1 className="tw-font-bold">Welcome to Fondue</h1>;
 };
--- docs/src/renderer/types.ts
+++ docs/src/renderer/types.ts
@@ -1,7 +1,5 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { type ReactElement } from 'react';
-
 import { type Component } from '#contentlayer/generated';
 
 // https://vike.dev/pageContext#typescript
@@ -10,7 +8,6 @@ declare global {
     // eslint-disable-next-line @typescript-eslint/no-namespace
     namespace Vike {
         interface PageContext {
-            Page: () => ReactElement;
             data?: {
                 /** Value for <title> defined dynamically by by /pages/some-page/+data.ts */
                 title?: string;
