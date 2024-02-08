2e91f4aa2880625f295b6665a287cf7f1da5ef20
Samuel Alev <samuel.alev@frontify.com>
2024-02-08 14:52:34 +0100

    feat: add more documentType

--- docs/contentlayer.config.ts
+++ docs/contentlayer.config.ts
@@ -2,9 +2,50 @@
 
 import { defineDocumentType, makeSource } from 'contentlayer/source-files';
 
-export const Component = defineDocumentType(() => ({
+const EXCLUDES = ['node_modules', '.contentlayer', 'dist', 'package.json', 'tsconfig.json', 'tsconfig.node.json'];
+const FONDUE_COMPONENTS_PATH = 'node_modules/@frontify/fondue-components';
+
+const Component = defineDocumentType(() => ({
     name: 'Component',
-    filePathPattern: '**/*.mdx',
+    filePathPattern: `${FONDUE_COMPONENTS_PATH}/**/*.mdx`,
+    contentType: 'mdx',
+    fields: {
+        title: { type: 'string', required: true },
+    },
+    computedFields: {
+        url: {
+            type: 'string',
+            resolve: (doc) => doc._raw.sourceFileDir.toLocaleLowerCase(),
+        },
+        route: {
+            type: 'string',
+            resolve: (doc) => doc._raw.sourceFileDir.toLocaleLowerCase().split('/').at(-1),
+        },
+    },
+}));
+
+const Icon = defineDocumentType(() => ({
+    name: 'Icon',
+    filePathPattern: `**/*.mdx`, // To be changed with the package path
+    contentType: 'mdx',
+    fields: {
+        title: { type: 'string', required: true },
+    },
+    computedFields: {
+        url: {
+            type: 'string',
+            resolve: (doc) => doc._raw.sourceFileDir.toLocaleLowerCase(),
+        },
+        route: {
+            type: 'string',
+            resolve: (doc) => doc._raw.sourceFileDir.toLocaleLowerCase().split('/').at(-1),
+        },
+    },
+}));
+
+const Token = defineDocumentType(() => ({
+    name: 'Token',
+    filePathPattern: `**/*.mdx`, // To be changed with the package path
     contentType: 'mdx',
     fields: {
         title: { type: 'string', required: true },
@@ -22,6 +63,8 @@ export const Component = defineDocumentType(() => ({
 }));
 
 export default makeSource({
-    contentDirPath: 'node_modules/@frontify/fondue-components',
-    documentTypes: [Component],
+    contentDirPath: '.',
+    contentDirInclude: [FONDUE_COMPONENTS_PATH],
+    contentDirExclude: [...EXCLUDES, ...EXCLUDES.map((path) => `${FONDUE_COMPONENTS_PATH}/${path}`)],
+    documentTypes: [Component, Icon, Token],
 });
