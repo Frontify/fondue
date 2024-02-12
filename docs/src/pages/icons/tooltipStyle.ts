502bebd14fa46f2150f423f4db628a659f838bd5
Samuel Alev <samuel.alev@frontify.com>
2024-02-12 12:16:00 +0100

    feat: add icon preview tooltip

---
+++ docs/src/pages/icons/tooltipStyle.ts
@@ -0,0 +1,4 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+export const tooltipStyle =
+    'before:tw-content-[attr(data-tooltip-text)] before:tw-absolute before:tw-opacity-0 before:tw-left-2/4 before:-tw-translate-x-2/4 before:tw-top-full before:-tw-mt-px before:tw-p-2 before:tw-whitespace-nowrap before:tw-rounded before:tw-text-center hover:before:tw-block focus-within:before:tw-block before:tw-bg-box-neutral-inverse before:tw-z-10 before:tw-text-white hover:before:tw-opacity-100 focus-within:before:tw-opacity-100 before:tw-transition-opacity motion-reduce:before:tw-transition-none';
--- docs/src/pages/tokens/+Page.tsx
+++ docs/src/pages/tokens/+Page.tsx
@@ -3,7 +3,7 @@
 export const Page = () => {
     return (
         <div className="tw-p-8">
-            <h1 className="tw-text-3xl tw-font-bold">Tokens</h1>
+            <h1 className="tw-text-2xl tw-font-bold">Tokens</h1>
         </div>
     );
 };
