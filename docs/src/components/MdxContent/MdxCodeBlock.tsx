523c408af7c09f60984188ffe47d77879deacaf1
Noah Waldner <noahwaldner@users.noreply.github.com>
2024-02-21 16:33:47 +0100

    Update docs/src/components/MdxContent/MdxCodeBlock.tsx
    Co-authored-by: Samuel Alev <samuel.alev@frontify.com>

--- docs/src/components/MdxContent/MdxCodeBlock.tsx
+++ docs/src/components/MdxContent/MdxCodeBlock.tsx
@@ -5,9 +5,9 @@ import { type DetailedHTMLProps, type HTMLAttributes } from 'react';
 import { EditableComponent } from '../EditableComponent/EditableComponent';
 
 export const MdxCodeBlock = (props: DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>) => {
-    if (props.children) {
-        return <EditableComponent language={props.className?.split('language-')[1]} code={props.children as string} />;
-    } else {
+    if (!props.children) {
         throw new Error('Invalid Code Content');
     }
+
+    return <EditableComponent language={props.className?.split('language-')[1]} code={props.children as string} />;
 };
