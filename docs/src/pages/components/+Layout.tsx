b82527d04bb0d335baf529f1136cc2201b9c2859
Noah Waldner <noah.waldner@frontify.com>
2024-02-21 16:15:05 +0100

    move navigation to component

--- docs/src/pages/components/+Layout.tsx
+++ docs/src/pages/components/+Layout.tsx
@@ -2,59 +2,16 @@
 
 import { type ReactNode } from 'react';
 
-import { type Component, allComponents } from '#contentlayer/generated';
-
 import { Layout as RootLayout } from '../+Layout';
+import { ComponentNavigation } from '../../components/ComponentNavigation/ComponentNavigation';
 
 export const Layout = ({ children }: { children: ReactNode }) => {
-    type ComponentTree = {
-        [key: string]: Component[];
-    };
-
-    const componentTree = allComponents.reduce((accumulator: ComponentTree, current) => {
-        if (!accumulator[current.parentFolder]) {
-            accumulator[current.parentFolder] = [];
-        }
-        accumulator[current.parentFolder]!.push(current);
-        return accumulator;
-    }, {});
-
     return (
         <RootLayout>
             <div className="tw-h-full tw-flex tw-w-full">
                 <nav className="tw-p-8 lg:tw-flex tw-hidden tw-flex-col tw-w-72 tw-shrink-0">
-                    <span className="tw-text-lg tw-font-bold">Components</span>
-                    <ul className="tw-mt-4">
-                        {Object.keys(componentTree).map((componentGroupKey) => {
-                            const componentGroup = componentTree[componentGroupKey] || [];
-                            if (componentGroup.length > 1) {
-                                return (
-                                    <li className="tw-my-2" key={componentGroupKey}>
-                                        <span className="tw-font-semibold tw-capitalize">{componentGroupKey}</span>
-                                        <ul className="tw-ml-4 tw-my-1">
-                                            {componentGroup.map((component) => (
-                                                <li className="tw-my-1" key={componentGroupKey}>
-                                                    <a href={`/components/${component.route}`}>{component.title}</a>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    </li>
-                                );
-                            }
-                            if (componentGroup[0]) {
-                                return (
-                                    <li key={componentGroupKey}>
-                                        <a className="tw-font-semibold" href={`/components/${componentGroup[0].route}`}>
-                                            {componentGroup[0].title}
-                                        </a>
-                                    </li>
-                                );
-                            }
-                            return null;
-                        })}
-                    </ul>
+                    <ComponentNavigation />
                 </nav>
-
                 {children}
             </div>
         </RootLayout>
