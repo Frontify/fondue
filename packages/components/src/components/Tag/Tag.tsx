5d55ba05df5ac8cb8d0dc3ce40decc96bde2e173
Noah Waldner <noah.waldner@frontify.com>
2024-02-21 16:40:09 +0100

    add tag styling

--- packages/components/src/components/Tag/Tag.tsx
+++ packages/components/src/components/Tag/Tag.tsx
@@ -8,7 +8,10 @@ type TagProps = {
 
 export const Tag = ({ children }: TagProps) => {
     return (
-        <div className="tw-text-text tw-underline" data-test-id="fondue-tag">
+        <div
+            className="tw-text-text tw-bg-box-selected tw-border-box-selected-strong tw-border-2 tw-p-2 tw-rounded tw-w-fit"
+            data-test-id="fondue-tag"
+        >
             {children}
         </div>
     );
