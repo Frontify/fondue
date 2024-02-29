151a1f39401835e9b733f866b9d693e065d3d0c1
Noah Waldner <noahwaldner@users.noreply.github.com>
2024-02-29 17:01:38 +0100

    Update docs/src/components/EditableComponent/CopyCodeButton.tsx
    Co-authored-by: Samuel Alev <samuel.alev@frontify.com>

--- docs/src/components/EditableComponent/CopyCodeButton.tsx
+++ docs/src/components/EditableComponent/CopyCodeButton.tsx
@@ -28,7 +28,7 @@ export const CopyCodeButton = ({ onClick }: CopyButtonProps) => {
                 className={`tw-transition-all tw-duration-500 tw-flex tw-overflow-hidden tw-absolute tw-right-0 tw-bottom-0 ${copiedSuccessfully ? '-tw-translate-y-full tw-opacity-0' : 'tw-opacity-1'}`}
             >
                 <span
-                    className={`tw-pr-2 tw-transition-all tw-duration-500 tw-whitespace-nowrap  ${isHovered ? 'tw-opacity-100' : 'tw-translate-x-full tw-opacity-0'}`}
+                    className={`tw-pr-2 tw-transition-all tw-duration-500 tw-whitespace-nowrap ${isHovered ? 'tw-opacity-100' : 'tw-translate-x-full tw-opacity-0'}`}
                 >
                     Copy to clipboard
                 </span>
