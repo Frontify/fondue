39ca3e431f87b526eb7ea96b0147b3c5809b17df
Noah Waldner <noah.waldner@frontify.com>
2024-03-04 10:39:48 +0100

    fix tests

--- docs/src/utils/codeUtil.ts
+++ docs/src/utils/codeUtil.ts
@@ -4,6 +4,6 @@ import { COMPONENT_IMPORT_PATH } from '#/constants';
 import { type Component } from '#contentlayer/generated';
 
 export const getImportStatement = (props: Component) => {
-    const additionalExports = props.additionalExports ? `, ${props.additionalExports}` : "";
-    return `import { ${props.title}${props.additionalExports?additionalExports:""} } from '${COMPONENT_IMPORT_PATH}';`;
+    const additionalExports = props.additionalExports ? `, ${props.additionalExports}` : '';
+    return `import { ${props.title}${props.additionalExports ? additionalExports : ''} } from '${COMPONENT_IMPORT_PATH}';`;
 };
