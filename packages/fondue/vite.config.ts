c12f131063420e453bc0376b1878b846568bcefe
Noah Waldner <noah.waldner@frontify.com>
2024-03-04 15:13:24 +0100

    export new components from fondue

--- packages/fondue/vite.config.ts
+++ packages/fondue/vite.config.ts
@@ -1,10 +1,10 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import react from '@vitejs/plugin-react';
 import { build } from 'esbuild';
 import { Plugin, defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import tsConfigPaths from 'vite-tsconfig-paths';
-import react from '@vitejs/plugin-react';
 
 import { dependencies as dependenciesMap, peerDependencies as peerDependenciesMap } from './package.json';
 
@@ -81,9 +81,10 @@ export default defineConfig({
     plugins: [react(), tsConfigPaths(), dts({ insertTypesEntry: true, rollupTypes: true }), bundleIconsInDevPlugin()],
     build: {
         lib: {
-            entry: './src/index.ts',
+            entry: ['./src/index.ts'],
             fileName: (format: string) => `[name].${format}.js`,
         },
+        outDir: "./dist/fondue",
         sourcemap: true,
         minify: true,
         rollupOptions: {
