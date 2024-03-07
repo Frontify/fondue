2b36c2c51763d7b746756aaa7c5ae68c75d900c1
Daniele <daniele.tortora@frontify.com>
2024-03-07 17:13:58 +0100

    test: add playwright ct to the component package

---
+++ packages/components/src/components/Tag/Tag.integ.tsx
@@ -0,0 +1,19 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { test, expect } from '@playwright/experimental-ct-react';
+
+import { Tag } from './Tag';
+
+test.use({ viewport: { width: 500, height: 500 } });
+
+test('should render without error', async ({ mount }) => {
+    const component = await mount(<Tag>Test</Tag>);
+    await expect(component).toContainText('Test');
+});
+
+// TODO: This test should be a responsibility of a Visual Regression tool
+test('should allow for the tag to have a border color and background color', async ({ mount }) => {
+    const component = await mount(<Tag>Test</Tag>);
+    await expect(component).toHaveCSS('background-color', 'rgb(240, 234, 250)');
+    await expect(component).toHaveCSS('border-color', 'rgb(124, 87, 255)');
+});
