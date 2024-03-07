39d9060636b2b083177f439a7e28a8caf22dc216
Noah Waldner <noahwaldner@users.noreply.github.com>
2024-03-07 11:16:31 +0100

    Update packages/fondue/vite.config.ts
    Co-authored-by: Daniele Tortora <45793869+floroz@users.noreply.github.com>

--- packages/fondue/vite.config.ts
+++ packages/fondue/vite.config.ts
@@ -9,6 +9,8 @@ import tsConfigPaths from 'vite-tsconfig-paths';
 import { dependencies as dependenciesMap, peerDependencies as peerDependenciesMap } from './package.json';
 
 const peerDependencies = Object.keys(peerDependenciesMap);
+// TODO: review this approach to exclude all deps from the final bundles.
+// The filtering is used for an hotfix and is a temporary workaround until we have refined how to improve and stabilize the deps building
 const dependencies = Object.keys(dependenciesMap).filter((dependency) => !dependency.startsWith('@frontify/fondue'));
 
 export const globals = {
