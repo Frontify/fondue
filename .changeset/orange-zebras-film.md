93be3c9d9c77c2d7e645e33aeb64b49aedb0d65e
Sam Creasey <sam.creasey@frontify.com>
2024-03-11 22:20:39 +0700

    fix: cleanup segmented controls (#1745)

---
+++ .changeset/orange-zebras-film.md
@@ -0,0 +1,7 @@
+---
+"@frontify/fondue": patch
+---
+
+fix(SegmentedControls): Update size calculations to prevent resizing glitches
+
+fix(SegmentedControls): Remove initial motion.div that appears before position has been calculated
--- packages/fondue/src/components/SegmentedControls/SegmentedControls.tsx
+++ packages/fondue/src/components/SegmentedControls/SegmentedControls.tsx
@@ -10,7 +10,7 @@ import { RadioGroupState, useRadioGroupState } from '@react-stately/radio';
 import { FOCUS_STYLE } from '@utilities/focusStyle';
 import { merge } from '@utilities/merge';
 import { motion } from 'framer-motion';
-import { ReactElement, forwardRef, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { ReactElement, forwardRef, useLayoutEffect, useMemo, useRef, useState } from 'react';
 
 export type IconItem = {
     id: string;
@@ -85,15 +85,7 @@ const SegmentedControlsItem = forwardRef<HTMLDivElement, SegmentedControlsItemPr
     };
 
     return (
-        <div
-            key={item.id}
-            ref={ref}
-            className={merge([
-                'tw-relative',
-                "after:tw-content-[''] after:tw-border-r after:tw-border-solid after:tw-border-line-x-strong after:tw-absolute after:tw-right-0 after:tw-h-[2.2rem] last:after:tw-hidden",
-                isFocusVisible && FOCUS_STYLE,
-            ])}
-        >
+        <div key={item.id} ref={ref} className={merge(['tw-relative', isFocusVisible && FOCUS_STYLE])}>
             <VisuallyHidden>
                 <input {...inputProps} {...focusProps} data-test-id="fondue-segmented-controls-input" ref={inputRef} />
             </VisuallyHidden>
@@ -104,7 +96,7 @@ const SegmentedControlsItem = forwardRef<HTMLDivElement, SegmentedControlsItemPr
                 role="none"
                 data-test-id={getSegmentedControlsItemTestId()}
                 className={merge([
-                    'tw-relative tw-w-full tw-py-2 tw-inline-flex tw-justify-center tw-items-center tw-font-sans tw-font-normal tw-h-full tw-text-center',
+                    'tw-relative tw-w-full tw-py-1.5 tw-inline-flex tw-justify-center tw-items-center tw-font-sans tw-font-normal tw-h-full tw-text-center',
                     size === 'small' ? 'tw-px-2' : 'tw-px-4',
                     isActive && !disabled
                         ? 'tw-transition tw-ease-in-out tw-delay-300 tw-text-text tw-bg-base'
@@ -140,8 +132,12 @@ export const SegmentedControls = ({
     const groupProps = { onChange, value: activeItemId, label: ariaLabel, isDisabled: disabled };
     const radioGroupState = useRadioGroupState(groupProps);
     const { radioGroupProps, labelProps } = useRadioGroup(groupProps, radioGroupState);
+
     const itemsRef = useRef<(HTMLDivElement | null)[]>([]);
-    const [activeBorderDimensions, setActiveBorderDimensions] = useState<{ x: string; width: string } | null>(null);
+    const itemContainerRef = useRef<HTMLFieldSetElement>(null);
+
+    const [activeBorderDimensions, setActiveBorderDimensions] = useState<{ left: string; width: string } | null>(null);
+
     const itemElements = useMemo(() => {
         return items.map((item, index) => (
             <SegmentedControlsItem
@@ -172,74 +168,66 @@ export const SegmentedControls = ({
           })
         : -1;
 
-    const width = hugWidth ? '' : 'tw-w-full';
     const alignment = hugWidth ? 'tw-flex' : 'tw-grid tw-grid-flow-col tw-auto-cols-fr tw-justify-evenly';
-    const isSmall = size === 'small';
-    const isLastElement = selectedIndex === itemsRef.current.length - 1;
 
-    const getSliderX = useCallback(() => {
-        let translateX = hugWidth ? -1 : 0;
-        translateX -= isSmall && isLastElement ? 1 : 0;
-        for (let i = 0; i < selectedIndex; i++) {
-            translateX += itemsRef.current[i]?.clientWidth ?? 0;
+    useLayoutEffect(() => {
+        const activeItem = itemsRef.current[selectedIndex];
+
+        if (activeItem && itemContainerRef.current) {
+            const fieldsetRect = itemContainerRef.current.getBoundingClientRect();
+            const activeItemRect = activeItem.getBoundingClientRect();
+
+            let left = activeItemRect.left - fieldsetRect.left;
+            let width = activeItemRect.width;
+
+            // Adjust for borders
+            if (selectedIndex === 0) {
+                // If it's the first item, cover the left border of the fieldset
+                left -= 1;
+                width += 2;
+            } else {
+                /* If it's the last item, cover the right border of the fieldset.
+                If it's not the last item, cover the left border of the next item */
+                width += 1;
+            }
+
+            const widthPercentage = (width / fieldsetRect.width) * 100;
+            const leftPercentage = (left / fieldsetRect.width) * 100;
+
+            setActiveBorderDimensions({ width: `${widthPercentage}%`, left: `${leftPercentage}%` });
         }
-
-        translateX -= isLastElement || hugWidth ? 0 : 1;
-        return `${translateX}px`;
-    }, [selectedIndex, isSmall, isLastElement, hugWidth]);
-
-    const getSliderWidth = useCallback(() => {
-        const width = isSmall || hugWidth ? -1 : 0;
-
-        return `${(itemsRef.current[selectedIndex]?.clientWidth ?? 0) + width}px`;
-    }, [selectedIndex, isSmall, hugWidth]);
-
-    const setSliderDimensions = useCallback(() => {
-        const dimensions = itemsRef.current ? { x: getSliderX(), width: getSliderWidth() } : { x: '0px', width: '0px' };
-        setActiveBorderDimensions(dimensions);
-    }, [getSliderWidth, getSliderX]);
-
-    useEffect(() => {
-        if (selectedIndex >= 0) {
-            setSliderDimensions();
-        }
-        window.addEventListener('resize', setSliderDimensions);
-
-        return () => {
-            window.removeEventListener('resize', setSliderDimensions);
-        };
-    }, [selectedIndex, setSliderDimensions]);
+    }, [activeItemId, selectedIndex, size]);
 
     return (
-        <div className="tw-flex">
+        <div className={merge([hugWidth ? 'tw-inline-flex' : 'tw-flex', 'tw-relative'])}>
             <fieldset
                 {...radioGroupProps}
                 data-test-id="fondue-segmented-controls"
                 className={merge([
-                    'tw-relative tw-h-9 tw-p-0 tw-border tw-border-solid tw-border-line-strong tw-m-0 tw-bg-base-alt tw-rounded tw-font-sans tw-text-sm tw-select-none',
-                    width,
+                    'tw-relative tw-divide-x tw-divide-line-strong tw-w-full tw-h-9 tw-p-0 tw-border tw-border-solid tw-border-line-strong tw-m-0 tw-bg-base-alt tw-rounded tw-font-sans tw-text-sm tw-select-none',
                     alignment,
                 ])}
+                ref={itemContainerRef}
             >
                 {itemElements}
             </fieldset>
             <VisuallyHidden>
                 <span {...labelProps}>{ariaLabel}</span>
             </VisuallyHidden>
-            <motion.div
-                aria-hidden="true"
-                // div border is not included in width so it must be subtracted from translation.
-                animate={activeBorderDimensions ?? { x: '0px', width: '0px' }}
-                initial={false}
-                transition={{ type: 'tween', duration: 0.3 }}
-                hidden={!activeItemId}
-                className={merge([
-                    'tw-absolute tw-h-9 tw-box-content tw-border tw-rounded tw-pointer-events-none tw-top-[0.96rem]',
-                    disabled
-                        ? 'tw-border-line-x-strong hover:tw-cursor-not-allowed'
-                        : 'tw-border-line-xx-strong tw-bg-transparent',
-                ])}
-            />
+            {activeItemId && activeBorderDimensions && (
+                <motion.div
+                    aria-hidden="true"
+                    animate={activeBorderDimensions}
+                    initial={false}
+                    transition={{ type: 'tween', duration: 0.3 }}
+                    className={merge([
+                        'tw-absolute tw-h-9 tw-box-border tw-border tw-rounded tw-pointer-events-none tw-top-0',
+                        disabled
+                            ? 'tw-border-line-x-strong hover:tw-cursor-not-allowed'
+                            : 'tw-border-line-xx-strong tw-bg-transparent',
+                    ])}
+                />
+            )}
         </div>
     );
 };
