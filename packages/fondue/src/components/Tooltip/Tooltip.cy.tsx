d2ee474778b3a8954a22542cbad2abb077284463
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2024-03-13 23:58:01 +0100

    fixed duplicate test

--- packages/fondue/src/components/Tooltip/Tooltip.cy.tsx
+++ packages/fondue/src/components/Tooltip/Tooltip.cy.tsx
@@ -1,5 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import { PopperPlacement } from '@components/Popper/types';
 import { Tooltip } from '@components/Tooltip/Tooltip';
 import IconIcon24 from '@foundation/Icon/Generated/IconIcon24';
 
@@ -26,101 +27,24 @@ describe('Tooltip Component', () => {
     });
 
     describe('Positioning', () => {
-        it('should render the tooltip in an initial Top placement', () => {
-            cy.mount(
-                <Tooltip content="Hello There" placement="top" flip={false}>
-                    <span data-test-id="tooltip-trigger">
-                        <IconIcon24 />
-                    </span>
-                </Tooltip>,
-            );
-            cy.get(TOOLTIP_TRIGGER).should('exist');
-            cy.get(TOOLTIP_TRIGGER).trigger('pointerover', {
-                eventConstructor: 'MouseEvent',
-                pointerType: 'mouse',
+        const positions = ['top', 'bottom', 'right', 'left', 'bottom-start', 'bottom-end'];
+        for (const position of positions) {
+            it(`should render the tooltip in a position: ${position}`, () => {
+                cy.mount(
+                    <Tooltip content="Hello Positioning" placement={`${position}` as PopperPlacement} flip={false}>
+                        <span data-test-id="tooltip-trigger">
+                            <IconIcon24 />
+                        </span>
+                    </Tooltip>,
+                );
+                cy.get(TOOLTIP_TRIGGER).should('exist');
+                cy.get(TOOLTIP_TRIGGER).trigger('pointerover', {
+                    eventConstructor: 'MouseEvent',
+                    pointerType: 'mouse',
+                });
+                cy.get(TOOLTIP_SELECTOR).parent().should('have.attr', 'data-popper-placement', position);
             });
-            cy.get(TOOLTIP_SELECTOR).parent().should('have.attr', 'data-popper-placement', 'top');
-        });
-
-        it('should render the tooltip in an initial Bottom placement', () => {
-            cy.mount(
-                <Tooltip content="Hello There" placement="bottom" flip={false}>
-                    <span data-test-id="tooltip-trigger">
-                        <IconIcon24 />
-                    </span>
-                </Tooltip>,
-            );
-            cy.get(TOOLTIP_TRIGGER).should('exist');
-            cy.get(TOOLTIP_TRIGGER).trigger('pointerover', {
-                eventConstructor: 'MouseEvent',
-                pointerType: 'mouse',
-            });
-            cy.get(TOOLTIP_SELECTOR).parent().should('have.attr', 'data-popper-placement', 'bottom');
-        });
-
-        it('should render the tooltip in an initial Right placement', () => {
-            cy.mount(
-                <Tooltip content="Hello There" placement="right" flip={false}>
-                    <span data-test-id="tooltip-trigger">
-                        <IconIcon24 />
-                    </span>
-                </Tooltip>,
-            );
-            cy.get(TOOLTIP_TRIGGER).should('exist');
-            cy.get(TOOLTIP_TRIGGER).trigger('pointerover', {
-                eventConstructor: 'MouseEvent',
-                pointerType: 'mouse',
-            });
-            cy.get(TOOLTIP_SELECTOR).parent().should('have.attr', 'data-popper-placement', 'right');
-        });
-
-        it('should render the tooltip in an initial Left placement', () => {
-            cy.mount(
-                <Tooltip content="Hello There" placement="left" flip={false}>
-                    <span data-test-id="tooltip-trigger">
-                        <IconIcon24 />
-                    </span>
-                </Tooltip>,
-            );
-            cy.get(TOOLTIP_TRIGGER).should('exist');
-            cy.get(TOOLTIP_TRIGGER).trigger('pointerover', {
-                eventConstructor: 'MouseEvent',
-                pointerType: 'mouse',
-            });
-            cy.get(TOOLTIP_SELECTOR).parent().should('have.attr', 'data-popper-placement', 'left');
-        });
-
-        it('should render the tooltip with start alignment', () => {
-            cy.mount(
-                <Tooltip content="Hello There" placement="bottom-start" flip={false}>
-                    <span data-test-id="tooltip-trigger">
-                        <IconIcon24 />
-                    </span>
-                </Tooltip>,
-            );
-            cy.get(TOOLTIP_TRIGGER).should('exist');
-            cy.get(TOOLTIP_TRIGGER).trigger('pointerover', {
-                eventConstructor: 'MouseEvent',
-                pointerType: 'mouse',
-            });
-            cy.get(TOOLTIP_SELECTOR).parent().should('have.attr', 'data-popper-placement', 'bottom-start');
-        });
-
-        it('should render the tooltip with start alignment', () => {
-            cy.mount(
-                <Tooltip content="Hello There" placement="bottom-end" flip={false}>
-                    <span data-test-id="tooltip-trigger">
-                        <IconIcon24 />
-                    </span>
-                </Tooltip>,
-            );
-            cy.get(TOOLTIP_TRIGGER).should('exist');
-            cy.get(TOOLTIP_TRIGGER).trigger('pointerover', {
-                eventConstructor: 'MouseEvent',
-                pointerType: 'mouse',
-            });
-            cy.get(TOOLTIP_SELECTOR).parent().should('have.attr', 'data-popper-placement', 'bottom-end');
-        });
+        }
     });
 
     describe('Flip', () => {
@@ -274,39 +198,28 @@ describe('Tooltip Component', () => {
             cy.get(TOOLTIP_SELECTOR).find('p').should('have.css', 'padding', '4px 8px 6px');
         });
 
-        it('should render with a default max width of 200px', () => {
-            cy.mount(
-                <Tooltip content="Lorem ipsum dolor sit amet, consectetur adipisicing elit.">
-                    <span data-test-id="tooltip-trigger">
-                        <IconIcon24 />
-                    </span>
-                </Tooltip>,
-            );
-            cy.get(TOOLTIP_TRIGGER).should('exist');
-            cy.get(TOOLTIP_TRIGGER).trigger('pointerover', {
-                eventConstructor: 'MouseEvent',
-                pointerType: 'mouse',
+        const widthTests = [
+            { value: undefined, expected: '200px' },
+            { value: 150, expected: '150px' },
+        ];
+        for (const test of widthTests) {
+            it(`should render with a max width of: ${test.expected}`, () => {
+                cy.mount(
+                    <Tooltip content="Hello max Width of the Tooltip" maxWidth={test.value}>
+                        <span data-test-id="tooltip-trigger">
+                            <IconIcon24 />
+                        </span>
+                    </Tooltip>,
+                );
+                cy.get(TOOLTIP_TRIGGER).should('exist');
+                cy.get(TOOLTIP_TRIGGER).trigger('pointerover', {
+                    eventConstructor: 'MouseEvent',
+                    pointerType: 'mouse',
+                });
+                cy.get(TOOLTIP_SELECTOR).should('have.text', 'Hello max Width of the Tooltip');
+                cy.get(TOOLTIP_SELECTOR).should('have.css', 'max-width', `${test.expected}`);
             });
-            cy.get(TOOLTIP_SELECTOR).should('have.text', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.');
-            cy.get(TOOLTIP_SELECTOR).should('have.css', 'max-width', '200px');
-        });
-
-        it('should render with a custom max width', () => {
-            cy.mount(
-                <Tooltip content="Lorem ipsum dolor sit amet, consectetur adipisicing elit." maxWidth={150}>
-                    <span data-test-id="tooltip-trigger">
-                        <IconIcon24 />
-                    </span>
-                </Tooltip>,
-            );
-            cy.get(TOOLTIP_TRIGGER).should('exist');
-            cy.get(TOOLTIP_TRIGGER).trigger('pointerover', {
-                eventConstructor: 'MouseEvent',
-                pointerType: 'mouse',
-            });
-            cy.get(TOOLTIP_SELECTOR).should('have.text', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.');
-            cy.get(TOOLTIP_SELECTOR).should('have.css', 'max-width', '150px');
-        });
+        }
 
         it('should render with a custom max height', () => {
             cy.mount(
