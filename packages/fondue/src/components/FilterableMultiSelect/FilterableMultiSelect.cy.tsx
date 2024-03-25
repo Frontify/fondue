383f4e9507116d19561f88cd73efcc26c8f00ff4
Zeid Selimovic <zeid.selimovic@frontify.com>
2024-03-25 16:26:46 +0100

    feat: Introducing FilterableMultiSelect (#1769)

---
+++ packages/fondue/src/components/FilterableMultiSelect/FilterableMultiSelect.cy.tsx
@@ -0,0 +1,155 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { Validation } from '@utilities/validation';
+import { useState } from 'react';
+import { FilterableMultiSelect } from './FilterableMultiSelect';
+import { TriggerEmphasis } from '@components/Trigger';
+
+const TRIGGER_ID = '[data-test-id=trigger]';
+const CHECKBOX_ID = '[data-test-id=checkbox]';
+const CHECKBOX_INPUT_ID = '[data-test-id=checkbox-input]';
+const TAG_ID = '[data-test-id=tag]';
+const TAG_CLOSE_BUTTON = '[data-test-id=tag-reject-icon]';
+const CHECKLIST_ID = '[data-test-id=checklist]';
+const EXCLAMATION_MARK_ICON_ID = '[data-test-id=error-state-exclamation-mark-icon]';
+const FILTER_TEXT_COMPONENT_ID = '[data-test-id=fondue-text-input-component]';
+const FILTER_TEXT_INPUT_ID = '[data-test-id=text-input]';
+
+const ITEMS = {
+    activeItemKeys: ['Short tag', 'Tag 74'],
+    items: [
+        {
+            value: 'Checkbox label 1',
+        },
+        {
+            value: 'Short tag',
+        },
+        {
+            value: 'Checkbox label 2',
+        },
+        {
+            value: 'Checkbox label 3',
+        },
+        {
+            value: 'Tag 74',
+        },
+        {
+            value: 'This is a long tag',
+        },
+    ],
+};
+
+type Props = {
+    validation?: Validation;
+    emphasis?: TriggerEmphasis;
+    disabled?: boolean;
+};
+
+const Component = ({
+    validation = Validation.Default,
+    emphasis = TriggerEmphasis.Default,
+    disabled = false,
+}: Props) => {
+    const [activeItems, setActiveItems] = useState<(string | number)[]>(ITEMS.activeItemKeys);
+    return (
+        <FilterableMultiSelect
+            items={ITEMS.items}
+            activeItemKeys={activeItems}
+            onSelectionChange={(keys) => setActiveItems(keys)}
+            validation={validation}
+            emphasis={emphasis}
+            disabled={disabled}
+            filterLabel="Filter"
+        />
+    );
+};
+
+describe('FilterableMultiSelect Component', () => {
+    it('renders with initial active items', () => {
+        cy.mount(<Component />);
+
+        cy.get(TAG_ID).should('have.length', 2);
+        cy.get(TAG_ID).should('contain', 'Short tag').and('contain', 'Tag 74');
+    });
+    it('opens dropdown select list on click', () => {
+        cy.mount(<Component />);
+
+        cy.get(TRIGGER_ID).click();
+        cy.get(CHECKLIST_ID).should('be.visible');
+    });
+    it('changes selection on click', () => {
+        cy.mount(<Component />);
+        cy.get(TRIGGER_ID).click();
+
+        cy.get(CHECKBOX_ID).first().find(CHECKBOX_INPUT_ID).click({ force: true });
+        cy.get(CHECKBOX_ID).find('svg').invoke('attr', 'name').should('eq', 'IconCheckMark16');
+        cy.get(TAG_ID).contains('Checkbox label 1');
+
+        cy.get(CHECKBOX_ID).eq(1).find(CHECKBOX_INPUT_ID).click({ force: true });
+        cy.get(CHECKBOX_ID).eq(1).find('svg').should('not.exist');
+        cy.get(TRIGGER_ID).contains('Short tag').should('not.exist');
+        cy.get(TAG_ID).first().click();
+
+        cy.get(CHECKBOX_ID).eq(1).find('svg').should('not.exist');
+
+        cy.get(CHECKBOX_ID).eq(2).find(CHECKBOX_INPUT_ID).click({ force: true });
+        cy.get(CHECKBOX_ID).eq(2).find('svg').invoke('attr', 'name').should('eq', 'IconCheckMark16');
+        cy.get(TAG_ID).contains('Checkbox label 2');
+    });
+
+    it('should show triangle exclamation icon in case of error or warning validation states', () => {
+        for (const validationState of Object.values(Validation)) {
+            if (validationState === Validation.Error || validationState === Validation.Warning) {
+                cy.mount(<Component validation={validationState} />);
+                cy.get(EXCLAMATION_MARK_ICON_ID).should('be.visible');
+                return;
+            }
+            cy.mount(<Component validation={validationState} />);
+            cy.get(TRIGGER_ID).find(EXCLAMATION_MARK_ICON_ID).should('have.length', 0);
+        }
+    });
+
+    it('should render Trigger Weak emphasis', () => {
+        cy.mount(<Component emphasis={TriggerEmphasis.Weak} />);
+        cy.get(TRIGGER_ID).should('have.class', 'tw-border-0');
+    });
+
+    it('when disabled, should hide close buttons from tags', () => {
+        cy.mount(<Component disabled={true} />);
+        cy.get(TAG_CLOSE_BUTTON).should('not.exist');
+    });
+
+    it('will display text input to search if filterable property is on', () => {
+        cy.mount(<Component />);
+        cy.get(TRIGGER_ID).click();
+        cy.get(FILTER_TEXT_COMPONENT_ID).should('exist');
+    });
+
+    it('will filter FilerableMultiSelect items based on input value', () => {
+        cy.mount(<Component />);
+        cy.get(TRIGGER_ID).click();
+        cy.get(FILTER_TEXT_COMPONENT_ID).type('checkbox label');
+        cy.get(CHECKLIST_ID).find(CHECKBOX_ID).should('have.length', 3);
+
+        cy.get(CHECKBOX_ID).first().contains('Checkbox label 1');
+    });
+
+    it('will reset filter input value when dropdown is closed', () => {
+        cy.mount(<Component />);
+        cy.get(TRIGGER_ID).click();
+
+        cy.get(FILTER_TEXT_COMPONENT_ID).type('checkbox label');
+        cy.get(CHECKLIST_ID).find(CHECKBOX_ID).should('have.length', 3);
+
+        cy.get(TRIGGER_ID).click();
+        cy.get(TRIGGER_ID).click();
+        cy.get(FILTER_TEXT_COMPONENT_ID).should('have.value', '');
+        cy.get(CHECKLIST_ID).find(CHECKBOX_ID).should('have.length', 6);
+    });
+
+    it('filter input should have focus on mount', () => {
+        cy.mount(<Component />);
+        cy.get(TRIGGER_ID).click();
+        cy.get(FILTER_TEXT_INPUT_ID).should('have.focus');
+    });
+});
---
+++ packages/fondue/src/components/FilterableMultiSelect/FilterableMultiSelect.tsx
@@ -0,0 +1,327 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { CheckboxState } from '@components/Checkbox/Checkbox';
+import { Checklist, ChecklistDirection } from '@components/Checklist/Checklist';
+import { Tag, TagSize, TagType } from '@components/Tag';
+import { Trigger, TriggerEmphasis } from '@components/Trigger';
+import { DEFAULT_DROPDOWN_MAX_HEIGHT, useDropdownAutoHeight } from '@hooks/useDropdownAutoHeight';
+import { useButton } from '@react-aria/button';
+import { FocusScope, useFocusRing } from '@react-aria/focus';
+import { Text } from '@typography/Text';
+import { EnablePortalWrapper } from '@utilities/dialogs/EnablePortalWrapper';
+import { merge } from '@utilities/merge';
+import { Validation } from '@utilities/validation';
+import { KeyboardEvent, ReactElement, ReactNode, useCallback, useEffect, useId, useRef, useState } from 'react';
+import { usePopper } from 'react-popper';
+import { getPaddingClasses, useClickOutside } from './helpers';
+import { TextInput } from '@components/TextInput';
+
+export enum FilterableMultiSelectType {
+    Default = 'Default',
+    Summarized = 'Summarized',
+}
+
+export enum FilterableMultiSelectSize {
+    Small = 'Small',
+    Medium = 'Medium',
+}
+
+export type FilterableMultiSelectItem = {
+    value: string;
+    isCategory?: boolean;
+    isDivider?: boolean;
+    avatar?: ReactNode;
+    imgSrc?: string;
+    ariaLabel?: string;
+};
+
+export type FilterableMultiSelectProps = {
+    items: FilterableMultiSelectItem[];
+    activeItemKeys: (string | number)[];
+    disabled?: boolean;
+    onSelectionChange: (keys: (string | number)[]) => void;
+    ariaLabel?: string;
+    label?: string;
+    placeholder?: string;
+    type?: FilterableMultiSelectType;
+    size?: FilterableMultiSelectSize;
+    validation?: Validation;
+    summarizedLabel?: string;
+    indeterminateItemKeys?: (string | number)[];
+    flip?: boolean;
+    emphasis?: TriggerEmphasis;
+    enablePortal?: boolean;
+    filterLabel?: string;
+    emptyFilteredResultsLabel?: string;
+};
+
+export type Item = {
+    label: string;
+    value: string;
+    avatar?: ReactNode;
+    isCategory?: boolean;
+    isDivider?: boolean;
+    imgSrc?: string;
+    ariaLabel?: string;
+};
+
+export const FilterableMultiSelect = ({
+    items,
+    activeItemKeys,
+    onSelectionChange,
+    ariaLabel = 'Select list',
+    disabled = false,
+    placeholder,
+    label,
+    type = FilterableMultiSelectType.Default,
+    size = FilterableMultiSelectSize.Medium,
+    validation = Validation.Default,
+    summarizedLabel: summarizedLabelFromProps,
+    indeterminateItemKeys,
+    flip = false,
+    emphasis = TriggerEmphasis.Default,
+    enablePortal = true,
+    filterLabel,
+    emptyFilteredResultsLabel,
+}: FilterableMultiSelectProps): ReactElement => {
+    const [open, setOpen] = useState(false);
+    const [checkboxes, setCheckboxes] = useState<Item[]>([]);
+    const [searchTerm, setSearchTerm] = useState<string>('');
+
+    const multiSelectRef = useRef<HTMLDivElement | null>(null);
+    const [multiSelectMenuRef, setMultiSelectMenuRef] = useState<null | HTMLDivElement>(null);
+    const [triggerRef, setTriggerRef] = useState<HTMLDivElement | null>(null);
+
+    const { isFocusVisible, focusProps } = useFocusRing();
+
+    const { maxHeight } = useDropdownAutoHeight({ current: triggerRef }, { isOpen: open, autoResize: true });
+
+    const hasSelectedItems = activeItemKeys.length > 0;
+    const summarizedLabel = summarizedLabelFromProps ?? [activeItemKeys.length, 'selected'].join(' ');
+
+    const handleClose = () => setOpen(false);
+
+    useClickOutside(null, handleClose, [multiSelectRef?.current as HTMLElement, multiSelectMenuRef as HTMLElement]);
+
+    const heightIsReady = maxHeight !== DEFAULT_DROPDOWN_MAX_HEIGHT;
+
+    const multiSelectContentId = useId();
+
+    const toggleOpen = () => setOpen((open) => !open);
+
+    const { buttonProps } = useButton(
+        {
+            onPress: toggleOpen,
+            onKeyDown: (e) => {
+                if (e.key === 'Escape') {
+                    setOpen(false);
+                }
+            },
+            elementType: 'div',
+        },
+        { current: triggerRef },
+    );
+
+    const toggleSelection = (key: string | number) => {
+        const keySet = new Set(activeItemKeys);
+
+        if (keySet.has(key)) {
+            keySet.delete(key);
+        } else {
+            keySet.add(key);
+        }
+
+        onSelectionChange(Array.from(keySet));
+    };
+
+    const handleSpaceBarToggle = (event: KeyboardEvent<HTMLDivElement>) => {
+        if (event.code === 'Space') {
+            toggleOpen();
+        }
+    };
+
+    const getTagType = () => {
+        if (!open) {
+            return TagType.Selected;
+        }
+        return TagType.SelectedWithFocus;
+    };
+
+    useEffect(() => {
+        setMultiSelectMenuRef(multiSelectRef?.current as HTMLDivElement);
+    }, []);
+
+    useEffect(() => {
+        setCheckboxes(
+            items.map((item) => {
+                if (indeterminateItemKeys?.includes(item.value)) {
+                    return { ...item, label: item.value, state: CheckboxState.Mixed };
+                }
+
+                return { ...item, label: item.value };
+            }),
+        );
+    }, [items, indeterminateItemKeys]);
+
+    const handleFilterChange = useCallback(
+        (newValue: string) => {
+            setSearchTerm(newValue);
+
+            setCheckboxes(
+                items.reduce((acc: Item[], item) => {
+                    if (
+                        item.isCategory ||
+                        item.isDivider ||
+                        item.value.toLowerCase().includes(newValue.toLowerCase())
+                    ) {
+                        if (indeterminateItemKeys?.includes(item.value)) {
+                            return [...acc, { ...item, label: item.value, state: CheckboxState.Mixed }];
+                        }
+
+                        return [...acc, { ...item, label: item.value }];
+                    }
+                    return acc;
+                }, []),
+            );
+        },
+        [indeterminateItemKeys, items],
+    );
+
+    useEffect(() => {
+        !open && handleFilterChange('');
+    }, [open, handleFilterChange]);
+
+    const popperInstance = usePopper(triggerRef, multiSelectMenuRef, {
+        placement: 'bottom-start',
+        strategy: 'fixed',
+        modifiers: [
+            {
+                name: 'offset',
+                options: {
+                    offset: [0, 8],
+                },
+            },
+            {
+                name: 'flip',
+                enabled: flip,
+            },
+        ],
+    });
+
+    useEffect(() => {
+        const updatePopper = async () => {
+            if (popperInstance.update) {
+                await popperInstance.update();
+            }
+        };
+
+        updatePopper().catch(console.error);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [activeItemKeys]);
+
+    return (
+        <div className="tw-relative" ref={multiSelectRef}>
+            <Trigger
+                disabled={disabled}
+                buttonProps={buttonProps}
+                isFocusVisible={isFocusVisible}
+                isOpen={open}
+                validation={validation}
+                emphasis={emphasis}
+            >
+                <div className={merge(['tw-flex tw-flex-1 tw-gap-2', getPaddingClasses(size)])} ref={setTriggerRef}>
+                    <div
+                        className="tw-flex tw-flex-1 tw-gap-2 focus:tw-outline-0"
+                        onClick={toggleOpen}
+                        role="combobox"
+                        aria-expanded={open}
+                        aria-controls={multiSelectContentId}
+                        aria-label={ariaLabel}
+                        {...focusProps}
+                        tabIndex={0}
+                        onKeyDown={handleSpaceBarToggle}
+                    >
+                        <div className="tw-flex tw-flex-wrap tw-gap-2 tw-outline-none tw-items-center tw-min-h-[28px]">
+                            {label && hasSelectedItems && <Text weight="strong">{label}</Text>}
+                            {type === FilterableMultiSelectType.Default &&
+                                activeItemKeys.map((key) => (
+                                    <Tag
+                                        key={key}
+                                        type={getTagType()}
+                                        label={key.toString()}
+                                        size={size === FilterableMultiSelectSize.Small ? TagSize.Small : TagSize.Medium}
+                                        onClick={disabled ? undefined : () => toggleSelection(key)}
+                                    />
+                                ))}
+
+                            {type === FilterableMultiSelectType.Summarized &&
+                                (hasSelectedItems || summarizedLabelFromProps) && (
+                                    <Tag
+                                        type={getTagType()}
+                                        label={summarizedLabel}
+                                        size={size === FilterableMultiSelectSize.Small ? TagSize.Small : TagSize.Medium}
+                                        onClick={
+                                            indeterminateItemKeys?.length === 0
+                                                ? () => onSelectionChange([])
+                                                : undefined
+                                        }
+                                    />
+                                )}
+
+                            {activeItemKeys.length === 0 && placeholder && <Text color="weak">{placeholder}</Text>}
+                        </div>
+                    </div>
+                </div>
+            </Trigger>
+
+            {open && heightIsReady && (
+                <EnablePortalWrapper enablePortal={enablePortal}>
+                    <div
+                        id={multiSelectContentId}
+                        ref={setMultiSelectMenuRef}
+                        className="tw-absolute tw-left-0 tw-w-full tw-overflow-hidden tw-p-0 tw-shadow-mid tw-list-none tw-m-0 tw-mt-2 tw-z-[120000] tw-bg-base tw-min-w-[18rem]"
+                        key="content"
+                        style={{
+                            ...popperInstance.styles.popper,
+                            width: triggerRef?.getBoundingClientRect().width,
+                        }}
+                        {...popperInstance.attributes.popper}
+                    >
+                        <FocusScope restoreFocus>
+                            <div className="tw-p-4 tw-pb-0">
+                                <TextInput
+                                    value={searchTerm}
+                                    placeholder={filterLabel}
+                                    clearable={true}
+                                    onChange={handleFilterChange}
+                                    focusOnMount={true}
+                                />
+                            </div>
+                            <div
+                                className="tw-p-4 tw-overflow-y-auto tw-overflow-x-hidden tw-w-full tw-relative"
+                                style={{ maxHeight }}
+                            >
+                                {checkboxes.length > 0 && (
+                                    <Checklist
+                                        activeValues={activeItemKeys.map((key) => key.toString())}
+                                        setActiveValues={onSelectionChange}
+                                        checkboxes={checkboxes.filter((item) => !item.isDivider && !item.isCategory)}
+                                        direction={ChecklistDirection.Vertical}
+                                        ariaLabel={ariaLabel}
+                                    />
+                                )}
+
+                                {checkboxes.length === 0 && searchTerm !== '' && (
+                                    <Text size="small" color="weak">
+                                        {emptyFilteredResultsLabel}
+                                    </Text>
+                                )}
+                            </div>
+                        </FocusScope>
+                    </div>
+                </EnablePortalWrapper>
+            )}
+        </div>
+    );
+};
+FilterableMultiSelect.displayName = 'FondueFilterableMultiSelect';
