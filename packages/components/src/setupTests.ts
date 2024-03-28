79faf64609b5c42807f09b3b5b01acea807b594c
Noah Waldner <noahwaldner@users.noreply.github.com>
2024-03-28 14:53:22 +0100

    fix: add assertion type for Jest DOM helpers (#1781)

--- packages/components/src/setupTests.ts
+++ packages/components/src/setupTests.ts
@@ -1,9 +1,14 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import { type TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
 import '@testing-library/jest-dom/vitest';
-
 import { cleanup, configure } from '@testing-library/react';
 import { afterEach, beforeAll } from 'vitest';
+
+declare module 'vitest' {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    interface Assertion<T = any> extends jest.Matchers<void, T>, TestingLibraryMatchers<T, void> {}
+}
 
 beforeAll(() => {
     configure({ testIdAttribute: 'data-test-id' });
