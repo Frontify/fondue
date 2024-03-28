e6f1b150ca65f75b980724c3e1a0fdb535ba831d
Noah Waldner <noah.waldner@frontify.com>
2024-03-28 13:25:35 +0100

    remove onboarding addon

--- storybook-docs/.storybook/main.ts
+++ storybook-docs/.storybook/main.ts
@@ -7,7 +7,6 @@ const config: StorybookConfig = {
         "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
     ],
     addons: [
-        "@storybook/addon-onboarding",
         "@storybook/addon-links",
         "@storybook/addon-essentials",
         "@chromatic-com/storybook",
