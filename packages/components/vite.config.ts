5429d72610785090a2a03999f38da6f7d8a9b2b6
Noah Waldner <noah.waldner@frontify.com>
2024-04-10 12:06:31 +0200

    remove twin-macro

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -5,26 +5,7 @@ import tsConfigPaths from 'vite-tsconfig-paths';
 import { UserConfig, defineConfig } from 'vitest/config';
 
 export default defineConfig({
-    plugins: [react({
-        babel: {
-            plugins: [
-                'babel-plugin-macros',
-                [
-                    '@emotion/babel-plugin-jsx-pragmatic',
-                    {
-                        export: 'jsx',
-                        import: '__cssprop',
-                        module: '@emotion/react',
-                    },
-                ],
-                [
-                    '@babel/plugin-transform-react-jsx',
-                    { pragma: '__cssprop' },
-                    'twin.macro',
-                ],
-            ],
-        },
-    }), tsConfigPaths()] as UserConfig["plugins"],
+    plugins: [react(), tsConfigPaths()] as UserConfig["plugins"],
     test: {
         environment: 'happy-dom',
         setupFiles: ['./src/setupTests.ts'],
