0564cef5d8a66732655877caf6645e09847672fa
Noah Waldner <noah.waldner@frontify.com>
2024-04-16 10:52:21 +0200

    add type generation

--- packages/fondue/components.vite.config.ts
+++ packages/fondue/components.vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 /**
  * @type {import('vite').UserConfig}
  */
 export default defineConfig({
+    plugins: [,
+        dts({ insertTypesEntry: true, exclude: ['**/*.stories.tsx'], include: 'src/subpackages/components.ts' }),
+    ],
     build: {
         lib: {
             entry: 'src/subpackages/components.ts',
