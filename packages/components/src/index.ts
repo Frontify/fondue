91f5ebf9a377f82973245a9ba68ee91da62eebac
Noah Waldner <noah.waldner@frontify.com>
2024-04-17 12:52:58 +0200

    linting

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -5,4 +5,3 @@ import './styles.css';
 export { Button } from './components/Button/Button';
 export { Divider } from './components/Divider/Divider';
 export { LoadingBar } from './components/LoadingBar/LoadingBar';
-
