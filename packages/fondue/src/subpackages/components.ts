fc26fe88d7cccb5d6e5b5d7ee723ee31727567a1
Noah Waldner <noah.waldner@frontify.com>
2024-04-17 13:18:01 +0200

    linting

--- packages/fondue/src/subpackages/components.ts
+++ packages/fondue/src/subpackages/components.ts
@@ -1,5 +1,5 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import "@frontify/fondue-components/styles";
+import '@frontify/fondue-components/styles';
 
 export * from '@frontify/fondue-components';
