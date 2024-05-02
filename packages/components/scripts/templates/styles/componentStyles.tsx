5e7a7806d288dab93a45fccef0312b919070b5e5
Samuel Alev <samuel.alev@frontify.com>
2024-05-02 21:00:00 +0200

    fix: linting

--- packages/components/scripts/templates/styles/componentStyles.tsx
+++ packages/components/scripts/templates/styles/componentStyles.tsx
@@ -1,7 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { toCamelCase } from 'scripts/transforms';
-
+import { toCamelCase } from '../../transforms';
 import { type ComponentFileBuilderResponse } from '../../types';
 
 export const Setup = (componentName: string): ComponentFileBuilderResponse => ({
