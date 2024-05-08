10afe742ccbe35c918c5ca99ad65394ee5b0bdbd
Noah Waldner <noah.waldner@frontify.com>
2024-05-08 16:39:39 +0200

    add tooltipcontent stories

---
+++ packages/components/src/components/Tooltip/TooltipContent.stories.tsx
@@ -0,0 +1,134 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { type Meta, type StoryObj } from '@storybook/react';
+
+import { Button } from '../Button/Button';
+
+import { Tooltip, TooltipContent } from './Tooltip';
+
+type Story = StoryObj<typeof TooltipContent>;
+const meta: Meta<typeof TooltipContent> = {
+    component: TooltipContent,
+    title: 'Components/Tooltip/Content',
+    tags: ['autodocs'],
+    args: {
+        padding: 'spacious',
+        side: 'top',
+        ignoreCollisions: false,
+        maxWidth: '200px',
+        children:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    parameters: {
+        status: {
+            type: 'in_progress',
+        },
+    },
+
+    render: ({ ...args }) => {
+        return (
+            <Tooltip.Root>
+                <Tooltip.Trigger>
+                    <Button>Hover over me!</Button>
+                </Tooltip.Trigger>
+                <Tooltip.Content {...args} />
+            </Tooltip.Root>
+        );
+    },
+};
+export default meta;
+
+export const Default: Story = {
+    args: {},
+};
+
+export const Compact: Story = {
+    args: {
+        padding: 'compact',
+    },
+};
+
+export const TopWithCollision: Story = {
+    args: {
+        side: 'top',
+    },
+};
+
+export const TopWithoutCollision: Story = {
+    args: {
+        side: 'top',
+    },
+    render: ({ ...args }) => {
+        return (
+            <div className="tw-mt-64">
+                <Tooltip.Root>
+                    <Tooltip.Trigger>
+                        <Button>Hover over me!</Button>
+                    </Tooltip.Trigger>
+                    <Tooltip.Content {...args} />
+                </Tooltip.Root>
+            </div>
+        );
+    },
+};
+
+export const LeftWithCollision: Story = {
+    args: {
+        side: 'left',
+    },
+};
+
+export const LeftWithoutCollision: Story = {
+    args: {
+        side: 'left',
+    },
+    render: ({ ...args }) => {
+        return (
+            <div className="tw-ml-64">
+                <Tooltip.Root>
+                    <Tooltip.Trigger>
+                        <Button>Hover over me!</Button>
+                    </Tooltip.Trigger>
+                    <Tooltip.Content {...args} />
+                </Tooltip.Root>
+            </div>
+        );
+    },
+};
+
+export const Bottom: Story = {
+    args: {
+        side: 'bottom',
+    },
+};
+
+export const Right: Story = {
+    args: {
+        side: 'right',
+    },
+};
+
+export const WithMaxWidth: Story = {
+    args: {
+        maxWidth: '400px',
+    },
+};
+
+export const IgnoreCollisions: Story = {
+    args: {
+        ignoreCollisions: true,
+        side: 'left',
+    },
+    render: ({ ...args }) => {
+        return (
+            <div className="tw-ml-20">
+                <Tooltip.Root>
+                    <Tooltip.Trigger>
+                        <Button>Hover over me!</Button>
+                    </Tooltip.Trigger>
+                    <Tooltip.Content {...args} />
+                </Tooltip.Root>
+            </div>
+        );
+    },
+};
