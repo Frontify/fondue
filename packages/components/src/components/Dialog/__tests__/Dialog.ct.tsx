58089b9a2a8f0726fbc555cdc9e20a07d720161d
Noah Waldner <noah.waldner@frontify.com>
2024-05-23 13:03:19 +0200

    prep tests

--- packages/components/src/components/Dialog/__tests__/Dialog.ct.tsx
+++ packages/components/src/components/Dialog/__tests__/Dialog.ct.tsx
@@ -2,13 +2,43 @@
 
 import { expect, test } from '@playwright/experimental-ct-react';
 
+import { Button } from '#/components/Button/Button';
+
 import { Dialog } from '../Dialog';
 
-const DIALOG_TEST_ID = 'test-dialog';
-const DIALOG_TEXT = 'sample dialog';
+const DIALOG_TRIGGER_TEXT = 'Click Me';
+const DIALOG_TRIGGER_TEST_ID = 'fondue-dialog-trigger';
+const DIALOG_HEADER_TEXT = 'Dialog Header';
+const DIALOG_HEADER_TEST_ID = 'fondue-dialog-header';
+const DIALOG_BODY_TEXT = 'Dialog Body';
+const DIALOG_BODY_TEST_ID = 'fondue-dialog-body';
+const DIALOG_FOOTER_TEXT = 'Dialog Footer';
+const DIALOG_FOOTER_TEST_ID = 'fondue-dialog-footer';
+const DIALOG_CONTENT_TEST_ID = 'fondue-dialog-content';
+const DIALOG_SIDE_CONTENT_TEST_ID = 'fondue-dialog-side-content';
 
 test('should render without error', async ({ mount }) => {
-    const component = await mount(<Dialog data-test-id={DIALOG_TEST_ID}>{DIALOG_TEXT}</Dialog>);
+    const component = await mount(
+        <Dialog.Root>
+            <Dialog.Trigger data-test-id={DIALOG_TRIGGER_TEST_ID}>
+                <Button>{DIALOG_TRIGGER_TEXT}</Button>
+            </Dialog.Trigger>
+            <Dialog.Content data-test-id={DIALOG_CONTENT_TEST_ID}>
+                <Dialog.SideContent data-test-id={DIALOG_SIDE_CONTENT_TEST_ID}>
+                    <div className="tw-bg-box-positive-strong tw-h-full tw-w-full"></div>
+                </Dialog.SideContent>
+                <Dialog.Header data-test-id={DIALOG_HEADER_TEST_ID} showCloseButton>
+                    {DIALOG_HEADER_TEXT}
+                </Dialog.Header>
+                <Dialog.Body data-test-id={DIALOG_BODY_TEST_ID}>{DIALOG_BODY_TEXT}</Dialog.Body>
+                <Dialog.Footer data-test-id={DIALOG_FOOTER_TEST_ID}>
+                    <div className="tw-flex tw-h-fit tw-justify-end tw-gap-2">
+                        <Button>{DIALOG_FOOTER_TEXT}</Button>
+                    </div>
+                </Dialog.Footer>
+            </Dialog.Content>
+        </Dialog.Root>,
+    );
     await expect(component).toBeVisible();
-    await expect(component).toContainText(DIALOG_TEXT);
+    await expect(component).toContainText(DIALOG_TRIGGER_TEXT);
 });
