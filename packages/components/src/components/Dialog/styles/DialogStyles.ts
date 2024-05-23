16610985e81f903c731f2f52fc2d1dac0b5900b3
Noah Waldner <noah.waldner@frontify.com>
2024-05-23 12:43:30 +0200

    fix text overflow

--- packages/components/src/components/Dialog/styles/DialogStyles.ts
+++ packages/components/src/components/Dialog/styles/DialogStyles.ts
@@ -3,7 +3,7 @@
 import { sv } from '#/utilities/styleUtilities';
 
 export const dialogContentStyles = sv({
-    base: 'tw-group tw-block tw-fixed tw-left-2 tw-right-2 tw-top-1/2 sm:tw-left-1/2 sm:tw-transform sm:tw--translate-x-1/2 tw--translate-y-1/2 tw-bg-base tw-shadow-mid tw-z-20 has-[[data-dialog-layout-component]]:tw-grid tw-grid-rows-[auto_auto_1fr_auto] sm:tw-grid-rows-[auto_1fr_auto] tw-min-w-[--dialog-min-width] tw-max-w-[--dialog-max-width] tw-min-h-[--dialog-min-height] tw-h-fit tw-overflow-hidden',
+    base: 'tw-group tw-block tw-fixed tw-left-2 tw-right-2 tw-top-1/2 sm:tw-left-1/2 sm:tw-transform sm:tw--translate-x-1/2 tw--translate-y-1/2 tw-bg-base tw-shadow-mid tw-z-20 has-[[data-dialog-layout-component]]:tw-grid tw-grid-rows-[auto_auto_minmax(0_,_1fr)_auto] sm:tw-grid-rows-[auto_minmax(0_,_1fr)_auto] tw-min-w-[--dialog-min-width] tw-max-w-[--dialog-max-width] tw-min-h-[--dialog-min-height] tw-max-h-[600px] tw-h-fit tw-overflow-hidden',
     variants: {
         rounded: {
             true: 'tw-rounded-lg',
@@ -44,7 +44,7 @@ export const dialogFooterStyles =
     'group-data-[dialog-spacing=spacious]:tw-p-10 ';
 
 export const dialogBodyStyles =
-    'tw-flex tw-flex-col tw-gap-4 ' +
+    'tw-overflow-hidden tw-flex tw-max-h-full tw-flex-col tw-gap-4 ' +
     'group-data-[dialog-spacing=compact]:tw-px-4 group-data-[dialog-spacing=compact]:tw-py-2 tw-col-start-1 sm:tw-col-start-2 tw-row-start-3 sm:tw-row-start-2 ' +
     'group-data-[dialog-spacing=comfortable]:tw-px-6 group-data-[dialog-spacing=comfortable]:tw-py-4 ' +
     'group-data-[dialog-spacing=spacious]:tw-px-10 group-data-[dialog-spacing=spacious]:tw-py-6 ';
