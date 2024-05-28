28521df7f838f71d0908631da890768a0bb2b76d
Noah Waldner <noah.waldner@frontify.com>
2024-05-28 12:07:02 +0200

    add dialog export

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -4,6 +4,7 @@ import './styles.css';
 
 export { Button } from './components/Button/Button';
 export { Checkbox } from './components/Checkbox/Checkbox';
+export { Dialog } from './components/Dialog/Dialog';
 export { Divider } from './components/Divider/Divider';
 export { Flyout } from './components/Flyout/Flyout';
 export { Label } from './components/Label/Label';
@@ -11,3 +12,4 @@ export { LoadingBar } from './components/LoadingBar/LoadingBar';
 export { SegmentedControl } from './components/SegmentedControl/SegmentedControl';
 export { TextInput } from './components/TextInput/TextInput';
 export { Tooltip } from './components/Tooltip/Tooltip';
+
