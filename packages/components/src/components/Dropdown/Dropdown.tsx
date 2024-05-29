f552a654095ee979393418f19fd56b2b7b6a69e1
Noah Waldner <noah.waldner@frontify.com>
2024-05-29 16:20:55 +0200

    add downshift

--- packages/components/src/components/Dropdown/Dropdown.tsx
+++ packages/components/src/components/Dropdown/Dropdown.tsx
@@ -1,15 +1,70 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { type ReactNode } from 'react';
-
-import { dropdownStyles } from './styles/dropdownStyles';
+import { useCombobox } from 'downshift';
+import { useState, type ReactNode } from 'react';
 
 export type DropdownProps = { children?: ReactNode };
 
 export const Dropdown = ({ children }: DropdownProps) => {
+    const items = [
+        'Neptunium',
+        'Plutonium',
+        'Americium',
+        'Curium',
+        'Berkelium',
+        'Californium',
+        'Einsteinium',
+        'Fermium',
+        'Mendelevium',
+        'Nobelium',
+        'Lawrencium',
+        'Rutherfordium',
+        'Dubnium',
+        'Seaborgium',
+        'Bohrium',
+        'Hassium',
+        'Meitnerium',
+        'Darmstadtium',
+        'Roentgenium',
+        'Copernicium',
+        'Nihonium',
+        'Flerovium',
+        'Moscovium',
+        'Livermorium',
+        'Tennessine',
+        'Oganesson',
+    ];
+    const [inputItems, setInputItems] = useState(items);
+    const { isOpen, getToggleButtonProps, getLabelProps, getMenuProps, getInputProps, highlightedIndex, getItemProps } =
+        useCombobox({
+            items: inputItems,
+            onInputValueChange: ({ inputValue }) => {
+                setInputItems(items.filter((item) => item.toLowerCase().startsWith(inputValue.toLowerCase())));
+            },
+        });
     return (
-        <div className={dropdownStyles()} data-test-id="dropdown">
-            {children}
+        <div>
+            <label htmlFor="abc" {...getLabelProps()}>
+                Choose an element:
+            </label>
+            <div>
+                <input id="abc" {...getInputProps()} />
+                <button type="button" {...getToggleButtonProps()} aria-label="toggle menu">
+                    &#8595;
+                </button>
+            </div>
+            <ul {...getMenuProps()}>
+                {isOpen &&
+                    inputItems.map((item, index) => (
+                        <li
+                            style={highlightedIndex === index ? { backgroundColor: '#bde4ff' } : {}}
+                            key={`${item}${index}`}
+                            {...getItemProps({ item, index })}
+                        >
+                            {item}
+                        </li>
+                    ))}
+            </ul>
         </div>
     );
 };
