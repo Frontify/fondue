84623eb4e89bbd2781e1cf7df857aafdd9bb3003
Samuel Alev <samuel.alev@frontify.com>
2024-06-04 10:59:41 +0200

    fix: remove double quote breaking tests (#1897)

---
+++ .changeset/lovely-frogs-judge.md
@@ -0,0 +1,5 @@
+---
+"@frontify/fondue-components": patch
+---
+
+fix: remove double quote breaking tests
--- packages/components/src/utilities/focusStyle.ts
+++ packages/components/src/utilities/focusStyle.ts
@@ -1,4 +1,4 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 export const FOCUS_OUTLINE =
-    'focus-visible:tw-outline has-[[data-show-focus-ring="true"]]:tw-outline tw-outline-4 tw-outline-offset-2 tw-outline-blue';
+    'focus-visible:tw-outline has-[[data-show-focus-ring=true]]:tw-outline tw-outline-4 tw-outline-offset-2 tw-outline-blue';
