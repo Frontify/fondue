0802636ee1b6d55dcad5ec8482428b98f428b660
Noah Waldner <noah.waldner@frontify.com>
2024-06-12 10:10:17 +0200

    simplify caret

--- packages/components/src/components/Select/SelectCaret.tsx
+++ packages/components/src/components/Select/SelectCaret.tsx
@@ -1,17 +1,9 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { IconCaretDown, IconCaretUp } from '@frontify/fondue-icons';
+import { IconCaretUp } from '@frontify/fondue-icons';
 
 import styles from './styles/select.module.scss';
 
-type SelectCaretProps = {
-    isOpen: boolean;
-};
-
-export const SelectCaret = ({ isOpen }: SelectCaretProps) => {
-    if (isOpen) {
-        return <IconCaretUp size={16} className={styles.caret} />;
-    } else {
-        return <IconCaretDown size={16} className={styles.caret} />;
-    }
+export const SelectCaret = () => {
+    return <IconCaretUp size={16} className={styles.caret} />;
 };
