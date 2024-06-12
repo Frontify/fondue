a3484a6b3eaea359d09bc1b8a955c991fb1053fe
Noah Waldner <noah.waldner@frontify.com>
2024-06-12 09:51:37 +0200

    remove unused callback

--- packages/components/src/components/Select/SelectClear.tsx
+++ packages/components/src/components/Select/SelectClear.tsx
@@ -4,26 +4,9 @@ import { IconCross } from '@frontify/fondue-icons';
 
 import styles from './styles/select.module.scss';
 
-type SelectClearProps = {
-    onClick: () => void;
-};
-
-export const SelectClear = ({ onClick }: SelectClearProps) => {
+export const SelectClear = () => {
     return (
-        <button
-            aria-label="clear input"
-            className={styles.clear}
-            onClick={(event) => {
-                event.stopPropagation();
-                onClick();
-            }}
-            onKeyDown={(event) => {
-                if (event.key === 'Enter') {
-                    event.stopPropagation();
-                    onClick();
-                }
-            }}
-        >
+        <button aria-label="clear input" className={styles.clear}>
             <IconCross size={16} />
         </button>
     );
