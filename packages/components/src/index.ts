8ec180333eba0a8d3815f85a31abf0b76c4759d0
Noah Waldner <noah.waldner@frontify.com>
2024-06-18 17:22:13 +0200

    add export

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -6,9 +6,11 @@ export { Button } from './components/Button/Button';
 export { Checkbox } from './components/Checkbox/Checkbox';
 export { Dialog } from './components/Dialog/Dialog';
 export { Divider } from './components/Divider/Divider';
+export { Dropdown } from './components/Dropdown/Dropdown';
 export { Flyout } from './components/Flyout/Flyout';
 export { Label } from './components/Label/Label';
 export { LoadingBar } from './components/LoadingBar/LoadingBar';
 export { SegmentedControl } from './components/SegmentedControl/SegmentedControl';
 export { TextInput } from './components/TextInput/TextInput';
 export { Tooltip } from './components/Tooltip/Tooltip';
+
