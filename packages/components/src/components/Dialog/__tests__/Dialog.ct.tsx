5ef6c989a237235e6c1c7cc6bad6143a7550dafe
Noah Waldner <noah.waldner@frontify.com>
2024-06-25 10:28:17 +0200

    Revert "add prop for close Button"
    This reverts commit 781b9fb56ef56fec6ddcb37832774fe0092793a6.

--- packages/components/src/components/Dialog/__tests__/Dialog.ct.tsx
+++ packages/components/src/components/Dialog/__tests__/Dialog.ct.tsx
@@ -106,9 +106,7 @@ test('should close on close button click', async ({ mount, page }) => {
                 <Dialog.SideContent data-test-id={DIALOG_SIDE_CONTENT_TEST_ID}>
                     <div className="tw-bg-box-positive-strong tw-h-full tw-w-full"></div>
                 </Dialog.SideContent>
-                <Dialog.Header showCloseButton data-test-id={DIALOG_HEADER_TEST_ID}>
-                    {DIALOG_HEADER_TEXT}
-                </Dialog.Header>
+                <Dialog.Header data-test-id={DIALOG_HEADER_TEST_ID}>{DIALOG_HEADER_TEXT}</Dialog.Header>
                 <Dialog.Body data-test-id={DIALOG_BODY_TEST_ID}>{DIALOG_BODY_TEXT}</Dialog.Body>
                 <Dialog.Footer data-test-id={DIALOG_FOOTER_TEST_ID}>
                     <div className="tw-flex tw-h-fit tw-justify-end tw-gap-2">
@@ -166,9 +164,7 @@ test('should call event handler on open and close', async ({ mount, page }) => {
                 <Dialog.SideContent data-test-id={DIALOG_SIDE_CONTENT_TEST_ID}>
                     <div className="tw-bg-box-positive-strong tw-h-full tw-w-full"></div>
                 </Dialog.SideContent>
-                <Dialog.Header showCloseButton data-test-id={DIALOG_HEADER_TEST_ID}>
-                    {DIALOG_HEADER_TEXT}
-                </Dialog.Header>
+                <Dialog.Header data-test-id={DIALOG_HEADER_TEST_ID}>{DIALOG_HEADER_TEXT}</Dialog.Header>
                 <Dialog.Body data-test-id={DIALOG_BODY_TEST_ID}>{DIALOG_BODY_TEXT}</Dialog.Body>
                 <Dialog.Footer data-test-id={DIALOG_FOOTER_TEST_ID}>
                     <div className="tw-flex tw-h-fit tw-justify-end tw-gap-2">
