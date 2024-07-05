398dd5ce6eab7275929a296a373ca8544b7289eb
Noah Waldner <noah.waldner@frontify.com>
2024-07-05 13:51:01 +0200

    reorder props to allow `onPress` when used as tooltip trigger

---
+++ .changeset/five-countries-switch.md
@@ -0,0 +1,5 @@
+---
+"@frontify/fondue-components": patch
+---
+
+bugfix: reorder props to allow `onPress` when used as tooltip trigger
--- packages/components/src/components/Button/Button.tsx
+++ packages/components/src/components/Button/Button.tsx
@@ -90,8 +90,8 @@ export const Button = forwardRef<HTMLButtonElement | null, ButtonProps>(
                     iconStyles({ style, ...props }),
                     className,
                 )}
-                onClick={onPress}
                 {...props}
+                onClick={onPress}
             >
                 {children}
             </button>
