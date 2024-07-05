83280560452c2c34c3fcd2ce1ad5d611abed4aaf
Noah Waldner <noah.waldner@frontify.com>
2024-07-05 14:22:27 +0200

    trigger onClick thats passed to slot

--- packages/components/src/components/Button/Button.tsx
+++ packages/components/src/components/Button/Button.tsx
@@ -91,7 +91,12 @@ export const Button = forwardRef<HTMLButtonElement | null, ButtonProps>(
                     className,
                 )}
                 {...props}
-                onClick={onPress}
+                onClick={(e) => {
+                    // @ts-expect-error - this is done to allow other components who use Button as a trigger to pass an action
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+                    props.onClick?.(e);
+                    onPress(e);
+                }}
             >
                 {children}
             </button>
