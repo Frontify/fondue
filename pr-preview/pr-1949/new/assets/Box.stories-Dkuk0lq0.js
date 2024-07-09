4dafb9273b74dd1b60be6682bd91c7313fa757ad
SamuelAlev <SamuelAlev@users.noreply.github.com>
2024-07-09 13:56:06 +0000

    Deploy preview for PR 1949 🛫

---
+++ pr-preview/pr-1949/new/assets/Box.stories-Dkuk0lq0.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-Nms4Y4qS.js";import{B as a,D as p}from"./Box-BE9IJ1Hk.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./propsToCssVariables-BE5Q_-YS.js";const d={title:"Layout/Box",component:a,tags:["autodocs"],parameters:{status:{type:"in_progress"}},args:{width:"100px",height:"100px"}},r={render:n=>t.jsx(a,{...n,children:t.jsx(p,{})})};var e,o,s;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: args => {
+    return <Box {...args}>
+                <DecorativeContent />
+            </Box>;
+  }
+}`,...(s=(o=r.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const g=["Default"];export{r as Default,g as __namedExportsOrder,d as default};
