f6f9df39d83300e22b92f8d6c91e74807e3c6b50
Jose Costa <jose.costa@frontify.com>
2024-07-12 16:49:31 +0200

    add jsdocs, minor cleanup, update lock file with new deps

--- packages/components/src/components/Slider/Slider.tsx
+++ packages/components/src/components/Slider/Slider.tsx
@@ -6,9 +6,12 @@ import { Label } from '../Label/Label';
 
 import styles from './styles/slider.module.scss';
 
-export type SliderProps = {
+/**
+ * Properties for the Slider component.
+ */
+export interface SliderProps {
     id?: string;
-    label: string;
+    label?: string;
     value?: number[];
     defaultValue?: number[];
     min?: number;
@@ -19,41 +22,55 @@ export type SliderProps = {
     disabled?: boolean;
     onChange?: (value: number[]) => void;
     onCommit?: (value: number[]) => void;
-    /**
-     * The aria label of the select component.
-     */
-    'aria-label': string; // This should be mandatory due to accessibility reasons
-    /**
-     * The data test id of the select component.
-     */
+    'aria-label': string; // Required for accessibility.
     'data-test-id'?: string;
-};
-
-function generateId() {
-    return `slider-${Math.random().toString(36).substring(2, 9)}`;
 }
 
-export const Slider = ({
+/**
+ * Generate a unique ID for slider component instances. This will be a generic util soon.
+ * @returns A unique string ID.
+ */
+const generateId = () => `slider-${Math.random().toString(36).substring(2, 9)}`;
+
+/**
+ * Slider component that provides a UI for range selection, supporting features like accessibility through ARIA labels,
+ * custom step increments, and optional min/max value display.
+ *
+ * @param {SliderProps} props The properties passed to the Slider component.
+ * - `id`: Optional. A unique identifier for the slider. If not provided, one is generated.
+ * - `label`: Descriptive label for the slider, used for accessibility and form labeling.
+ * - `value`: Optional. Current value(s) of the slider. If provided, the slider is controlled externally.
+ * - `defaultValue`: Optional. Initial value(s) of the slider when it mounts, used if `value` is not provided. Enables uncontrolled usage.
+ * - `min`: Optional. Minimum value of the slider.
+ * - `max`: Optional. Maximum value of the slider.
+ * - `step`: Optional. The granularity with which the slider can step through values.
+ * - `minStepsBetweenThumbs`: Optional. Minimum steps between thumbs in a range slider.
+ * - `showMinMax`: Optional. Determines whether to display the minimum and maximum values below the slider.
+ * - `disabled`: Optional. If true, disables slider interaction.
+ * - `onChange`: Optional. Function called with the current value when it changes.
+ * - `onCommit`: Optional. Function called when the user stops changing the value.
+ * - `aria-label`: Accessibility label for the slider. Mandatory for screen readers.
+ * - `data-test-id`: Optional. An identifier used for testing purposes. Defaults to 'fondue-slider'.
+ */
+export const Slider: React.FC<SliderProps> = ({
     id = generateId(),
     label,
     value,
     defaultValue = [0],
-    min,
-    max,
-    step,
+    min = 0,
+    max = 100,
+    step = 1,
     minStepsBetweenThumbs,
-    showMinMax,
-    disabled,
+    showMinMax = false,
+    disabled = false,
     onChange,
     onCommit,
-    'aria-label': ariaLabel = label,
+    'aria-label': ariaLabel,
     'data-test-id': dataTestId = 'fondue-slider',
-}: SliderProps) => {
+}) => {
     return (
         <div data-test-id={dataTestId}>
-            <Label className="LabelRoot" htmlFor={id}>
-                {label}
-            </Label>
+            <Label htmlFor={id}>{label}</Label>
             <RadixSlider.Root
                 id={id}
                 className={styles.SliderRoot}
