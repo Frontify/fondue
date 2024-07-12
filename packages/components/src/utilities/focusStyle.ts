435537e33e649c18a64b480ff8b1676f5c4364f8
Jose Costa <jose.costa@frontify.com>
2024-07-12 17:16:51 +0200

    add more specific outline definition

--- packages/components/src/utilities/focusStyle.ts
+++ packages/components/src/utilities/focusStyle.ts
@@ -1,4 +1,4 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 export const FOCUS_OUTLINE =
-    'focus-visible:tw-outline has-[[data-show-focus-ring=true]]:tw-outline tw-outline-4 tw-outline-offset-2 tw-outline-blue';
+    'focus-visible:tw-outline has-[[data-show-focus-ring=true]]:tw-outline tw-outline-4 tw-outline-offset-2 tw-outline-blue focus-visible:tw-outline-blue'; // second declaration of tw-outline-blue is to assure that in firefox the outline isn't overriden by a global definition of :focus-visible which will take precedence over a class definition (go figure)
