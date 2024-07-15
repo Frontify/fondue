2273ebfb97622219d4fd9da0fde06217b3e6c829
fulopdaniel <fulopdaniel@users.noreply.github.com>
2024-07-15 12:16:22 +0000

    Deploy preview for PR 1955 🛫

---
+++ pr-preview/pr-1955/new/assets/Button.stories-COd8BlQg.js
@@ -0,0 +1,27 @@
+import{j as e}from"./jsx-runtime-Nms4Y4qS.js";import"./index-BwDkhjyp.js";import{o as z,c as O}from"./fondue-icons311-o6oMneN6.js";import{a as j}from"./chunk-MZXVCX43-CM0pFb8Z.js";import{B as c}from"./Button-D7cn0I_A.js";import"./_commonjsHelpers-BosuxZz1.js";import"./v4-CQkTLCs1.js";import"./styleUtilities-C-w4CMuR.js";import"./focusStyle-IgMhxCMT.js";const F={title:"Components/Button",component:c,tags:["autodocs"],parameters:{status:{type:"released"}},args:{type:"button",style:"default",emphasis:"default",size:"medium",rounding:"medium",disabled:!1,hugWidth:!0,children:"Button Text",aspect:"default",onPress:j("onPress")}},n={name:"Icon and Text Label",render:a=>e.jsxs(c,{...a,children:[e.jsx(z,{size:16}),"Button Text"]})},r={},o={name:"Icon Only",args:{aspect:"square",children:e.jsx(O,{size:20})}},t={name:"Icon Only Rounded",args:{rounding:"full",title:"Icon Only Rounded",children:e.jsx(O,{size:20}),aspect:"square"}},s={name:"Icon and Text Label",render:a=>e.jsxs(c,{...a,children:[e.jsx(z,{size:16}),"Button Text"]})};var d,u,i;n.parameters={...n.parameters,docs:{...(d=n.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  name: 'Icon and Text Label',
+  render: args => <Button {...args}>
+            <IconColorFan size={16} />
+            Button Text
+        </Button>
+}`,...(i=(u=n.parameters)==null?void 0:u.docs)==null?void 0:i.source}}};var m,l,p;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(p=(l=r.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var I,x,g;o.parameters={...o.parameters,docs:{...(I=o.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  name: 'Icon Only',
+  args: {
+    aspect: 'square',
+    children: <IconIcon size={20} />
+  }
+}`,...(g=(x=o.parameters)==null?void 0:x.docs)==null?void 0:g.source}}};var h,f,B;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  name: 'Icon Only Rounded',
+  args: {
+    rounding: 'full',
+    title: 'Icon Only Rounded',
+    children: <IconIcon size={20} />,
+    aspect: 'square'
+  }
+}`,...(B=(f=t.parameters)==null?void 0:f.docs)==null?void 0:B.source}}};var y,T,b;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  name: 'Icon and Text Label',
+  render: args => <Button {...args}>
+            <IconColorFan size={16} />
+            Button Text
+        </Button>
+}`,...(b=(T=s.parameters)==null?void 0:T.docs)==null?void 0:b.source}}};const P=["Default","TextLabelOnly","WithIcon","WithRoundedIcon","WithIconAndLabel"];export{n as Default,r as TextLabelOnly,o as WithIcon,s as WithIconAndLabel,t as WithRoundedIcon,P as __namedExportsOrder,F as default};
