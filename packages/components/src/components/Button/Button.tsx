38ed29e4b0d7f5fab863f5400b16f014e882e465
Noah Waldner <noahwaldner@users.noreply.github.com>
2024-07-15 08:39:00 +0200

    Update packages/components/src/components/Button/Button.tsx
    Co-authored-by: Samuel Alev <samuel.alev@frontify.com>

--- packages/components/src/components/Button/Button.tsx
+++ packages/components/src/components/Button/Button.tsx
@@ -95,7 +95,7 @@ export const Button = forwardRef<HTMLButtonElement | null, ButtonProps>(
                     // @ts-expect-error - this is done to allow other components who use Button as a trigger to pass an action
                     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
                     props.onClick?.(e);
-                    onPress(e);
+                    onPress?.(e);
                 }}
             >
                 {children}
