b8b07c3776a7ff7f471da452c9633ff64e691734
Jose Costa <jose.costa@frontify.com>
2024-07-23 13:29:02 +0200

    add test for keyboard navigation

--- packages/components/src/components/Switch/__tests__/Switch.ct.tsx
+++ packages/components/src/components/Switch/__tests__/Switch.ct.tsx
@@ -5,11 +5,15 @@ import sinon from 'sinon';
 
 import { Switch } from '../Switch';
 
+const ARIA_LABEL = 'Toggle airplane mode';
+
 test('should toggle switch state', async ({ mount }) => {
     const onChange = sinon.spy();
-    const component = await mount(<Switch onChange={onChange} defaultValue={false} />);
+    const component = await mount(<Switch aria-label={ARIA_LABEL} onChange={onChange} defaultValue={false} />);
+
     await expect(component).toHaveAttribute('aria-checked', 'false');
     await component.click();
+
     sinon.assert.calledOnceWithExactly(onChange, true);
     await expect(component).toHaveAttribute('aria-checked', 'true');
 });
@@ -19,28 +23,46 @@ test('should handle controlled component behavior', async ({ mount }) => {
     const onChange = sinon.spy((newChecked: boolean) => {
         checked = newChecked;
     });
-    const component = await mount(<Switch value={checked} onChange={onChange} />);
+
+    const component = await mount(<Switch aria-label={ARIA_LABEL} value={checked} onChange={onChange} />);
+
     expect(await component.getAttribute('aria-checked')).toBe('false');
+
     await component.click();
     sinon.assert.calledOnceWithExactly(onChange, true);
 });
 
 test('should apply size and full width classes', async ({ mount }) => {
-    const component = await mount(<Switch />);
+    const component = await mount(<Switch aria-label={ARIA_LABEL} />);
     await expect(component).toHaveClass(/medium/);
 
-    await component.update(<Switch size="large" />);
+    await component.update(<Switch aria-label={ARIA_LABEL} size="large" />);
     await expect(component).toHaveClass(/large/);
 
-    await component.update(<Switch size="small" />);
+    await component.update(<Switch aria-label={ARIA_LABEL} size="small" />);
     await expect(component).toHaveClass(/small/);
 
-    await component.update(<Switch hugWidth={true} />);
+    await component.update(<Switch aria-label={ARIA_LABEL} hug={true} />);
     await expect(component).toHaveClass(/fullwidth/);
 });
 
+test('should update value when user focuses and presses enter', async ({ mount }) => {
+    const onChange = sinon.spy();
+    const component = await mount(<Switch aria-label={ARIA_LABEL} onChange={onChange} />);
+
+    await component.focus();
+
+    await component.press('Enter');
+    expect(await component.getAttribute('aria-checked')).toBe('true');
+    sinon.assert.calledWithExactly(onChange, true);
+
+    await component.press('Enter');
+    expect(await component.getAttribute('aria-checked')).toBe('false');
+    sinon.assert.calledWithExactly(onChange, false);
+});
+
 test('should respect the disabled state', async ({ mount }) => {
-    const component = await mount(<Switch disabled={true} />);
+    const component = await mount(<Switch aria-label={ARIA_LABEL} disabled={true} />);
     await expect(component).toHaveAttribute('disabled');
 });
 
@@ -49,7 +71,7 @@ test('should prevent form submission if switch is required and not toggled', asy
 
     const component = await mount(
         <form onSubmit={onSubmit}>
-            <Switch name="consent" data-test-id="consent-switch" required />
+            <Switch aria-label={ARIA_LABEL} name="consent" data-test-id="consent-switch" required />
             <br />
             <button data-test-id="submit-button" type="submit">
                 Submit
