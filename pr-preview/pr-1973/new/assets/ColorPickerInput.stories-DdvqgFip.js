d3401125bec7fea1e6775720c7cd3b9eff0025dc
noahwaldner <noahwaldner@users.noreply.github.com>
2024-07-26 08:55:04 +0000

    Deploy preview for PR 1973 🛫

---
+++ pr-preview/pr-1973/new/assets/ColorPickerInput.stories-DdvqgFip.js
@@ -0,0 +1,14 @@
+import{j as p}from"./jsx-runtime-DBEcOq3S.js";import{d as m,c as a}from"./ColorPicker-B_Dc85R2.js";import"./index-DtJulBIN.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-Dhc4yksV.js";import"./index-BqTDQIm7.js";import"./index-GpQCGCAv.js";import"./fondue-icons311-BZCDKIoV.js";import"./Select-DQc9wKL_.js";import"./index-fOb33GKb.js";import"./index-DYI_kRwo.js";import"./index-P-l_ls5c.js";import"./index-24aZOQX_.js";import"./Combination-DuhGN8JR.js";import"./index-DBH0Kh6a.js";import"./index-BYh9UA9V.js";import"./index-CMjj8zSi.js";import"./index-RzzeDlDa.js";import"./index-D3ylJrlI.js";import"./TextInput-_sF7mbua.js";import"./styleUtilities-T76WvCte.js";const R={title:"Components/ColorPicker/Input",component:m,tags:["autodocs"],parameters:{status:{type:"in_progress"}},args:{}},r={args:{currentColor:{red:230,green:100,blue:50,alpha:.8,name:"custom-color"}},render:n=>p.jsx(a.Input,{...n,onClear:()=>{}})};var o,t,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    currentColor: {
+      red: 230,
+      green: 100,
+      blue: 50,
+      alpha: 0.8,
+      name: 'custom-color'
+    }
+  },
+  render: args => {
+    return <ColorPicker.Input {...args} onClear={() => {}} />;
+  }
+}`,...(e=(t=r.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const S=["Default"];export{r as Default,S as __namedExportsOrder,R as default};
