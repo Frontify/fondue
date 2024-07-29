9dd014ca7171172261e09ba94fcc9bea6ac9927f
Noah Waldner <noah.waldner@frontify.com>
2024-07-29 14:29:21 +0200

    add initil component test

--- packages/components/src/components/ColorPicker/__tests__/ColorPicker.ct.tsx
+++ packages/components/src/components/ColorPicker/__tests__/ColorPicker.ct.tsx
@@ -4,11 +4,18 @@ import { expect, test } from '@playwright/experimental-ct-react';
 
 import { ColorPicker } from '../ColorPicker';
 
-const COLOR_PICKER_TEST_ID = 'test-color-picker';
-const COLOR_PICKER_TEXT = 'sample color-picker';
+const COLOR_PICKER_TEST_ID = 'fondue-color-picker';
+const COLOR_PICKER_VALUE_INPUT_TEST_ID = 'fondue-color-picker-value-input';
+const COLOR_PICKER_GRADIENT_INPUT_TEST_ID = 'fondue-color-picker-gradient-input';
 
 test('should render without error', async ({ mount }) => {
-    const component = await mount(<ColorPicker.Root data-test-id={COLOR_PICKER_TEST_ID}></ColorPicker.Root>);
+    const component = await mount(
+        <ColorPicker.Root data-test-id={COLOR_PICKER_TEST_ID}>
+            <ColorPicker.Values data-test-id={COLOR_PICKER_VALUE_INPUT_TEST_ID} />
+            <ColorPicker.Gradient data-test-id={COLOR_PICKER_GRADIENT_INPUT_TEST_ID} />
+        </ColorPicker.Root>,
+    );
     await expect(component).toBeVisible();
-    await expect(component).toContainText(COLOR_PICKER_TEXT);
+    await expect(component.getByTestId(COLOR_PICKER_VALUE_INPUT_TEST_ID)).toBeVisible();
+    await expect(component.getByTestId(COLOR_PICKER_GRADIENT_INPUT_TEST_ID)).toBeVisible();
 });
