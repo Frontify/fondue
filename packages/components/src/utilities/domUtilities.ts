2f70f94d623cab3bd4943dbf620f201e5a69b621
Jose Costa <jose.costa@frontify.com>
2024-08-05 16:44:10 +0200

    lint

--- packages/components/src/utilities/domUtilities.ts
+++ packages/components/src/utilities/domUtilities.ts
@@ -1,6 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { ForwardedRef, RefObject } from 'react';
+import { type ForwardedRef, type RefObject } from 'react';
 
 export const MAX_HEIGHT_MARGIN = 16;
 
