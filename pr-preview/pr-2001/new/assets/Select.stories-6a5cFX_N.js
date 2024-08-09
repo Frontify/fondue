be64630a5a6b81f59cc3f1201b98045c4f92eec6
jose-costa-frontify <jose-costa-frontify@users.noreply.github.com>
2024-08-09 11:02:50 +0000

    Deploy preview for PR 2001 🛫

---
+++ pr-preview/pr-2001/new/assets/Select.stories-6a5cFX_N.js
@@ -0,0 +1,248 @@
+import{j as e}from"./jsx-runtime-DBEcOq3S.js";import{r as ce}from"./index-DtJulBIN.js";import{c as l}from"./fondue-icons324-Bw2AQYl8.js";import{B as ae}from"./Button-CeW1VRZi.js";import{S as me,a as ne,b as oe,c as Se,d as ue,e as t}from"./Select-COGvVFjm.js";import"./_commonjsHelpers-BosuxZz1.js";import"./styleUtilities-T76WvCte.js";import"./focusStyle-IgMhxCMT.js";import"./index-CNnLFFmR.js";import"./index-BHYJtXto.js";import"./index-Dhc4yksV.js";import"./index-BqTDQIm7.js";import"./index-P-l_ls5c.js";import"./index-COuo08O9.js";import"./Combination-CBds3eJs.js";import"./index-Bw4vj2Av.js";import"./index-CTHx29zj.js";import"./index-CvTA8V3i.js";import"./index-D21DJKX5.js";import"./index-D3ylJrlI.js";import"./usePreventDropdownOverflow-BbhSVJTt.js";const Ae={component:me,subcomponents:{"Select.Combobox":ne,"Select.Slot":oe,"Select.Group":Se,"Select.Item":ue},tags:["autodocs"],parameters:{status:{type:"in_progress"}},args:{"aria-label":"Select an item",placeholder:"Select an item"}},r={name:"Select",render:s=>e.jsxs(t,{...s,children:[e.jsx(t.Item,{value:"test1",children:"Test1"}),e.jsx(t.Item,{value:"test2",children:"Test2"}),e.jsx(t.Item,{value:"test3",children:"Test3"})]})},c={args:{placeholder:"Select an item"},render:s=>e.jsxs(t.Combobox,{...s,children:[e.jsx(t.Item,{value:"test1",children:"Test1"}),e.jsx(t.Item,{value:"test2",children:"Test2"}),e.jsx(t.Item,{value:"test3",children:"Test3"})]})},a={args:{placeholder:"Select an item",allowCustomValue:!0},render:s=>e.jsxs(t.Combobox,{...s,children:[e.jsx(t.Item,{value:"test1",children:"Test1"}),e.jsx(t.Item,{value:"test1",children:"Test2"}),e.jsx(t.Item,{value:"test1",children:"Test3"})]})},m={args:{disabled:!0},render:s=>e.jsx(t,{...s,children:e.jsxs(t.Slot,{name:"menu",children:[e.jsx(t.Item,{value:"test1",children:"Test1"}),e.jsx(t.Item,{value:"test2",children:"Test2"}),e.jsx(t.Item,{value:"test3",children:"Test3"})]})})},n={args:{placeholder:"Select an item"},render:s=>e.jsx(t,{...s,children:e.jsxs(t.Slot,{name:"menu",children:[e.jsx(t.Item,{value:"test1",children:"Test1"}),e.jsx(t.Item,{value:"test2",children:"Test2"}),e.jsx(t.Item,{value:"test3",children:"Test3"})]})})},o={args:{placeholder:"Select an item"},render:s=>e.jsxs(t,{...s,children:[e.jsx(t.Slot,{name:"left",children:e.jsx(l,{size:16})}),e.jsx(t.Slot,{name:"right",children:e.jsx(l,{size:16})}),e.jsxs(t.Slot,{name:"menu",children:[e.jsx(t.Item,{value:"test1",children:"Test1"}),e.jsx(t.Item,{value:"test2",children:"Test2"}),e.jsx(t.Item,{value:"test3",children:"Test3"})]})]})},S={args:{placeholder:"Select an item"},render:s=>e.jsxs(t,{...s,children:[e.jsx(t.Slot,{name:"right",children:e.jsx(l,{size:16})}),e.jsx(t.Slot,{name:"clear"}),e.jsxs(t.Slot,{name:"menu",children:[e.jsx(t.Item,{value:"test1",children:"Test1"}),e.jsx(t.Item,{value:"test2",children:"Test2"}),e.jsx(t.Item,{value:"test3",children:"Test3"})]})]})},u={args:{placeholder:"Select an item"},render:s=>e.jsxs(t,{...s,children:[e.jsx(t.Slot,{name:"clear",children:"Clear"}),e.jsx(t.Slot,{name:"left",children:e.jsx(l,{size:16})}),e.jsx(t.Slot,{name:"right",children:e.jsx(l,{size:16})}),e.jsxs(t.Slot,{name:"menu",children:[e.jsx(t.Item,{value:"test1",children:"Test1"}),e.jsx(t.Item,{value:"test2",children:"Test2"}),e.jsx(t.Item,{value:"test3",children:"Test3"})]})]})},i={args:{defaultValue:"test1"},render:s=>e.jsx(t,{...s,children:e.jsxs(t.Slot,{name:"menu",children:[e.jsx(t.Item,{value:"test1",children:"Test1"}),e.jsx(t.Item,{value:"test2",children:"Test2"}),e.jsx(t.Item,{value:"test3",children:"Test3"})]})})},d={render:s=>e.jsx(t,{...s,children:e.jsxs(t.Slot,{name:"menu",children:[e.jsx(t.Item,{value:"test1",children:"Test1"}),e.jsx(t.Item,{value:"test2",children:"Test2"}),e.jsxs(t.Group,{groupId:"Group 1",children:[e.jsx(t.Item,{value:"test3",children:"Test3"}),e.jsx(t.Item,{value:"test4",children:"Test4"}),e.jsx(t.Item,{value:"test5",children:"Test5"})]}),e.jsx(t.Item,{value:"test6",children:"Test6"})]})})},I={render:s=>e.jsx(t,{...s,children:e.jsxs(t.Slot,{name:"menu",children:[e.jsx(t.Item,{value:"ch",label:"Switzerland",children:e.jsxs("div",{className:"tw-flex tw-items-center tw-gap-4",children:[e.jsx("img",{src:"https://flagsapi.com/CH/flat/16.png",alt:"Switzerland"}),e.jsx("p",{children:"Switzerland"})]})}),e.jsx(t.Item,{value:"icon",label:"Component",children:e.jsxs("div",{className:"tw-flex tw-items-center tw-gap-4",children:[e.jsx("p",{children:"With a Component"}),e.jsx(l,{size:16})]})}),e.jsx(t.Item,{value:"basic",label:"Basic",children:"Basic"})]})})},v={decorators:[s=>e.jsx("div",{className:"tw-flex tw-gap-4",children:e.jsx(s,{})})],render:s=>{const[le,p]=ce.useState();return e.jsxs(e.Fragment,{children:[e.jsx(t,{onSelect:re=>{p(()=>re)},value:le,...s,children:e.jsxs(t.Slot,{name:"menu",children:[e.jsx(t.Item,{value:"test1",children:"Test1"}),e.jsx(t.Item,{value:"test2",children:"Test2"}),e.jsx(t.Item,{value:"test3",children:"Test3"})]})}),e.jsx(ae,{onPress:()=>p("test1"),children:"Set Test1"})]})}},x={decorators:[s=>e.jsx("div",{style:{display:"flex",flexDirection:"column",gap:32},children:e.jsx(s,{})})],parameters:{viewport:{viewports:{mobile:{name:"Mobile",styles:{width:"300px",height:"272px"}}},defaultViewport:"mobile"}},render:s=>e.jsxs(e.Fragment,{children:[e.jsxs(t,{...s,children:[e.jsx(t.Item,{value:"test1",children:"Test1"}),e.jsx(t.Item,{value:"test2",children:"Test2"}),e.jsx(t.Item,{value:"test3",children:"Test3"}),e.jsx(t.Item,{value:"test4",children:"Test4"}),e.jsx(t.Item,{value:"test5",children:"Test5"}),e.jsx(t.Item,{value:"test6",children:"Test6"}),e.jsx(t.Item,{value:"test7",children:"Test7"})]}),e.jsxs(t,{...s,children:[e.jsx(t.Item,{value:"test1",children:"Test1"}),e.jsx(t.Item,{value:"test2",children:"Test2"}),e.jsx(t.Item,{value:"test3",children:"Test3"}),e.jsx(t.Item,{value:"test4",children:"Test4"}),e.jsx(t.Item,{value:"test5",children:"Test5"}),e.jsx(t.Item,{value:"test6",children:"Test6"}),e.jsx(t.Item,{value:"test7",children:"Test7"})]}),e.jsxs(t,{...s,children:[e.jsx(t.Item,{value:"test1",children:"Test1"}),e.jsx(t.Item,{value:"test2",children:"Test2"}),e.jsx(t.Item,{value:"test3",children:"Test3"}),e.jsx(t.Item,{value:"test4",children:"Test4"}),e.jsx(t.Item,{value:"test5",children:"Test5"}),e.jsx(t.Item,{value:"test6",children:"Test6"}),e.jsx(t.Item,{value:"test7",children:"Test7"})]}),e.jsxs(t,{...s,children:[e.jsx(t.Item,{value:"test1",children:"Test1"}),e.jsx(t.Item,{value:"test2",children:"Test2"}),e.jsx(t.Item,{value:"test3",children:"Test3"}),e.jsx(t.Item,{value:"test4",children:"Test4"}),e.jsx(t.Item,{value:"test5",children:"Test5"}),e.jsx(t.Item,{value:"test6",children:"Test6"}),e.jsx(t.Item,{value:"test7",children:"Test7"})]})]})};var h,T,j;r.parameters={...r.parameters,docs:{...(h=r.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  name: 'Select',
+  render: args => {
+    return <Select {...args}>
+                <Select.Item value="test1">Test1</Select.Item>
+                <Select.Item value="test2">Test2</Select.Item>
+                <Select.Item value="test3">Test3</Select.Item>
+            </Select>;
+  }
+}`,...(j=(T=r.parameters)==null?void 0:T.docs)==null?void 0:j.source}}};var g,b,w;c.parameters={...c.parameters,docs:{...(g=c.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Select an item'
+  },
+  render: args => {
+    return <Select.Combobox {...args}>
+                <Select.Item value="test1">Test1</Select.Item>
+                <Select.Item value="test2">Test2</Select.Item>
+                <Select.Item value="test3">Test3</Select.Item>
+            </Select.Combobox>;
+  }
+}`,...(w=(b=c.parameters)==null?void 0:b.docs)==null?void 0:w.source}}};var f,C,z;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Select an item',
+    allowCustomValue: true
+  },
+  render: args => {
+    return <Select.Combobox {...args}>
+                <Select.Item value="test1">Test1</Select.Item>
+                <Select.Item value="test1">Test2</Select.Item>
+                <Select.Item value="test1">Test3</Select.Item>
+            </Select.Combobox>;
+  }
+}`,...(z=(C=a.parameters)==null?void 0:C.docs)==null?void 0:z.source}}};var y,G,B;m.parameters={...m.parameters,docs:{...(y=m.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  },
+  render: args => {
+    return <Select {...args}>
+                <Select.Slot name="menu">
+                    <Select.Item value="test1">Test1</Select.Item>
+                    <Select.Item value="test2">Test2</Select.Item>
+                    <Select.Item value="test3">Test3</Select.Item>
+                </Select.Slot>
+            </Select>;
+  }
+}`,...(B=(G=m.parameters)==null?void 0:G.docs)==null?void 0:B.source}}};var D,V,E;n.parameters={...n.parameters,docs:{...(D=n.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Select an item'
+  },
+  render: args => {
+    return <Select {...args}>
+                <Select.Slot name="menu">
+                    <Select.Item value="test1">Test1</Select.Item>
+                    <Select.Item value="test2">Test2</Select.Item>
+                    <Select.Item value="test3">Test3</Select.Item>
+                </Select.Slot>
+            </Select>;
+  }
+}`,...(E=(V=n.parameters)==null?void 0:V.docs)==null?void 0:E.source}}};var A,N,W;o.parameters={...o.parameters,docs:{...(A=o.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Select an item'
+  },
+  render: args => {
+    return <Select {...args}>
+                <Select.Slot name="left">
+                    <IconIcon size={16} />
+                </Select.Slot>
+                <Select.Slot name="right">
+                    <IconIcon size={16} />
+                </Select.Slot>
+                <Select.Slot name="menu">
+                    <Select.Item value="test1">Test1</Select.Item>
+                    <Select.Item value="test2">Test2</Select.Item>
+                    <Select.Item value="test3">Test3</Select.Item>
+                </Select.Slot>
+            </Select>;
+  }
+}`,...(W=(N=o.parameters)==null?void 0:N.docs)==null?void 0:W.source}}};var P,O,_;S.parameters={...S.parameters,docs:{...(P=S.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Select an item'
+  },
+  render: args => {
+    return <Select {...args}>
+                <Select.Slot name="right">
+                    <IconIcon size={16} />
+                </Select.Slot>
+                <Select.Slot name="clear" />
+                <Select.Slot name="menu">
+                    <Select.Item value="test1">Test1</Select.Item>
+                    <Select.Item value="test2">Test2</Select.Item>
+                    <Select.Item value="test3">Test3</Select.Item>
+                </Select.Slot>
+            </Select>;
+  }
+}`,...(_=(O=S.parameters)==null?void 0:O.docs)==null?void 0:_.source}}};var F,H,M;u.parameters={...u.parameters,docs:{...(F=u.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Select an item'
+  },
+  render: args => {
+    return <Select {...args}>
+                <Select.Slot name="clear">Clear</Select.Slot>
+                <Select.Slot name="left">
+                    <IconIcon size={16} />
+                </Select.Slot>
+                <Select.Slot name="right">
+                    <IconIcon size={16} />
+                </Select.Slot>
+                <Select.Slot name="menu">
+                    <Select.Item value="test1">Test1</Select.Item>
+                    <Select.Item value="test2">Test2</Select.Item>
+                    <Select.Item value="test3">Test3</Select.Item>
+                </Select.Slot>
+            </Select>;
+  }
+}`,...(M=(H=u.parameters)==null?void 0:H.docs)==null?void 0:M.source}}};var R,k,q;i.parameters={...i.parameters,docs:{...(R=i.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    defaultValue: 'test1'
+  },
+  render: args => {
+    return <Select {...args}>
+                <Select.Slot name="menu">
+                    <Select.Item value="test1">Test1</Select.Item>
+                    <Select.Item value="test2">Test2</Select.Item>
+                    <Select.Item value="test3">Test3</Select.Item>
+                </Select.Slot>
+            </Select>;
+  }
+}`,...(q=(k=i.parameters)==null?void 0:k.docs)==null?void 0:q.source}}};var J,K,L;d.parameters={...d.parameters,docs:{...(J=d.parameters)==null?void 0:J.docs,source:{originalSource:`{
+  render: args => {
+    return <Select {...args}>
+                <Select.Slot name="menu">
+                    <Select.Item value="test1">Test1</Select.Item>
+                    <Select.Item value="test2">Test2</Select.Item>
+                    <Select.Group groupId="Group 1">
+                        <Select.Item value="test3">Test3</Select.Item>
+                        <Select.Item value="test4">Test4</Select.Item>
+                        <Select.Item value="test5">Test5</Select.Item>
+                    </Select.Group>
+                    <Select.Item value="test6">Test6</Select.Item>
+                </Select.Slot>
+            </Select>;
+  }
+}`,...(L=(K=d.parameters)==null?void 0:K.docs)==null?void 0:L.source}}};var Q,U,X;I.parameters={...I.parameters,docs:{...(Q=I.parameters)==null?void 0:Q.docs,source:{originalSource:`{
+  render: args => {
+    return <Select {...args}>
+                <Select.Slot name="menu">
+                    <Select.Item value="ch" label="Switzerland">
+                        <div className="tw-flex tw-items-center tw-gap-4">
+                            <img src="https://flagsapi.com/CH/flat/16.png" alt="Switzerland" />
+                            <p>Switzerland</p>
+                        </div>
+                    </Select.Item>
+                    <Select.Item value="icon" label="Component">
+                        <div className="tw-flex tw-items-center tw-gap-4">
+                            <p>With a Component</p>
+                            <IconIcon size={16} />
+                        </div>
+                    </Select.Item>
+                    <Select.Item value="basic" label="Basic">
+                        Basic
+                    </Select.Item>
+                </Select.Slot>
+            </Select>;
+  }
+}`,...(X=(U=I.parameters)==null?void 0:U.docs)==null?void 0:X.source}}};var Y,Z,$;v.parameters={...v.parameters,docs:{...(Y=v.parameters)==null?void 0:Y.docs,source:{originalSource:`{
+  decorators: [Story => {
+    return <div className="tw-flex tw-gap-4">
+                    <Story />
+                </div>;
+  }],
+  render: args => {
+    const [activeItem, setActiveItem] = useState<string>();
+    return <>
+                <Select onSelect={selectedItem => {
+        setActiveItem(() => selectedItem);
+      }} value={activeItem} {...args}>
+                    <Select.Slot name="menu">
+                        <Select.Item value="test1">Test1</Select.Item>
+                        <Select.Item value="test2">Test2</Select.Item>
+                        <Select.Item value="test3">Test3</Select.Item>
+                    </Select.Slot>
+                </Select>
+                <Button onPress={() => setActiveItem('test1')}>Set Test1</Button>
+            </>;
+  }
+}`,...($=(Z=v.parameters)==null?void 0:Z.docs)==null?void 0:$.source}}};var ee,te,se;x.parameters={...x.parameters,docs:{...(ee=x.parameters)==null?void 0:ee.docs,source:{originalSource:`{
+  decorators: [Story => <div style={{
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 32
+  }}>
+                <Story />
+            </div>],
+  parameters: {
+    viewport: {
+      viewports: {
+        mobile: {
+          name: 'Mobile',
+          styles: {
+            width: '300px',
+            height: '272px'
+          }
+        }
+      },
+      defaultViewport: 'mobile'
+    }
+  },
+  render: args => {
+    return <>
+                <Select {...args}>
+                    <Select.Item value="test1">Test1</Select.Item>
+                    <Select.Item value="test2">Test2</Select.Item>
+                    <Select.Item value="test3">Test3</Select.Item>
+                    <Select.Item value="test4">Test4</Select.Item>
+                    <Select.Item value="test5">Test5</Select.Item>
+                    <Select.Item value="test6">Test6</Select.Item>
+                    <Select.Item value="test7">Test7</Select.Item>
+                </Select>
+                <Select {...args}>
+                    <Select.Item value="test1">Test1</Select.Item>
+                    <Select.Item value="test2">Test2</Select.Item>
+                    <Select.Item value="test3">Test3</Select.Item>
+                    <Select.Item value="test4">Test4</Select.Item>
+                    <Select.Item value="test5">Test5</Select.Item>
+                    <Select.Item value="test6">Test6</Select.Item>
+                    <Select.Item value="test7">Test7</Select.Item>
+                </Select>
+                <Select {...args}>
+                    <Select.Item value="test1">Test1</Select.Item>
+                    <Select.Item value="test2">Test2</Select.Item>
+                    <Select.Item value="test3">Test3</Select.Item>
+                    <Select.Item value="test4">Test4</Select.Item>
+                    <Select.Item value="test5">Test5</Select.Item>
+                    <Select.Item value="test6">Test6</Select.Item>
+                    <Select.Item value="test7">Test7</Select.Item>
+                </Select>
+                <Select {...args}>
+                    <Select.Item value="test1">Test1</Select.Item>
+                    <Select.Item value="test2">Test2</Select.Item>
+                    <Select.Item value="test3">Test3</Select.Item>
+                    <Select.Item value="test4">Test4</Select.Item>
+                    <Select.Item value="test5">Test5</Select.Item>
+                    <Select.Item value="test6">Test6</Select.Item>
+                    <Select.Item value="test7">Test7</Select.Item>
+                </Select>
+            </>;
+  }
+}`,...(se=(te=x.parameters)==null?void 0:te.docs)==null?void 0:se.source}}};const Ne=["SimpleSelect","Combobox","AllowCustomValue","Disabled","WithPlaceholder","WithDecorator","Clearable","CustomClearElement","DefaultItem","ItemGroups","CustomItem","ExternallyControlled","Overflow"];export{a as AllowCustomValue,S as Clearable,c as Combobox,u as CustomClearElement,I as CustomItem,i as DefaultItem,m as Disabled,v as ExternallyControlled,d as ItemGroups,x as Overflow,r as SimpleSelect,o as WithDecorator,n as WithPlaceholder,Ne as __namedExportsOrder,Ae as default};
