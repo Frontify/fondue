24baed54810dfa5cd1aa199fcb6acf83eee25b82
SamuelAlev <SamuelAlev@users.noreply.github.com>
2024-08-12 10:07:06 +0000

    Deploy preview for PR 2004 🛫

---
+++ pr-preview/pr-2004/legacy/assets/InContextTooltip.stories-B4IIWJ4N.js
@@ -0,0 +1,83 @@
+import{j as t}from"./jsx-runtime-Nms4Y4qS.js";import{a as y}from"./chunk-454WOBUV-CM0pFb8Z.js";import{r as d}from"./index-BwDkhjyp.js";import{B as s}from"./Box-FKHogLfP.js";import{B as l}from"./Button-CYWlMQhW.js";import{C as W}from"./Card-B0S5VVOD.js";import{D as F}from"./DialogBody-Do0JJJbs.js";import{D as A}from"./Dropdown-Tqz_EKj1.js";import{F as u}from"./Flex-C6vSrqTu.js";import{I as H}from"./InlineDialog-CI2VhUfH.js";import{T as e}from"./Tooltip-CP_K22RC.js";import{I as N}from"./IconIcon24-DxzSV-8K.js";import{I as R,a as q}from"./IconJohanna-DA7UWbqY.js";import"./v4-CQkTLCs1.js";import"./_commonjsHelpers-BosuxZz1.js";import"./borderStyle-D9jrV09G.js";import"./merge-DI-veSMP.js";import"./focusStyle-CBFElZiz.js";import"./input-DFAxtcQv.js";import"./Divider-DCi-HcbY.js";import"./FormControl-mFNqy0mf.js";import"./InputLabel-DTQphj_R.js";import"./TooltipIcon-CeymCZzr.js";import"./LegacyTooltip-C061YhHi.js";import"./useLink-DorAuuFQ.js";import"./useFocusable-CAGu0lKI.js";import"./useFocusRing-4Vnvtzpc.js";import"./SSRProvider-CYXx33DY.js";import"./chain-D5h1pgIF.js";import"./usePopper-DPXU-o95.js";import"./index-DxjWwZXO.js";import"./index-CfyPTyT-.js";import"./Portal-0C9N3MSP.js";import"./IconSize-BcCrF_mi.js";import"./useMemoizedId-EtHZ7Sxa.js";import"./IconCheckMark-BVQYjlwc.js";import"./IconExclamationMarkCircle-BkFYjnTO.js";import"./IconExclamationMarkCircle24-ChDY1Cve.js";import"./IconExclamationMarkTriangle-BucVSAOs.js";import"./IconExclamationMarkTriangle24-C3Ss9MHq.js";import"./IconQuestionMarkCircle-CMNESB9s.js";import"./validation-C9S5KgfE.js";import"./LoadingCircle-CZrma5le.js";import"./SegmentedControls-D3WaRgzO.js";import"./useFormValidationState-Vs0Uifwm.js";import"./FocusScope-DYM0gkGr.js";import"./useField-BhCfq6jD.js";import"./useLabels-DH9bA-v0.js";import"./context-C2PaECDE.js";import"./VisuallyHidden-YFPGrsWM.js";import"./useControlledState-C0C-dzuH.js";import"./motion-C48bHxkn.js";import"./Switch-nqb1dGOt.js";import"./TabItem-DYJEWVSF.js";import"./elements-BOYmskOL.js";import"./TextInput-Bn1Cza0a.js";import"./IconEyeOff-BaqRyiPL.js";import"./IconCross-BUeQrH0h.js";import"./Tree-CHGGPXgG.js";import"./immer-DBm1MMjE.js";import"./IconGrabHandle12-EPEmE8ra.js";import"./Trigger-Dr0pG8ZW.js";import"./IconCaretDown-C-LXN2Z7.js";import"./IconCaretDown16-Bqoo7UzD.js";import"./IconTrashBin-ruSc1bNE.js";import"./IconTrashBin16-D8pYIjKc.js";import"./IconTrashBin24-DrDNK9RT.js";import"./useButton-DofEV6SJ.js";import"./dialog-Dre1eVQK.js";import"./useOverlayTriggerState-Bq4IkdK0.js";import"./useMenuTrigger-tJGR04eL.js";import"./useLocalizedStringFormatter-D7Kogc_3.js";import"./helper-Cw1sh7G0.js";import"./isScrollable-Bts8_1NI.js";import"./SelectionManager-BuW52QpN.js";import"./MenuItem-BEWw9_Ux.js";import"./IconCaretRight-BUEYSaCS.js";import"./EnablePortalWrapper-DvpkRq0I.js";import"./useClickOutside-CKV3CrvD.js";const xe={title:"Components/Tooltip/In Context",tags:["autodocs"],parameters:{status:{type:"deprecated"},docs:{description:{component:"### This component is deprecated, please use the [new Tooltip component](/docs/current_components-tooltip--documentation) instead."}}}},M=()=>{const[p,m]=d.useState(!1),c=d.useRef(null);return t.jsxs(u,{justify:"center",children:[t.jsx(l,{icon:t.jsx(R,{}),ref:c,onClick:()=>m(!p),children:"Open InlineDialog"}),t.jsx(H,{handleClose:()=>m(!1),anchor:c,open:p,children:t.jsxs(F,{padding:"comfortable",children:[t.jsx(s,{className:"tw-mb-2",children:t.jsxs(u,{justify:"start",children:[t.jsx(e,{content:"Some Information",children:t.jsx(N,{})}),t.jsx("p",{children:"Information"})]})}),t.jsx(A,{enablePortal:!1,onChange:O=>console.log(O),activeItemId:"1",menuBlocks:[{id:"block1",menuItems:[{id:"1",title:"Item 1"},{id:"2",title:"Item 2"},{id:"3",title:"Item 3"},{id:"4",title:"Item 4"},{id:"5",title:"Item 5"}]}]}),t.jsx("p",{className:"tw-my-2",children:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur eveniet harum iste officia totam unde ut. Aperiam earum laborum nesciunt numquam perferendis ratione, ut. Asperiores cumque minima nemo officia rerum!"}),t.jsx(l,{onClick:()=>m(!1),children:"Close"})]})})]})},L=()=>t.jsx(s,{className:"tw-w-[200px]",children:t.jsxs(W,{onClick:()=>y("click"),children:[t.jsx(s,{className:"tw-w-[200px] tw-h-[200px] tw-bg-box-neutral-mighty"}),t.jsx(s,{className:"tw-p-4",children:t.jsx(e,{content:"Some Information",children:t.jsx("h5",{"data-test-id":"heading",className:"tw-font-heading tw-max-w-full tw-font-medium tw-text-heading-large tw-text-text tw-no-underline tw-break-normal tw-whitespace-nowrap tw-text-ellipsis tw-overflow-hidden",children:t.jsx("span",{children:"Lorem ipsum dolor sit amet"})})})})]})}),E=()=>t.jsxs(t.Fragment,{children:[t.jsx(e,{leaveDelay:0,content:"Some Information",children:t.jsx(N,{})}),t.jsx(e,{leaveDelay:0,content:"Something else",children:t.jsx(q,{})})]}),J=()=>t.jsx("div",{children:t.jsxs("span",{children:["Hover over the text to see the tooltip:"," ",t.jsx(e,{content:"Some Information",children:t.jsx("span",{className:"tw-text-text-negative",children:"Hover Me"})}),"...This is a tooltip"]})}),_=()=>t.jsx(e,{content:"Some Information",children:t.jsx(l,{onClick:y("click"),children:"Hello"})}),o=M.bind({}),i=L.bind({}),n=E.bind({}),r=J.bind({}),a=_.bind({});var x,h,f;o.parameters={...o.parameters,docs:{...(x=o.parameters)==null?void 0:x.docs,source:{originalSource:`() => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const triggerRef = useRef<HTMLButtonElement | null>(null);
+  return <Flex justify="center">
+            <Button icon={<IconJohanna />} ref={triggerRef} onClick={() => setIsDialogOpen(!isDialogOpen)}>
+                Open InlineDialog
+            </Button>
+            <InlineDialog handleClose={() => setIsDialogOpen(false)} anchor={triggerRef} open={isDialogOpen}>
+                <DialogBody padding="comfortable">
+                    <Box className="tw-mb-2">
+                        <Flex justify="start">
+                            <Tooltip content="Some Information">
+                                <IconIcon24 />
+                            </Tooltip>
+                            <p>Information</p>
+                        </Flex>
+                    </Box>
+                    <Dropdown enablePortal={false} onChange={id => console.log(id)} activeItemId="1" menuBlocks={[{
+          id: 'block1',
+          menuItems: [{
+            id: '1',
+            title: 'Item 1'
+          }, {
+            id: '2',
+            title: 'Item 2'
+          }, {
+            id: '3',
+            title: 'Item 3'
+          }, {
+            id: '4',
+            title: 'Item 4'
+          }, {
+            id: '5',
+            title: 'Item 5'
+          }]
+        }]} />
+                    <p className="tw-my-2">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur eveniet harum iste
+                        officia totam unde ut. Aperiam earum laborum nesciunt numquam perferendis ratione, ut.
+                        Asperiores cumque minima nemo officia rerum!
+                    </p>
+
+                    <Button onClick={() => setIsDialogOpen(false)}>Close</Button>
+                </DialogBody>
+            </InlineDialog>
+        </Flex>;
+}`,...(f=(h=o.parameters)==null?void 0:h.docs)==null?void 0:f.source}}};var g,I,w;i.parameters={...i.parameters,docs:{...(g=i.parameters)==null?void 0:g.docs,source:{originalSource:`() => {
+  return <Box className="tw-w-[200px]">
+            <Card onClick={() => action('click')}>
+                <Box className="tw-w-[200px] tw-h-[200px] tw-bg-box-neutral-mighty" />
+                <Box className="tw-p-4">
+                    <Tooltip content="Some Information">
+                        <h5 data-test-id="heading" className="tw-font-heading tw-max-w-full tw-font-medium tw-text-heading-large tw-text-text tw-no-underline tw-break-normal tw-whitespace-nowrap tw-text-ellipsis tw-overflow-hidden">
+                            <span>Lorem ipsum dolor sit amet</span>
+                        </h5>
+                    </Tooltip>
+                </Box>
+            </Card>
+        </Box>;
+}`,...(w=(I=i.parameters)==null?void 0:I.docs)==null?void 0:w.source}}};var j,T,D;n.parameters={...n.parameters,docs:{...(j=n.parameters)==null?void 0:j.docs,source:{originalSource:`() => {
+  return <>
+            <Tooltip leaveDelay={0} content="Some Information">
+                <IconIcon24 />
+            </Tooltip>
+            <Tooltip leaveDelay={0} content="Something else">
+                <IconJohanna24 />
+            </Tooltip>
+        </>;
+}`,...(D=(T=n.parameters)==null?void 0:T.docs)==null?void 0:D.source}}};var B,C,v;r.parameters={...r.parameters,docs:{...(B=r.parameters)==null?void 0:B.docs,source:{originalSource:`() => {
+  return <div>
+            <span>
+                Hover over the text to see the tooltip:{' '}
+                {<Tooltip content="Some Information">
+                        <span className="tw-text-text-negative">Hover Me</span>
+                    </Tooltip>}
+                ...This is a tooltip
+            </span>
+        </div>;
+}`,...(v=(C=r.parameters)==null?void 0:C.docs)==null?void 0:v.source}}};var b,S,k;a.parameters={...a.parameters,docs:{...(b=a.parameters)==null?void 0:b.docs,source:{originalSource:`() => {
+  return <Tooltip content="Some Information">
+            <Button onClick={action('click')}>Hello</Button>
+        </Tooltip>;
+}`,...(k=(S=a.parameters)==null?void 0:S.docs)==null?void 0:k.source}}};const he=["WithinInlineDialog","WithinInteractiveCard","MultipleTooltips","WithinAText","WrappingFondueButton"];export{n as MultipleTooltips,r as WithinAText,o as WithinInlineDialog,i as WithinInteractiveCard,a as WrappingFondueButton,he as __namedExportsOrder,xe as default};
