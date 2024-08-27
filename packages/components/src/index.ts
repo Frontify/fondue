8350be937e1f5ee7773cacd51d6ec9db45761aca
Jose Costa <jose.costa@frontify.com>
2024-08-27 10:12:55 +0200

    added missing exports

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -2,16 +2,22 @@
 
 import './styles.scss';
 
+export { Box } from './components/Box/Box';
 export { Button } from './components/Button/Button';
 export { Checkbox } from './components/Checkbox/Checkbox';
 export { ColorPicker } from './components/ColorPicker/ColorPicker';
 export { Dialog } from './components/Dialog/Dialog';
 export { Divider } from './components/Divider/Divider';
 export { Dropdown } from './components/Dropdown/Dropdown';
+export { Flex } from './components/Flex/Flex';
 export { Flyout } from './components/Flyout/Flyout';
+export { Grid } from './components/Grid/Grid';
 export { Label } from './components/Label/Label';
 export { LoadingBar } from './components/LoadingBar/LoadingBar';
+export { Section } from './components/Section/Section';
 export { SegmentedControl } from './components/SegmentedControl/SegmentedControl';
+export { Slider } from './components/Slider/Slider';
+export { Switch } from './components/Switch/Switch';
 export { Select } from './components/Select/Select';
 export { TextInput } from './components/TextInput/TextInput';
 export { Tooltip } from './components/Tooltip/Tooltip';
