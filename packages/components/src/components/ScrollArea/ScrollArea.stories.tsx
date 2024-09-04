5a94e51fa689c929743112035ac650b3ce31b735
Jose Costa <jose.costa@frontify.com>
2024-09-04 20:41:21 +0200

    add component, changeset, tests, stories

---
+++ packages/components/src/components/ScrollArea/ScrollArea.stories.tsx
@@ -0,0 +1,78 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import { type Meta, type StoryObj } from '@storybook/react';
+
+import { ScrollArea } from './ScrollArea';
+
+type Story = StoryObj<typeof ScrollArea>;
+const meta: Meta<typeof ScrollArea> = {
+    component: ScrollArea,
+    tags: ['autodocs'],
+    parameters: {
+        status: {
+            type: 'in_progress',
+        },
+    },
+    args: {},
+};
+export default meta;
+
+export const Default: Story = {
+    decorators: [
+        (Story) => {
+            return <Story />;
+        },
+    ],
+    render: () => {
+        return (
+            <ScrollArea maxHeight={200} maxWidth={300}>
+                <p style={{ width: 500 }}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec dui in nunc ultricies ornare.
+                    Quisque auctor, nunc nec aliquam fermentum, odio turpis ultricies elit, in ultricies nunc justo
+                    vitae purus. Integer auctor, libero in ultricies tincidunt, odio libero posuere justo, vel ultricies
+                    nunc justo vitae purus. Integer auctor, libero in ultricies tincidunt, odio libero posuere justo,
+                    vel ultricies nunc justo vitae purus. Integer auctor, libero in ultricies tincidunt, odio libero
+                    posuere justo, vel ultricies nunc justo vitae purus. Integer auctor, libero in ultricies tincidunt,
+                    odio libero posuere justo, vel ultricies nunc justo vitae purus. Integer auctor, libero in ultricies
+                    tincidunt, odio libero posuere justo, vel ultricies nunc justo vitae purus. Integer auctor, libero
+                    in ultricies tincidunt, odio libero posuere justo, vel ultricies nunc justo vitae purus. Integer
+                    auctor, libero in ultricies tincidunt, odio libero posuere justo, vel ultricies nunc justo vitae
+                    purus. Integer auctor, libero in ultricies tincidunt, odio libero posuere justo, vel ultricies nunc
+                    justo vitae purus. Integer auctor, libero in ultricies tincidunt, odio libero posuere justo, vel
+                    ultricies nunc justo vitae purus.
+                </p>
+            </ScrollArea>
+        );
+    },
+};
+
+export const WithWrapper: Story = {
+    decorators: [
+        (Story) => {
+            return <Story />;
+        },
+    ],
+    render: () => {
+        return (
+            <div style={{ width: 300, height: 200 }}>
+                <ScrollArea>
+                    <p style={{ width: 500 }}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec dui in nunc ultricies
+                        ornare. Quisque auctor, nunc nec aliquam fermentum, odio turpis ultricies elit, in ultricies
+                        nunc justo vitae purus. Integer auctor, libero in ultricies tincidunt, odio libero posuere
+                        justo, vel ultricies nunc justo vitae purus. Integer auctor, libero in ultricies tincidunt, odio
+                        libero posuere justo, vel ultricies nunc justo vitae purus. Integer auctor, libero in ultricies
+                        tincidunt, odio libero posuere justo, vel ultricies nunc justo vitae purus. Integer auctor,
+                        libero in ultricies tincidunt, odio libero posuere justo, vel ultricies nunc justo vitae purus.
+                        Integer auctor, libero in ultricies tincidunt, odio libero posuere justo, vel ultricies nunc
+                        justo vitae purus. Integer auctor, libero in ultricies tincidunt, odio libero posuere justo, vel
+                        ultricies nunc justo vitae purus. Integer auctor, libero in ultricies tincidunt, odio libero
+                        posuere justo, vel ultricies nunc justo vitae purus. Integer auctor, libero in ultricies
+                        tincidunt, odio libero posuere justo, vel ultricies nunc justo vitae purus. Integer auctor,
+                        libero in ultricies tincidunt, odio libero posuere justo, vel ultricies nunc justo vitae purus.
+                    </p>
+                </ScrollArea>
+            </div>
+        );
+    },
+};
---
+++ packages/components/src/components/ScrollArea/ScrollArea.tsx
@@ -0,0 +1,64 @@
+/* (c) Copyright Frontify Ltd., all rights reserved. */
+
+import * as RadixScrollArea from '@radix-ui/react-scroll-area';
+import { forwardRef, type ReactNode } from 'react';
+
+import styles from './styles/scrollArea.module.scss';
+
+export type ScrollAreaProps = {
+    /**
+     * Maximum height of the scroll area
+     * @default '100%'
+     */
+    maxHeight?: string | number;
+    /**
+     * Minimum width of the scroll area
+     * @default '100%'
+     */
+    maxWidth?: string | number;
+    'data-test-id'?: string;
+    children: ReactNode;
+};
+
+const ScrollAreaComponent = ({
+    maxHeight = '100%',
+    maxWidth = '100%',
+    'data-test-id': dataTestId = 'fondue-scroll-area',
+    children,
+}: ScrollAreaProps) => {
+    return (
+        <RadixScrollArea.Root className={styles.root} style={{ maxWidth }} data-test-id={dataTestId}>
+            <RadixScrollArea.Viewport
+                className={styles.viewport}
+                style={{ maxHeight }}
+                data-test-id={`${dataTestId}-viewport`}
+            >
+                {children}
+            </RadixScrollArea.Viewport>
+            <RadixScrollArea.Scrollbar
+                className={styles.scrollbar}
+                orientation="vertical"
+                data-test-id={`${dataTestId}-vertical-scrollbar`}
+            >
+                <RadixScrollArea.Thumb
+                    className={styles.thumb}
+                    data-test-id={`${dataTestId}-vertical-scrollbar-thumb`}
+                />
+            </RadixScrollArea.Scrollbar>
+            <RadixScrollArea.Scrollbar
+                className={styles.scrollbar}
+                orientation="horizontal"
+                data-test-id={`${dataTestId}-horizontal-scrollbar`}
+            >
+                <RadixScrollArea.Thumb
+                    className={styles.thumb}
+                    data-test-id={`${dataTestId}-horizontal-scrollbar-thumb`}
+                />
+            </RadixScrollArea.Scrollbar>
+            <RadixScrollArea.Corner className={styles.corner} data-test-id={`${dataTestId}-corner`} />
+        </RadixScrollArea.Root>
+    );
+};
+
+export const ScrollArea = forwardRef<HTMLButtonElement, ScrollAreaProps>(ScrollAreaComponent);
+ScrollArea.displayName = 'ScrollArea';
