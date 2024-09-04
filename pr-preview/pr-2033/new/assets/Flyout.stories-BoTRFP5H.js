ac40e4861add02bdc0480e433efe2ae2cd3beb66
SamuelAlev <SamuelAlev@users.noreply.github.com>
2024-09-04 18:39:44 +0000

    Deploy preview for PR 2033 🛫

---
+++ pr-preview/pr-2033/new/assets/Flyout.stories-BoTRFP5H.js
@@ -0,0 +1,208 @@
+import{j as e}from"./jsx-runtime-DBEcOq3S.js";import{r as Be}from"./index-DtJulBIN.js";import{B as t}from"./Button-DUGArIHC.js";import{T as C}from"./TextInput-BEZcqdCr.js";import{a as fe,b as Ce,c as Te,d as be,e as He,f as we,F as r}from"./Flyout-CcMiOItB.js";import"./_commonjsHelpers-BosuxZz1.js";import"./styleUtilities-BnElyh0X.js";import"./focusStyle-IgMhxCMT.js";import"./fondue-icons324-Bw2AQYl8.js";import"./index-BaMjp9_F.js";import"./index-BHYJtXto.js";import"./index-Dhc4yksV.js";import"./index-BqTDQIm7.js";import"./index-P-l_ls5c.js";import"./index-xqgHIrVP.js";import"./index-CvTA8V3i.js";import"./usePreventDropdownOverflow-C4c1BL1y.js";import"./index-Bw4vj2Av.js";import"./index-D21DJKX5.js";const Je={component:fe,subcomponents:{"Flyout.Root":Ce,"Flyout.Trigger":Te,"Flyout.Header":be,"Flyout.Body":He,"Flyout.Footer":we},tags:["autodocs"],parameters:{status:{type:"in_progress"}},args:{children:"Hello World"},render:({...o})=>e.jsxs(r.Root,{children:[e.jsx(r.Trigger,{children:e.jsx(t,{children:"Open flyout"})}),e.jsxs(r.Content,{...o,children:[e.jsx(r.Header,{showCloseButton:!0,children:"Header"}),e.jsx(r.Body,{...o}),e.jsx(r.Footer,{children:e.jsxs("div",{className:"tw-flex tw-justify-end tw-gap-2",children:[e.jsx(t,{emphasis:"default",children:"Cancel"}),e.jsx(t,{children:"Submit"})]})})]})]})},n={args:{children:"Hello World"},render:({...o})=>e.jsxs(r.Root,{children:[e.jsx(r.Trigger,{children:e.jsx(t,{children:"Open flyout"})}),e.jsx(r.Content,{...o,children:e.jsx(r.Body,{...o})})]})},s={render:({...o})=>e.jsxs(r.Root,{children:[e.jsx(r.Trigger,{children:e.jsx(t,{children:"Open flyout"})}),e.jsxs(r.Content,{...o,children:[e.jsx(r.Header,{children:"Header"}),e.jsx(r.Body,{children:"I am a flyout"})]})]})},i={render:({...o})=>e.jsxs(r.Root,{children:[e.jsx(r.Trigger,{children:e.jsx(t,{children:"Open flyout"})}),e.jsxs(r.Content,{...o,children:[e.jsx(r.Body,{children:"I am a flyout"}),e.jsx(r.Footer,{children:e.jsx(t,{children:"Submit"})})]})]})},a={args:{children:"Hello World"},render:({...o})=>e.jsxs(r.Root,{children:[e.jsx(r.Trigger,{children:e.jsx(t,{children:"Open flyout"})}),e.jsxs(r.Content,{...o,children:[e.jsx(r.Header,{children:"Header"}),e.jsxs(r.Body,{...o,children:[e.jsx(C,{}),e.jsx(C,{}),e.jsx(C,{})]})]})]})},l={},d={render:({...o})=>e.jsxs(r.Root,{children:[e.jsx(r.Trigger,{children:e.jsx(t,{children:"Open flyout"})}),e.jsxs(r.Content,{...o,children:[e.jsx(r.Header,{showCloseButton:!0,children:"Header"}),e.jsx(r.Body,{children:"I am a flyout"})]})]})},u={args:{padding:"tight"}},c={args:{padding:"comfortable"}},m={args:{padding:"spacious"}},p={render:({...o})=>e.jsxs(r.Root,{children:[e.jsx(r.Trigger,{children:e.jsx(t,{children:"Open flyout"})}),e.jsx(r.Content,{...o,children:e.jsx("div",{className:"tw-bg-box-neutral tw-p-10",children:"This is FlyoutContent"})})]})},g={args:{side:"right"}},y={args:{side:"right",align:"center"}},h={args:{side:"right",align:"end"}},x={args:{side:"bottom",children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}},F={args:{maxWidth:"200px",side:"bottom",children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}},j={parameters:{viewport:{viewports:{mobile:{name:"Mobile",styles:{width:"375px",height:"667px"}}},defaultViewport:"mobile"}},render:({...o})=>e.jsxs(r.Root,{children:[e.jsx(r.Trigger,{children:e.jsx(t,{children:"Open flyout"})}),e.jsxs(r.Content,{...o,children:[e.jsx(r.Header,{showCloseButton:!0,children:"Header"}),e.jsx(r.Body,{children:"I am a flyout"}),e.jsx(r.Footer,{children:e.jsx(t,{children:"Submit"})})]})]})},B={decorators:[o=>e.jsx("div",{style:{display:"flex",flexDirection:"column",gap:32,margin:"100px 300px"},children:e.jsx(o,{})})],parameters:{viewport:{viewports:{desktop:{name:"Desktop",styles:{width:"800px",height:"500px"}}},defaultViewport:"desktop"}},render:({...o})=>e.jsx(e.Fragment,{children:Array.from({length:4}).map((Re,f)=>Be.createElement(r.Root,{...o,key:f},e.jsx(r.Trigger,{children:e.jsxs(t,{children:["Trigger ",f]})}),e.jsxs(r.Content,{children:[e.jsxs(r.Header,{children:["Flyout ",f]}),e.jsxs(r.Body,{children:[e.jsx("p",{children:"Lorem ipsum dolor sit amet"}),e.jsx("p",{children:"Lorem ipsum dolor sit amet"}),e.jsx("p",{children:"Lorem ipsum dolor sit amet"}),e.jsx("p",{children:"Lorem ipsum dolor sit amet"}),e.jsx("p",{children:"Lorem ipsum dolor sit amet"}),e.jsx("p",{children:"Lorem ipsum dolor sit amet"})]})]})))})};var T,b,H;n.parameters={...n.parameters,docs:{...(T=n.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    children: 'Hello World'
+  },
+  render: ({
+    ...args
+  }) => {
+    return <Flyout.Root>
+                <Flyout.Trigger>
+                    <Button>Open flyout</Button>
+                </Flyout.Trigger>
+                <Flyout.Content {...args}>
+                    <Flyout.Body {...args} />
+                </Flyout.Content>
+            </Flyout.Root>;
+  }
+}`,...(H=(b=n.parameters)==null?void 0:b.docs)==null?void 0:H.source}}};var w,R,v;s.parameters={...s.parameters,docs:{...(w=s.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: ({
+    ...args
+  }) => {
+    return <Flyout.Root>
+                <Flyout.Trigger>
+                    <Button>Open flyout</Button>
+                </Flyout.Trigger>
+                <Flyout.Content {...args}>
+                    <Flyout.Header>Header</Flyout.Header>
+                    <Flyout.Body>I am a flyout</Flyout.Body>
+                </Flyout.Content>
+            </Flyout.Root>;
+  }
+}`,...(v=(R=s.parameters)==null?void 0:R.docs)==null?void 0:v.source}}};var W,S,L;i.parameters={...i.parameters,docs:{...(W=i.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  render: ({
+    ...args
+  }) => {
+    return <Flyout.Root>
+                <Flyout.Trigger>
+                    <Button>Open flyout</Button>
+                </Flyout.Trigger>
+                <Flyout.Content {...args}>
+                    <Flyout.Body>I am a flyout</Flyout.Body>
+                    <Flyout.Footer>
+                        <Button>Submit</Button>
+                    </Flyout.Footer>
+                </Flyout.Content>
+            </Flyout.Root>;
+  }
+}`,...(L=(S=i.parameters)==null?void 0:S.docs)==null?void 0:L.source}}};var O,q,I;a.parameters={...a.parameters,docs:{...(O=a.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  args: {
+    children: 'Hello World'
+  },
+  render: ({
+    ...args
+  }) => {
+    return <Flyout.Root>
+                <Flyout.Trigger>
+                    <Button>Open flyout</Button>
+                </Flyout.Trigger>
+                <Flyout.Content {...args}>
+                    <Flyout.Header>Header</Flyout.Header>
+                    <Flyout.Body {...args}>
+                        <TextInput />
+                        <TextInput />
+                        <TextInput />
+                    </Flyout.Body>
+                </Flyout.Content>
+            </Flyout.Root>;
+  }
+}`,...(I=(q=a.parameters)==null?void 0:q.docs)==null?void 0:I.source}}};var k,A,D;l.parameters={...l.parameters,docs:{...(k=l.parameters)==null?void 0:k.docs,source:{originalSource:"{}",...(D=(A=l.parameters)==null?void 0:A.docs)==null?void 0:D.source}}};var E,M,P;d.parameters={...d.parameters,docs:{...(E=d.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  render: ({
+    ...args
+  }) => {
+    return <Flyout.Root>
+                <Flyout.Trigger>
+                    <Button>Open flyout</Button>
+                </Flyout.Trigger>
+                <Flyout.Content {...args}>
+                    <Flyout.Header showCloseButton>Header</Flyout.Header>
+                    <Flyout.Body>I am a flyout</Flyout.Body>
+                </Flyout.Content>
+            </Flyout.Root>;
+  }
+}`,...(P=(M=d.parameters)==null?void 0:M.docs)==null?void 0:P.source}}};var V,_,U;u.parameters={...u.parameters,docs:{...(V=u.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    padding: 'tight'
+  }
+}`,...(U=(_=u.parameters)==null?void 0:_.docs)==null?void 0:U.source}}};var N,z,G;c.parameters={...c.parameters,docs:{...(N=c.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    padding: 'comfortable'
+  }
+}`,...(G=(z=c.parameters)==null?void 0:z.docs)==null?void 0:G.source}}};var J,K,Q;m.parameters={...m.parameters,docs:{...(J=m.parameters)==null?void 0:J.docs,source:{originalSource:`{
+  args: {
+    padding: 'spacious'
+  }
+}`,...(Q=(K=m.parameters)==null?void 0:K.docs)==null?void 0:Q.source}}};var X,Y,Z;p.parameters={...p.parameters,docs:{...(X=p.parameters)==null?void 0:X.docs,source:{originalSource:`{
+  render: ({
+    ...args
+  }) => {
+    return <Flyout.Root>
+                <Flyout.Trigger>
+                    <Button>Open flyout</Button>
+                </Flyout.Trigger>
+                <Flyout.Content {...args}>
+                    <div className="tw-bg-box-neutral tw-p-10">This is FlyoutContent</div>
+                </Flyout.Content>
+            </Flyout.Root>;
+  }
+}`,...(Z=(Y=p.parameters)==null?void 0:Y.docs)==null?void 0:Z.source}}};var $,ee,re;g.parameters={...g.parameters,docs:{...($=g.parameters)==null?void 0:$.docs,source:{originalSource:`{
+  args: {
+    side: 'right'
+  }
+}`,...(re=(ee=g.parameters)==null?void 0:ee.docs)==null?void 0:re.source}}};var oe,te,ne;y.parameters={...y.parameters,docs:{...(oe=y.parameters)==null?void 0:oe.docs,source:{originalSource:`{
+  args: {
+    side: 'right',
+    align: 'center'
+  }
+}`,...(ne=(te=y.parameters)==null?void 0:te.docs)==null?void 0:ne.source}}};var se,ie,ae;h.parameters={...h.parameters,docs:{...(se=h.parameters)==null?void 0:se.docs,source:{originalSource:`{
+  args: {
+    side: 'right',
+    align: 'end'
+  }
+}`,...(ae=(ie=h.parameters)==null?void 0:ie.docs)==null?void 0:ae.source}}};var le,de,ue;x.parameters={...x.parameters,docs:{...(le=x.parameters)==null?void 0:le.docs,source:{originalSource:`{
+  args: {
+    side: 'bottom',
+    children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+  }
+}`,...(ue=(de=x.parameters)==null?void 0:de.docs)==null?void 0:ue.source}}};var ce,me,pe;F.parameters={...F.parameters,docs:{...(ce=F.parameters)==null?void 0:ce.docs,source:{originalSource:`{
+  args: {
+    maxWidth: '200px',
+    side: 'bottom',
+    children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+  }
+}`,...(pe=(me=F.parameters)==null?void 0:me.docs)==null?void 0:pe.source}}};var ge,ye,he;j.parameters={...j.parameters,docs:{...(ge=j.parameters)==null?void 0:ge.docs,source:{originalSource:`{
+  parameters: {
+    viewport: {
+      viewports: {
+        mobile: {
+          name: 'Mobile',
+          styles: {
+            width: '375px',
+            height: '667px'
+          }
+        }
+      },
+      defaultViewport: 'mobile'
+    }
+  },
+  render: ({
+    ...args
+  }) => {
+    return <Flyout.Root>
+                <Flyout.Trigger>
+                    <Button>Open flyout</Button>
+                </Flyout.Trigger>
+                <Flyout.Content {...args}>
+                    <Flyout.Header showCloseButton>Header</Flyout.Header>
+                    <Flyout.Body>I am a flyout</Flyout.Body>
+                    <Flyout.Footer>
+                        <Button>Submit</Button>
+                    </Flyout.Footer>
+                </Flyout.Content>
+            </Flyout.Root>;
+  }
+}`,...(he=(ye=j.parameters)==null?void 0:ye.docs)==null?void 0:he.source}}};var xe,Fe,je;B.parameters={...B.parameters,docs:{...(xe=B.parameters)==null?void 0:xe.docs,source:{originalSource:`{
+  decorators: [Story => <div style={{
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 32,
+    margin: '100px 300px'
+  }}>
+                <Story />
+            </div>],
+  parameters: {
+    viewport: {
+      viewports: {
+        desktop: {
+          name: 'Desktop',
+          styles: {
+            width: '800px',
+            height: '500px'
+          }
+        }
+      },
+      defaultViewport: 'desktop'
+    }
+  },
+  render: ({
+    ...args
+  }) => <>
+            {Array.from({
+      length: 4
+    }).map((_, index) => <Flyout.Root {...args} key={index}>
+                    <Flyout.Trigger>
+                        <Button>Trigger {index}</Button>
+                    </Flyout.Trigger>
+                    <Flyout.Content>
+                        <Flyout.Header>Flyout {index}</Flyout.Header>
+                        <Flyout.Body>
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <p>Lorem ipsum dolor sit amet</p>
+                        </Flyout.Body>
+                    </Flyout.Content>
+                </Flyout.Root>)}
+        </>
+}`,...(je=(Fe=B.parameters)==null?void 0:Fe.docs)==null?void 0:je.source}}};const Ke=["BasicContent","WithHeader","WithFooter","WithFocusableContent","WithHeaderAndFooter","WithCloseButton","WithTightPadding","WithComfortablePadding","WithSpaciousPadding","WithCustomContent","RightSide","AlignCenter","AlignEnd","LongContent","WithCustomMaxWidth","MobileView","Overflow"];export{y as AlignCenter,h as AlignEnd,n as BasicContent,x as LongContent,j as MobileView,B as Overflow,g as RightSide,d as WithCloseButton,c as WithComfortablePadding,p as WithCustomContent,F as WithCustomMaxWidth,a as WithFocusableContent,i as WithFooter,s as WithHeader,l as WithHeaderAndFooter,m as WithSpaciousPadding,u as WithTightPadding,Ke as __namedExportsOrder,Je as default};
