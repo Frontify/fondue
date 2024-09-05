e35f46be2969396b637e02f7018c5fb9640caec9
jose-costa-frontify <jose-costa-frontify@users.noreply.github.com>
2024-09-05 09:10:29 +0000

    Deploy preview for PR 2036 🛫

---
+++ pr-preview/pr-2036/new/assets/Flex.stories-DQERDwU5.js
@@ -0,0 +1,92 @@
+import{j as e}from"./jsx-runtime-DBEcOq3S.js";import{B,D as d}from"./Box-CcGIJvaO.js";import{F as r}from"./Flex-01brZM-J.js";import"./index-DtJulBIN.js";import"./_commonjsHelpers-BosuxZz1.js";import"./propsToCssVariables-BE5Q_-YS.js";const o=s=>e.jsx(B,{width:"100px",height:"100px",...s,children:e.jsx(d,{})}),w={title:"Layout/Flex",component:r,tags:["autodocs"],parameters:{status:{type:"in_progress"}},args:{gap:"20px",direction:"column"}},x={render:s=>e.jsxs(r,{...s,children:[e.jsxs(r,{gapX:"10px",children:[e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{})]}),e.jsxs(r,{gapX:"10px",children:[e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{})]})]})},n={render:s=>e.jsxs(r,{...s,children:[e.jsxs(r,{gapX:"10px",children:[e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsxs(r,{children:[e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{})]})]}),e.jsxs(r,{p:"20px",children:[e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsxs(r,{gapX:"10px",children:[e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{})]})]})]})},a={render:s=>e.jsxs(r,{...s,direction:{base:"row",sm:"column"},children:[e.jsxs(r,{gapX:{sm:"10px"},gapY:{base:"10px",sm:"0px"},direction:{base:"column",sm:"row"},children:[e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{})]}),e.jsxs(r,{gapX:{sm:"10px"},gapY:{base:"10px",sm:"0px"},direction:{base:"column",sm:"row"},children:[e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{}),e.jsx(o,{})]})]})};var t,c,i;x.parameters={...x.parameters,docs:{...(t=x.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: args => {
+    return <Flex {...args}>
+                <Flex gapX="10px">
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                </Flex>
+                <Flex gapX="10px">
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                </Flex>
+            </Flex>;
+  }
+}`,...(i=(c=x.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};var p,j,l;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: args => {
+    return <Flex {...args}>
+                <Flex gapX="10px">
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <Flex>
+                        <DecorativeBox />
+                        <DecorativeBox />
+                        <DecorativeBox />
+                        <DecorativeBox />
+                        <DecorativeBox />
+                    </Flex>
+                </Flex>
+                <Flex p="20px">
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <Flex gapX="10px">
+                        <DecorativeBox />
+                        <DecorativeBox />
+                        <DecorativeBox />
+                        <DecorativeBox />
+                        <DecorativeBox />
+                    </Flex>
+                </Flex>
+            </Flex>;
+  }
+}`,...(l=(j=n.parameters)==null?void 0:j.docs)==null?void 0:l.source}}};var m,D,v;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: args => {
+    return <Flex {...args} direction={{
+      base: 'row',
+      sm: 'column'
+    }}>
+                <Flex gapX={{
+        sm: '10px'
+      }} gapY={{
+        base: '10px',
+        sm: '0px'
+      }} direction={{
+        base: 'column',
+        sm: 'row'
+      }}>
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                </Flex>
+
+                <Flex gapX={{
+        sm: '10px'
+      }} gapY={{
+        base: '10px',
+        sm: '0px'
+      }} direction={{
+        base: 'column',
+        sm: 'row'
+      }}>
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                    <DecorativeBox />
+                </Flex>
+            </Flex>;
+  }
+}`,...(v=(D=a.parameters)==null?void 0:D.docs)==null?void 0:v.source}}};const f=["Default","Nested","Responsive"];export{x as Default,n as Nested,a as Responsive,f as __namedExportsOrder,w as default};
