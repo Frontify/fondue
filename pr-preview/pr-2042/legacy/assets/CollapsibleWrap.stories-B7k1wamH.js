adb1f20baf32be7bd0d80ca2d8b7e7fd1e8d323a
jose-costa-frontify <jose-costa-frontify@users.noreply.github.com>
2024-09-06 13:58:02 +0000

    Deploy preview for PR 2042 🛫

---
+++ pr-preview/pr-2042/legacy/assets/CollapsibleWrap.stories-B7k1wamH.js
@@ -0,0 +1,25 @@
+import{j as e}from"./jsx-runtime-Nms4Y4qS.js";import{C as m}from"./Card-L2KQuesb.js";import{H as h}from"./Heading-80600rn_.js";import{T as a}from"./Text-BU2xdpZ1.js";import{C as l}from"./CollapsibleWrap-DiKmRSc0.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./useButton-DFNFXdnN.js";import"./useFocusable-wKuCPQI4.js";import"./useFocusRing-Vzyl7-Lk.js";import"./SSRProvider-CYXx33DY.js";import"./chain-D5h1pgIF.js";import"./focusStyle-CBFElZiz.js";import"./merge-DI-veSMP.js";import"./records-DFIz8BfL.js";import"./index-hyK68Dd8.js";import"./motion-C48bHxkn.js";const H={title:"Components/Collapsible Wrap",component:l,tags:["autodocs"],args:{isOpen:!0,animateOpacity:!0}},t=o=>e.jsx(m,{children:e.jsxs("div",{className:"tw-divide-y tw-divide-black-10",children:[e.jsx("div",{className:"tw-p-3",children:e.jsx(h,{children:"Collapsible Wrap"})}),e.jsx(l,{...o,children:e.jsxs("div",{className:"tw-h-[200px] tw-p-5 tw-bg-black-5 tw-flex tw-justify-center tw-items-center tw-flex-col",children:[e.jsx(a,{children:"I am the child content of the Collapsible Wrap Component."}),e.jsx(a,{children:"Toggle the open state to hide me."})]})})]})}),i=o=>e.jsx(m,{children:e.jsxs("div",{className:"tw-divide-y tw-divide-black-10",children:[e.jsx("div",{className:"tw-p-3",children:e.jsx(h,{children:"Collapsible Wrap Without First Animation"})}),e.jsx(l,{...o,preventInitialAnimation:!0,children:e.jsxs("div",{className:"tw-h-[200px] tw-p-5 tw-bg-black-5 tw-flex tw-justify-center tw-items-center tw-flex-col",children:[e.jsx(a,{children:"I am the child content of the Collapsible Wrap Component."}),e.jsx(a,{children:"Toggle the open state to hide me."})]})})]})});t.__docgenInfo={description:"",methods:[],displayName:"CollapsibleWrap"};i.__docgenInfo={description:"",methods:[],displayName:"CollapsibleWrapNoStartingAnimation"};var s,n,r;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`args => <Card>
+        <div className="tw-divide-y tw-divide-black-10">
+            <div className="tw-p-3">
+                <Heading>Collapsible Wrap</Heading>
+            </div>
+            <CollapsibleWrapComponent {...args}>
+                <div className="tw-h-[200px] tw-p-5 tw-bg-black-5 tw-flex tw-justify-center tw-items-center tw-flex-col">
+                    <Text>I am the child content of the Collapsible Wrap Component.</Text>
+                    <Text>Toggle the open state to hide me.</Text>
+                </div>
+            </CollapsibleWrapComponent>
+        </div>
+    </Card>`,...(r=(n=t.parameters)==null?void 0:n.docs)==null?void 0:r.source}}};var p,d,c;i.parameters={...i.parameters,docs:{...(p=i.parameters)==null?void 0:p.docs,source:{originalSource:`args => <Card>
+        <div className="tw-divide-y tw-divide-black-10">
+            <div className="tw-p-3">
+                <Heading>Collapsible Wrap Without First Animation</Heading>
+            </div>
+            <CollapsibleWrapComponent {...args} preventInitialAnimation={true}>
+                <div className="tw-h-[200px] tw-p-5 tw-bg-black-5 tw-flex tw-justify-center tw-items-center tw-flex-col">
+                    <Text>I am the child content of the Collapsible Wrap Component.</Text>
+                    <Text>Toggle the open state to hide me.</Text>
+                </div>
+            </CollapsibleWrapComponent>
+        </div>
+    </Card>`,...(c=(d=i.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};const S=["CollapsibleWrap","CollapsibleWrapNoStartingAnimation"];export{t as CollapsibleWrap,i as CollapsibleWrapNoStartingAnimation,S as __namedExportsOrder,H as default};
