0fe7d7afdd55e346c3905c12f81cb5f58f353dc9
noahwaldner <noahwaldner@users.noreply.github.com>
2024-09-11 11:58:56 +0000

    Deploy preview for PR 2051 🛫

---
+++ pr-preview/pr-2051/legacy/assets/DropZone.stories-CU0-f8YU.js
@@ -0,0 +1,28 @@
+import{j as e}from"./jsx-runtime-Nms4Y4qS.js";import{a as c}from"./chain-D5h1pgIF.js";import{r as I}from"./index-BwDkhjyp.js";import{L as r,s as M,r as p}from"./renderContent-CM5deum1.js";import"./SSRProvider-CYXx33DY.js";import"./_commonjsHelpers-BosuxZz1.js";import"./merge-DI-veSMP.js";import"./useFocusRing-Vzyl7-Lk.js";import"./focusStyle-CBFElZiz.js";const O={title:"Components/Drop Zone",component:r,tags:["autodocs"],args:{dragDisabled:!1},argTypes:{onMove:{action:"onMove"}}},n=({onMove:s,dragDisabled:i})=>{const[o,u]=I.useState(M),a=t=>{const v=o.map(d=>{const m=t.find(h=>h.id===d.id);return m?{...m}:{...d}});u(v)};return e.jsxs(e.Fragment,{children:[e.jsx("div",{className:"tw-m-auto tw-w-[600px] tw-pb-6",children:e.jsx(r,{items:o,onMove:c(a,s),dragDisabled:i,renderContent:(...t)=>p(...t)})}),e.jsx("div",{className:"tw-m-auto tw-w-[600px]",children:e.jsx(r,{items:o,onMove:c(a,s),dragDisabled:i,renderContent:(...t)=>p(...t)})})]})};n.__docgenInfo={description:"",methods:[],displayName:"DropZoneWithOrderableList"};var f,l,g;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`({
+  onMove,
+  dragDisabled
+}) => {
+  const [items, setItems] = useState(storyItems);
+  const handleMove = (modifiedItems: LegacyOrderableListItem<StoryListItem>[]) => {
+    const modifiedArray = items.map(item => {
+      const matchingModifiedItem = modifiedItems.find(modifiedItem => modifiedItem.id === item.id);
+      if (matchingModifiedItem) {
+        return {
+          ...matchingModifiedItem
+        };
+      }
+      return {
+        ...item
+      };
+    });
+    setItems(modifiedArray);
+  };
+  return <>
+            <div className="tw-m-auto tw-w-[600px] tw-pb-6">
+                <DropZoneComponent items={items} onMove={chain(handleMove, onMove)} dragDisabled={dragDisabled} renderContent={(...args) => renderContent(...args)} />
+            </div>
+            <div className="tw-m-auto tw-w-[600px]">
+                <DropZoneComponent items={items} onMove={chain(handleMove, onMove)} dragDisabled={dragDisabled} renderContent={(...args) => renderContent(...args)} />
+            </div>
+        </>;
+}`,...(g=(l=n.parameters)==null?void 0:l.docs)==null?void 0:g.source}}};const N=["DropZoneWithOrderableList"];export{n as DropZoneWithOrderableList,N as __namedExportsOrder,O as default};
