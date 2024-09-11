e33db674d0b0ec5056fc45f0e43b69ef74cfac59
Noah Waldner <noah.waldner@frontify.com>
2024-09-11 08:11:28 +0200

    fix linting

--- packages/components/src/components/Select/__tests__/Select.ct.tsx
+++ packages/components/src/components/Select/__tests__/Select.ct.tsx
@@ -4,10 +4,10 @@ import { IconIcon } from '@frontify/fondue-icons';
 import { expect, test } from '@playwright/experimental-ct-react';
 import * as sinon from 'sinon';
 
-import { Select } from '../Select';
-
 import { FOCUS_BORDER_CSS, FOCUS_OUTLINE_CSS } from '#/helpers/constants';
 import { MAX_HEIGHT_MARGIN } from '#/utilities/domUtilities';
+
+import { Select } from '../Select';
 
 const SELECT_TEST_ID = 'test-select';
 const SELECT_MENU_TEST_ID = 'fondue-select-menu';
