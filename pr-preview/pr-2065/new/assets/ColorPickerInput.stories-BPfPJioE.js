615e5c4abef738ab93cc61aa0e5af76a6a6cd840
jose-costa-frontify <jose-costa-frontify@users.noreply.github.com>
2024-09-18 12:57:52 +0000

    Deploy preview for PR 2065 🛫

---
+++ pr-preview/pr-2065/new/assets/ColorPickerInput.stories-BPfPJioE.js
@@ -0,0 +1,19 @@
+import{j as i}from"./jsx-runtime-Nms4Y4qS.js";import{d as u,c}from"./ColorPicker-D4rYFydb.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-Brw_kfkv.js";import"./index-DxjWwZXO.js";import"./index-YYnwjzoa.js";import"./fondue-icons326-DXd15KB3.js";import"./Select-Dxd4fGd4.js";import"./index-FKkhIU6G.js";import"./index-DOnKqARA.js";import"./index-BWD2N0gV.js";import"./index-9q6yCIGA.js";import"./index-kUwV4muI.js";import"./usePreventDropdownOverflow-hl17Ppn0.js";import"./index-DduqEaLY.js";import"./index-BwIM2_zL.js";import"./index-DGaPG0k3.js";import"./extends-CF3RwP-h.js";import"./index-D3ylJrlI.js";import"./TextInput-entYNURv.js";import"./styleUtilities-BnElyh0X.js";const z={title:"Components/ColorPicker/Input",component:u,tags:["autodocs"],parameters:{status:{type:"in_progress"}},args:{}},r={args:{currentColor:{red:230,green:100,blue:50,alpha:.8,name:"custom-color"}},render:e=>i.jsx(c.Input,{...e,onClear:()=>{}})},o={args:{},render:e=>i.jsx(c.Input,{...e,onClear:()=>{}})};var t,n,a;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    currentColor: {
+      red: 230,
+      green: 100,
+      blue: 50,
+      alpha: 0.8,
+      name: 'custom-color'
+    }
+  },
+  render: args => {
+    return <ColorPicker.Input {...args} onClear={() => {}} />;
+  }
+}`,...(a=(n=r.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};var s,p,m;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <ColorPicker.Input {...args} onClear={() => {}} />;
+  }
+}`,...(m=(p=o.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const A=["Default","Empty"];export{r as Default,o as Empty,A as __namedExportsOrder,z as default};
