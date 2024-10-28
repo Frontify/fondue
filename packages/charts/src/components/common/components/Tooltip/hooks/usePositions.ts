9303a7d236e6e8bba96a75b287f10988bd5afdf0
Jose Costa <jose.costa@frontify.com>
2024-10-28 17:49:03 +0100

    fix: ts issue (left hand operator was never nullish)

--- packages/charts/src/components/common/components/Tooltip/hooks/usePositions.ts
+++ packages/charts/src/components/common/components/Tooltip/hooks/usePositions.ts
@@ -29,9 +29,8 @@ export const usePositions = (hideGlyphs: boolean, horizontal: boolean, stackingG
             const entry = dataRegistry?.get(key);
             const xAccessor = entry?.xAccessor;
             const yAccessor = entry?.yAccessor;
-            const left =
-                xScale && xAccessor ? (Number(xScale(xAccessor(datum))) + xScaleBandwidth / 2 ?? 0) : undefined;
-            const top = yScale && yAccessor ? (Number(yScale(yAccessor(datum))) + yScaleBandwidth / 2 ?? 0) : undefined;
+            const left = xScale && xAccessor ? Number(xScale(xAccessor(datum))) + xScaleBandwidth / 2 || 0 : undefined;
+            const top = yScale && yAccessor ? Number(yScale(yAccessor(datum))) + yScaleBandwidth / 2 || 0 : undefined;
 
             return { left, top };
         },
