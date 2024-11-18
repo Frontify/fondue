e5b75977eeb7f84a891117e4cc5f80d69f048efd
Jose Costa <jose.costa@frontify.com>
2024-11-18 15:28:03 +0100

    fix: type error

--- packages/components/src/components/Table/__tests__/Table.ct.tsx
+++ packages/components/src/components/Table/__tests__/Table.ct.tsx
@@ -8,7 +8,7 @@ const TABLE_TEST_ID = 'test-table';
 const TABLE_TEXT = 'sample table';
 
 test('should render without error', async ({ mount }) => {
-    const component = await mount(<Table data-test-id={TABLE_TEST_ID}>{TABLE_TEXT}</Table>);
+    const component = await mount(<Table.Root data-test-id={TABLE_TEST_ID}>{TABLE_TEXT}</Table.Root>);
     await expect(component).toBeVisible();
     await expect(component).toContainText(TABLE_TEXT);
 });
