2c45d868ade2e9a342908f193d277728e750107e
noahwaldner <noahwaldner@users.noreply.github.com>
2024-12-04 11:02:40 +0000

    Deploy preview for PR 2125 🛫

---
+++ pr-preview/pr-2125/legacy/assets/ScrollWrapper.stories-B9FsPeQB.js
@@ -0,0 +1,35 @@
+import{j as e}from"./jsx-runtime-Nms4Y4qS.js";import{$ as c}from"./chain-CSFqNvhK.js";import{r as l}from"./index-BwDkhjyp.js";import{B as m}from"./Button-BBdFcQ_2.js";import{F as d,a as x,b as f}from"./FormControl-DbVvWZHc.js";import{T as b}from"./TextInput-3-VhK_fn.js";import{a,S as i}from"./ScrollWrapper-D6IKkMLY.js";import"./SSRProvider-CYXx33DY.js";import"./_commonjsHelpers-BosuxZz1.js";import"./useButton-B0CVDvll.js";import"./useFocusable-ClqOF1PM.js";import"./useFocusRing-Vzyl7-Lk.js";import"./IconSize-BcCrF_mi.js";import"./focusStyle-CBFElZiz.js";import"./merge-DI-veSMP.js";import"./InputLabel-DuXda3bY.js";import"./TooltipIcon-CI6qleYl.js";import"./LegacyTooltip-NmtBsAB1.js";import"./useLink-BrkukSAv.js";import"./usePopper-C28Vz6Qz.js";import"./index-CS4BWtGh.js";import"./index-CfyPTyT-.js";import"./Portal-BBACcjoK.js";import"./useMemoizedId-EtHZ7Sxa.js";import"./IconCheckMark-ByVuQE55.js";import"./IconExclamationMarkCircle-DIK3qNWl.js";import"./IconExclamationMarkCircle24-Vs83y98-.js";import"./IconExclamationMarkTriangle-DvvXw1YP.js";import"./IconExclamationMarkTriangle24-NdDZZD89.js";import"./IconQuestionMarkCircle-DpV6l-09.js";import"./validation-C9S5KgfE.js";import"./LoadingCircle-CZrma5le.js";import"./IconEyeOff-BAmb6qT8.js";import"./IconCross-CxP4FPDR.js";const X={title:"Components/ScrollWrapper",component:a,tags:["autodocs"],argTypes:{scrollShadows:{control:{type:"boolean"},defaultValue:!1},direction:{options:Object.values(i),control:{type:"select"},defaultValue:i.Vertical},tabindex:{control:{type:"number"},defaultValue:0}},args:{direction:i.Vertical,tabindex:0,"data-test-id":"custom-data-test-id",scrollShadows:!0},parameters:{status:{type:"deprecated"},docs:{description:{component:"### This component is deprecated, please use the [new Scroll Area](/docs/current_components-text-scroll-area--documentation) instead."}}}},t=p=>{const[n,u]=l.useState("");return e.jsx("div",{className:"tw-max-h-[300px] tw-flex tw-flex-col",children:e.jsxs(a,{...p,children:[e.jsx("p",{className:"tw-w-[200px]",children:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor velit reiciendis nobis assumenda itaque tenetur ducimus quia qui! Tempore enim beatae est suscipit excepturi iure explicabo, fugiat perferendis consequatur."}),e.jsx("p",{className:"tw-w-[200px] ",children:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor velit reiciendis nobis assumenda itaque tenetur ducimus quia qui! Tempore enim beatae est suscipit excepturi iure explicabo, fugiat perferendis consequatur."}),e.jsx("p",{className:"tw-w-[200px]",children:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor velit reiciendis nobis assumenda itaque tenetur ducimus quia qui! Tempore enim beatae est suscipit excepturi iure explicabo, fugiat perferendis consequatur."}),e.jsx("div",{className:"tw-my-4",children:e.jsx(d,{style:x.Primary,direction:f.Vertical,label:{children:"Input Label",required:!1,htmlFor:c(),tooltip:{content:"Tooltip Text"}},children:e.jsx(b,{value:n,onChange:u})})}),e.jsx(m,{children:"Action"})]})})};t.__docgenInfo={description:"",methods:[],displayName:"Default"};var r,o,s;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`args => {
+  const [input, setInput] = useState('');
+  return <div className="tw-max-h-[300px] tw-flex tw-flex-col">
+            <ScrollWrapper {...args}>
+                <p className="tw-w-[200px]">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor velit reiciendis nobis assumenda
+                    itaque tenetur ducimus quia qui! Tempore enim beatae est suscipit excepturi iure explicabo, fugiat
+                    perferendis consequatur.
+                </p>
+                <p className="tw-w-[200px] ">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor velit reiciendis nobis assumenda
+                    itaque tenetur ducimus quia qui! Tempore enim beatae est suscipit excepturi iure explicabo, fugiat
+                    perferendis consequatur.
+                </p>
+                <p className="tw-w-[200px]">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor velit reiciendis nobis assumenda
+                    itaque tenetur ducimus quia qui! Tempore enim beatae est suscipit excepturi iure explicabo, fugiat
+                    perferendis consequatur.
+                </p>
+                <div className="tw-my-4">
+                    <FormControl style={FormControlStyle.Primary} direction={FormControlDirection.Vertical} label={{
+          children: 'Input Label',
+          required: false,
+          htmlFor: useId(),
+          tooltip: {
+            content: 'Tooltip Text'
+          }
+        }}>
+                        <TextInput value={input} onChange={setInput} />
+                    </FormControl>
+                </div>
+                <Button>Action</Button>
+            </ScrollWrapper>
+        </div>;
+}`,...(s=(o=t.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const Y=["Default"];export{t as Default,Y as __namedExportsOrder,X as default};
