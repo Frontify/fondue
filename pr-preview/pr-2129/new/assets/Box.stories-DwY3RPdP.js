54eafe8d94aa2cc8a668c262c045d2fef8b4975a
joewoodson <joewoodson@users.noreply.github.com>
2024-12-09 07:53:24 +0000

    Remove preview for PR 2129 🛬

--- pr-preview/pr-2129/new/assets/Box.stories-DwY3RPdP.js
+++
@@ -1,7 +0,0 @@
-import{j as r}from"./jsx-runtime-Nms4Y4qS.js";import{B as s,D as p}from"./Box-DM6BOoKX.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./propsToCssVariables-BE5Q_-YS.js";const x={title:"Layout/Box",component:s,tags:["autodocs"],parameters:{status:{type:"released"}},args:{width:"100px",height:"100px"}},e={render:n=>r.jsx(s,{...n,children:r.jsx(p,{})})};var t,o,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: args => {
-    return <Box {...args}>
-                <DecorativeContent />
-            </Box>;
-  }
-}`,...(a=(o=e.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};const l=["Default"];export{e as Default,l as __namedExportsOrder,x as default};
