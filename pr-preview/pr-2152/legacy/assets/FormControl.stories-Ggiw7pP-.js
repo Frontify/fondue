4daf268bc6b12589dd80c5f5e6a1b7bddc6ba13a
SamuelAlev <SamuelAlev@users.noreply.github.com>
2025-01-10 14:00:17 +0000

    Deploy preview for PR 2152 🛫

---
+++ pr-preview/pr-2152/legacy/assets/FormControl.stories-Ggiw7pP-.js
@@ -0,0 +1,55 @@
+import{j as t}from"./jsx-runtime-Nms4Y4qS.js";import{r as c}from"./index-BwDkhjyp.js";import{C as T,a as _}from"./Checklist-BWI-YG2d.js";import{D as j,a as y}from"./Dropdown-HuTeSDig.js";import{M as d}from"./MenuItem-DJQXtueh.js";import{S as D}from"./SegmentedControls-BSWtFswn.js";import{T as W}from"./TextInput-DxIcqcvy.js";import{g as V}from"./useMemoizedId-EtHZ7Sxa.js";import{F as m,a as u,b as h,H as B}from"./FormControl-BQeGKqv3.js";import"./_commonjsHelpers-BosuxZz1.js";import"./useFocusable-ClqOF1PM.js";import"./useFocusRing-Vzyl7-Lk.js";import"./SSRProvider-CYXx33DY.js";import"./chain-CSFqNvhK.js";import"./useField-CqwgNQmU.js";import"./useLabels-Bp6d37Qr.js";import"./Checkbox-BUIQ0igJ.js";import"./useFormValidationState-Bo5Bz1o-.js";import"./useControlledState-C0C-dzuH.js";import"./InputLabel-XVdppRg-.js";import"./TooltipIcon-Ki09kA5t.js";import"./LegacyTooltip-DmnzjIEq.js";import"./useLink-BrkukSAv.js";import"./usePopper-C28Vz6Qz.js";import"./index-CS4BWtGh.js";import"./index-CfyPTyT-.js";import"./Button-wjaYzy_L.js";import"./useButton-B0CVDvll.js";import"./IconSize-BcCrF_mi.js";import"./focusStyle-CBFElZiz.js";import"./merge-DI-veSMP.js";import"./Portal-BBACcjoK.js";import"./IconCheckMark-CuwFbSck.js";import"./IconExclamationMarkCircle24-Vs83y98-.js";import"./IconExclamationMarkTriangle-DSMv5KKP.js";import"./IconExclamationMarkTriangle24-NdDZZD89.js";import"./IconQuestionMarkCircle-Buo6J1lY.js";import"./IconMinus-Yzz9MQwg.js";import"./IconMinus16-D_g1WimF.js";import"./FocusScope-CryrrNNO.js";import"./useOverlayTriggerState-CiJgB9B9.js";import"./useMenuTrigger-2zUxbnkC.js";import"./useLocalizedStringFormatter-D7Kogc_3.js";import"./context-C2PaECDE.js";import"./VisuallyHidden-BDm5AEDr.js";import"./helper-DRMjeydv.js";import"./isScrollable-Bts8_1NI.js";import"./SelectionManager-f1Y_ghrx.js";import"./LoadingCircle-CZrma5le.js";import"./Trigger-DIiMo1j3.js";import"./IconCaretDown-DH0KSiPA.js";import"./IconCaretDown16-mzuMIk3a.js";import"./IconCross-rmHxPLkJ.js";import"./IconTrashBin16-g_sB0yBJ.js";import"./IconTrashBin24-DyoUj1kc.js";import"./validation-CJXPAjNB.js";import"./EnablePortalWrapper-B_d3Ldct.js";import"./IconCaretRight-BS4vVtQJ.js";import"./elements-BOYmskOL.js";import"./motion-C48bHxkn.js";const Ot={title:"Components/Form Control",component:m,tags:["autodocs"],args:{style:u.Primary,disabled:!1,direction:h.Vertical,label:{children:"Input Label",required:!1,htmlFor:V(),tooltip:{content:"Tooltip Text"}},extra:"Extra Text or Element",helper:{text:"Helper Text (before/after) and variant (Primary/Success/Danger)",position:B.After},children:""},argTypes:{orientation:{options:Object.values(h),control:"select"},children:{table:{disable:!0}},style:{options:Object.values(u),control:"radio"}}},i=e=>{const[o,r]=c.useState("");return t.jsx(m,{clickable:!0,...e,children:t.jsx(W,{value:o,onChange:r})})},n=e=>{const[o,r]=c.useState("a");return t.jsx(m,{...e,children:t.jsx(D,{items:[{id:"1",value:"abc"},{id:"2",value:"def"},{id:"3",value:"ghi"}],activeItemId:o,onChange:r})})},s=e=>{const[o,r]=c.useState();return t.jsx(m,{...e,children:t.jsx(j,{onChange:p=>r(p),activeItemId:o,size:y.Small,menuBlocks:[{id:"block1",menuItems:[{id:"1",title:"Small 1",size:d.Small},{id:"2",title:"Small 2",size:d.Small},{id:"3",title:"Small 3",size:d.Small}]}]})})},a=e=>{const[o,r]=c.useState([]),p=Array.from({length:5}).fill({}).map(($,l)=>({id:`checkbox-${l}`,value:`checkbox-${l}`,label:`Checkbox Nr. ${l}`,tooltip:{content:`Random Tooltip ${l}`}}));return t.jsx(m,{...e,children:t.jsx(T,{activeValues:o,setActiveValues:r,direction:_.Vertical,checkboxes:p})})};i.__docgenInfo={description:"",methods:[],displayName:"WithTextInput"};n.__docgenInfo={description:"",methods:[],displayName:"WithSegmentedControls"};s.__docgenInfo={description:"",methods:[],displayName:"WithDropdown"};a.__docgenInfo={description:"",methods:[],displayName:"WithVerticalChecklist"};var I,x,g;i.parameters={...i.parameters,docs:{...(I=i.parameters)==null?void 0:I.docs,source:{originalSource:`args => {
+  const [input, setInput] = useState('');
+  return <FormControl clickable={true} {...args}>
+            <TextInput value={input} onChange={setInput} />
+        </FormControl>;
+}`,...(g=(x=i.parameters)==null?void 0:x.docs)==null?void 0:g.source}}};var S,C,v;n.parameters={...n.parameters,docs:{...(S=n.parameters)==null?void 0:S.docs,source:{originalSource:`args => {
+  const [activeItemId, setActiveItemId] = useState<string>('a');
+  return <FormControl {...args}>
+            <SegmentedControls items={[{
+      id: '1',
+      value: 'abc'
+    }, {
+      id: '2',
+      value: 'def'
+    }, {
+      id: '3',
+      value: 'ghi'
+    }]} activeItemId={activeItemId} onChange={setActiveItemId} />
+        </FormControl>;
+}`,...(v=(C=n.parameters)==null?void 0:C.docs)==null?void 0:v.source}}};var b,f,k;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:`args => {
+  const [activeItemId, setActiveItemId] = useState<string | number | undefined>();
+  return <FormControl {...args}>
+            <Dropdown onChange={id => setActiveItemId(id)} activeItemId={activeItemId} size={DropdownSize.Small} menuBlocks={[{
+      id: 'block1',
+      menuItems: [{
+        id: '1',
+        title: 'Small 1',
+        size: MenuItemContentSize.Small
+      }, {
+        id: '2',
+        title: 'Small 2',
+        size: MenuItemContentSize.Small
+      }, {
+        id: '3',
+        title: 'Small 3',
+        size: MenuItemContentSize.Small
+      }]
+    }]} />
+        </FormControl>;
+}`,...(k=(f=s.parameters)==null?void 0:f.docs)==null?void 0:k.source}}};var F,z,A;a.parameters={...a.parameters,docs:{...(F=a.parameters)==null?void 0:F.docs,source:{originalSource:`args => {
+  const [activeBoxes, setActiveBoxes] = useState<string[]>([]);
+  const checkboxes = Array.from({
+    length: 5
+  }).fill({}).map((_, index) => ({
+    id: \`checkbox-\${index}\`,
+    value: \`checkbox-\${index}\`,
+    label: \`Checkbox Nr. \${index}\`,
+    tooltip: {
+      content: \`Random Tooltip \${index}\`
+    }
+  }));
+  return <FormControl {...args}>
+            <Checklist activeValues={activeBoxes} setActiveValues={setActiveBoxes} direction={ChecklistDirection.Vertical} checkboxes={checkboxes} />
+        </FormControl>;
+}`,...(A=(z=a.parameters)==null?void 0:z.docs)==null?void 0:A.source}}};const Pt=["WithTextInput","WithSegmentedControls","WithDropdown","WithVerticalChecklist"];export{s as WithDropdown,n as WithSegmentedControls,i as WithTextInput,a as WithVerticalChecklist,Pt as __namedExportsOrder,Ot as default};
