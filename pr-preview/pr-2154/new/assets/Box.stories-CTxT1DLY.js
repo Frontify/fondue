c1485699987f90af3b640b78c6441b836a126f34
noahwaldner <noahwaldner@users.noreply.github.com>
2025-01-13 15:02:34 +0000

    Deploy preview for PR 2154 🛫

---
+++ pr-preview/pr-2154/new/assets/Box.stories-CTxT1DLY.js
@@ -0,0 +1,17 @@
+import{j as t}from"./jsx-runtime-Nms4Y4qS.js";import{B as s,D as m}from"./Box-Bs_B20k3.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./propsToCssVariables-DuhnUZvM.js";const B={title:"Layout/Box",component:s,tags:["autodocs"],parameters:{status:{type:"released"}},args:{width:"100px",height:"100px"}},e={render:o=>t.jsx(s,{...o,children:t.jsx(m,{})})},r={args:{width:25,height:25},render:o=>t.jsx(s,{...o,children:t.jsx(m,{})})};var a,n,i;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: args => {
+    return <Box {...args}>
+                <DecorativeContent />
+            </Box>;
+  }
+}`,...(i=(n=e.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};var c,p,d;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    width: 25,
+    height: 25
+  },
+  render: args => {
+    return <Box {...args}>
+                <DecorativeContent />
+            </Box>;
+  }
+}`,...(d=(p=r.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};const j=["Default","WithSizeToken"];export{e as Default,r as WithSizeToken,j as __namedExportsOrder,B as default};
