de4dccd32b56982d1eb513b35784756d5de19bd6
noahwaldner <noahwaldner@users.noreply.github.com>
2025-01-22 07:39:49 +0000

    Deploy preview for PR 2170 🛫

---
+++ pr-preview/pr-2170/legacy/assets/Checkbox.stories-BaGiTGH4.js
@@ -0,0 +1,44 @@
+import{j as t}from"./jsx-runtime-Nms4Y4qS.js";import{r as p}from"./index-BwDkhjyp.js";import{T as o}from"./TooltipIcon-Ki09kA5t.js";import{I as x}from"./LegacyTooltip-DmnzjIEq.js";import{I as T}from"./IconInfo-BX81QEEn.js";import{I as f}from"./IconQuestionMarkCircle-Buo6J1lY.js";import{C as r,a as I,b as w,c as q}from"./Checkbox-B-cEW3gS.js";import"./_commonjsHelpers-BosuxZz1.js";import"./IconSize-BcCrF_mi.js";import"./focusStyle-CBFElZiz.js";import"./merge-DI-veSMP.js";import"./useLink-BrkukSAv.js";import"./useFocusable-ClqOF1PM.js";import"./useFocusRing-Vzyl7-Lk.js";import"./SSRProvider-CYXx33DY.js";import"./chain-CSFqNvhK.js";import"./usePopper-C28Vz6Qz.js";import"./index-CS4BWtGh.js";import"./index-CfyPTyT-.js";import"./Button-wjaYzy_L.js";import"./useButton-B0CVDvll.js";import"./Portal-BBACcjoK.js";import"./useMemoizedId-EtHZ7Sxa.js";import"./IconCheckMark-CuwFbSck.js";import"./IconExclamationMarkCircle24-Vs83y98-.js";import"./IconExclamationMarkTriangle-DSMv5KKP.js";import"./IconExclamationMarkTriangle24-NdDZZD89.js";import"./useFormValidationState-Bo5Bz1o-.js";import"./useControlledState-C0C-dzuH.js";import"./InputLabel-XVdppRg-.js";import"./IconMinus-Dg4Ytceu.js";const oe={title:"Components/Checkbox",tags:["autodocs"],parameters:{status:{type:"deprecated"},docs:{description:{component:"### This component is deprecated, please use the [new Checkbox component](/docs/current_components-checkbox--documentation) instead."}}},argTypes:{state:{options:Object.values(r),control:{type:"radio"}},emphasis:{options:Object.values(I),control:{type:"radio"}},size:{options:Object.values(w),control:{type:"radio"}},onChange:{table:{disable:!1},action:"Change"}},args:{state:r.Unchecked,disabled:!1,required:!1,emphasis:"Default",size:"Default",value:"whatever-you'd-like",label:"Checkbox label",hideLabel:!1,ariaLabel:"Custom aria label",helperText:"Note about this input",tooltip:{content:"Lorem ipsum dolor sit amet."}}},e=({wrapperClasses:y,...s})=>{const[S,i]=p.useState(s.state||r.Unchecked);return p.useEffect(()=>i(s.state||r.Unchecked),[s.state]),t.jsx("div",{className:y,children:t.jsx(q,{...s,state:S,onChange:c=>{i(c?r.Checked:r.Unchecked),s.onChange&&s.onChange(c)}})})},a=e.bind({});a.args={...e.args,tooltip:[{triggerIcon:t.jsx(T,{}),triggerStyle:o.Danger,content:"Tooltip 1"},{triggerIcon:t.jsx(f,{}),triggerStyle:o.Primary,content:"Tooltip 2"},{triggerIcon:t.jsx(x,{filled:!0}),triggerStyle:o.Warning,content:"Tooltip 3"}]};const n=e.bind({});n.args={...e.args,tooltip:[{triggerIcon:t.jsx(T,{}),triggerStyle:o.Danger,content:"Tooltip 1"},{triggerIcon:t.jsx(f,{}),triggerStyle:o.Primary,content:"Tooltip 2"},{triggerIcon:t.jsx(x,{filled:!0}),triggerStyle:o.Warning,content:"Tooltip 3"}],wrapperClasses:"tw-w-40"};e.__docgenInfo={description:"",methods:[],displayName:"Checkbox",props:{id:{required:!1,tsType:{name:"string"},description:""},state:{required:!1,tsType:{name:"CheckboxState"},description:""},emphasis:{required:!1,tsType:{name:"CheckboxEmphasis"},description:""},size:{required:!1,tsType:{name:"CheckboxSize"},description:""},disabled:{required:!1,tsType:{name:"boolean"},description:""},required:{required:!1,tsType:{name:"boolean"},description:""},value:{required:!1,tsType:{name:"string"},description:""},onChange:{required:!1,tsType:{name:"signature",type:"function",raw:"(isChecked: boolean) => void",signature:{arguments:[{type:{name:"boolean"},name:"isChecked"}],return:{name:"void"}}},description:""},label:{required:!1,tsType:{name:"string"},description:""},hideLabel:{required:!1,tsType:{name:"boolean"},description:""},tooltip:{required:!1,tsType:{name:"union",raw:`| (LegacyTooltipProps & Pick<TooltipIconProps, 'triggerStyle' | 'triggerIcon'>)
+| (LegacyTooltipProps & Pick<TooltipIconProps, 'triggerStyle' | 'triggerIcon'>)[]`,elements:[{name:"unknown"},{name:"Array",elements:[{name:"unknown"}],raw:"(LegacyTooltipProps & Pick<TooltipIconProps, 'triggerStyle' | 'triggerIcon'>)[]"}]},description:""},helperText:{required:!1,tsType:{name:"string"},description:""},"aria-label":{required:!1,tsType:{name:"string"},description:""},groupInputProps:{required:!1,tsType:{name:"HTMLAttributes",elements:[{name:"HTMLElement"}],raw:"HTMLAttributes<HTMLElement>"},description:""},"data-test-id":{required:!1,tsType:{name:"string"},description:""},wrapperClasses:{required:!0,tsType:{name:"string"},description:""}}};var d,l,h;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`({
+  wrapperClasses,
+  ...args
+}: CheckboxProps & {
+  wrapperClasses: string;
+}) => {
+  const [checked, setChecked] = useState<CheckboxState>(args.state || CheckboxState.Unchecked);
+  useEffect(() => setChecked(args.state || CheckboxState.Unchecked), [args.state]);
+  return <div className={wrapperClasses}>
+            <CheckboxComponent {...args} state={checked} onChange={isChecked => {
+      setChecked(isChecked ? CheckboxState.Checked : CheckboxState.Unchecked);
+      args.onChange && args.onChange(isChecked);
+    }} />
+        </div>;
+}`,...(h=(l=e.parameters)==null?void 0:l.docs)==null?void 0:h.source}}};var m,g,C;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`({
+  wrapperClasses,
+  ...args
+}: CheckboxProps & {
+  wrapperClasses: string;
+}) => {
+  const [checked, setChecked] = useState<CheckboxState>(args.state || CheckboxState.Unchecked);
+  useEffect(() => setChecked(args.state || CheckboxState.Unchecked), [args.state]);
+  return <div className={wrapperClasses}>
+            <CheckboxComponent {...args} state={checked} onChange={isChecked => {
+      setChecked(isChecked ? CheckboxState.Checked : CheckboxState.Unchecked);
+      args.onChange && args.onChange(isChecked);
+    }} />
+        </div>;
+}`,...(C=(g=a.parameters)==null?void 0:g.docs)==null?void 0:C.source}}};var k,u,b;n.parameters={...n.parameters,docs:{...(k=n.parameters)==null?void 0:k.docs,source:{originalSource:`({
+  wrapperClasses,
+  ...args
+}: CheckboxProps & {
+  wrapperClasses: string;
+}) => {
+  const [checked, setChecked] = useState<CheckboxState>(args.state || CheckboxState.Unchecked);
+  useEffect(() => setChecked(args.state || CheckboxState.Unchecked), [args.state]);
+  return <div className={wrapperClasses}>
+            <CheckboxComponent {...args} state={checked} onChange={isChecked => {
+      setChecked(isChecked ? CheckboxState.Checked : CheckboxState.Unchecked);
+      args.onChange && args.onChange(isChecked);
+    }} />
+        </div>;
+}`,...(b=(u=n.parameters)==null?void 0:u.docs)==null?void 0:b.source}}};const ae=["Checkbox","CheckboxWithMultipleTooltips","CheckboxWithMultipleTooltipsAndEllipsis"];export{e as Checkbox,a as CheckboxWithMultipleTooltips,n as CheckboxWithMultipleTooltipsAndEllipsis,ae as __namedExportsOrder,oe as default};
---
+++ pr-preview/pr-2170/legacy/assets/InlineDialog.stories-sCnHV7TV.js
@@ -0,0 +1,127 @@
+import{j as e}from"./jsx-runtime-Nms4Y4qS.js";import{r as d}from"./index-BwDkhjyp.js";import{B as x}from"./Box-DnvrccDH.js";import{c as f}from"./Button-wjaYzy_L.js";import{D as q}from"./DialogBody-BGcfsRcP.js";import{P}from"./constantStories-DEfmjXOT.js";import{I as v}from"./IconJohanna-Cbcr3K_r.js";import{I as W}from"./InlineDialog-DAZcziId.js";import{M as l}from"./dialog-Dre1eVQK.js";import"./_commonjsHelpers-BosuxZz1.js";import"./borderStyle-D9jrV09G.js";import"./merge-DI-veSMP.js";import"./focusStyle-CBFElZiz.js";import"./input-C98YeDUw.js";import"./Checkbox-B-cEW3gS.js";import"./useFormValidationState-Bo5Bz1o-.js";import"./SSRProvider-CYXx33DY.js";import"./useFocusRing-Vzyl7-Lk.js";import"./useFocusable-ClqOF1PM.js";import"./chain-CSFqNvhK.js";import"./useControlledState-C0C-dzuH.js";import"./InputLabel-XVdppRg-.js";import"./TooltipIcon-Ki09kA5t.js";import"./LegacyTooltip-DmnzjIEq.js";import"./useLink-BrkukSAv.js";import"./usePopper-C28Vz6Qz.js";import"./index-CS4BWtGh.js";import"./index-CfyPTyT-.js";import"./Portal-BBACcjoK.js";import"./IconSize-BcCrF_mi.js";import"./useMemoizedId-EtHZ7Sxa.js";import"./IconCheckMark-CuwFbSck.js";import"./IconExclamationMarkCircle24-Vs83y98-.js";import"./IconExclamationMarkTriangle-DSMv5KKP.js";import"./IconExclamationMarkTriangle24-NdDZZD89.js";import"./IconQuestionMarkCircle-Buo6J1lY.js";import"./IconMinus-Dg4Ytceu.js";import"./Divider-DCi-HcbY.js";import"./FormControl-BQeGKqv3.js";import"./validation-CJXPAjNB.js";import"./LoadingCircle-CZrma5le.js";import"./SegmentedControls-BSWtFswn.js";import"./FocusScope-CryrrNNO.js";import"./useField-CqwgNQmU.js";import"./useLabels-Bp6d37Qr.js";import"./context-C2PaECDE.js";import"./VisuallyHidden-BDm5AEDr.js";import"./motion-C48bHxkn.js";import"./Switch-atUUSKtW.js";import"./TabItem-DYJEWVSF.js";import"./elements-BOYmskOL.js";import"./TextInput-DxIcqcvy.js";import"./IconCross-rmHxPLkJ.js";import"./Tooltip-BlJs8OXG.js";import"./useClickOutside-CKV3CrvD.js";import"./EnablePortalWrapper-B_d3Ldct.js";import"./Tree-BhvdmAx4.js";import"./immer-DBm1MMjE.js";import"./Trigger-DIiMo1j3.js";import"./IconCaretDown-DH0KSiPA.js";import"./IconCaretDown16-mzuMIk3a.js";import"./IconTrashBin16-g_sB0yBJ.js";import"./IconTrashBin24-DyoUj1kc.js";import"./useButton-B0CVDvll.js";const $e={title:"Experimental/InlineDialog",component:W,tags:["autodocs"],parameters:{status:{type:"deprecated"},docs:{description:{component:"### This component is deprecated, please use the [new Flyout component](/docs/current_components-flyout--documentation) instead."}}},args:{placement:"bottom-start",modality:l.Modal,offset:[0,8],flip:!0,width:360,minWidth:360,maxWidth:360,autoHeight:!1,maxHeight:"auto",minHeight:0,roundedCorners:!0,strategy:"absolute",enablePortal:!1},argTypes:{...P,modality:{options:Object.values(l),control:{type:"select"}},"data-test-id":{type:"string"},role:{type:"string"},autoHeight:{type:"boolean"},roundedCorners:{type:"boolean"},zIndex:{table:{disable:!0}},unsafe_zIndex:{type:"number"}}},g=()=>e.jsx("p",{className:"tw-my-2 tw-text-text",children:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio fugiat hic incidunt, inventore odio praesentium qui quisquam! Dignissimos nemo quisquam unde voluptatem. Animi ea iusto numquam odio omnis. Dicta, voluptatum."}),t=_=>{const[m,c]=d.useState(!1),u=d.useRef(null);return e.jsxs(x,{className:"tw-w-fit",children:[e.jsx(f,{icon:e.jsx(v,{}),onClick:()=>c(!m),ref:u,children:"Trigger"}),e.jsx(W,{..._,anchor:u,open:m,handleClose:()=>c(!1),children:e.jsx(q,{padding:"comfortable",children:e.jsxs(x,{className:"tw-text-text",children:[e.jsx(g,{}),e.jsx(g,{}),e.jsx(g,{}),e.jsx(f,{onClick:()=>c(!m),children:"Close"})]})})})]})},p=t.bind({}),n=t.bind({});n.args={modality:l.NonModal};const o=t.bind({});o.args={modality:l.Modal};const s=t.bind({});s.args={modality:l.BlockingModal};const r=t.bind({});r.args={maxHeight:300};const a=t.bind({});a.args={autoHeight:!0};const i=t.bind({});i.args={roundedCorners:!1};var B,O,I;p.parameters={...p.parameters,docs:{...(B=p.parameters)==null?void 0:B.docs,source:{originalSource:`args => {
+  const [isOpen, setIsOpen] = useState(false);
+  const triggerRef = useRef<HTMLButtonElement | null>(null);
+  return <Box className="tw-w-fit">
+            <Button icon={<IconJohanna />} onClick={() => setIsOpen(!isOpen)} ref={triggerRef}>
+                Trigger
+            </Button>
+            <InlineDialog {...args} anchor={triggerRef} open={isOpen} handleClose={() => setIsOpen(false)}>
+                <DialogBody padding="comfortable">
+                    <Box className="tw-text-text">
+                        <TextExample />
+                        <TextExample />
+                        <TextExample />
+                        <Button onClick={() => setIsOpen(!isOpen)}>Close</Button>
+                    </Box>
+                </DialogBody>
+            </InlineDialog>
+        </Box>;
+}`,...(I=(O=p.parameters)==null?void 0:O.docs)==null?void 0:I.source}}};var h,T,D;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`args => {
+  const [isOpen, setIsOpen] = useState(false);
+  const triggerRef = useRef<HTMLButtonElement | null>(null);
+  return <Box className="tw-w-fit">
+            <Button icon={<IconJohanna />} onClick={() => setIsOpen(!isOpen)} ref={triggerRef}>
+                Trigger
+            </Button>
+            <InlineDialog {...args} anchor={triggerRef} open={isOpen} handleClose={() => setIsOpen(false)}>
+                <DialogBody padding="comfortable">
+                    <Box className="tw-text-text">
+                        <TextExample />
+                        <TextExample />
+                        <TextExample />
+                        <Button onClick={() => setIsOpen(!isOpen)}>Close</Button>
+                    </Box>
+                </DialogBody>
+            </InlineDialog>
+        </Box>;
+}`,...(D=(T=n.parameters)==null?void 0:T.docs)==null?void 0:D.source}}};var C,R,E;o.parameters={...o.parameters,docs:{...(C=o.parameters)==null?void 0:C.docs,source:{originalSource:`args => {
+  const [isOpen, setIsOpen] = useState(false);
+  const triggerRef = useRef<HTMLButtonElement | null>(null);
+  return <Box className="tw-w-fit">
+            <Button icon={<IconJohanna />} onClick={() => setIsOpen(!isOpen)} ref={triggerRef}>
+                Trigger
+            </Button>
+            <InlineDialog {...args} anchor={triggerRef} open={isOpen} handleClose={() => setIsOpen(false)}>
+                <DialogBody padding="comfortable">
+                    <Box className="tw-text-text">
+                        <TextExample />
+                        <TextExample />
+                        <TextExample />
+                        <Button onClick={() => setIsOpen(!isOpen)}>Close</Button>
+                    </Box>
+                </DialogBody>
+            </InlineDialog>
+        </Box>;
+}`,...(E=(R=o.parameters)==null?void 0:R.docs)==null?void 0:E.source}}};var y,w,b;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`args => {
+  const [isOpen, setIsOpen] = useState(false);
+  const triggerRef = useRef<HTMLButtonElement | null>(null);
+  return <Box className="tw-w-fit">
+            <Button icon={<IconJohanna />} onClick={() => setIsOpen(!isOpen)} ref={triggerRef}>
+                Trigger
+            </Button>
+            <InlineDialog {...args} anchor={triggerRef} open={isOpen} handleClose={() => setIsOpen(false)}>
+                <DialogBody padding="comfortable">
+                    <Box className="tw-text-text">
+                        <TextExample />
+                        <TextExample />
+                        <TextExample />
+                        <Button onClick={() => setIsOpen(!isOpen)}>Close</Button>
+                    </Box>
+                </DialogBody>
+            </InlineDialog>
+        </Box>;
+}`,...(b=(w=s.parameters)==null?void 0:w.docs)==null?void 0:b.source}}};var N,M,k;r.parameters={...r.parameters,docs:{...(N=r.parameters)==null?void 0:N.docs,source:{originalSource:`args => {
+  const [isOpen, setIsOpen] = useState(false);
+  const triggerRef = useRef<HTMLButtonElement | null>(null);
+  return <Box className="tw-w-fit">
+            <Button icon={<IconJohanna />} onClick={() => setIsOpen(!isOpen)} ref={triggerRef}>
+                Trigger
+            </Button>
+            <InlineDialog {...args} anchor={triggerRef} open={isOpen} handleClose={() => setIsOpen(false)}>
+                <DialogBody padding="comfortable">
+                    <Box className="tw-text-text">
+                        <TextExample />
+                        <TextExample />
+                        <TextExample />
+                        <Button onClick={() => setIsOpen(!isOpen)}>Close</Button>
+                    </Box>
+                </DialogBody>
+            </InlineDialog>
+        </Box>;
+}`,...(k=(M=r.parameters)==null?void 0:M.docs)==null?void 0:k.source}}};var H,S,A;a.parameters={...a.parameters,docs:{...(H=a.parameters)==null?void 0:H.docs,source:{originalSource:`args => {
+  const [isOpen, setIsOpen] = useState(false);
+  const triggerRef = useRef<HTMLButtonElement | null>(null);
+  return <Box className="tw-w-fit">
+            <Button icon={<IconJohanna />} onClick={() => setIsOpen(!isOpen)} ref={triggerRef}>
+                Trigger
+            </Button>
+            <InlineDialog {...args} anchor={triggerRef} open={isOpen} handleClose={() => setIsOpen(false)}>
+                <DialogBody padding="comfortable">
+                    <Box className="tw-text-text">
+                        <TextExample />
+                        <TextExample />
+                        <TextExample />
+                        <Button onClick={() => setIsOpen(!isOpen)}>Close</Button>
+                    </Box>
+                </DialogBody>
+            </InlineDialog>
+        </Box>;
+}`,...(A=(S=a.parameters)==null?void 0:S.docs)==null?void 0:A.source}}};var j,J,L;i.parameters={...i.parameters,docs:{...(j=i.parameters)==null?void 0:j.docs,source:{originalSource:`args => {
+  const [isOpen, setIsOpen] = useState(false);
+  const triggerRef = useRef<HTMLButtonElement | null>(null);
+  return <Box className="tw-w-fit">
+            <Button icon={<IconJohanna />} onClick={() => setIsOpen(!isOpen)} ref={triggerRef}>
+                Trigger
+            </Button>
+            <InlineDialog {...args} anchor={triggerRef} open={isOpen} handleClose={() => setIsOpen(false)}>
+                <DialogBody padding="comfortable">
+                    <Box className="tw-text-text">
+                        <TextExample />
+                        <TextExample />
+                        <TextExample />
+                        <Button onClick={() => setIsOpen(!isOpen)}>Close</Button>
+                    </Box>
+                </DialogBody>
+            </InlineDialog>
+        </Box>;
+}`,...(L=(J=i.parameters)==null?void 0:J.docs)==null?void 0:L.source}}};const et=["Default","AsANonModal","AsAModal","AsABlockingModal","WithMaxHeight","WithAutoHeight","WithNoRoundedCorners"];export{s as AsABlockingModal,o as AsAModal,n as AsANonModal,p as Default,a as WithAutoHeight,r as WithMaxHeight,i as WithNoRoundedCorners,et as __namedExportsOrder,$e as default};
