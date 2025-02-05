d30fcc2dd75ab7cb44b177f0f61ecba59b01175c
noahwaldner <noahwaldner@users.noreply.github.com>
2025-02-05 18:15:27 +0000

    Deploy preview for PR 2188 🛫

---
+++ pr-preview/pr-2188/new/assets/Box-BBCocSiH.js
@@ -0,0 +1,8 @@
+import{j as e}from"./jsx-runtime-BSxFkHFN.js";import{r as m}from"./index-i66itsuO.js";import{p as g}from"./propsToCssVariables-DVyyXmoh.js";const a=()=>e.jsx("div",{style:{background:`
+                    conic-gradient(
+                        #FFF 90deg,
+                        #E6E6E6 90deg 180deg,
+                        #FFF 180deg 270deg,
+                        #E6E6E6 270deg
+                    )
+                `,backgroundSize:"20px 20px",backgroundPosition:"top left",backgroundRepeat:"repeat",height:"100%",width:"100%"}});a.displayName="DecorativeContent";a.__docgenInfo={description:"This component is used to create a decorative background for stories.",methods:[],displayName:"DecorativeContent"};const x="_root_yrjuz_5",y={root:x},o=m.forwardRef(({as:t="div","data-test-id":r="fondue-box",children:d,role:s,"aria-label":i,"aria-hidden":n,"aria-describedby":p,"aria-labelledby":l,"aria-expanded":c,"aria-haspopup":u,...b},f)=>e.jsx(t,{className:y.root,"data-test-id":r,style:g(b),role:s,"aria-label":i,"aria-hidden":n,"aria-describedby":p,"aria-labelledby":l,"aria-expanded":c,"aria-haspopup":u,ref:f,children:d}));o.displayName="Box";o.__docgenInfo={description:"",methods:[],displayName:"Box",props:{as:{defaultValue:{value:"'div'",computed:!1},required:!1},"data-test-id":{defaultValue:{value:"'fondue-box'",computed:!1},required:!1}}};export{o as B,a as D};
