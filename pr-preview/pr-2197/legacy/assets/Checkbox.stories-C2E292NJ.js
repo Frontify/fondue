37c69a10bff59a1c386fda6a71270ae4f347521a
noahwaldner <noahwaldner@users.noreply.github.com>
2025-02-07 13:26:56 +0000

    Deploy preview for PR 2197 🛫

---
+++ pr-preview/pr-2197/legacy/assets/Checkbox.stories-C2E292NJ.js
@@ -0,0 +1,44 @@
+import{j as t}from"./jsx-runtime-DH9XN8A8.js";import{r as p}from"./index-v7USjxUg.js";import{T as o}from"./TooltipIcon-DWdr_hgc.js";import{I as x}from"./LegacyTooltip-BReeJmjq.js";import{I as T}from"./IconInfo-B34JZb6S.js";import{I as f}from"./IconQuestionMarkCircle-DT_LrHkX.js";import{C as r,a as I,b as w,c as q}from"./Checkbox-BYzjmVQc.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./IconSize-BcCrF_mi.js";import"./focusStyle-CBFElZiz.js";import"./merge-DI-veSMP.js";import"./useLink-C2PgwcjG.js";import"./useFocusable-BRFkliNn.js";import"./useFocusRing-CP9UJNUw.js";import"./useEffectEvent-BaECT7_f.js";import"./chain-C8AhrK0W.js";import"./usePopper-BO8FF5JQ.js";import"./index-voQlCD4e.js";import"./index-DlnOb27V.js";import"./Button-BfBJpGQT.js";import"./useButton-B6orroTH.js";import"./Portal-BMcVKwlx.js";import"./useMemoizedId-Cl5PI4eu.js";import"./IconCheckMark-CMS57PRn.js";import"./IconExclamationMarkCircle24-CS86ZjBV.js";import"./IconExclamationMarkTriangle-CTTfs2nR.js";import"./IconExclamationMarkTriangle24-ZUoV6XtQ.js";import"./useFormValidationState-BKAFCnHb.js";import"./useControlledState-BSgwHaZM.js";import"./InputLabel-CAkTbk9Q.js";import"./IconMinus-7GNK1BVK.js";import"./IconMinus16-DqVdYs6S.js";const ae={title:"Components/Checkbox",tags:["autodocs"],parameters:{status:{type:"deprecated"},docs:{description:{component:"### This component is deprecated, please use the [new Checkbox component](/docs/current_components-checkbox--documentation) instead."}}},argTypes:{state:{options:Object.values(r),control:{type:"radio"}},emphasis:{options:Object.values(I),control:{type:"radio"}},size:{options:Object.values(w),control:{type:"radio"}},onChange:{table:{disable:!1},action:"Change"}},args:{state:r.Unchecked,disabled:!1,required:!1,emphasis:"Default",size:"Default",value:"whatever-you'd-like",label:"Checkbox label",hideLabel:!1,ariaLabel:"Custom aria label",helperText:"Note about this input",tooltip:{content:"Lorem ipsum dolor sit amet."}}},e=({wrapperClasses:y,...s})=>{const[S,i]=p.useState(s.state||r.Unchecked);return p.useEffect(()=>i(s.state||r.Unchecked),[s.state]),t.jsx("div",{className:y,children:t.jsx(q,{...s,state:S,onChange:c=>{i(c?r.Checked:r.Unchecked),s.onChange&&s.onChange(c)}})})},a=e.bind({});a.args={...e.args,tooltip:[{triggerIcon:t.jsx(T,{}),triggerStyle:o.Danger,content:"Tooltip 1"},{triggerIcon:t.jsx(f,{}),triggerStyle:o.Primary,content:"Tooltip 2"},{triggerIcon:t.jsx(x,{filled:!0}),triggerStyle:o.Warning,content:"Tooltip 3"}]};const n=e.bind({});n.args={...e.args,tooltip:[{triggerIcon:t.jsx(T,{}),triggerStyle:o.Danger,content:"Tooltip 1"},{triggerIcon:t.jsx(f,{}),triggerStyle:o.Primary,content:"Tooltip 2"},{triggerIcon:t.jsx(x,{filled:!0}),triggerStyle:o.Warning,content:"Tooltip 3"}],wrapperClasses:"tw-w-40"};e.__docgenInfo={description:"",methods:[],displayName:"Checkbox",props:{id:{required:!1,tsType:{name:"string"},description:""},state:{required:!1,tsType:{name:"CheckboxState"},description:""},emphasis:{required:!1,tsType:{name:"CheckboxEmphasis"},description:""},size:{required:!1,tsType:{name:"CheckboxSize"},description:""},disabled:{required:!1,tsType:{name:"boolean"},description:""},required:{required:!1,tsType:{name:"boolean"},description:""},value:{required:!1,tsType:{name:"string"},description:""},onChange:{required:!1,tsType:{name:"signature",type:"function",raw:"(isChecked: boolean) => void",signature:{arguments:[{type:{name:"boolean"},name:"isChecked"}],return:{name:"void"}}},description:""},label:{required:!1,tsType:{name:"string"},description:""},hideLabel:{required:!1,tsType:{name:"boolean"},description:""},tooltip:{required:!1,tsType:{name:"union",raw:`| (LegacyTooltipProps & Pick<TooltipIconProps, 'triggerStyle' | 'triggerIcon'>)
+| (LegacyTooltipProps & Pick<TooltipIconProps, 'triggerStyle' | 'triggerIcon'>)[]`,elements:[{name:"unknown"},{name:"Array",elements:[{name:"unknown"}],raw:"(LegacyTooltipProps & Pick<TooltipIconProps, 'triggerStyle' | 'triggerIcon'>)[]"}]},description:""},helperText:{required:!1,tsType:{name:"string"},description:""},"aria-label":{required:!1,tsType:{name:"string"},description:""},groupInputProps:{required:!1,tsType:{name:"HTMLAttributes",elements:[{name:"HTMLElement"}],raw:"HTMLAttributes<HTMLElement>"},description:""},"data-test-id":{required:!1,tsType:{name:"string"},description:""},wrapperClasses:{required:!0,tsType:{name:"string"},description:""}}};var d,l,h;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`({
+  wrapperClasses,
+  ...args
+}: CheckboxProps & {
+  wrapperClasses: string;
+}) => {
+  const [checked, setChecked] = useState<CheckboxState>(args.state || CheckboxState.Unchecked);
+  useEffect(() => setChecked(args.state || CheckboxState.Unchecked), [args.state]);
+  return <div className={wrapperClasses}>
+            <CheckboxComponent {...args} state={checked} onChange={isChecked => {
+      setChecked(isChecked ? CheckboxState.Checked : CheckboxState.Unchecked);
+      args.onChange && args.onChange(isChecked);
+    }} />
+        </div>;
+}`,...(h=(l=e.parameters)==null?void 0:l.docs)==null?void 0:h.source}}};var m,g,C;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`({
+  wrapperClasses,
+  ...args
+}: CheckboxProps & {
+  wrapperClasses: string;
+}) => {
+  const [checked, setChecked] = useState<CheckboxState>(args.state || CheckboxState.Unchecked);
+  useEffect(() => setChecked(args.state || CheckboxState.Unchecked), [args.state]);
+  return <div className={wrapperClasses}>
+            <CheckboxComponent {...args} state={checked} onChange={isChecked => {
+      setChecked(isChecked ? CheckboxState.Checked : CheckboxState.Unchecked);
+      args.onChange && args.onChange(isChecked);
+    }} />
+        </div>;
+}`,...(C=(g=a.parameters)==null?void 0:g.docs)==null?void 0:C.source}}};var k,u,b;n.parameters={...n.parameters,docs:{...(k=n.parameters)==null?void 0:k.docs,source:{originalSource:`({
+  wrapperClasses,
+  ...args
+}: CheckboxProps & {
+  wrapperClasses: string;
+}) => {
+  const [checked, setChecked] = useState<CheckboxState>(args.state || CheckboxState.Unchecked);
+  useEffect(() => setChecked(args.state || CheckboxState.Unchecked), [args.state]);
+  return <div className={wrapperClasses}>
+            <CheckboxComponent {...args} state={checked} onChange={isChecked => {
+      setChecked(isChecked ? CheckboxState.Checked : CheckboxState.Unchecked);
+      args.onChange && args.onChange(isChecked);
+    }} />
+        </div>;
+}`,...(b=(u=n.parameters)==null?void 0:u.docs)==null?void 0:b.source}}};const ne=["Checkbox","CheckboxWithMultipleTooltips","CheckboxWithMultipleTooltipsAndEllipsis"];export{e as Checkbox,a as CheckboxWithMultipleTooltips,n as CheckboxWithMultipleTooltipsAndEllipsis,ne as __namedExportsOrder,ae as default};
---
+++ pr-preview/pr-2197/new/assets/Box.stories-BMK1hx2H.js
@@ -0,0 +1,17 @@
+import{j as t}from"./jsx-runtime-BSxFkHFN.js";import{D as d}from"./DecorativeContent-C0jy9Sg6.js";import{B as o}from"./Box-DAzbSsog.js";import"./index-i66itsuO.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./propsToCssVariables-DVyyXmoh.js";const f={title:"Layout/Box",component:o,tags:["autodocs"],parameters:{status:{type:"released"}},args:{width:25,height:25}},r={render:s=>t.jsx(o,{...s,children:t.jsx(d,{})})},e={args:{width:"100px",height:"100px"},render:s=>t.jsx(o,{...s,children:t.jsx(d,{})})};var a,n,i;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: args => {
+    return <Box {...args}>
+                <DecorativeContent />
+            </Box>;
+  }
+}`,...(i=(n=r.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};var p,c,m;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    width: '100px',
+    height: '100px'
+  },
+  render: args => {
+    return <Box {...args}>
+                <DecorativeContent />
+            </Box>;
+  }
+}`,...(m=(c=e.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const j=["Default","PixelValues"];export{r as Default,e as PixelValues,j as __namedExportsOrder,f as default};
