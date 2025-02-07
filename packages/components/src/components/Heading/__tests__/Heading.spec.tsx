e020467397d25def1fdbfe6f87c38b3a29b08df2
Red Ochsenbein <red.ochsenbein@frontify.com>
2025-02-07 07:54:58 +0100

    test: Add tests

--- packages/components/src/components/Heading/__tests__/Heading.spec.tsx
+++ packages/components/src/components/Heading/__tests__/Heading.spec.tsx
@@ -1,6 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { render } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
 import { Heading } from '../Heading';
@@ -9,9 +9,39 @@ const HEADING_TEST_ID = 'test-heading';
 const HEADING_TEXT = 'sample heading';
 
 describe('Heading Component', () => {
-    it('should render foo text correctly', () => {
-        const { getByTestId } = render(<Heading data-test-id={HEADING_TEST_ID}>{HEADING_TEXT}</Heading>);
-        const component = getByTestId(HEADING_TEST_ID);
-        expect(component).toContain(HEADING_TEXT);
+    it('should render correctly', () => {
+        render(<Heading data-test-id={HEADING_TEST_ID}>{HEADING_TEXT}</Heading>);
+
+        const component = screen.getByTestId(HEADING_TEST_ID);
+
+        expect(component).toBeInTheDocument();
+        expect(component.tagName).toBe('SPAN');
+        expect(screen.getByText(HEADING_TEXT)).toBeInTheDocument();
+    });
+
+    it('should render h1 correctly', () => {
+        render(
+            <Heading as="p" data-test-id={HEADING_TEST_ID}>
+                {HEADING_TEXT}
+            </Heading>,
+        );
+
+        const component = screen.getByTestId(HEADING_TEST_ID);
+
+        expect(component).toBeInTheDocument();
+        expect(component.tagName).toBe('P');
+    });
+
+    it('should render h1 correctly', () => {
+        render(
+            <Heading as="h1" data-test-id={HEADING_TEST_ID}>
+                {HEADING_TEXT}
+            </Heading>,
+        );
+
+        const component = screen.getByTestId(HEADING_TEST_ID);
+
+        expect(component).toBeInTheDocument();
+        expect(component.tagName).toBe('H1');
     });
 });
