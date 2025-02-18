ad7fa46b168f9261af2d1b791774ea60c1953b13
Red Ochsenbein <red.ochsenbein@frontify.com>
2025-02-18 15:13:05 +0100

    chore: more TSDoc

--- packages/components/src/components/Accordion/Accordion.tsx
+++ packages/components/src/components/Accordion/Accordion.tsx
@@ -15,7 +15,13 @@ export type AccordionProps = {
      * @default true
      */
     border?: boolean;
+    /**
+     * Children of the Accordion component. This should contain the `Accordion.Item` components
+     */
     children?: ReactNode;
+    /**
+     * additional classed for custom styling
+     */
     className?: string;
     /**
      * The value of the items whose contents are expanded when the accordion is initially rendered. Use
@@ -67,7 +73,13 @@ export type AccordionItemProps = {
 
 export const AccordionItem = ({
     'data-test-id': dataTestId = 'fondue-accordion-item',
+    /**
+     * Children of the Accordion item. This should contain the `Accordion.Header` and `Accordion.Content` components
+     */
     children,
+    /**
+     * additional classed for custom styling
+     */
     className,
     /**
      * Whether or not an accordion item is disabled from user interaction.
@@ -75,6 +87,9 @@ export const AccordionItem = ({
      * @default false
      */
     disabled,
+    /**
+     * click callback for this item
+     */
     onClick,
     /**
      * A string value for the accordion item. All items within an accordion should use a unique value.
@@ -101,7 +116,16 @@ export const AccordionItem = ({
 };
 AccordionItem.displayName = 'Accordion.Item';
 
-export type AccordionHeaderProps = { children?: ReactNode; className?: string };
+export type AccordionHeaderProps = {
+    /**
+     * Children of the Accordion header. This should contain `Accordion.Trigger`
+     */
+    children?: ReactNode;
+    /**
+     * additional classed for custom styling
+     */
+    className?: string;
+};
 
 export const AccordionHeader = ({ children, className }: AccordionHeaderProps) => {
     return <RadixAccordion.Header className={cn([className])}>{children}</RadixAccordion.Header>;
@@ -112,7 +136,13 @@ export type AccordionTriggerProps = { children?: ReactNode; className?: string; 
 
 export const AccordionTrigger = ({
     'data-test-id': dataTestId = 'fondue-accordion-trigger',
+    /**
+     * Children of the Accordion trigger. This contains the actually clickable and visible header content
+     */
     children,
+    /**
+     * additional classed for custom styling
+     */
     className,
 }: AccordionTriggerProps) => {
     return (
@@ -126,12 +156,21 @@ AccordionTrigger.displayName = 'Accordion.Trigger';
 
 type AccordionContentProps = {
     'data-test-id'?: string;
+    /**
+     * Children of the Accordion content. This contains the main content
+     */
     children?: ReactNode;
+    /**
+     * additional classed for custom styling
+     */
     className?: string;
     /**
      * adds a divider line between the header and the content
      */
     divider?: boolean;
+    /**
+     * click callback for the content
+     */
     onClick?: MouseEventHandler<HTMLDivElement>;
     /**
      * controls if we show paddings around the content
