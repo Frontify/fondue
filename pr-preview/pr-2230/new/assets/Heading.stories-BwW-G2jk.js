c16113fe0b2bc5591a67a3c2f564714379121f4f
noahwaldner <noahwaldner@users.noreply.github.com>
2025-02-25 09:53:11 +0000

    Deploy preview for PR 2230 🛫

---
+++ pr-preview/pr-2230/new/assets/Heading.stories-BwW-G2jk.js
@@ -0,0 +1,5 @@
+import{H as a}from"./Heading-Dxlzvdof.js";import"./jsx-runtime-BSxFkHFN.js";import"./index-i66itsuO.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./styleUtilities-rhQWLmVU.js";const d={title:"Typography/Heading",component:a,tags:["autodocs"],parameters:{status:{type:"beta"}},args:{},argTypes:{as:{type:"string",control:"select",options:[void 0,"span","p","h1","h2","h3","h4","h5","h6"]},color:{type:"string",control:"select",options:[void 0,"default","weak","x-weak","disabled","negative","positive","warning","interactive"]},size:{type:"string",control:"select",options:[void 0,"medium","large","x-large","xx-large"]},weight:{type:"string",control:"select",options:[void 0,"default","strong"]},"data-test-id":{type:"string",control:"text"}}},e={args:{children:"Hello World"}};var t,r,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    children: 'Hello World'
+  }
+}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,d as default};
