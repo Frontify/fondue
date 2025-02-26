8ed347303ba55bfb11ab13ea14d45c2ac85528bf
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2025-02-26 11:15:52 +0100

    fixed types

--- packages/fondue/src/components/RichTextEditor/serializer/markdown/MarkdownToSlate.ts
+++ packages/fondue/src/components/RichTextEditor/serializer/markdown/MarkdownToSlate.ts
@@ -5,22 +5,21 @@ import remarkGfm from 'remark-gfm';
 import parse from 'remark-parse';
 import { unified } from 'unified';
 
+import { type TreeOfNodes } from '@components/RichTextEditor/types';
+
+import { MarkdownTransformer } from './MarkdownTransformer';
 import deserializer from './deserializer';
 import { options as plateEditorOptions } from './options';
-// eslint-disable-next-line import/order
-import { MarkdownTransformer } from './MarkdownTransformer';
-import { type NodeType } from './types';
-// eslint-disable-next-line import/order
 import { remarkFondue } from './remarkFondue';
 
-export class MarkdownToSlate extends MarkdownTransformer<string, NodeType[]> {
-    process(value: string, options?: { escapeValue?: boolean }): NodeType[] {
+export class MarkdownToSlate extends MarkdownTransformer<string, TreeOfNodes> {
+    process(value: string, options?: { escapeValue?: boolean }) {
         const parsedValue = options?.escapeValue ? escape(value) : value;
         return unified()
             .use(parse)
             .use(remarkGfm)
             .use(remarkFondue)
             .use(deserializer, plateEditorOptions(this.editor))
-            .processSync(parsedValue).result as NodeType[];
+            .processSync(parsedValue).result as TreeOfNodes;
     }
 }
--- packages/fondue/src/components/RichTextEditor/serializer/markdown/SlateToMarkdown.ts
+++ packages/fondue/src/components/RichTextEditor/serializer/markdown/SlateToMarkdown.ts
@@ -1,14 +1,15 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import { type TreeOfNodes } from '@components/RichTextEditor/types';
+
 import { serialize } from './serializer';
 // eslint-disable-next-line import/order
 import { options } from './options';
 // eslint-disable-next-line import/order
 import { MarkdownTransformer } from './MarkdownTransformer';
-import { type NodeType } from './types';
 
-export class SlateToMarkdown extends MarkdownTransformer<NodeType[], string> {
-    process(value: NodeType[], _?: { escapeValue?: boolean }) {
+export class SlateToMarkdown extends MarkdownTransformer<TreeOfNodes, string> {
+    process(value: TreeOfNodes) {
         return serialize(options(this.editor))(value);
     }
 }
