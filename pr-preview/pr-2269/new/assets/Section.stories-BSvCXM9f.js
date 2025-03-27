cf19e62f74a897763948b07d7745670f58949b13
bojangles-m <bojangles-m@users.noreply.github.com>
2025-03-27 13:55:16 +0000

    Remove preview for PR 2269 🛬

--- pr-preview/pr-2269/new/assets/Section.stories-BSvCXM9f.js
+++
@@ -1,5 +0,0 @@
-import{S as a}from"./Section-DUy1JF06.js";import"./jsx-runtime-BSxFkHFN.js";import"./index-i66itsuO.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./propsToCssVariables-DVyyXmoh.js";const p={title:"Layout/Section",component:a,tags:["autodocs"],parameters:{status:{type:"released"}},args:{}},e={args:{children:"Hello World"}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    children: 'Hello World'
-  }
-}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const d=["Default"];export{e as Default,d as __namedExportsOrder,p as default};
