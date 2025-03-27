b75ac77a0cc40f92c2c712658c10e4b1fda006c1
bojangles-m <517677+bojangles-m@users.noreply.github.com>
2025-03-27 13:57:12 +0000

    Deploying to gh-pages from @ Frontify/fondue@0fb93f0fc7d596bc75017cfffcbb14c0683003db 🚀

---
+++ legacy/assets/TextInput.stories-BLBpLXz8.js
@@ -0,0 +1,121 @@
+import{j as t}from"./jsx-runtime-DH9XN8A8.js";import{a as F}from"./chunk-D5ZWXAHU-DlerbUHg.js";import{r as y}from"./index-v7USjxUg.js";import{I as i}from"./IconIcon-Czev5ZcQ.js";import{I as Ie}from"./IconSize-BcCrF_mi.js";import{V as P}from"./validation-CJXPAjNB.js";import{T as he,b as T}from"./TextInput-DncHZpfW.js";import{F as fe}from"./FormField-BIjOmg6X.js";import"./v4-CtRu48qb.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./IconIcon16-KBXYRe7H.js";import"./IconIcon24-HjPMrfFS.js";import"./useFocusRing-CP9UJNUw.js";import"./useEffectEvent-BaECT7_f.js";import"./LoadingCircle-BSQayp2F.js";import"./merge-DI-veSMP.js";import"./IconCheckMark-CMS57PRn.js";import"./IconCross-LL1GOU_p.js";import"./IconExclamationMarkTriangle-CTTfs2nR.js";import"./IconExclamationMarkTriangle24-ZUoV6XtQ.js";import"./useMemoizedId-Cl5PI4eu.js";import"./focusStyle-CBFElZiz.js";import"./LegacyTooltip-BReeJmjq.js";import"./useLink-C2PgwcjG.js";import"./useFocusable-BRFkliNn.js";import"./chain-C8AhrK0W.js";import"./usePopper-BO8FF5JQ.js";import"./index-voQlCD4e.js";import"./index-DlnOb27V.js";import"./Button-BfBJpGQT.js";import"./useButton-B6orroTH.js";import"./Portal-BMcVKwlx.js";import"./IconExclamationMarkCircle24-CS86ZjBV.js";import"./VisuallyHidden-CBFC6H_6.js";import"./Tooltip-PCAYiURV.js";import"./useClickOutside-nHRsWOx2.js";import"./EnablePortalWrapper-DzTSSIqA.js";import"./elements-BOYmskOL.js";import"./dialog-Dre1eVQK.js";import"./input-Dxrv2jWk.js";import"./Checkbox-C8fDjqts.js";import"./useFormValidationState-BKAFCnHb.js";import"./useControlledState-BSgwHaZM.js";import"./InputLabel-CAkTbk9Q.js";import"./TooltipIcon-DWdr_hgc.js";import"./IconQuestionMarkCircle-DT_LrHkX.js";import"./IconMinus-DtF84-GK.js";import"./Divider-BI8G7m_S.js";import"./FormControl-DeLflFlr.js";import"./Switch-CibQ61jG.js";import"./TabItem-BOEZCaC7.js";import"./Tree-Bt1Is369.js";import"./_Uint8Array-Ck34k-eX.js";import"./Trigger-BvTVBjbf.js";import"./IconCaretDown-BxzyP7sS.js";import"./IconCaretDown16-DV8CvCws.js";import"./IconTrashBin16-Db8fv2tm.js";import"./IconTrashBin24-Dz-kV-1n.js";const Pt={title:"Components/Text Input",component:he,tags:["autodocs"],args:{clearable:!1,disabled:!1,required:!1,autocomplete:!1,readonly:!1,dotted:!1,type:T.Text,validation:P.Default,spellcheck:!0,copyable:!1},argTypes:{validation:{options:Object.values(P),control:{type:"select"}},spellcheck:{options:[void 0,!0,!1],control:{type:"select"}},placeholder:{type:"string"},maxLength:{type:"number"},disabled:{control:{type:"boolean"},defaultValue:!1},readonly:{control:{type:"boolean"},defaultValue:!1},value:{table:{disable:!0}},obfuscated:{table:{disable:!0}},type:{table:{disable:!0}},decorator:{table:{disable:!0}},onInput:{table:{disable:!0},action:"onInput"},onBlur:{table:{disable:!0},action:"onBlur"},onClear:{table:{disable:!0},action:"onClear"},onFocus:{table:{disable:!0},action:"onFocus"}},parameters:{status:{type:"deprecated"},docs:{description:{component:"### This component is deprecated, please use the [new TextInput component](/docs/current_components-text-input--documentation) instead."}}}},e=h=>{const[Fe,S]=y.useState("");return y.useEffect(()=>S(`${h.value??""}`),[h.value]),t.jsx(fe,{disabled:h.disabled,label:{text:"sample"},hiddenLabel:!0,children:t.jsx(he,{...h,value:Fe,onChange:S})})},l=e.bind({});l.argTypes={value:{table:{disable:!1},type:"string"}};const p=e.bind({});p.args={value:"Value text"};const u=e.bind({});u.args={placeholder:"Placeholder text"};const d=e.bind({});d.args={value:"Value text",decorator:t.jsx(i,{size:Ie.Size16})};const c=e.bind({});c.args={maxLength:10};const m=e.bind({});m.args={value:"This a disabled input",disabled:!0};const g=e.bind({});g.args={decorator:"X",placeholder:"Placeholder text"};const f={obfuscated:{control:{type:"boolean"},table:{disable:!1}}},v={type:T.Password,value:"Secret"},a=e.bind({});a.argTypes=f;a.args={...v};const r=e.bind({});r.argTypes=f;r.args={...v,decorator:t.jsx(i,{size:Ie.Size16})};const s=e.bind({});s.argTypes=f;s.args={...v,decorator:"P"};const n=e.bind({});n.argTypes=f;n.args={...v,obfuscated:!1};const o=e.bind({});o.argTypes={value:{table:{disable:!1},type:"number"}};o.args={type:T.Number};const b=e.bind({});b.args={value:"Value text",selectable:!0};const x=e.bind({});x.args={value:"Value text",extraActions:[{title:"Extra action A",onClick:F("extraActionA"),icon:t.jsx(i,{})},{title:"Extra action B",onClick:F("extraActionB"),icon:t.jsx(i,{})}]};const I=e.bind({});I.args={value:"Value text",extraActions:[{title:"First extra action",onClick:F("extraAction1"),icon:t.jsx(i,{}),tooltip:{content:"First extra action tooltip content",withArrow:!0}},{title:"Second extra action",onClick:F("extraAction2"),icon:t.jsx(i,{}),tooltip:{content:"Second extra action tooltip content",withArrow:!0}}]};var C,E,L;l.parameters={...l.parameters,docs:{...(C=l.parameters)==null?void 0:C.docs,source:{originalSource:`(args: TextInputStoryProps) => {
+  const [input, setInput] = useState('');
+  useEffect(() => setInput(\`\${args.value ?? ''}\`), [args.value]);
+  return <FormField disabled={args.disabled} label={{
+    text: 'sample'
+  }} hiddenLabel>
+            <TextInput {...args} value={input} onChange={setInput} />
+        </FormField>;
+}`,...(L=(E=l.parameters)==null?void 0:E.docs)==null?void 0:L.source}}};var W,w,A;p.parameters={...p.parameters,docs:{...(W=p.parameters)==null?void 0:W.docs,source:{originalSource:`(args: TextInputStoryProps) => {
+  const [input, setInput] = useState('');
+  useEffect(() => setInput(\`\${args.value ?? ''}\`), [args.value]);
+  return <FormField disabled={args.disabled} label={{
+    text: 'sample'
+  }} hiddenLabel>
+            <TextInput {...args} value={input} onChange={setInput} />
+        </FormField>;
+}`,...(A=(w=p.parameters)==null?void 0:w.docs)==null?void 0:A.source}}};var $,j,D;u.parameters={...u.parameters,docs:{...($=u.parameters)==null?void 0:$.docs,source:{originalSource:`(args: TextInputStoryProps) => {
+  const [input, setInput] = useState('');
+  useEffect(() => setInput(\`\${args.value ?? ''}\`), [args.value]);
+  return <FormField disabled={args.disabled} label={{
+    text: 'sample'
+  }} hiddenLabel>
+            <TextInput {...args} value={input} onChange={setInput} />
+        </FormField>;
+}`,...(D=(j=u.parameters)==null?void 0:j.docs)==null?void 0:D.source}}};var V,k,z;d.parameters={...d.parameters,docs:{...(V=d.parameters)==null?void 0:V.docs,source:{originalSource:`(args: TextInputStoryProps) => {
+  const [input, setInput] = useState('');
+  useEffect(() => setInput(\`\${args.value ?? ''}\`), [args.value]);
+  return <FormField disabled={args.disabled} label={{
+    text: 'sample'
+  }} hiddenLabel>
+            <TextInput {...args} value={input} onChange={setInput} />
+        </FormField>;
+}`,...(z=(k=d.parameters)==null?void 0:k.docs)==null?void 0:z.source}}};var B,_,N;c.parameters={...c.parameters,docs:{...(B=c.parameters)==null?void 0:B.docs,source:{originalSource:`(args: TextInputStoryProps) => {
+  const [input, setInput] = useState('');
+  useEffect(() => setInput(\`\${args.value ?? ''}\`), [args.value]);
+  return <FormField disabled={args.disabled} label={{
+    text: 'sample'
+  }} hiddenLabel>
+            <TextInput {...args} value={input} onChange={setInput} />
+        </FormField>;
+}`,...(N=(_=c.parameters)==null?void 0:_.docs)==null?void 0:N.source}}};var M,O,q;m.parameters={...m.parameters,docs:{...(M=m.parameters)==null?void 0:M.docs,source:{originalSource:`(args: TextInputStoryProps) => {
+  const [input, setInput] = useState('');
+  useEffect(() => setInput(\`\${args.value ?? ''}\`), [args.value]);
+  return <FormField disabled={args.disabled} label={{
+    text: 'sample'
+  }} hiddenLabel>
+            <TextInput {...args} value={input} onChange={setInput} />
+        </FormField>;
+}`,...(q=(O=m.parameters)==null?void 0:O.docs)==null?void 0:q.source}}};var R,X,G;g.parameters={...g.parameters,docs:{...(R=g.parameters)==null?void 0:R.docs,source:{originalSource:`(args: TextInputStoryProps) => {
+  const [input, setInput] = useState('');
+  useEffect(() => setInput(\`\${args.value ?? ''}\`), [args.value]);
+  return <FormField disabled={args.disabled} label={{
+    text: 'sample'
+  }} hiddenLabel>
+            <TextInput {...args} value={input} onChange={setInput} />
+        </FormField>;
+}`,...(G=(X=g.parameters)==null?void 0:X.docs)==null?void 0:G.source}}};var H,J,K;a.parameters={...a.parameters,docs:{...(H=a.parameters)==null?void 0:H.docs,source:{originalSource:`(args: TextInputStoryProps) => {
+  const [input, setInput] = useState('');
+  useEffect(() => setInput(\`\${args.value ?? ''}\`), [args.value]);
+  return <FormField disabled={args.disabled} label={{
+    text: 'sample'
+  }} hiddenLabel>
+            <TextInput {...args} value={input} onChange={setInput} />
+        </FormField>;
+}`,...(K=(J=a.parameters)==null?void 0:J.docs)==null?void 0:K.source}}};var Q,U,Y;r.parameters={...r.parameters,docs:{...(Q=r.parameters)==null?void 0:Q.docs,source:{originalSource:`(args: TextInputStoryProps) => {
+  const [input, setInput] = useState('');
+  useEffect(() => setInput(\`\${args.value ?? ''}\`), [args.value]);
+  return <FormField disabled={args.disabled} label={{
+    text: 'sample'
+  }} hiddenLabel>
+            <TextInput {...args} value={input} onChange={setInput} />
+        </FormField>;
+}`,...(Y=(U=r.parameters)==null?void 0:U.docs)==null?void 0:Y.source}}};var Z,ee,te;s.parameters={...s.parameters,docs:{...(Z=s.parameters)==null?void 0:Z.docs,source:{originalSource:`(args: TextInputStoryProps) => {
+  const [input, setInput] = useState('');
+  useEffect(() => setInput(\`\${args.value ?? ''}\`), [args.value]);
+  return <FormField disabled={args.disabled} label={{
+    text: 'sample'
+  }} hiddenLabel>
+            <TextInput {...args} value={input} onChange={setInput} />
+        </FormField>;
+}`,...(te=(ee=s.parameters)==null?void 0:ee.docs)==null?void 0:te.source}}};var ae,re,se;n.parameters={...n.parameters,docs:{...(ae=n.parameters)==null?void 0:ae.docs,source:{originalSource:`(args: TextInputStoryProps) => {
+  const [input, setInput] = useState('');
+  useEffect(() => setInput(\`\${args.value ?? ''}\`), [args.value]);
+  return <FormField disabled={args.disabled} label={{
+    text: 'sample'
+  }} hiddenLabel>
+            <TextInput {...args} value={input} onChange={setInput} />
+        </FormField>;
+}`,...(se=(re=n.parameters)==null?void 0:re.docs)==null?void 0:se.source}}};var ne,oe,ie;o.parameters={...o.parameters,docs:{...(ne=o.parameters)==null?void 0:ne.docs,source:{originalSource:`(args: TextInputStoryProps) => {
+  const [input, setInput] = useState('');
+  useEffect(() => setInput(\`\${args.value ?? ''}\`), [args.value]);
+  return <FormField disabled={args.disabled} label={{
+    text: 'sample'
+  }} hiddenLabel>
+            <TextInput {...args} value={input} onChange={setInput} />
+        </FormField>;
+}`,...(ie=(oe=o.parameters)==null?void 0:oe.docs)==null?void 0:ie.source}}};var le,pe,ue;b.parameters={...b.parameters,docs:{...(le=b.parameters)==null?void 0:le.docs,source:{originalSource:`(args: TextInputStoryProps) => {
+  const [input, setInput] = useState('');
+  useEffect(() => setInput(\`\${args.value ?? ''}\`), [args.value]);
+  return <FormField disabled={args.disabled} label={{
+    text: 'sample'
+  }} hiddenLabel>
+            <TextInput {...args} value={input} onChange={setInput} />
+        </FormField>;
+}`,...(ue=(pe=b.parameters)==null?void 0:pe.docs)==null?void 0:ue.source}}};var de,ce,me;x.parameters={...x.parameters,docs:{...(de=x.parameters)==null?void 0:de.docs,source:{originalSource:`(args: TextInputStoryProps) => {
+  const [input, setInput] = useState('');
+  useEffect(() => setInput(\`\${args.value ?? ''}\`), [args.value]);
+  return <FormField disabled={args.disabled} label={{
+    text: 'sample'
+  }} hiddenLabel>
+            <TextInput {...args} value={input} onChange={setInput} />
+        </FormField>;
+}`,...(me=(ce=x.parameters)==null?void 0:ce.docs)==null?void 0:me.source}}};var ge,be,xe;I.parameters={...I.parameters,docs:{...(ge=I.parameters)==null?void 0:ge.docs,source:{originalSource:`(args: TextInputStoryProps) => {
+  const [input, setInput] = useState('');
+  useEffect(() => setInput(\`\${args.value ?? ''}\`), [args.value]);
+  return <FormField disabled={args.disabled} label={{
+    text: 'sample'
+  }} hiddenLabel>
+            <TextInput {...args} value={input} onChange={setInput} />
+        </FormField>;
+}`,...(xe=(be=I.parameters)==null?void 0:be.docs)==null?void 0:xe.source}}};const Ct=["Text","Prefilled","WithPlaceholder","WithIconDecorator","WithMaxLength","Disabled","WithCharacterDecorator","Password","PasswordWithIconDecorator","PasswordWithCharacterDecorator","PasswordVisible","TypeNumber","SelectableInput","WithExtraActions","WithExtraActionWithTooltip"];export{m as Disabled,a as Password,n as PasswordVisible,s as PasswordWithCharacterDecorator,r as PasswordWithIconDecorator,p as Prefilled,b as SelectableInput,l as Text,o as TypeNumber,g as WithCharacterDecorator,I as WithExtraActionWithTooltip,x as WithExtraActions,d as WithIconDecorator,c as WithMaxLength,u as WithPlaceholder,Ct as __namedExportsOrder,Pt as default};
