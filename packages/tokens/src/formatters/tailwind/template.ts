80bf10ef722596ddfcef9c7960c97ddb5d210649
Noah Waldner <noah.waldner@frontify.com>
2025-05-06 13:04:46 +0200

    export diatype font utility

--- packages/tokens/src/formatters/tailwind/template.ts
+++ packages/tokens/src/formatters/tailwind/template.ts
@@ -8,50 +8,52 @@ type TemplateOptions = {
 export const template = ({ theme, plugin }: TemplateOptions) => {
     return `import plugin from 'tailwindcss/plugin';
 
-  export default {
-      darkMode: "class",
-      prefix: "tw-",
-      theme: ${JSON.stringify(theme)},
-      plugins: [
+export default {
+    darkMode: "class",
+    prefix: "tw-",
+    theme: ${JSON.stringify(theme)},
+    plugins: [
         plugin(function({ addComponents, addUtilities}) {
             const components = ${JSON.stringify(plugin)};
             addComponents(components);
         }),
-        plugin(({ addUtilities }) => {
-            addUtilities({
-                '.italic, i, em': {
-                    fontStyle: 'var(--italic-style, normal)',
-                    fontFeatureSettings: '"ss01"',
-                    fontVariationSettings: '"slnt" -12',
-                },
-            });
-        }),
-      ]
-  }`;
+    ]
+}
+
+export const fondueDiatypeTypographyUtil = plugin(({ addUtilities }) => {
+    addUtilities({
+        '.italic, i, em': {
+            fontStyle: normal,
+            fontFeatureSettings: '"ss01"',
+            fontVariationSettings: '"slnt" -12',
+        },
+    });
+});`;
 };
 
 export const templateDebug = ({ theme, plugin }: TemplateOptions) => {
     return `import plugin from 'tailwindcss/plugin';
 
-  export default {
-      darkMode: "class",
-      prefix: "tw-",
-      safelist: [{ pattern: /.*/ }],
-      theme: ${JSON.stringify(theme)},
-      plugins: [
-        plugin(function({ addComponents, addUtilities}) {
+export default {
+    darkMode: "class",
+    prefix: "tw-",
+    safelist: [{ pattern: /.*/ }],
+    theme: ${JSON.stringify(theme)},
+    plugins: [
+        plugin(function ({ addComponents, addUtilities }) {
             const components = ${JSON.stringify(plugin)};
             addComponents(components);
         }),
-        plugin(({ addUtilities }) => {
-            addUtilities({
-                '.italic, i, em': {
-                    fontStyle: 'var(--italic-style, normal)',
-                    fontFeatureSettings: '"ss01"',
-                    fontVariationSettings: '"slnt" -12',
-                },
-            });
-        }),
-      ]
-  }`;
+    ]
+}
+
+export const fondueDiatypeTypographyUtil = plugin(({ addUtilities }) => {
+    addUtilities({
+        '.italic, i, em': {
+            fontStyle: normal,
+            fontFeatureSettings: '"ss01"',
+            fontVariationSettings: '"slnt" -12',
+        },
+    });
+}); `;
 };
