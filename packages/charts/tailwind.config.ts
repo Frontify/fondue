faecf2e8d99fab54fa27bb6b290d7c0b7358e67d
Noah Waldner <noah.waldner@frontify.com>
2025-05-20 17:12:50 +0200

    load token mapping in charts package

--- packages/charts/tailwind.config.ts
+++ packages/charts/tailwind.config.ts
@@ -1,10 +1,12 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 // @ts-expect-error Untyped
+import legacyTailwindConfig from '@frontify/fondue-tokens/legacy/tailwind';
+// @ts-expect-error Untyped
 import frontifyTailwindConfig from '@frontify/fondue-tokens/tailwind';
 import { type Config } from 'tailwindcss';
 
 export default {
-    presets: [frontifyTailwindConfig as Config],
+    presets: [legacyTailwindConfig as Config, frontifyTailwindConfig as Config],
     content: ['./src/**/*.{ts,tsx}'],
 } satisfies Config;
