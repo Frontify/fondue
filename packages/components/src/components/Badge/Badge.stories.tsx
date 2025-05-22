3768b0c5e7b9672e25607a8c064494ab2eebc7f2
Red Ochsenbein <red.ochsenbein@frontify.com>
2025-05-22 11:01:59 +0200

    feat(Badge): add badge with icon

--- packages/components/src/components/Badge/Badge.stories.tsx
+++ packages/components/src/components/Badge/Badge.stories.tsx
@@ -1,5 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import { IconColorFan } from '@frontify/fondue-icons';
 import { type Meta, type StoryObj } from '@storybook/react';
 
 import { Badge } from './Badge';
@@ -25,4 +26,14 @@ export default meta;
 export const Default: Story = {
     name: 'Badge',
     render: (args) => <Badge {...args}>Text</Badge>,
+};
+
+export const BadgeWithIcon: Story = {
+    name: 'Badge with Icon',
+    render: (args) => (
+        <Badge {...args}>
+            <IconColorFan size="16" />
+            Text
+        </Badge>
+    ),
 };
