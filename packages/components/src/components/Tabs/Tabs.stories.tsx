640e41856356faedb9084994a997055f4d3c9228
Red Ochsenbein <red.ochsenbein@frontify.com>
2025-05-30 11:56:29 +0200

    feat(Tabs): Add badges to tabs (#2313)

--- packages/components/src/components/Tabs/Tabs.stories.tsx
+++ packages/components/src/components/Tabs/Tabs.stories.tsx
@@ -5,6 +5,8 @@ import { action } from '@storybook/addon-actions';
 import { type Meta, type StoryObj } from '@storybook/react';
 import { useState } from 'react';
 
+import { Badge } from '../Badge/Badge';
+
 import { Tabs, TabsContent, TabsRoot, TabsTab, TabsTrigger } from './Tabs';
 
 type Story = StoryObj<typeof meta>;
@@ -411,6 +413,44 @@ export const WithPillVariant: Story = {
             </Tabs.Tab>
             <Tabs.Tab value="third">
                 <Tabs.Trigger>Third Tab</Tabs.Trigger>
+                <Tabs.Content>
+                    <b>Third Content</b>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat.
+                </Tabs.Content>
+            </Tabs.Tab>
+        </Tabs.Root>
+    ),
+};
+
+export const PillVariantWithBadge: Story = {
+    render: (args) => (
+        <Tabs.Root variant="pill" {...args}>
+            <Tabs.Tab value="first">
+                <Tabs.Trigger>
+                    First Tab <Badge variant="highlight">Beta</Badge>
+                </Tabs.Trigger>
+                <Tabs.Content>
+                    <b>First Content</b>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat.
+                </Tabs.Content>
+            </Tabs.Tab>
+            <Tabs.Tab value="second">
+                <Tabs.Trigger>Second Tab</Tabs.Trigger>
+                <Tabs.Content>
+                    <b>Second Content</b>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat.
+                </Tabs.Content>
+            </Tabs.Tab>
+            <Tabs.Tab value="third">
+                <Tabs.Trigger>
+                    Third Tab <Badge variant="highlight">Beta</Badge>
+                </Tabs.Trigger>
                 <Tabs.Content>
                     <b>Third Content</b>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
