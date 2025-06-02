71eebc444310c07ed6cd563458addf8fa6f35564
Red Ochsenbein <red.ochsenbein@frontify.com>
2025-06-02 07:27:40 +0200

    feat(Tabs): add badge story to default tab style

--- packages/components/src/components/Tabs/Tabs.stories.tsx
+++ packages/components/src/components/Tabs/Tabs.stories.tsx
@@ -216,6 +216,44 @@ export const WithDecorator: Story = {
     ),
 };
 
+export const WithBadge: Story = {
+    render: (args) => (
+        <Tabs.Root {...args}>
+            <Tabs.Tab value="first">
+                <Tabs.Trigger>
+                    First Tab <Badge variant="highlight">Beta</Badge>
+                </Tabs.Trigger>
+                <Tabs.Content>
+                    <b>First Content</b>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat.
+                </Tabs.Content>
+            </Tabs.Tab>
+            <Tabs.Tab value="second">
+                <Tabs.Trigger>Second Tab</Tabs.Trigger>
+                <Tabs.Content>
+                    <b>Second Content</b>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat.
+                </Tabs.Content>
+            </Tabs.Tab>
+            <Tabs.Tab value="third">
+                <Tabs.Trigger>
+                    Third Tab <Badge variant="highlight">Beta</Badge>
+                </Tabs.Trigger>
+                <Tabs.Content>
+                    <b>Third Content</b>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat.
+                </Tabs.Content>
+            </Tabs.Tab>
+        </Tabs.Root>
+    ),
+};
+
 export const WithNoPadding: Story = {
     args: {
         padding: 'comfortable',
