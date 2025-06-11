8ff0fd88d99669e26ecec8156127054be3d572ea
Noah Waldner <noah.waldner@frontify.com>
2025-06-11 10:09:44 +0200

    adjust checkbox styles and generate tokens

--- packages/components/src/components/Checkbox/styles/checkboxStyles.ts
+++ packages/components/src/components/Checkbox/styles/checkboxStyles.ts
@@ -5,7 +5,7 @@ import { sv } from '#/utilities/styleUtilities';
 
 export const checkboxStyles = sv({
     base:
-        'tw-peer tw-relative tw-inline-flex tw-bg-surface tw-text-white tw-shrink-0 tw-rounded tw-border tw-border-line-strong group-hover:tw-border-line-strong hover:tw-border-line-strong tw-transition-colors ' +
+        'tw-peer tw-relative tw-inline-flex tw-bg-surface tw-text-white tw-shrink-0 tw-rounded-medium tw-border tw-border-line-strong group-hover:tw-border-line-strong hover:tw-border-line-strong tw-transition-colors ' +
         'data-[state="checked"]:tw-border-transparent data-[state="indeterminate"]:tw-border-transparent ' +
         'disabled:tw-border-line-strong disabled:tw-bg-surface disabled:tw-cursor-not-allowed data-[state="checked"]:disabled:tw-bg-disabled ' +
         `${FOCUS_OUTLINE}`,
