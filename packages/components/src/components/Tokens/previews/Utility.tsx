2e0c2542f7099199763f8e50fa229412919b7e93
Noah Waldner <noah.waldner@frontify.com>
2025-07-14 09:04:43 +0200

    use generic for typing

--- packages/components/src/components/Tokens/previews/Utility.tsx
+++ packages/components/src/components/Tokens/previews/Utility.tsx
@@ -10,13 +10,11 @@ export const getUtilityClassName = ({ path }: Utility) => {
 export const UtilityPreview = ({ properties }: Utility) => {
     const { textContent } = usePreviewConfig();
 
-    const style = Object.entries(properties).reduce(
-        (acc, [key, value]) => {
-            acc[key.replaceAll(/-./g, (match) => match.slice(-1).toUpperCase())] = `var(--${value.name})`;
-            return acc;
-        },
-        {} as Record<string, string>,
-    );
+    const style = Object.entries(properties).reduce<Record<string, string>>((acc, [key, value]) => {
+        acc[key.replaceAll(/-./g, (match) => match.slice(-1).toUpperCase())] = `var(--${value.name})`;
+        return acc;
+    }, {});
+
     return (
         <div style={style} className="tw-text-surface-on-surface tw-flex tw-items-center tw-justify-center tw-h-full">
             {textContent}
---
+++ packages/tokens/dist/theme/themes.module.css.d.ts
@@ -0,0 +1,13 @@
+declare const styles: {
+        /**
+         * dark theme class
+         */
+        dark: string;
+
+        /**
+         * light theme class
+         */
+        light: string;
+};
+
+export default styles;
