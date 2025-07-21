2762adaaba8646dfc9aac9921e4aeb814604ccf7
Noah Waldner <noah.waldner@frontify.com>
2025-07-21 13:10:54 +0200

    split up more types

--- packages/fondue/src/components/TooltipIcon/TooltipIcon.tsx
+++ packages/fondue/src/components/TooltipIcon/TooltipIcon.tsx
@@ -13,7 +13,7 @@ export type TooltipIconProps = {
     tooltip?: LegacyTooltipProps;
     iconSize?: IconSize;
     triggerIcon?: ReactElement<IconProps>;
-    triggerStyle?: TooltipIconTriggerStyle;
+    triggerStyle?: TooltipIconTriggerStyle | 'danger' | 'warning' | 'primary';
     'data-test-id'?: string;
 };
 
@@ -23,16 +23,22 @@ export enum TooltipIconTriggerStyle {
     Primary = 'Primary',
 }
 
-const tooltipTriggerStyleClass: Record<TooltipIconTriggerStyle, string> = {
+const tooltipTriggerStyleClass: Record<TooltipIconTriggerStyle | 'danger' | 'warning' | 'primary', string> = {
     [TooltipIconTriggerStyle.Danger]: 'tw-text-box-negative-inverse',
     [TooltipIconTriggerStyle.Warning]: 'tw-text-box-warning-inverse',
     [TooltipIconTriggerStyle.Primary]: 'tw-text-text-weak',
+    danger: 'tw-text-box-negative-inverse',
+    warning: 'tw-text-box-warning-inverse',
+    primary: 'tw-text-text-weak',
 };
 
 const tooltipHoverClasses = {
     [TooltipIconTriggerStyle.Danger]: 'hover:tw-text-box-negative-inverse-hover hover:tw-bg-box-neutral',
     [TooltipIconTriggerStyle.Warning]: 'hover:tw-text-box-warning-inverse-hover hover:tw-bg-box-neutral',
     [TooltipIconTriggerStyle.Primary]: 'hover:tw-text-text hover:tw-bg-box-neutral',
+    danger: 'hover:tw-text-box-negative-inverse-hover hover:tw-bg-box-neutral',
+    warning: 'hover:tw-text-box-warning-inverse-hover hover:tw-bg-box-neutral',
+    primary: 'hover:tw-text-text hover:tw-bg-box-neutral',
 };
 
 /**
