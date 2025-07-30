a4eb6526d6a597cf17a439d206a429cf47e12f36
Noah Waldner <noah.waldner@frontify.com>
2025-07-30 11:52:04 +0200

    remove unused export

--- packages/fondue/src/components/ActionMenu/Aria/index.ts
+++
@@ -1,4 +0,0 @@
-/* (c) Copyright Frontify Ltd., all rights reserved. */
-
-export * from './AriaSection';
-export { getDisabledItemIds, getKeyItemRecord } from './helper';
--- packages/fondue/src/components/ActionMenu/index.ts
+++ packages/fondue/src/components/ActionMenu/index.ts
@@ -1,4 +1,3 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 export { type ActionMenuProps } from './ActionMenu';
-export * from './Aria';
