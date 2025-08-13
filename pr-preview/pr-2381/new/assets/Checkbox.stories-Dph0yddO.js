3cfea86119b5358ee5fdb6ee372e185d9268988c
syeo66 <syeo66@users.noreply.github.com>
2025-08-13 13:34:16 +0000

    Deploy preview for PR 2381 🛫

---
+++ pr-preview/pr-2381/new/assets/Checkbox.stories-Dph0yddO.js
@@ -0,0 +1,32 @@
+import{j as e}from"./jsx-runtime-BYYWji4R.js";import{a}from"./index-B-lxVbXh.js";import{r as S}from"./index-ClcD9ViR.js";import{F as o}from"./Flex-CjzBXb8-.js";import{L as s}from"./Label-DLdJYH0f.js";import{C as D,a as i}from"./Checkbox-Dy4Y6KK3.js";import"./v4-CtRu48qb.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./propsToCssVariables-DuhnUZvM.js";import"./index-Cr2iZZCw.js";import"./index-Cho7bklK.js";import"./index-BUNfiIdP.js";import"./styleUtilities-Byf6Fdv2.js";import"./fondue-icons351-Cs7ytFtN.js";import"./index-CWz5EflU.js";import"./index-BIU06K05.js";import"./index-6uuHhnjI.js";import"./index-sY83p_TZ.js";import"./index-Dkr9yJHh.js";import"./focusStyle-IgMhxCMT.js";const W={title:"Components/Checkbox",component:D,tags:["autodocs"],parameters:{status:{type:"released"}},args:{onChange:a("onChange"),onBlur:a("onBlur"),onFocus:a("onFocus")},render:r=>{const[v,w]=S.useState(r.value),y=L=>{a("onChange")(L),w(d=>d==="indeterminate"?!0:!d)};return e.jsxs("div",{className:"tw-flex tw-gap-1.5 tw-items-center",children:[e.jsx(i,{...r,id:"checkbox","aria-labelledby":"label",value:v,onChange:y}),e.jsx(s,{id:"label",htmlFor:"checkbox",children:"Checkbox"})]})}},l={},n={args:{value:"indeterminate"}},t={args:{disabled:!0}},c={render:r=>e.jsxs(o,{gap:"1rem",direction:"column",children:[e.jsxs(o,{gap:"4px",direction:"row",children:[e.jsx(i,{...r,id:"checkbox","aria-labelledby":"label",value:!0,onChange:()=>{}}),e.jsx(s,{id:"label",htmlFor:"checkbox",children:"Checkbox 1"})]}),e.jsxs(o,{gap:"4px",direction:"row",children:[e.jsx(i,{...r,id:"checkbox","aria-labelledby":"label",value:!1,onChange:()=>{}}),e.jsx(s,{id:"label",htmlFor:"checkbox",children:"Checkbox 2"})]}),e.jsxs(o,{gap:"4px",direction:"row",children:[e.jsx(i,{...r,id:"checkbox","aria-labelledby":"label",value:"indeterminate",onChange:()=>{}}),e.jsx(s,{id:"label",htmlFor:"checkbox",children:"Checkbox 3"})]})]})};var m,b,x;l.parameters={...l.parameters,docs:{...(m=l.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(x=(b=l.parameters)==null?void 0:b.docs)==null?void 0:x.source}}};var p,h,u;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    value: 'indeterminate'
+  }
+}`,...(u=(h=n.parameters)==null?void 0:h.docs)==null?void 0:u.source}}};var g,k,C;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(C=(k=t.parameters)==null?void 0:k.docs)==null?void 0:C.source}}};var F,j,f;c.parameters={...c.parameters,docs:{...(F=c.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: args => {
+    return <Flex gap="1rem" direction="column">
+                <Flex gap="4px" direction="row">
+                    <Checkbox {...args} id="checkbox" aria-labelledby="label" value={true} onChange={() => {}} />
+                    <Label id="label" htmlFor="checkbox">
+                        Checkbox 1
+                    </Label>
+                </Flex>
+                <Flex gap="4px" direction="row">
+                    <Checkbox {...args} id="checkbox" aria-labelledby="label" value={false} onChange={() => {}} />
+                    <Label id="label" htmlFor="checkbox">
+                        Checkbox 2
+                    </Label>
+                </Flex>
+                <Flex gap="4px" direction="row">
+                    <Checkbox {...args} id="checkbox" aria-labelledby="label" value="indeterminate" onChange={() => {}} />
+                    <Label id="label" htmlFor="checkbox">
+                        Checkbox 3
+                    </Label>
+                </Flex>
+            </Flex>;
+  }
+}`,...(f=(j=c.parameters)==null?void 0:j.docs)==null?void 0:f.source}}};const X=["Default","Indeterminate","Disabled","Checklist"];export{c as Checklist,l as Default,t as Disabled,n as Indeterminate,X as __namedExportsOrder,W as default};
---
+++ pr-preview/pr-2381/new/assets/Label.stories-oDpcBszM.js
@@ -0,0 +1,100 @@
+import{j as e}from"./jsx-runtime-BYYWji4R.js";import"./index-ClcD9ViR.js";import{c as P}from"./fondue-icons351-Cs7ytFtN.js";import{a as U}from"./Checkbox-Dy4Y6KK3.js";import{S as l}from"./Select-DJ6acwXT.js";import{T as x}from"./TextInput-BYRo-HZF.js";import{T as u}from"./Tooltip-DbfWYHFM.js";import{a as q,L as n}from"./Label-DLdJYH0f.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-BUNfiIdP.js";import"./index-CWz5EflU.js";import"./index-BIU06K05.js";import"./index-6uuHhnjI.js";import"./index-sY83p_TZ.js";import"./index-Dkr9yJHh.js";import"./index-Cr2iZZCw.js";import"./index-Cho7bklK.js";import"./styleUtilities-Byf6Fdv2.js";import"./focusStyle-IgMhxCMT.js";import"./index-DISCH4w8.js";import"./index-DuL23WnM.js";import"./Combination-Dejovr5Y.js";import"./LoadingCircle-BOPZyYIZ.js";import"./ThemeProvider-DsLTl1GE.js";const ie={title:"Components/Label",component:q,tags:["autodocs"],parameters:{status:{type:"released"}},args:{children:"Label"}},a={args:{required:!0},render:t=>{const o=r=>e.jsx(x,{...r});return o.displayName="TextInput",e.jsxs("div",{className:"tw-flex tw-flex-col tw-gap-2",children:[e.jsx(n,{...t,htmlFor:"input-mandatory"}),e.jsx(o,{id:"input-mandatory",placeholder:"Enter your name"})]})}},s={args:{children:"Hello World"},render:t=>{const o=r=>e.jsx(x,{...r});return o.displayName="TextInput",e.jsxs("div",{className:"tw-flex tw-flex-col tw-gap-2",children:[e.jsx(n,{...t,htmlFor:"input-text"}),e.jsx(o,{id:"input-text",placeholder:"Enter your name"})]})}},p={args:{children:"Hello World"},render:t=>{const o=r=>e.jsx(x,{...r});return o.displayName="TextInput",e.jsxs("div",{className:"tw-flex tw-flex-col tw-gap-2",children:[e.jsx(n,{...t,htmlFor:"input-disabled"}),e.jsx(o,{id:"input-disabled",placeholder:"Enter your name",disabled:!0})]})}},i={args:{children:"Hello World"},render:t=>e.jsxs("div",{className:"tw-flex tw-flex-col tw-gap-2",children:[e.jsx(n,{...t,id:"label-select",htmlFor:"select"}),e.jsxs(l,{id:"select","aria-labelledby":"label-select",placeholder:"Select...",children:[e.jsx(l.Item,{value:"1",children:"Option 1"}),e.jsx(l.Item,{value:"2",children:"Option 2"})]})]})},c={args:{children:"Hello World"},render:t=>e.jsxs("div",{className:"tw-flex tw-flex-col tw-gap-2",children:[e.jsx(n,{...t,id:"label-combobox",htmlFor:"combobox"}),e.jsxs(l.Combobox,{id:"combobox","aria-labelledby":"label-combobox",placeholder:"Select...",children:[e.jsx(l.Item,{value:"1",children:"Option 1"}),e.jsx(l.Item,{value:"2",children:"Option 2"})]})]})},d={args:{children:"Hello World"},render:t=>e.jsxs("div",{className:"tw-flex tw-gap-1.5",children:[e.jsx(U,{id:"input-checkbox","aria-labelledby":"label"}),e.jsx(n,{...t,id:"label",htmlFor:"input-checkbox"})]})},m={args:{required:!0},render:t=>{const o=r=>e.jsx(x,{...r});return o.displayName="TextInput",e.jsxs("div",{className:"tw-flex tw-flex-col tw-gap-1.5",children:[e.jsxs(n,{...t,htmlFor:"input-tooltip",children:["Hello World",e.jsxs(u.Root,{children:[e.jsx(u.Trigger,{children:e.jsx(P,{size:16})}),e.jsx(u.Content,{children:"Tooltip"})]})]}),e.jsx(o,{id:"input-tooltip",placeholder:"Enter your name"})]})}};var h,b,I;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    required: true
+  },
+  render: args => {
+    // Used to get the correct component name in the Storybook of the \`TextInput\` (instead of \`TextInput.Root\`)
+    const STextInput = (props: ComponentProps<typeof TextInput>) => <TextInput {...props} />;
+    STextInput.displayName = 'TextInput';
+    return <div className="tw-flex tw-flex-col tw-gap-2">
+                <Label {...args} htmlFor="input-mandatory" />
+                <STextInput id="input-mandatory" placeholder="Enter your name" />
+            </div>;
+  }
+}`,...(I=(b=a.parameters)==null?void 0:b.docs)==null?void 0:I.source}}};var T,g,S;s.parameters={...s.parameters,docs:{...(T=s.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    children: 'Hello World'
+  },
+  render: args => {
+    // Used to get the correct component name in the Storybook of the \`TextInput\` (instead of \`TextInput.Root\`)
+    const STextInput = (props: ComponentProps<typeof TextInput>) => <TextInput {...props} />;
+    STextInput.displayName = 'TextInput';
+    return <div className="tw-flex tw-flex-col tw-gap-2">
+                <Label {...args} htmlFor="input-text" />
+                <STextInput id="input-text" placeholder="Enter your name" />
+            </div>;
+  }
+}`,...(S=(g=s.parameters)==null?void 0:g.docs)==null?void 0:S.source}}};var f,w,y;p.parameters={...p.parameters,docs:{...(f=p.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    children: 'Hello World'
+  },
+  render: args => {
+    // Used to get the correct component name in the Storybook of the \`TextInput\` (instead of \`TextInput.Root\`)
+    const STextInput = (props: ComponentProps<typeof TextInput>) => <TextInput {...props} />;
+    STextInput.displayName = 'TextInput';
+    return <div className="tw-flex tw-flex-col tw-gap-2">
+                <Label {...args} htmlFor="input-disabled" />
+                <STextInput id="input-disabled" placeholder="Enter your name" disabled />
+            </div>;
+  }
+}`,...(y=(w=p.parameters)==null?void 0:w.docs)==null?void 0:y.source}}};var j,v,W;i.parameters={...i.parameters,docs:{...(j=i.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    children: 'Hello World'
+  },
+  render: args => {
+    return <div className="tw-flex tw-flex-col tw-gap-2">
+                <Label {...args} id="label-select" htmlFor="select" />
+                <Select id="select" aria-labelledby="label-select" placeholder="Select...">
+                    <Select.Item value="1">Option 1</Select.Item>
+                    <Select.Item value="2">Option 2</Select.Item>
+                </Select>
+            </div>;
+  }
+}`,...(W=(v=i.parameters)==null?void 0:v.docs)==null?void 0:W.source}}};var N,C,F;c.parameters={...c.parameters,docs:{...(N=c.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    children: 'Hello World'
+  },
+  render: args => {
+    return <div className="tw-flex tw-flex-col tw-gap-2">
+                <Label {...args} id="label-combobox" htmlFor="combobox" />
+                <Select.Combobox id="combobox" aria-labelledby="label-combobox" placeholder="Select...">
+                    <Select.Item value="1">Option 1</Select.Item>
+                    <Select.Item value="2">Option 2</Select.Item>
+                </Select.Combobox>
+            </div>;
+  }
+}`,...(F=(C=c.parameters)==null?void 0:C.docs)==null?void 0:F.source}}};var k,L,H;d.parameters={...d.parameters,docs:{...(k=d.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    children: 'Hello World'
+  },
+  render: args => {
+    // Used to get the correct component name in the Storybook of the \`TextInput\` (instead of \`TextInput.Root\`)
+    const STextInput = (props: ComponentProps<typeof TextInput>) => <TextInput {...props} />;
+    STextInput.displayName = 'TextInput';
+    return <div className="tw-flex tw-gap-1.5">
+                <Checkbox id="input-checkbox" aria-labelledby="label" />
+                <Label {...args} id="label" htmlFor="input-checkbox" />
+            </div>;
+  }
+}`,...(H=(L=d.parameters)==null?void 0:L.docs)==null?void 0:H.source}}};var E,O,R;m.parameters={...m.parameters,docs:{...(E=m.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    required: true
+  },
+  render: args => {
+    // Used to get the correct component name in the Storybook of the \`TextInput\` (instead of \`TextInput.Root\`)
+    const STextInput = (props: ComponentProps<typeof TextInput>) => <TextInput {...props} />;
+    STextInput.displayName = 'TextInput';
+    return <div className="tw-flex tw-flex-col tw-gap-1.5">
+                <Label {...args} htmlFor="input-tooltip">
+                    Hello World
+                    <Tooltip.Root>
+                        <Tooltip.Trigger>
+                            <IconIcon size={16} />
+                        </Tooltip.Trigger>
+                        <Tooltip.Content>Tooltip</Tooltip.Content>
+                    </Tooltip.Root>
+                </Label>
+                <STextInput id="input-tooltip" placeholder="Enter your name" />
+            </div>;
+  }
+}`,...(R=(O=m.parameters)==null?void 0:O.docs)==null?void 0:R.source}}};const ce=["Mandatory","WithTextInput","WithDisabledInput","WithSelect","WithCombobox","WithCheckbox","WithTooltip"];export{a as Mandatory,d as WithCheckbox,c as WithCombobox,p as WithDisabledInput,i as WithSelect,s as WithTextInput,m as WithTooltip,ce as __namedExportsOrder,ie as default};
