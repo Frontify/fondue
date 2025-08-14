c42a5265f14890d477ea142a2a9d46f94cf1a66a
jcosta33 <jcosta33@users.noreply.github.com>
2025-08-14 11:19:03 +0000

    Deploy preview for PR 2377 🛫

---
+++ pr-preview/pr-2377/new/assets/Button.stories-uRx0F4pm.js
@@ -0,0 +1,27 @@
+import{j as e}from"./jsx-runtime-BYYWji4R.js";import"./index-ClcD9ViR.js";import{o as z,c as O}from"./fondue-icons351-C8qTqSxT.js";import{a as j}from"./index-B-lxVbXh.js";import{B as c}from"./Button-DsdYaWQP.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./v4-CtRu48qb.js";import"./styleUtilities-Byf6Fdv2.js";import"./focusStyle-IgMhxCMT.js";const F={title:"Components/Button",component:c,tags:["autodocs"],parameters:{status:{type:"released"}},args:{type:"button",variant:"default",emphasis:"default",size:"medium",rounding:"medium",disabled:!1,hugWidth:!0,children:"Button Text",aspect:"default",onPress:j("onPress")}},n={name:"Icon and Text Label",render:s=>e.jsxs(c,{...s,children:[e.jsx(z,{size:16}),"Button Text"]})},r={},o={name:"Icon Only",args:{aspect:"square",children:e.jsx(O,{size:20})}},t={name:"Icon Only Rounded",args:{rounding:"full",title:"Icon Only Rounded",children:e.jsx(O,{size:20}),aspect:"square"}},a={name:"Icon and Text Label",render:s=>e.jsxs(c,{...s,children:[e.jsx(z,{size:16}),"Button Text"]})};var d,i,u;n.parameters={...n.parameters,docs:{...(d=n.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  name: 'Icon and Text Label',
+  render: args => <Button {...args}>
+            <IconColorFan size={16} />
+            Button Text
+        </Button>
+}`,...(u=(i=n.parameters)==null?void 0:i.docs)==null?void 0:u.source}}};var m,l,p;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:"{}",...(p=(l=r.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var I,x,g;o.parameters={...o.parameters,docs:{...(I=o.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  name: 'Icon Only',
+  args: {
+    aspect: 'square',
+    children: <IconIcon size={20} />
+  }
+}`,...(g=(x=o.parameters)==null?void 0:x.docs)==null?void 0:g.source}}};var h,f,B;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  name: 'Icon Only Rounded',
+  args: {
+    rounding: 'full',
+    title: 'Icon Only Rounded',
+    children: <IconIcon size={20} />,
+    aspect: 'square'
+  }
+}`,...(B=(f=t.parameters)==null?void 0:f.docs)==null?void 0:B.source}}};var T,b,y;a.parameters={...a.parameters,docs:{...(T=a.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  name: 'Icon and Text Label',
+  render: args => <Button {...args}>
+            <IconColorFan size={16} />
+            Button Text
+        </Button>
+}`,...(y=(b=a.parameters)==null?void 0:b.docs)==null?void 0:y.source}}};const P=["Default","TextLabelOnly","WithIcon","WithRoundedIcon","WithIconAndLabel"];export{n as Default,r as TextLabelOnly,o as WithIcon,a as WithIconAndLabel,t as WithRoundedIcon,P as __namedExportsOrder,F as default};
---
+++ pr-preview/pr-2377/new/assets/ColorPickerInput.stories-Dw6PzgNj.js
@@ -0,0 +1,24 @@
+import{j as a}from"./jsx-runtime-BYYWji4R.js";import{d as C,c as n}from"./ColorPicker-D32ZX7fe.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-Q_VQqEV0.js";import"./fondue-icons351-C8qTqSxT.js";import"./Select-C9HQNUPR.js";import"./index-CQF4cXo6.js";import"./index-DulyF36n.js";import"./index-CWz5EflU.js";import"./index-4mPnUTGd.js";import"./index-CMmqAaFT.js";import"./index-BUMxx5F6.js";import"./index-kkVLZR_L.js";import"./index-BZQsQlpo.js";import"./component-D3gEvQxA.js";import"./index-DPfZWLhs.js";import"./LoadingCircle-BOPZyYIZ.js";import"./ThemeProvider-Dbar5Ost.js";import"./TextInput-Cb3qK-GO.js";import"./styleUtilities-Byf6Fdv2.js";const F={title:"Components/ColorPicker/Input",component:C,tags:["autodocs"],parameters:{status:{type:"released"}},args:{}},e={args:{currentColor:{red:230,green:100,blue:50,alpha:.8,name:"custom-color"}},render:r=>a.jsx(n.Input,{...r,onClear:()=>{}})},o={args:{},render:r=>a.jsx(n.Input,{...r,onClear:()=>{}})},t={args:{},render:r=>a.jsx(n.Input,{...r,disabled:!0,onClear:()=>{}})};var s,p,m;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    currentColor: {
+      red: 230,
+      green: 100,
+      blue: 50,
+      alpha: 0.8,
+      name: 'custom-color'
+    }
+  },
+  render: args => {
+    return <ColorPicker.Input {...args} onClear={() => {}} />;
+  }
+}`,...(m=(p=e.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var i,c,u;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <ColorPicker.Input {...args} onClear={() => {}} />;
+  }
+}`,...(u=(c=o.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};var l,d,g;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <ColorPicker.Input {...args} disabled onClear={() => {}} />;
+  }
+}`,...(g=(d=t.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};const G=["Default","Empty","Disabled"];export{e as Default,t as Disabled,o as Empty,G as __namedExportsOrder,F as default};
