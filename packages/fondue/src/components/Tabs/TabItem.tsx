944245ce7bc7504b0b2236a822ad142c2bfa4ec1
José Costa <j.costa.3394@gmail.com>
2025-08-15 09:52:56 +0200

    refactor: clean up unused props in TabItem component
    - Removed unused props from the `TabItemProps` type in the `TabItem` component, enhancing code clarity and maintainability.
    - Streamlined import statements for better organization.

--- packages/fondue/src/components/Tabs/TabItem.tsx
+++ packages/fondue/src/components/Tabs/TabItem.tsx
@@ -1,22 +1,12 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { type ReactElement, type ReactNode, useEffect, useState } from 'react';
-
-import { type BadgeProps } from '@components/Badge';
 import { checkIfContainInteractiveElements } from '@utilities/elements';
 import { merge } from '@utilities/merge';
+import { type ReactElement, type ReactNode, useEffect, useState } from 'react';
 
 export type TabItemProps = {
     id: string;
-    // eslint-disable-next-line @eslint-react/no-unused-props
-    label: string;
-    // eslint-disable-next-line @eslint-react/no-unused-props
-    onClick?: () => void;
     disabled?: boolean;
-    // eslint-disable-next-line @eslint-react/no-unused-props
-    decorator?: ReactElement;
-    // eslint-disable-next-line @eslint-react/no-unused-props
-    badge?: BadgeProps;
     children: ReactNode;
     active?: boolean;
 };
