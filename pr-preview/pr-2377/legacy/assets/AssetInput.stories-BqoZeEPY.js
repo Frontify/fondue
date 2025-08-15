068f7ca18e3bf6431344181359ac3673f0a38681
jcosta33 <jcosta33@users.noreply.github.com>
2025-08-15 08:09:56 +0000

    Deploy preview for PR 2377 🛫

---
+++ pr-preview/pr-2377/legacy/assets/AssetInput.stories-BqoZeEPY.js
@@ -0,0 +1,109 @@
+import{j as K}from"./jsx-runtime-DF2Pcvd1.js";import{A as u,a as B,b as d,E as p,M as g}from"./example-assets-Bs44DRd7.js";import"./index-B2-qRKKC.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./Button-B03MOMoD.js";import"./useButton-BrS_O84a.js";import"./useFocusable-ChjCOJSh.js";import"./useFocusRing-BL9DkcEN.js";import"./useEffectEvent-BOE4tD14.js";import"./chain-z5L_5vtA.js";import"./index-CFX93qP1.js";import"./IconSize-BcCrF_mi.js";import"./focusStyle-BtNTXUM4.js";import"./merge-DI-veSMP.js";import"./useMemoizedId-BB39KVn8.js";import"./ActionMenu-DW22eObv.js";import"./MenuItem-DgG-AEwR.js";import"./IconCaretRight-DMA6RW71.js";import"./IconCheckMark-Db5JnUcj.js";import"./elements-BOYmskOL.js";import"./helper-DOAGjx3a.js";import"./isScrollable-BSEN4xi5.js";import"./FocusScope-BnEotdBu.js";import"./context-CzRDNd1J.js";import"./SelectionManager-CwM1DjHu.js";import"./useControlledState-CmdEzstM.js";import"./Switch-DDWwL_vA.js";import"./InputLabel-w6niovgZ.js";import"./TooltipIcon-CF3uJZkE.js";import"./LegacyTooltip-B35WBZfv.js";import"./useLink-CzmlOd4E.js";import"./usePopper-jmXS5rjc.js";import"./index-DlnOb27V.js";import"./Portal-CuQPhhwF.js";import"./IconExclamationMarkCircle24-DMv0ETG3.js";import"./IconExclamationMarkTriangle-BrhtYKnY.js";import"./IconExclamationMarkTriangle24-DvJpj6jb.js";import"./IconQuestionMarkCircle-DkhS77IB.js";import"./IconCaretDown-CAp4CI-i.js";import"./IconCaretDown16-CINgO7jA.js";import"./useMenuTrigger-Dz6Ie1en.js";import"./useLabels-BhN0CJHD.js";import"./useLocalizedStringFormatter-BbKkG-z0.js";import"./VisuallyHidden-DbOTlmfM.js";import"./useOverlayTriggerState-BBiMiYGb.js";import"./IconMusicNote-BvSzniF8.js";import"./index-D309HAth.js";import"./motion-bD4j9jgw.js";import"./IconCross-DWAMoEGy.js";import"./IconIcon-BnIZz21f.js";import"./IconIcon16-CMDvX-CB.js";import"./IconIcon24-Cbk6AYQl.js";const Js={title:"Components/Asset Input",component:B,tags:["autodocs"],argTypes:{size:{options:Object.values(u),control:{type:"radio"}},onItemClick:{action:"onItemClick",table:{disable:!0}},onMultiAssetClick:{action:"onMultiAssetClick"}},args:{size:u.Small,isLoading:!1,hideSize:!1,hideExtension:!1,numberOfLocations:1}},e=s=>{if(s!=null&&s.actions)for(const H of s.actions)for(const J of H.menuItems)J.onClick=s.onItemClick;return s.isLoading&&s.assets&&(s.assets=void 0),K.jsx(B,{...s})},o=e.bind({});o.argTypes={onUploadClick:{action:"onUploadClick",table:{disable:!0}},onLibraryClick:{action:"onLibraryClick",table:{disable:!0}},size:{table:{disable:!0}},isLoading:{table:{disable:!0}},hideSize:{table:{disable:!0}},hideExtension:{table:{disable:!0}},numberOfLocations:{table:{disable:!0}},onMultiAssetClick:{table:{disable:!0}}};const a=e.bind({});a.argTypes={onUploadClick:{action:"onUploadClick",table:{disable:!0}},size:{table:{disable:!0}},isLoading:{table:{disable:!0}},hideSize:{table:{disable:!0}},hideExtension:{table:{disable:!0}},numberOfLocations:{table:{disable:!0}},onMultiAssetClick:{table:{disable:!0}}};const n=e.bind({});n.argTypes={onLibraryClick:{action:"onLibraryClick",table:{disable:!0}},size:{table:{disable:!0}},isLoading:{table:{disable:!0}},hideSize:{table:{disable:!0}},hideExtension:{table:{disable:!0}},numberOfLocations:{table:{disable:!0}},onMultiAssetClick:{table:{disable:!0}}};const i=e.bind({});i.args={assets:[p[0]],actions:d};const c=e.bind({});c.args={assets:[p[1]],size:u.Large,actions:d};const l=e.bind({});l.args={assets:[g[3]],actions:d};const m=e.bind({});m.args={assets:[g[2]],actions:d};const F={onUploadClick:{action:"onUploadClick",table:{disable:!0}},onLibraryClick:{action:"onLibraryClick",table:{disable:!0}},size:{table:{disable:!0}},isLoading:{table:{disable:!0}},hideSize:{table:{disable:!0}},hideExtension:{table:{disable:!0}}},t=e.bind({});t.args={assets:[p[0],...g.slice(2,4)],numberOfLocations:1};t.argTypes=F;const r=e.bind({});r.args={assets:p,numberOfLocations:2};r.argTypes=F;var b,f,k;o.parameters={...o.parameters,docs:{...(b=o.parameters)==null?void 0:b.docs,source:{originalSource:`args => {
+  if (args?.actions) {
+    for (const block of args.actions) {
+      for (const item of block.menuItems) {
+        item.onClick = args.onItemClick;
+      }
+    }
+  }
+  if (args.isLoading && args.assets) {
+    args.assets = undefined;
+  }
+  return <AssetInput {...args} />;
+}`,...(k=(f=o.parameters)==null?void 0:f.docs)==null?void 0:k.source}}};var I,C,L;a.parameters={...a.parameters,docs:{...(I=a.parameters)==null?void 0:I.docs,source:{originalSource:`args => {
+  if (args?.actions) {
+    for (const block of args.actions) {
+      for (const item of block.menuItems) {
+        item.onClick = args.onItemClick;
+      }
+    }
+  }
+  if (args.isLoading && args.assets) {
+    args.assets = undefined;
+  }
+  return <AssetInput {...args} />;
+}`,...(L=(C=a.parameters)==null?void 0:C.docs)==null?void 0:L.source}}};var A,y,S;n.parameters={...n.parameters,docs:{...(A=n.parameters)==null?void 0:A.docs,source:{originalSource:`args => {
+  if (args?.actions) {
+    for (const block of args.actions) {
+      for (const item of block.menuItems) {
+        item.onClick = args.onItemClick;
+      }
+    }
+  }
+  if (args.isLoading && args.assets) {
+    args.assets = undefined;
+  }
+  return <AssetInput {...args} />;
+}`,...(S=(y=n.parameters)==null?void 0:y.docs)==null?void 0:S.source}}};var h,z,E;i.parameters={...i.parameters,docs:{...(h=i.parameters)==null?void 0:h.docs,source:{originalSource:`args => {
+  if (args?.actions) {
+    for (const block of args.actions) {
+      for (const item of block.menuItems) {
+        item.onClick = args.onItemClick;
+      }
+    }
+  }
+  if (args.isLoading && args.assets) {
+    args.assets = undefined;
+  }
+  return <AssetInput {...args} />;
+}`,...(E=(z=i.parameters)==null?void 0:z.docs)==null?void 0:E.source}}};var x,O,M;c.parameters={...c.parameters,docs:{...(x=c.parameters)==null?void 0:x.docs,source:{originalSource:`args => {
+  if (args?.actions) {
+    for (const block of args.actions) {
+      for (const item of block.menuItems) {
+        item.onClick = args.onItemClick;
+      }
+    }
+  }
+  if (args.isLoading && args.assets) {
+    args.assets = undefined;
+  }
+  return <AssetInput {...args} />;
+}`,...(M=(O=c.parameters)==null?void 0:O.docs)==null?void 0:M.source}}};var T,U,P;l.parameters={...l.parameters,docs:{...(T=l.parameters)==null?void 0:T.docs,source:{originalSource:`args => {
+  if (args?.actions) {
+    for (const block of args.actions) {
+      for (const item of block.menuItems) {
+        item.onClick = args.onItemClick;
+      }
+    }
+  }
+  if (args.isLoading && args.assets) {
+    args.assets = undefined;
+  }
+  return <AssetInput {...args} />;
+}`,...(P=(U=l.parameters)==null?void 0:U.docs)==null?void 0:P.source}}};var _,j,v;m.parameters={...m.parameters,docs:{...(_=m.parameters)==null?void 0:_.docs,source:{originalSource:`args => {
+  if (args?.actions) {
+    for (const block of args.actions) {
+      for (const item of block.menuItems) {
+        item.onClick = args.onItemClick;
+      }
+    }
+  }
+  if (args.isLoading && args.assets) {
+    args.assets = undefined;
+  }
+  return <AssetInput {...args} />;
+}`,...(v=(j=m.parameters)==null?void 0:j.docs)==null?void 0:v.source}}};var X,D,G;t.parameters={...t.parameters,docs:{...(X=t.parameters)==null?void 0:X.docs,source:{originalSource:`args => {
+  if (args?.actions) {
+    for (const block of args.actions) {
+      for (const item of block.menuItems) {
+        item.onClick = args.onItemClick;
+      }
+    }
+  }
+  if (args.isLoading && args.assets) {
+    args.assets = undefined;
+  }
+  return <AssetInput {...args} />;
+}`,...(G=(D=t.parameters)==null?void 0:D.docs)==null?void 0:G.source}}};var R,q,w;r.parameters={...r.parameters,docs:{...(R=r.parameters)==null?void 0:R.docs,source:{originalSource:`args => {
+  if (args?.actions) {
+    for (const block of args.actions) {
+      for (const item of block.menuItems) {
+        item.onClick = args.onItemClick;
+      }
+    }
+  }
+  if (args.isLoading && args.assets) {
+    args.assets = undefined;
+  }
+  return <AssetInput {...args} />;
+}`,...(w=(q=r.parameters)==null?void 0:q.docs)==null?void 0:w.source}}};const Ks=["Placeholder","PlaceholderUploadOnly","PlaceholderLibraryOnly","Image","ImageLibraryLarge","Audio","Icon","MixedAssets","ImageAssets"];export{l as Audio,m as Icon,i as Image,r as ImageAssets,c as ImageLibraryLarge,t as MixedAssets,o as Placeholder,n as PlaceholderLibraryOnly,a as PlaceholderUploadOnly,Ks as __namedExportsOrder,Js as default};
---
+++ pr-preview/pr-2377/legacy/assets/InContextPopover.stories-Inslj_Df.js
@@ -0,0 +1,25 @@
+import{j as t}from"./jsx-runtime-DF2Pcvd1.js";import{r as p}from"./index-B2-qRKKC.js";import{a as l}from"./Box-BP_ud88A.js";import{c as d}from"./Button-B03MOMoD.js";import{D as u}from"./DialogBody-B4WXGxMQ.js";import{D as g}from"./DialogFooter-4tNvP-M2.js";import{D as f}from"./DialogHeader-DcjJgx_D.js";import{P as n}from"./Popover-wclfm0Sa.js";import{I as x}from"./IconJohanna-iR26Notd.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./borderStyle-D9jrV09G.js";import"./merge-DI-veSMP.js";import"./focusStyle-BtNTXUM4.js";import"./input-wAKuAg3x.js";import"./Dropdown-B61-MwLf.js";import"./useButton-BrS_O84a.js";import"./useFocusable-ChjCOJSh.js";import"./useFocusRing-BL9DkcEN.js";import"./useEffectEvent-BOE4tD14.js";import"./chain-z5L_5vtA.js";import"./index-CFX93qP1.js";import"./FocusScope-BnEotdBu.js";import"./useOverlayTriggerState-BBiMiYGb.js";import"./useControlledState-CmdEzstM.js";import"./useMenuTrigger-Dz6Ie1en.js";import"./useLabels-BhN0CJHD.js";import"./useLocalizedStringFormatter-BbKkG-z0.js";import"./context-CzRDNd1J.js";import"./VisuallyHidden-DbOTlmfM.js";import"./helper-DOAGjx3a.js";import"./isScrollable-BSEN4xi5.js";import"./SelectionManager-CwM1DjHu.js";import"./useField-B4NYN7RF.js";import"./useFormValidationState-BwMCpXua.js";import"./usePopper-jmXS5rjc.js";import"./index-DlnOb27V.js";import"./MenuItem-DgG-AEwR.js";import"./IconSize-BcCrF_mi.js";import"./IconCaretRight-DMA6RW71.js";import"./IconCheckMark-Db5JnUcj.js";import"./elements-BOYmskOL.js";import"./LoadingCircle-DyxURJrK.js";import"./Trigger-CHM3HBtN.js";import"./IconCaretDown-CAp4CI-i.js";import"./IconCaretDown16-CINgO7jA.js";import"./IconCross-DWAMoEGy.js";import"./IconExclamationMarkTriangle-BrhtYKnY.js";import"./IconExclamationMarkTriangle24-DvJpj6jb.js";import"./IconTrashBin16-DHeQSCR9.js";import"./IconTrashBin24-D7tnFyiH.js";import"./validation-CJXPAjNB.js";import"./EnablePortalWrapper-C3Ay3AqL.js";import"./Portal-CuQPhhwF.js";import"./useMemoizedId-BB39KVn8.js";import"./FormControl-VeeO6oFR.js";import"./InputLabel-w6niovgZ.js";import"./TooltipIcon-CF3uJZkE.js";import"./LegacyTooltip-B35WBZfv.js";import"./useLink-CzmlOd4E.js";import"./IconExclamationMarkCircle24-DMv0ETG3.js";import"./IconQuestionMarkCircle-DkhS77IB.js";import"./TabItem-DN__oU-9.js";import"./TextInput-CMhZqw9a.js";import"./Tooltip-CBYC6XKv.js";import"./useClickOutside-ChWTKoTa.js";import"./dialog-Dre1eVQK.js";import"./Tree-DPGsNpw7.js";import"./_Uint8Array-CwPxMkg2.js";import"./Flex-DnmUJ_wR.js";const yt={title:"Experimental/Popover/In Context",Component:n,tags:["autodocs"],args:{theme:"light"}},h=c=>{const[i,e]=p.useState(!1),r=p.useRef(null);return t.jsxs("div",{className:"tw-h-32 tw-flex tw-justify-center tw-items-center",children:[t.jsx(d,{icon:t.jsx(x,{}),onClick:()=>e(!i),ref:r,children:"Trigger"}),t.jsx(n,{...c,anchor:r,open:i,handleClose:()=>e(!1),children:t.jsxs(l,{className:"tw-w-[400px]",children:[t.jsx(f,{title:"Title",padding:"compact"}),t.jsx(u,{padding:"compact",children:t.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos explicabo iure placeat quae quidem reiciendis sequi, veniam. Cupiditate eos necessitatibus nemo non obcaecati, quod ullam. Beatae deserunt eum quibusdam ratione."})}),t.jsx(g,{padding:"compact",actionButtons:[{children:"Confirm",onClick:()=>e(!1)}]})]})})]})},o=h.bind({});o.args={};var m,a,s;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`args => {
+  const [isOpen, setIsOpen] = useState(false);
+  const triggerRef = useRef<HTMLButtonElement | null>(null);
+  return <div className="tw-h-32 tw-flex tw-justify-center tw-items-center">
+            <Button icon={<IconJohanna />} onClick={() => setIsOpen(!isOpen)} ref={triggerRef}>
+                Trigger
+            </Button>
+            <Popover {...args} anchor={triggerRef} open={isOpen} handleClose={() => setIsOpen(false)}>
+                <Box className="tw-w-[400px]">
+                    <DialogHeader title="Title" padding="compact"></DialogHeader>
+                    <DialogBody padding="compact">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos explicabo iure placeat quae
+                            quidem reiciendis sequi, veniam. Cupiditate eos necessitatibus nemo non obcaecati, quod
+                            ullam. Beatae deserunt eum quibusdam ratione.
+                        </p>
+                    </DialogBody>
+                    <DialogFooter padding="compact" actionButtons={[{
+          children: 'Confirm',
+          onClick: () => setIsOpen(false)
+        }]}></DialogFooter>
+                </Box>
+            </Popover>
+        </div>;
+}`,...(s=(a=o.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const Ft=["WithHeaderAndFooter"];export{o as WithHeaderAndFooter,Ft as __namedExportsOrder,yt as default};
