c1f86532681d004c9d1a4e8f63a5136191fb5999
Maciej Baruch <maciej.baruch@frontify.com>
2025-08-25 14:33:06 +0200

    fix(Rich Text Editor): Font Color picker floating bar
    CU-869a423yr

--- packages/fondue/src/components/RichTextEditor/Plugins/FontColorPlugin/index.ts
+++ packages/fondue/src/components/RichTextEditor/Plugins/FontColorPlugin/index.ts
@@ -2,8 +2,6 @@
 
 import { createFontColorPlugin } from '@udecode/plate-font';
 
-import { Position } from '@components/RichTextEditor/components/EditorPositioningWrapper';
-
 import { Plugin, type PluginProps } from '../Plugin';
 
 import { FontColorButton } from './FontColorButton';
@@ -12,8 +10,6 @@ export class FontColorPlugin extends Plugin {
     constructor(props?: PluginProps) {
         super('font-color-plugin', { button: FontColorButton, ...props });
     }
-
-    showIn = [Position.BOTTOM, Position.TOP];
 
     plugins = () => [createFontColorPlugin()];
 }
