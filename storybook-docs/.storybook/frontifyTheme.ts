2498b698e191e9f1a1671eb881cfad994815059c
Noah Waldner <noah.waldner@frontify.com>
2025-08-27 14:18:39 +0200

    adjust logo

--- storybook-docs/.storybook/frontifyTheme.ts
+++ storybook-docs/.storybook/frontifyTheme.ts
@@ -9,7 +9,6 @@ export const dark = create({
         'Diatype, Geist, Arial, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
     fontCode: 'Menlo, Courier, monospace',
 
-    brandTitle: 'Fondue Design System - Frontify',
+    brandTitle: `<img src="/img/fondue-logo.png" />`,
     brandUrl: 'https://fondue-components.frontify.com',
-    brandImage: '/img/fondue-logo.png',
 });
