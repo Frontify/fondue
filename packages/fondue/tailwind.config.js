ed59efeeb27563524e5baeeca06cfda26ea21377
Noah Waldner <noah.waldner@frontify.com>
2025-09-01 15:54:27 +0200

    update tree component to new tokens

--- packages/fondue/tailwind.config.js
+++ packages/fondue/tailwind.config.js
@@ -2,11 +2,25 @@
 
 // @ts-expect-error Untyped
 import frontifyTailwindConfig from '@frontify/fondue-tokens/tailwind';
+import resolveConfig from 'tailwindcss/resolveConfig';
 
 import legacyComponentsTailwindConfig from './legacyTokens/legacyTokens.tailwind.config';
 
+const resolvedLegacyConfig = resolveConfig(legacyComponentsTailwindConfig);
+
 export default {
-    presets: [frontifyTailwindConfig, legacyComponentsTailwindConfig],
+    presets: [
+        frontifyTailwindConfig,
+        {
+            ...resolvedLegacyConfig,
+            presets: [frontifyTailwindConfig],
+            theme: {
+                extend: {
+                    ...resolvedLegacyConfig.theme,
+                },
+            },
+        },
+    ],
     content: ['./src/**/*.{js,ts,tsx}'],
     prefix: 'tw-',
 };
