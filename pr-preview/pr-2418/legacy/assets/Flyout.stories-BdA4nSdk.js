2e97f5f19a5344e38df67b172fe5dffa7dd2f0ad
syeo66 <syeo66@users.noreply.github.com>
2025-09-03 05:42:18 +0000

    Deploy preview for PR 2418 🛫

---
+++ pr-preview/pr-2418/legacy/assets/Flyout.stories-BdA4nSdk.js
@@ -0,0 +1,100 @@
+import{j as e}from"./jsx-runtime-DF2Pcvd1.js";import{a as r}from"./chain-z5L_5vtA.js";import{a as v}from"./index-B-lxVbXh.js";import{r as l}from"./index-B2-qRKKC.js";import{a as D,c as fe}from"./Badge-DCrYCtEj.js";import{c as T,a as f,b as g}from"./Button-BhpRDDwF.js";import{D as ge}from"./DatePicker-CCJsnVly.js";import{D as he}from"./Divider-DRaE_xlp.js";import{c as be}from"./Dropdown-CXsLvcYg.js";import{F as S}from"./FormControl-zCS9f8-Z.js";import{L as we}from"./LegacyTextarea-2nOXwQGM.js";import{S as xe}from"./SegmentedControls-D88nu0MT.js";import{T as Ce}from"./TextInput-zlbBzSP9.js";import{a as B}from"./TooltipIcon-m4kEMbWe.js";import{I as ce}from"./IconDotsVertical-C-DZeCM3.js";import{I as N}from"./LegacyTooltip-DIy9iZdG.js";import{I as h}from"./IconIcon-BnIZz21f.js";import{F as ye}from"./focusStyle-BtNTXUM4.js";import{m as Oe}from"./merge-DI-veSMP.js";import{F as k,a as c,b as pe,c as je,d as Ie}from"./Flyout-C92zvh9s.js";import"./useEffectEvent-BOE4tD14.js";import"./v4-CtRu48qb.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./IconCross-DWAMoEGy.js";import"./IconSize-BcCrF_mi.js";import"./colors-cqDZVL9s.js";import"./useButton-CoG0sslk.js";import"./useFocusable-CKeIV6w5.js";import"./useFocusRing-DYm6KDop.js";import"./index-CFX93qP1.js";import"./floating-ui.dom-BeV4s8NL.js";import"./IconCaretRight-DMA6RW71.js";import"./validation-CJXPAjNB.js";import"./IconCaretDown-CAp4CI-i.js";import"./IconCaretDown16-CINgO7jA.js";import"./IconCaretUp-UZkBSE-H.js";import"./FocusScope-A-qD6oFS.js";import"./useOverlayTriggerState-DopQ1ggU.js";import"./useControlledState-CmdEzstM.js";import"./useMenuTrigger-BqhL5dAN.js";import"./useLabels-BhN0CJHD.js";import"./useLocalizedStringFormatter-BbKkG-z0.js";import"./context-CzRDNd1J.js";import"./VisuallyHidden-DtqDAt8u.js";import"./helper-mGz0u3DV.js";import"./isScrollable-BSEN4xi5.js";import"./SelectionManager-CwM1DjHu.js";import"./useField-BDMBLdww.js";import"./useFormValidationState-BMR3-DSm.js";import"./usePopper-jmXS5rjc.js";import"./index-DlnOb27V.js";import"./MenuItem-zhiNABq6.js";import"./IconCheckMark-Db5JnUcj.js";import"./elements-BOYmskOL.js";import"./LoadingCircle-DyxURJrK.js";import"./Trigger-CRKR-lVE.js";import"./IconExclamationMarkTriangle-BrhtYKnY.js";import"./IconExclamationMarkTriangle24-DvJpj6jb.js";import"./IconTrashBin16-DHeQSCR9.js";import"./IconTrashBin24-D7tnFyiH.js";import"./EnablePortalWrapper-C3Ay3AqL.js";import"./Portal-CuQPhhwF.js";import"./useMemoizedId-BB39KVn8.js";import"./InputLabel-b0eYxraM.js";import"./react-textarea-autosize.browser.esm-CqlTlmzP.js";import"./motion-bD4j9jgw.js";import"./IconQuestionMarkCircle-DkhS77IB.js";import"./IconDotsVertical16-BGKyN-O3.js";import"./useLink-isS_9sO9.js";import"./IconExclamationMarkCircle24-DMv0ETG3.js";import"./IconIcon16-CMDvX-CB.js";import"./IconIcon24-Cbk6AYQl.js";import"./getVerticalPositioning-Bdpvu71J.js";import"./useDialog-HjQ--0VR.js";import"./FieldsetHeader-ByU_pc9y.js";import"./Switch-BAZMdmCV.js";import"./IconMinus-Bj705SiY.js";import"./IconMinus16-Dp9IF57y.js";import"./IconPlus-tBL-i4Ty.js";import"./IconPlus16-D2kONyKV.js";const aa={title:"Deprecated/Flyout",component:c,tags:["autodocs"],parameters:{status:{type:"deprecated"},docs:{description:{component:"### This component is deprecated, please use the [new Flyout component](/docs/current_components-flyout--documentation) instead."}}},argTypes:{onCancel:{action:"onCancel",table:{disable:!0}},onOpenChange:{action:"onOpenChange",table:{disable:!0}},placement:{options:Object.values(k),control:{type:"select"},defaultValue:k.BottomLeft},offset:{control:{type:"number"},defaultValue:5}},args:{trigger:e.jsx("span",{className:"tw-flex tw-h-full tw-items-center tw-p-1 tw-rounded tw-bg-black-20 hover:tw-bg-black-30 dark:tw-bg-black-80 dark:hover:tw-bg-black-70",children:e.jsx(ce,{})})}},y=(t=!1,o=!1)=>s=>{const[n,i]=l.useState("a"),[I,me]=l.useState(""),[ue,F]=l.useState(!1);return e.jsxs("div",{className:"dark:tw-text-white",children:[e.jsxs("div",{className:"tw-flex tw-items-center",children:["Some text",e.jsxs(c,{...s,isOpen:ue,onOpenChange:r(s.onOpenChange,F),onCancel:r(s.onCancel,()=>F(!1)),onConfirm:s.onConfirm&&r(s.onConfirm,F),children:[e.jsxs("div",{className:"tw-flex tw-flex-col tw-gap-y-8 tw-p-8",children:[e.jsx(S,{label:{children:"Input Label",htmlFor:"input-id",tooltip:{content:"Input tooltip"}},extra:"Extra Text",children:e.jsx(Ce,{value:I,onChange:me,focusOnMount:o})}),e.jsx(he,{color:Ie,height:je}),e.jsx(S,{label:{children:"SegmentedControls Label",htmlFor:"segmented-controls-id"},children:e.jsx(xe,{activeItemId:n,onChange:i,items:[{id:"a",value:"abc"},{id:"b",value:"def"},{id:"c",value:"ghi"}]})})]}),e.jsx("div",{className:"tw-p-8",children:e.jsx(S,{label:{children:"LegacyTextarea Label",htmlFor:"textarea-id"},children:e.jsx(we,{placeholder:"This is a placeholder"})})})]})]}),e.jsx("div",{children:"Deserunt voluptate deserunt laborum dolor excepteur. Reprehenderit amet cillum ad ut. Magna labore consequat enim tempor amet in qui. In esse proident officia aliquip ea in in nulla aliqua in laborum anim ipsum est."}),t&&Array.from(Array(15).keys()).map(de=>e.jsx("div",{children:"Deserunt voluptate deserunt laborum dolor excepteur. Reprehenderit amet cillum ad ut. Magna labore consequat enim tempor amet in qui. In esse proident officia aliquip ea in in nulla aliqua in laborum anim ipsum est."},`content${de}`))]})},O=y(!1,!1).bind({}),p=y(!1,!1).bind({});p.argTypes={onConfirm:{action:"onConfirm"}};p.args={title:"Header title",decorator:e.jsx(h,{}),onConfirm:v("onConfirm")};const b=y(!1,!1).bind({});b.args={title:"Header title",decorator:e.jsx(h,{}),badges:[{style:D.Positive,children:"Badge 1"},{status:fe.Danger,children:"Badge 2"},{style:D.Danger,icon:e.jsx(h,{}),children:"Badge 3"}]};const Fe=t=>{const[o,a]=l.useState(!1),[s,n]=l.useState();return e.jsx(c,{...t,trigger:({"aria-label":i},I)=>e.jsx(T,{style:g.Default,emphasis:f.Strong,onClick:()=>a(!o),ref:I,"aria-label":i,children:"Button"}),isOpen:o,onOpenChange:r(t.onOpenChange,a),onCancel:r(t.onCancel,()=>a(!1)),children:e.jsxs("div",{className:"tw-p-4",children:[e.jsxs("p",{className:"tw-flex tw-items-center tw-my-2",children:["Label 1",e.jsx("span",{className:"tw-ml-2",children:e.jsx(B,{tooltip:{content:"Lorem ipsum dolor sit amet."},triggerIcon:e.jsx(N,{})})})]}),e.jsx(be,{onChange:i=>console.log(i),activeItemId:"1",menuBlocks:[{id:"block1",menuItems:[{id:"1",title:"Item 1"},{id:"2",title:"Item 2"},{id:"3",title:"Item 3"},{id:"4",title:"Item 4"},{id:"5",title:"Item 5"}]}]}),e.jsxs("p",{className:"tw-flex tw-items-center tw-my-2",children:["Label 2",e.jsx("span",{className:"tw-ml-2",children:e.jsx(B,{tooltip:{content:"Lorem ipsum dolor sit amet."},triggerIcon:e.jsx(N,{})})})]}),e.jsx(ge,{value:s,onChange:n})]})})},m=Fe.bind({});m.args={title:"Header title",decorator:e.jsx(h,{})};m.argTypes={trigger:{table:{disable:!0}},decorator:{table:{disable:!0}}};const Se=t=>{const[o,a]=l.useState(!1);return e.jsxs("div",{className:"dark:tw-text-white",children:[e.jsxs("div",{className:"tw-flex tw-items-center",children:["Some text",e.jsx(c,{...t,isOpen:o,onOpenChange:s=>a(s),fixedFooter:e.jsx(pe,{buttons:[{style:g.Default,emphasis:f.Default,children:"Cancel",onClick:r(v("onCancel"),()=>a(!1))},{style:g.Default,emphasis:f.Strong,children:"Add",onClick:r(v("onConfirm"),()=>a(!1))}]}),children:e.jsx("p",{className:"tw-text-center tw-py-8",children:"Your flyout content with custom footer buttons!"})})]}),e.jsx("div",{children:"Deserunt voluptate deserunt laborum dolor excepteur. Reprehenderit amet cillum ad ut. Magna labore consequat enim tempor amet in qui. In esse proident officia aliquip ea in in nulla aliqua in laborum anim ipsum est."})]})},u=Se.bind({});u.args={title:"Header title",decorator:e.jsx(h,{}),trigger:e.jsx("span",{className:"tw-flex tw-h-full tw-items-center tw-p-1 tw-rounded tw-bg-black-20 hover:tw-bg-black-30 dark:tw-bg-black-80 dark:hover:tw-bg-black-70",children:e.jsx(ce,{})}),legacyFooter:!1};u.argTypes={trigger:{table:{disable:!0}},decorator:{table:{disable:!0}}};const ve=t=>{const[o,a]=l.useState(!1);return e.jsx(c,{...t,trigger:({"aria-label":s},n)=>e.jsx(T,{style:g.Default,emphasis:f.Strong,onClick:()=>a(!o),ref:n,"aria-label":s,children:"Click me"}),isOpen:o,onOpenChange:r(t.onOpenChange,a),onCancel:r(t.onCancel,()=>a(!1)),children:e.jsx("p",{className:"tw-text-center tw-py-8",children:"Flyout Content"})})},w=ve.bind({});w.args={legacyFooter:!1,fixedHeader:e.jsx("div",{className:"tw-py-5 tw-px-8 tw-bg-white dark:tw-bg-black-95 tw-border-b tw-border-b-black-10",children:"Custom Header"}),fixedFooter:e.jsx(pe,{buttons:[{style:g.Default,emphasis:f.Strong,children:"A button"}]})};const ke=t=>{const[o,a]=l.useState(!1);return e.jsx(c,{...t,trigger:(s,n,i)=>e.jsx("div",{...s,ref:n,className:Oe(["tw-border tw-rounded tw-w-[200px] tw-p-2 tw-text-s tw-text-center tw-h-[60px] tw-outline-none tw-items-center tw-flex tw-justify-center",i.isFocusVisible&&ye,i.isPressed&&"tw-bg-black-10"]),children:i.isPressed?"I'm Pressed!":" Accessible custom trigger"}),isOpen:o,onOpenChange:r(t.onOpenChange,a),onCancel:r(t.onCancel,()=>a(!1)),children:e.jsx("p",{className:"tw-text-center tw-py-8",children:"Fun with Flyouts and Buttons!"})})},d=ke.bind({});d.args={title:"Header title",decorator:e.jsx(h,{})};d.argTypes={trigger:{table:{disable:!0}},decorator:{table:{disable:!0}}};const x=y(!1,!1).bind({});x.args={contentMinHeight:200};const Te=t=>{const[o,a]=l.useState(!1);return e.jsx(c,{...t,trigger:({"aria-label":s},n)=>e.jsx("div",{className:"tw-flex tw-justify-center tw-items-center tw-w-full tw-h-screen",children:e.jsx(T,{style:g.Default,emphasis:f.Strong,onClick:()=>a(!o),ref:n,"aria-label":s,children:"Click me"})}),isOpen:o,onOpenChange:r(t.onOpenChange,a),onCancel:r(t.onCancel,()=>a(!1)),children:e.jsx("p",{className:"tw-text-center tw-py-8",children:"Flyout Content"})})},C=Te.bind({});C.args={placement:k.Top,offset:20};const j=y(!0,!0).bind({});var L,W,E;O.parameters={...O.parameters,docs:{...(L=O.parameters)==null?void 0:L.docs,source:{originalSource:"FlyoutTemplate(false, false).bind({})",...(E=(W=O.parameters)==null?void 0:W.docs)==null?void 0:E.source}}};var H,M,A;p.parameters={...p.parameters,docs:{...(H=p.parameters)==null?void 0:H.docs,source:{originalSource:"FlyoutTemplate(false, false).bind({})",...(A=(M=p.parameters)==null?void 0:M.docs)==null?void 0:A.source}}};var q,P,R;b.parameters={...b.parameters,docs:{...(q=b.parameters)==null?void 0:q.docs,source:{originalSource:"FlyoutTemplate(false, false).bind({})",...(R=(P=b.parameters)==null?void 0:P.docs)==null?void 0:R.source}}};var _,V,Y;m.parameters={...m.parameters,docs:{...(_=m.parameters)==null?void 0:_.docs,source:{originalSource:`args => {
+  const [open, setOpen] = useState(false);
+  const [selectedDate, setSelectedDate] = useState<Date | null>();
+  return <Flyout {...args} trigger={({
+    'aria-label': ariaLabel
+  }, ref) => <Button style={ButtonStyle.Default} emphasis={ButtonEmphasis.Strong} onClick={() => setOpen(!open)} ref={ref as MutableRefObject<HTMLButtonElement>} aria-label={ariaLabel}>
+                    Button
+                </Button>} isOpen={open} onOpenChange={chain(args.onOpenChange, setOpen)} onCancel={chain(args.onCancel, () => setOpen(false))}>
+            <div className="tw-p-4">
+                <p className="tw-flex tw-items-center tw-my-2">
+                    Label 1
+                    <span className="tw-ml-2">
+                        <TooltipIcon tooltip={{
+            content: 'Lorem ipsum dolor sit amet.'
+          }} triggerIcon={<IconExclamationMarkCircle />} />
+                    </span>
+                </p>
+                <Dropdown onChange={id => console.log(id)} activeItemId={'1'} menuBlocks={[{
+        id: 'block1',
+        menuItems: [{
+          id: '1',
+          title: 'Item 1'
+        }, {
+          id: '2',
+          title: 'Item 2'
+        }, {
+          id: '3',
+          title: 'Item 3'
+        }, {
+          id: '4',
+          title: 'Item 4'
+        }, {
+          id: '5',
+          title: 'Item 5'
+        }]
+      }]} />
+                <p className="tw-flex tw-items-center tw-my-2">
+                    Label 2
+                    <span className="tw-ml-2">
+                        <TooltipIcon tooltip={{
+            content: 'Lorem ipsum dolor sit amet.'
+          }} triggerIcon={<IconExclamationMarkCircle />} />
+                    </span>
+                </p>
+                <DatePicker value={selectedDate as Date} onChange={setSelectedDate} />
+            </div>
+        </Flyout>;
+}`,...(Y=(V=m.parameters)==null?void 0:V.docs)==null?void 0:Y.source}}};var U,$,G;u.parameters={...u.parameters,docs:{...(U=u.parameters)==null?void 0:U.docs,source:{originalSource:`args => {
+  const [open, setOpen] = useState(false);
+  return <div className="dark:tw-text-white">
+            <div className="tw-flex tw-items-center">
+                Some text
+                <Flyout {...args} isOpen={open} onOpenChange={isOpen => setOpen(isOpen)} fixedFooter={<FlyoutFooter buttons={[{
+        style: ButtonStyle.Default,
+        emphasis: ButtonEmphasis.Default,
+        children: 'Cancel',
+        onClick: chain(action('onCancel'), () => setOpen(false))
+      }, {
+        style: ButtonStyle.Default,
+        emphasis: ButtonEmphasis.Strong,
+        children: 'Add',
+        onClick: chain(action('onConfirm'), () => setOpen(false))
+      }]} />}>
+                    <p className="tw-text-center tw-py-8">Your flyout content with custom footer buttons!</p>
+                </Flyout>
+            </div>
+            <div>
+                Deserunt voluptate deserunt laborum dolor excepteur. Reprehenderit amet cillum ad ut. Magna labore
+                consequat enim tempor amet in qui. In esse proident officia aliquip ea in in nulla aliqua in laborum
+                anim ipsum est.
+            </div>
+        </div>;
+}`,...(G=($=u.parameters)==null?void 0:$.docs)==null?void 0:G.source}}};var z,J,K;w.parameters={...w.parameters,docs:{...(z=w.parameters)==null?void 0:z.docs,source:{originalSource:`args => {
+  const [isOpen, setIsOpen] = useState(false);
+  return <Flyout {...args} trigger={({
+    'aria-label': ariaLabel
+  }, ref) => <Button style={ButtonStyle.Default} emphasis={ButtonEmphasis.Strong} onClick={() => setIsOpen(!isOpen)} ref={ref as MutableRefObject<HTMLButtonElement>} aria-label={ariaLabel}>
+                    Click me
+                </Button>} isOpen={isOpen} onOpenChange={chain(args.onOpenChange, setIsOpen)} onCancel={chain(args.onCancel, () => setIsOpen(false))}>
+            <p className="tw-text-center tw-py-8">Flyout Content</p>
+        </Flyout>;
+}`,...(K=(J=w.parameters)==null?void 0:J.docs)==null?void 0:K.source}}};var Q,X,Z;d.parameters={...d.parameters,docs:{...(Q=d.parameters)==null?void 0:Q.docs,source:{originalSource:`args => {
+  const [open, setOpen] = useState(false);
+  return <Flyout {...args} trigger={(props, ref, state) => <div {...props} ref={ref as MutableRefObject<HTMLDivElement>} className={merge(['tw-border tw-rounded tw-w-[200px] tw-p-2 tw-text-s tw-text-center tw-h-[60px] tw-outline-none tw-items-center tw-flex tw-justify-center', state.isFocusVisible && FOCUS_STYLE, state.isPressed && 'tw-bg-black-10'])}>
+                    {state.isPressed ? "I'm Pressed!" : ' Accessible custom trigger'}
+                </div>} isOpen={open} onOpenChange={chain(args.onOpenChange, setOpen)} onCancel={chain(args.onCancel, () => setOpen(false))}>
+            <p className="tw-text-center tw-py-8">Fun with Flyouts and Buttons!</p>
+        </Flyout>;
+}`,...(Z=(X=d.parameters)==null?void 0:X.docs)==null?void 0:Z.source}}};var ee,te,ae;x.parameters={...x.parameters,docs:{...(ee=x.parameters)==null?void 0:ee.docs,source:{originalSource:"FlyoutTemplate(false, false).bind({})",...(ae=(te=x.parameters)==null?void 0:te.docs)==null?void 0:ae.source}}};var se,oe,re;C.parameters={...C.parameters,docs:{...(se=C.parameters)==null?void 0:se.docs,source:{originalSource:`args => {
+  const [isOpen, setIsOpen] = useState(false);
+  return <Flyout {...args} trigger={({
+    'aria-label': ariaLabel
+  }, ref) => <div className="tw-flex tw-justify-center tw-items-center tw-w-full tw-h-screen">
+                    <Button style={ButtonStyle.Default} emphasis={ButtonEmphasis.Strong} onClick={() => setIsOpen(!isOpen)} ref={ref as MutableRefObject<HTMLButtonElement>} aria-label={ariaLabel}>
+                        Click me
+                    </Button>
+                </div>} isOpen={isOpen} onOpenChange={chain(args.onOpenChange, setIsOpen)} onCancel={chain(args.onCancel, () => setIsOpen(false))}>
+            <p className="tw-text-center tw-py-8">Flyout Content</p>
+        </Flyout>;
+}`,...(re=(oe=C.parameters)==null?void 0:oe.docs)==null?void 0:re.source}}};var ne,ie,le;j.parameters={...j.parameters,docs:{...(ne=j.parameters)==null?void 0:ne.docs,source:{originalSource:"FlyoutTemplate(true, true).bind({})",...(le=(ie=j.parameters)==null?void 0:ie.docs)==null?void 0:le.source}}};const sa=["WithoutHeader","WithOnclick","WithBadges","WithButtonTrigger","WithCustomFooter","WithCustomFooterAndHeader","WithRenderFunctionTrigger","WithContentMinHeight","WithPlacementAndOffset","WithTextInputAutofocusAndPageScrolling"];export{b as WithBadges,m as WithButtonTrigger,x as WithContentMinHeight,u as WithCustomFooter,w as WithCustomFooterAndHeader,p as WithOnclick,C as WithPlacementAndOffset,d as WithRenderFunctionTrigger,j as WithTextInputAutofocusAndPageScrolling,O as WithoutHeader,sa as __namedExportsOrder,aa as default};
