6fdffb3d9a0c8b4249a0e02031ea27946a0eb439
syeo66 <syeo66@users.noreply.github.com>
2025-09-11 08:19:09 +0000

    Deploy preview for PR 2438 🛫

---
+++ pr-preview/pr-2438/new/assets/ColorPickerInput.stories-DciSbwiW.js
@@ -0,0 +1,24 @@
+import{j as a}from"./jsx-runtime-BYYWji4R.js";import{d as C,c as n}from"./ColorPicker-BFZ0LWH5.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-Q_VQqEV0.js";import"./fondue-icons351-BwRByKtE.js";import"./Select-DGvHl0LV.js";import"./index-CnYQXn0Q.js";import"./index-DulyF36n.js";import"./index-CWz5EflU.js";import"./index-JHbJ-wYR.js";import"./index-CMmqAaFT.js";import"./index-BUMxx5F6.js";import"./index-BezEZHzC.js";import"./index-BZQsQlpo.js";import"./component-CwHimZIW.js";import"./LoadingCircle-C0VoU-pr.js";import"./ThemeProvider-C2jQuMgB.js";import"./TextInput-LLK5vTTo.js";import"./styleUtilities-A7Ia-tuh.js";const B={title:"Components/ColorPicker/Input",component:C,tags:["autodocs"],parameters:{status:{type:"released"}},args:{}},e={args:{currentColor:{red:230,green:100,blue:50,alpha:.8,name:"custom-color"}},render:r=>a.jsx(n.Input,{...r,onClear:()=>{}})},o={args:{},render:r=>a.jsx(n.Input,{...r,onClear:()=>{}})},t={args:{},render:r=>a.jsx(n.Input,{...r,disabled:!0,onClear:()=>{}})};var s,p,m;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    currentColor: {
+      red: 230,
+      green: 100,
+      blue: 50,
+      alpha: 0.8,
+      name: 'custom-color'
+    }
+  },
+  render: args => {
+    return <ColorPicker.Input {...args} onClear={() => {}} />;
+  }
+}`,...(m=(p=e.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var c,i,u;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <ColorPicker.Input {...args} onClear={() => {}} />;
+  }
+}`,...(u=(i=o.parameters)==null?void 0:i.docs)==null?void 0:u.source}}};var l,d,g;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <ColorPicker.Input {...args} disabled onClear={() => {}} />;
+  }
+}`,...(g=(d=t.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};const F=["Default","Empty","Disabled"];export{e as Default,t as Disabled,o as Empty,F as __namedExportsOrder,B as default};
---
+++ pr-preview/pr-2438/new/assets/Tag.stories-DHi_61vk.js
@@ -0,0 +1,119 @@
+import{j as e}from"./jsx-runtime-BYYWji4R.js";import"./index-ClcD9ViR.js";import{o as s,e as le}from"./fondue-icons351-BwRByKtE.js";import{a}from"./index-B-lxVbXh.js";import{T as me,a as Te,b as n}from"./Tag-0TqWIi5K.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./v4-CtRu48qb.js";const ye={title:"Components/Tag",component:n,subcomponents:{"Tag.HoverContent":Te,"Tag.SecondaryContent":me},tags:["autodocs"],parameters:{status:{type:"released"}},args:{emphasis:"strong",variant:"default",size:"default",disabled:!1}},o={render:r=>e.jsx(n,{...r,children:"Text"})},t={render:r=>e.jsxs(n,{...r,children:["Text",e.jsx(n.SecondaryContent,{children:"Secondary"})]})},c={args:{onClick:a("onClick")},render:r=>e.jsx(n,{...r,children:"Text"})},i={args:{onDismiss:a("onDismiss")},render:r=>e.jsx(n,{...r,children:"Text"})},d={args:{onAddClick:a("onAddClick")},render:r=>e.jsx(n,{...r,children:"Text"})},g={args:{onClick:a("onClick"),onDismiss:a("onDismiss")},render:r=>e.jsx(n,{...r,children:"Text"})},l={render:r=>e.jsxs(n,{...r,children:[e.jsx(s,{size:"16"}),"Text"]})},m={render:r=>e.jsxs(n,{...r,children:[e.jsx(s,{size:"16"}),"Text",e.jsx(s,{size:"16"})]})},T={args:{onClick:a("onClick")},render:r=>e.jsxs(n,{...r,children:[e.jsx(s,{size:"16"})," Text"]})},x={render:r=>e.jsxs(e.Fragment,{children:["before"," ",e.jsxs(n,{...r,children:[e.jsx(s,{size:"16"})," Text"]})," ","after"]})},p={args:{onClick:a("onClick")},render:r=>e.jsxs(n,{...r,children:["This is a tag!",e.jsx(n.HoverContent,{children:e.jsx("div",{className:"tw-flex tw-w-full tw-justify-center",children:e.jsx(le,{})})})]})},C={render:r=>e.jsxs(n,{...r,children:[e.jsx(n.SecondaryContent,{children:"Before"}),"Main content"]})},u={render:r=>e.jsxs(n,{...r,children:[e.jsx(n.SecondaryContent,{children:"First"}),"Main content",e.jsx(n.SecondaryContent,{children:"Second"})]})},h={args:{onClick:a("onClick")},render:r=>e.jsxs(n,{...r,children:[e.jsx(n.SecondaryContent,{children:"Prefix"}),"Main content",e.jsx(n.HoverContent,{children:e.jsx(le,{})}),e.jsx(n.SecondaryContent,{children:"Suffix"})]})},S={render:r=>e.jsxs("div",{className:"tw-flex tw-flex-nowrap tw-border tw-border-line-mid tw-rounded-medium tw-p-2 tw-gap-2",children:[e.jsxs(n,{...r,children:[e.jsx(s,{size:"16"})," Text"]}),e.jsxs(n,{...r,children:[e.jsx(s,{size:"16"})," Text"]}),e.jsxs(n,{...r,children:[e.jsx(s,{size:"16"})," Text"]}),e.jsxs(n,{...r,children:[e.jsx(s,{size:"16"})," Text"]}),e.jsxs(n,{...r,children:[e.jsx(s,{size:"16"})," Text"]}),e.jsxs(n,{...r,children:[e.jsx(s,{size:"16"})," Text"]})]})};var j,y,f;o.parameters={...o.parameters,docs:{...(j=o.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: args => <Tag {...args}>Text</Tag>
+}`,...(f=(y=o.parameters)==null?void 0:y.docs)==null?void 0:f.source}}};var k,w,z;t.parameters={...t.parameters,docs:{...(k=t.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: args => <Tag {...args}>
+            Text
+            <Tag.SecondaryContent>Secondary</Tag.SecondaryContent>
+        </Tag>
+}`,...(z=(w=t.parameters)==null?void 0:w.docs)==null?void 0:z.source}}};var W,I,v;c.parameters={...c.parameters,docs:{...(W=c.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    onClick: action('onClick')
+  },
+  render: args => <Tag {...args}>Text</Tag>
+}`,...(v=(I=c.parameters)==null?void 0:I.docs)==null?void 0:v.source}}};var D,F,b;i.parameters={...i.parameters,docs:{...(D=i.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    onDismiss: action('onDismiss')
+  },
+  render: args => <Tag {...args}>Text</Tag>
+}`,...(b=(F=i.parameters)==null?void 0:F.docs)==null?void 0:b.source}}};var H,O,A;d.parameters={...d.parameters,docs:{...(H=d.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    onAddClick: action('onAddClick')
+  },
+  render: args => <Tag {...args}>Text</Tag>
+}`,...(A=(O=d.parameters)==null?void 0:O.docs)==null?void 0:A.source}}};var B,M,E;g.parameters={...g.parameters,docs:{...(B=g.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    onClick: action('onClick'),
+    onDismiss: action('onDismiss')
+  },
+  render: args => <Tag {...args}>Text</Tag>
+}`,...(E=(M=g.parameters)==null?void 0:M.docs)==null?void 0:E.source}}};var N,R,L;l.parameters={...l.parameters,docs:{...(N=l.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  render: args => <Tag {...args}>
+            <IconColorFan size="16" />
+            Text
+        </Tag>
+}`,...(L=(R=l.parameters)==null?void 0:R.docs)==null?void 0:L.source}}};var P,_,q;m.parameters={...m.parameters,docs:{...(P=m.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  render: args => <Tag {...args}>
+            <IconColorFan size="16" />
+            Text
+            <IconColorFan size="16" />
+        </Tag>
+}`,...(q=(_=m.parameters)==null?void 0:_.docs)==null?void 0:q.source}}};var G,J,K;T.parameters={...T.parameters,docs:{...(G=T.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  args: {
+    onClick: action('onClick')
+  },
+  render: args => <Tag {...args}>
+            <IconColorFan size="16" /> Text
+        </Tag>
+}`,...(K=(J=T.parameters)==null?void 0:J.docs)==null?void 0:K.source}}};var Q,U,V;x.parameters={...x.parameters,docs:{...(Q=x.parameters)==null?void 0:Q.docs,source:{originalSource:`{
+  render: args => <>
+            before{' '}
+            <Tag {...args}>
+                <IconColorFan size="16" /> Text
+            </Tag>{' '}
+            after
+        </>
+}`,...(V=(U=x.parameters)==null?void 0:U.docs)==null?void 0:V.source}}};var X,Y,Z;p.parameters={...p.parameters,docs:{...(X=p.parameters)==null?void 0:X.docs,source:{originalSource:`{
+  args: {
+    onClick: action('onClick')
+  },
+  render: args => {
+    return <Tag {...args}>
+                This is a tag!
+                <Tag.HoverContent>
+                    <div className="tw-flex tw-w-full tw-justify-center">
+                        <IconDotsHorizontal />
+                    </div>
+                </Tag.HoverContent>
+            </Tag>;
+  }
+}`,...(Z=(Y=p.parameters)==null?void 0:Y.docs)==null?void 0:Z.source}}};var $,ee,re;C.parameters={...C.parameters,docs:{...($=C.parameters)==null?void 0:$.docs,source:{originalSource:`{
+  render: args => {
+    return <Tag {...args}>
+                <Tag.SecondaryContent>Before</Tag.SecondaryContent>
+                Main content
+            </Tag>;
+  }
+}`,...(re=(ee=C.parameters)==null?void 0:ee.docs)==null?void 0:re.source}}};var ne,se,ae;u.parameters={...u.parameters,docs:{...(ne=u.parameters)==null?void 0:ne.docs,source:{originalSource:`{
+  render: args => {
+    return <Tag {...args}>
+                <Tag.SecondaryContent>First</Tag.SecondaryContent>
+                Main content
+                <Tag.SecondaryContent>Second</Tag.SecondaryContent>
+            </Tag>;
+  }
+}`,...(ae=(se=u.parameters)==null?void 0:se.docs)==null?void 0:ae.source}}};var oe,te,ce;h.parameters={...h.parameters,docs:{...(oe=h.parameters)==null?void 0:oe.docs,source:{originalSource:`{
+  args: {
+    onClick: action('onClick')
+  },
+  render: args => {
+    return <Tag {...args}>
+                <Tag.SecondaryContent>Prefix</Tag.SecondaryContent>
+                Main content
+                <Tag.HoverContent>
+                    <IconDotsHorizontal />
+                </Tag.HoverContent>
+                <Tag.SecondaryContent>Suffix</Tag.SecondaryContent>
+            </Tag>;
+  }
+}`,...(ce=(te=h.parameters)==null?void 0:te.docs)==null?void 0:ce.source}}};var ie,de,ge;S.parameters={...S.parameters,docs:{...(ie=S.parameters)==null?void 0:ie.docs,source:{originalSource:`{
+  render: args => <div className="tw-flex tw-flex-nowrap tw-border tw-border-line-mid tw-rounded-medium tw-p-2 tw-gap-2">
+            <Tag {...args}>
+                <IconColorFan size="16" /> Text
+            </Tag>
+            <Tag {...args}>
+                <IconColorFan size="16" /> Text
+            </Tag>
+            <Tag {...args}>
+                <IconColorFan size="16" /> Text
+            </Tag>
+            <Tag {...args}>
+                <IconColorFan size="16" /> Text
+            </Tag>
+            <Tag {...args}>
+                <IconColorFan size="16" /> Text
+            </Tag>
+            <Tag {...args}>
+                <IconColorFan size="16" /> Text
+            </Tag>
+        </div>
+}`,...(ge=(de=S.parameters)==null?void 0:de.docs)==null?void 0:ge.source}}};const fe=["LabelOnly","WithSecondaryContent","WithOnClick","WithDismiss","WithAddable","WithOnClickAndDismiss","WithIcon","WithIconOnBothSides","WithOnClickAndIcon","BetweenElements","WithHoverContent","WithSecondaryContentBefore","WithMultipleSecondaryContent","WithBothSlotTypes","MultipleTagsInRow"];export{x as BetweenElements,o as LabelOnly,S as MultipleTagsInRow,d as WithAddable,h as WithBothSlotTypes,i as WithDismiss,p as WithHoverContent,l as WithIcon,m as WithIconOnBothSides,u as WithMultipleSecondaryContent,c as WithOnClick,g as WithOnClickAndDismiss,T as WithOnClickAndIcon,t as WithSecondaryContent,C as WithSecondaryContentBefore,fe as __namedExportsOrder,ye as default};
