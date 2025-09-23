8565448e1dc10a3f1a9cc496234f1e3f7a4afedf
Noah Waldner <noah.waldner@frontify.com>
2025-09-23 09:56:02 +0200

    remove unused exports

--- packages/fondue/src/hooks/index.ts
+++ packages/fondue/src/hooks/index.ts
@@ -1,6 +1,5 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
 export * from './useCopy';
-export * from './useDebounce';
 export * from './useMemoizedId';
 export * from './useMobileDetection';
