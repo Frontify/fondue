ad9d1c4035c68005cb7c5e4fe8c60ab7b6d7c0ff
syeo66 <syeo66@users.noreply.github.com>
2025-10-06 13:06:42 +0000

    Deploy preview for PR 2460 🛫

---
+++ pr-preview/pr-2460/legacy/assets/Dialog.stories-D_YsnoOP.js
@@ -0,0 +1,64 @@
+import{j as e}from"./jsx-runtime-DF2Pcvd1.js";import{r as l}from"./index-B2-qRKKC.js";import{c as b}from"./Button-BhpRDDwF.js";import{D as x}from"./DialogBody-B4WXGxMQ.js";import{D as I}from"./DialogFooter-D48ewOnA.js";import{I as A}from"./IconJohanna-iR26Notd.js";import{D as O}from"./Dialog-BUnxlUw4.js";import{M as r}from"./dialog-Dre1eVQK.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./useButton-CoG0sslk.js";import"./useFocusable-CKeIV6w5.js";import"./useFocusRing-DYm6KDop.js";import"./useEffectEvent-BOE4tD14.js";import"./chain-z5L_5vtA.js";import"./index-CFX93qP1.js";import"./IconSize-BcCrF_mi.js";import"./focusStyle-BtNTXUM4.js";import"./merge-DI-veSMP.js";import"./Box-CU-CHpjj.js";import"./borderStyle-D9jrV09G.js";import"./input-BLZR8YLK.js";import"./Checkbox-D87_ARjc.js";import"./useFormValidationState-BMR3-DSm.js";import"./useControlledState-CmdEzstM.js";import"./InputLabel-b0eYxraM.js";import"./TooltipIcon-m4kEMbWe.js";import"./LegacyTooltip-DIy9iZdG.js";import"./useLink-isS_9sO9.js";import"./usePopper-jmXS5rjc.js";import"./index-DlnOb27V.js";import"./Portal-CuQPhhwF.js";import"./useMemoizedId-BB39KVn8.js";import"./IconCheckMark-Db5JnUcj.js";import"./IconExclamationMarkCircle24-DMv0ETG3.js";import"./IconExclamationMarkTriangle-BrhtYKnY.js";import"./IconExclamationMarkTriangle24-DvJpj6jb.js";import"./IconQuestionMarkCircle-DkhS77IB.js";import"./IconMinus-DJt4iUyt.js";import"./LoadingCircle-DyxURJrK.js";import"./Switch-BAZMdmCV.js";import"./TabItem-B3Gc2cXC.js";import"./elements-BOYmskOL.js";import"./TextInput-Czpn01XC.js";import"./IconCross-Bq7BKjxu.js";import"./validation-CJXPAjNB.js";import"./Tooltip-zb4ozB6j.js";import"./useClickOutside-ChWTKoTa.js";import"./EnablePortalWrapper-C3Ay3AqL.js";import"./Tree-DPGsNpw7.js";import"./_Uint8Array-CwPxMkg2.js";import"./Trigger-BD4IgEHF.js";import"./IconCaretDown-CAp4CI-i.js";import"./IconCaretDown16-CINgO7jA.js";import"./IconTrashBin16-DHeQSCR9.js";import"./IconTrashBin24-D7tnFyiH.js";import"./Flex-1WJMUSOD.js";const Me={title:"Experimental/Dialog",component:O,tags:["autodocs"],parameters:{status:{type:"deprecated"},docs:{description:{component:"### This component is deprecated, please use the [new Dialog](/docs/current_components-dialog--documentation) component instead."}}},args:{modality:r.Modal,strategy:"absolute",minWidth:600,maxWidth:0,autoHeight:!1,maxHeight:"auto",minHeight:200,darkUnderlay:!1,verticalAlignment:"centered"},argTypes:{enablePortal:{control:{type:"boolean"}},modality:{options:Object.values(r),control:{type:"select"}},strategy:{options:["absolute","fixed"],control:{type:"radio"}},"data-test-id":{type:"string"},role:{type:"string"},darkUnderlay:{type:"boolean"},autoHeight:{type:"boolean"},verticalAlignment:{options:["top","centered"],control:{type:"radio"}},zIndex:{table:{disable:!0}},isDetached:{table:{disable:!0}}}},a=B=>{const[n,s]=l.useState(!1),p=l.useRef(null);return e.jsxs(e.Fragment,{children:[e.jsx(b,{icon:e.jsx(A,{}),ref:p,onClick:()=>s(!n),children:"Dialog Trigger"}),e.jsxs(O,{...B,anchor:p,open:n,handleClose:()=>s(!1),children:[e.jsx(x,{padding:"spacious",children:e.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad illum impedit iure numquam praesentium vel. Distinctio perferendis, suscipit! Dolor doloremque et ex, modi nobis officiis perspiciatis quis tempora temporibus voluptates?"})}),e.jsx(I,{actionButtons:[{children:"Close",onClick:()=>s(!1)}]})]})]})},o=a.bind({});o.args={modality:r.NonModal};const t=a.bind({});t.args={modality:r.Modal};const i=a.bind({});i.args={modality:r.BlockingModal,darkUnderlay:!0};var m,c,d;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`args => {
+  const [isOpen, setIsOpen] = useState(false);
+  const triggerRef = useRef<HTMLButtonElement | null>(null);
+  return <>
+            <Button icon={<IconJohanna />} ref={triggerRef} onClick={() => setIsOpen(!isOpen)}>
+                Dialog Trigger
+            </Button>
+            <Dialog {...args} anchor={triggerRef} open={isOpen} handleClose={() => setIsOpen(false)}>
+                <DialogBody padding="spacious">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad illum impedit iure numquam
+                        praesentium vel. Distinctio perferendis, suscipit! Dolor doloremque et ex, modi nobis officiis
+                        perspiciatis quis tempora temporibus voluptates?
+                    </p>
+                </DialogBody>
+                <DialogFooter actionButtons={[{
+        children: 'Close',
+        onClick: () => setIsOpen(false)
+      }]}></DialogFooter>
+            </Dialog>
+        </>;
+}`,...(d=(c=o.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var u,g,f;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`args => {
+  const [isOpen, setIsOpen] = useState(false);
+  const triggerRef = useRef<HTMLButtonElement | null>(null);
+  return <>
+            <Button icon={<IconJohanna />} ref={triggerRef} onClick={() => setIsOpen(!isOpen)}>
+                Dialog Trigger
+            </Button>
+            <Dialog {...args} anchor={triggerRef} open={isOpen} handleClose={() => setIsOpen(false)}>
+                <DialogBody padding="spacious">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad illum impedit iure numquam
+                        praesentium vel. Distinctio perferendis, suscipit! Dolor doloremque et ex, modi nobis officiis
+                        perspiciatis quis tempora temporibus voluptates?
+                    </p>
+                </DialogBody>
+                <DialogFooter actionButtons={[{
+        children: 'Close',
+        onClick: () => setIsOpen(false)
+      }]}></DialogFooter>
+            </Dialog>
+        </>;
+}`,...(f=(g=t.parameters)==null?void 0:g.docs)==null?void 0:f.source}}};var D,h,y;i.parameters={...i.parameters,docs:{...(D=i.parameters)==null?void 0:D.docs,source:{originalSource:`args => {
+  const [isOpen, setIsOpen] = useState(false);
+  const triggerRef = useRef<HTMLButtonElement | null>(null);
+  return <>
+            <Button icon={<IconJohanna />} ref={triggerRef} onClick={() => setIsOpen(!isOpen)}>
+                Dialog Trigger
+            </Button>
+            <Dialog {...args} anchor={triggerRef} open={isOpen} handleClose={() => setIsOpen(false)}>
+                <DialogBody padding="spacious">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad illum impedit iure numquam
+                        praesentium vel. Distinctio perferendis, suscipit! Dolor doloremque et ex, modi nobis officiis
+                        perspiciatis quis tempora temporibus voluptates?
+                    </p>
+                </DialogBody>
+                <DialogFooter actionButtons={[{
+        children: 'Close',
+        onClick: () => setIsOpen(false)
+      }]}></DialogFooter>
+            </Dialog>
+        </>;
+}`,...(y=(h=i.parameters)==null?void 0:h.docs)==null?void 0:y.source}}};const Re=["AsANonModal","AsAModal","AsABlockingModalWithDarkUnderlay"];export{i as AsABlockingModalWithDarkUnderlay,t as AsAModal,o as AsANonModal,Re as __namedExportsOrder,Me as default};
