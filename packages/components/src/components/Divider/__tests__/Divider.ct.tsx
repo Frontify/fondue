49246bd38664053aec7f3018938cb40aadeeff10
Red Ochsenbein <red.ochsenbein@frontify.com>
2025-10-07 14:26:13 +0200

    tests: fix tests

--- packages/components/src/components/Divider/__tests__/Divider.ct.tsx
+++ packages/components/src/components/Divider/__tests__/Divider.ct.tsx
@@ -56,7 +56,7 @@ test('should render with color x-strong', async ({ mount }) => {
 
 test('should render with direction vertical', async ({ mount }) => {
     const component = await mount(<Divider direction="vertical" />);
-    await expect(component).toHaveClass(/tw-border-r/);
+    await expect(component).toHaveClass(/tw-border-e/);
 });
 
 test('should render with padding none horizontal', async ({ mount }) => {
