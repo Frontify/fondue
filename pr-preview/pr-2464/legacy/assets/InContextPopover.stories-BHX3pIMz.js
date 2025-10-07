fb857ae02d07e2bfea531f283168c33225b53fab
syeo66 <syeo66@users.noreply.github.com>
2025-10-07 08:55:11 +0000

    Deploy preview for PR 2464 🛫

---
+++ pr-preview/pr-2464/legacy/assets/InContextPopover.stories-BHX3pIMz.js
@@ -0,0 +1,25 @@
+import{j as t}from"./jsx-runtime-DF2Pcvd1.js";import{r as p}from"./index-B2-qRKKC.js";import{a as l}from"./Box-DZxvrOue.js";import{c as d}from"./Button-BhpRDDwF.js";import{D as u}from"./DialogBody-B4WXGxMQ.js";import{D as g}from"./DialogFooter-XryC1x6a.js";import{D as f}from"./DialogHeader-dABtHKgX.js";import{P as n}from"./Popover-DT6yK9yu.js";import{I as x}from"./IconJohanna-iR26Notd.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./borderStyle-D9jrV09G.js";import"./merge-DI-veSMP.js";import"./focusStyle-BtNTXUM4.js";import"./input-2aiCCGjN.js";import"./Checkbox-D87_ARjc.js";import"./useFocusable-CKeIV6w5.js";import"./useFocusRing-DYm6KDop.js";import"./useEffectEvent-BOE4tD14.js";import"./chain-z5L_5vtA.js";import"./index-CFX93qP1.js";import"./useFormValidationState-BMR3-DSm.js";import"./useControlledState-CmdEzstM.js";import"./InputLabel-b0eYxraM.js";import"./TooltipIcon-m4kEMbWe.js";import"./LegacyTooltip-DIy9iZdG.js";import"./useLink-isS_9sO9.js";import"./usePopper-jmXS5rjc.js";import"./index-DlnOb27V.js";import"./Portal-CuQPhhwF.js";import"./IconSize-BcCrF_mi.js";import"./useMemoizedId-BB39KVn8.js";import"./IconCheckMark-Db5JnUcj.js";import"./IconExclamationMarkCircle24-DMv0ETG3.js";import"./IconExclamationMarkTriangle-BrhtYKnY.js";import"./IconExclamationMarkTriangle24-DvJpj6jb.js";import"./IconQuestionMarkCircle-DkhS77IB.js";import"./IconMinus-DJt4iUyt.js";import"./Divider-DRaE_xlp.js";import"./Dropdown-BjB9Yz7j.js";import"./useButton-CoG0sslk.js";import"./FocusScope-A-qD6oFS.js";import"./useOverlayTriggerState-DopQ1ggU.js";import"./useMenuTrigger-BqhL5dAN.js";import"./useLabels-BhN0CJHD.js";import"./useLocalizedStringFormatter-BbKkG-z0.js";import"./context-CzRDNd1J.js";import"./VisuallyHidden-DtqDAt8u.js";import"./helper-mGz0u3DV.js";import"./isScrollable-BSEN4xi5.js";import"./SelectionManager-CwM1DjHu.js";import"./useField-BDMBLdww.js";import"./MenuItem-zhiNABq6.js";import"./IconCaretRight-DMA6RW71.js";import"./elements-BOYmskOL.js";import"./LoadingCircle-DyxURJrK.js";import"./Trigger-BD4IgEHF.js";import"./IconCaretDown-CAp4CI-i.js";import"./IconCaretDown16-CINgO7jA.js";import"./IconCross-Bq7BKjxu.js";import"./IconTrashBin16-DHeQSCR9.js";import"./IconTrashBin24-D7tnFyiH.js";import"./validation-CJXPAjNB.js";import"./EnablePortalWrapper-C3Ay3AqL.js";import"./Switch-BAZMdmCV.js";import"./TabItem-B3Gc2cXC.js";import"./TextInput-Czpn01XC.js";import"./Tooltip-zb4ozB6j.js";import"./useClickOutside-ChWTKoTa.js";import"./dialog-Dre1eVQK.js";import"./Tree-DPGsNpw7.js";import"./_Uint8Array-CwPxMkg2.js";import"./Flex-l3rnIP6F.js";const Nt={title:"Experimental/Popover/In Context",Component:n,tags:["autodocs"],args:{theme:"light"}},h=c=>{const[e,i]=p.useState(!1),r=p.useRef(null);return t.jsxs("div",{className:"tw-h-32 tw-flex tw-justify-center tw-items-center",children:[t.jsx(d,{icon:t.jsx(x,{}),onClick:()=>i(!e),ref:r,children:"Trigger"}),t.jsx(n,{...c,anchor:r,open:e,handleClose:()=>i(!1),children:t.jsxs(l,{className:"tw-w-[400px]",children:[t.jsx(f,{title:"Title",padding:"compact"}),t.jsx(u,{padding:"compact",children:t.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos explicabo iure placeat quae quidem reiciendis sequi, veniam. Cupiditate eos necessitatibus nemo non obcaecati, quod ullam. Beatae deserunt eum quibusdam ratione."})}),t.jsx(g,{padding:"compact",actionButtons:[{children:"Confirm",onClick:()=>i(!1)}]})]})})]})},o=h.bind({});o.args={};var m,a,s;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`args => {
+  const [isOpen, setIsOpen] = useState(false);
+  const triggerRef = useRef<HTMLButtonElement | null>(null);
+  return <div className="tw-h-32 tw-flex tw-justify-center tw-items-center">
+            <Button icon={<IconJohanna />} onClick={() => setIsOpen(!isOpen)} ref={triggerRef}>
+                Trigger
+            </Button>
+            <Popover {...args} anchor={triggerRef} open={isOpen} handleClose={() => setIsOpen(false)}>
+                <Box className="tw-w-[400px]">
+                    <DialogHeader title="Title" padding="compact"></DialogHeader>
+                    <DialogBody padding="compact">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos explicabo iure placeat quae
+                            quidem reiciendis sequi, veniam. Cupiditate eos necessitatibus nemo non obcaecati, quod
+                            ullam. Beatae deserunt eum quibusdam ratione.
+                        </p>
+                    </DialogBody>
+                    <DialogFooter padding="compact" actionButtons={[{
+          children: 'Confirm',
+          onClick: () => setIsOpen(false)
+        }]}></DialogFooter>
+                </Box>
+            </Popover>
+        </div>;
+}`,...(s=(a=o.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const Lt=["WithHeaderAndFooter"];export{o as WithHeaderAndFooter,Lt as __namedExportsOrder,Nt as default};
