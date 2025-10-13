1aa695755ac481963fa5be9103a8a0bdf8c342ff
Red Ochsenbein <red.ochsenbein@frontify.com>
2025-10-13 14:57:42 +0200

    feat(Dialog): Add storybook story with ScrollArea

--- packages/components/src/components/Dialog/Dialog.stories.tsx
+++ packages/components/src/components/Dialog/Dialog.stories.tsx
@@ -6,6 +6,7 @@ import { forwardRef, useState } from 'react';
 import { Button } from '../Button/Button';
 import { Flex } from '../Flex/Flex';
 import { Flyout } from '../Flyout/Flyout';
+import { ScrollArea } from '../ScrollArea/ScrollArea';
 import { TextInput } from '../TextInput/TextInput';
 import { Tooltip } from '../Tooltip/Tooltip';
 
@@ -686,4 +687,72 @@ export const WithTooltip: Story = {
             </Dialog.Content>
         </Dialog.Root>
     ),
+};
+
+export const WithScrollArea: Story = {
+    render: (args) => {
+        return (
+            <Dialog.Root>
+                <Dialog.Trigger>
+                    <Button>Open dialog</Button>
+                </Dialog.Trigger>
+                <Dialog.Content {...args}>
+                    <Dialog.Header showCloseButton>
+                        <Dialog.Title>Dialog with ScrollArea</Dialog.Title>
+                    </Dialog.Header>
+                    <Dialog.Body padding="none">
+                        <ScrollArea maxHeight="400px" padding="comfortable">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            <br />
+                            <br />
+                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+                            architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+                            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                            voluptatem sequi nesciunt.
+                            <br />
+                            <br />
+                            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+                            velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam
+                            quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
+                            suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+                            <br />
+                            <br />
+                            Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+                            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et
+                            accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti
+                            atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non
+                            provident.
+                            <br />
+                            <br />
+                            Similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum
+                            fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum
+                            soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere
+                            possimus, omnis voluptas assumenda est, omnis dolor repellendus.
+                            <br />
+                            <br />
+                            Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut
+                            et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a
+                            sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis
+                            doloribus asperiores repellat.
+                            <br />
+                            <br />
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Consequat interdum varius sit amet mattis vulputate enim
+                            nulla aliquet. Eu lobortis elementum nibh tellus molestie nunc non blandit. Massa tincidunt
+                            nunc pulvinar sapien et ligula ullamcorper malesuada proin libero.
+                        </ScrollArea>
+                    </Dialog.Body>
+                    <Dialog.Footer>
+                        <Button emphasis="default">Cancel</Button>
+                        <Button>Submit</Button>
+                    </Dialog.Footer>
+                </Dialog.Content>
+            </Dialog.Root>
+        );
+    },
 };
