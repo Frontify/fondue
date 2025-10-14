879bfcd2541f13716e1773407d6d5f9c0dfb454f
SamuelAlev <SamuelAlev@users.noreply.github.com>
2025-10-14 20:17:15 +0000

    Remove preview for PR 2490 🛬

--- pr-preview/pr-2490/new/assets/client-D4mN_y2B.js
+++
@@ -1,1 +0,0 @@
-import{a as e,o as t,q as r}from"./iframe-B9g9N6v4.js";const c=Object.freeze(Object.defineProperty({__proto__:null,MDXProvider:t,useMDXComponents:r},Symbol.toStringTag,{value:"Module"}));var a,o=e;a=o.createRoot,o.hydrateRoot;export{a as c,c as i};
