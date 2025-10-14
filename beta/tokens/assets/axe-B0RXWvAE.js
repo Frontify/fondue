1aa12fb709fe04a03c3c3c3a4c58747a31186a79
SamuelAlev <samuele1503@gmail.com>
2025-10-14 21:29:15 +0000

    Deploying to gh-pages from @ Frontify/fondue@2bc788ade5ee39f53eab761f8560cce07615a8e4 🚀

--- beta/tokens/assets/axe-B0RXWvAE.js
+++ beta/tokens/assets/axe-B0RXWvAE.js
@@ -1,4 +1,4 @@
-import{g as OT,a as MT}from"./iframe-CgwGmt2P.js";var Ju={exports:{}};/*! axe v4.10.3
+import{g as OT,a as MT}from"./iframe-CIwqHMWf.js";var Ju={exports:{}};/*! axe v4.10.3
  * Copyright (c) 2015 - 2025 Deque Systems, Inc.
  *
  * Your use of this Source Code Form is subject to the terms of the Mozilla Public
--- beta/tokens/assets/client-jvgXhZmi.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./iframe-CgwGmt2P.js";var t,o=r;t=o.createRoot,o.hydrateRoot;export{t as c};
