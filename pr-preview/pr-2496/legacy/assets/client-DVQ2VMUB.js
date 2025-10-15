ebc976fbc76ecfb587e142dea13d43952d0f6bd9
SamuelAlev <SamuelAlev@users.noreply.github.com>
2025-10-15 12:42:52 +0000

    Remove preview for PR 2496 🛬

--- pr-preview/pr-2496/legacy/assets/client-DVQ2VMUB.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-CW7dYZLD.js";var t,o=r;t=o.createRoot,o.hydrateRoot;export{t as c};
--- pr-preview/pr-2496/new/assets/Section.stories-BbjQQEKp.js
+++
@@ -1,5 +0,0 @@
-import{S as a}from"./Section-D-33uP-4.js";import"./iframe-B6_3Xiss.js";import"./preload-helper-6OOq6SSv.js";const l={title:"Layout/Section",component:a,tags:["autodocs"],parameters:{status:{type:"released"}},args:{}},e={args:{children:"Hello World"}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    children: 'Hello World'
-  }
-}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const d=["Default"];export{e as Default,d as __namedExportsOrder,l as default};
