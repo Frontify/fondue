abf4f4513a83ca3ac901f58a559d21574e89252e
syeo66 <syeo66@users.noreply.github.com>
2025-10-24 15:15:25 +0000

    Remove preview for PR 2511 🛬

--- pr-preview/pr-2511/new/assets/LoadingBar.stories-CAYc3KK1.js
+++
@@ -1,5 +0,0 @@
-import{L as d}from"./LoadingBar-DZOZfV5r.js";import"./iframe-a6c4vmT5.js";import"./preload-helper-TvS9Qkvp.js";import"./index-KohtT3UD.js";import"./index-Cs3GP0w6.js";import"./styleUtilities-Byf6Fdv2.js";const f={title:"Components/Loading Bar",component:d,tags:["autodocs"],parameters:{status:{type:"released"}},args:{value:42,max:100,rounded:!0,"aria-label":"Fondue Loading Bar"}},e={},a={args:{value:null}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var s,n,m;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    value: null
-  }
-}`,...(m=(n=a.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const L=["WithDefinedValue","Indeterminate"];export{a as Indeterminate,e as WithDefinedValue,L as __namedExportsOrder,f as default};
