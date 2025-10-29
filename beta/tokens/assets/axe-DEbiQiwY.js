a906186c1120f7645136ccea1dcc90b7ec3d0261
noahwaldner <noahwaldner@users.noreply.github.com>
2025-10-29 14:17:32 +0000

    Deploying to gh-pages from @ Frontify/fondue@a76c4633c16d22d38ee987dc790bec8cfded30b5 🚀

--- beta/tokens/assets/axe-DEbiQiwY.js
+++ beta/tokens/assets/axe-DEbiQiwY.js
@@ -1,4 +1,4 @@
-import{g as OT,a as MT}from"./iframe-CTTEmuf5.js";var Ju={exports:{}};/*! axe v4.10.3
+import{g as OT,a as MT}from"./iframe-Co_i2mUZ.js";var Ju={exports:{}};/*! axe v4.10.3
  * Copyright (c) 2015 - 2025 Deque Systems, Inc.
  *
  * Your use of this Source Code Form is subject to the terms of the Mozilla Public
---
+++ beta/tokens/assets/client-DAea8nNc.js
@@ -0,0 +1,1 @@
+import{b as r}from"./iframe-Co_i2mUZ.js";var t,o=r;t=o.createRoot,o.hydrateRoot;export{t as c};
