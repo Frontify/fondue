74aed4cc298e4173ef2c21710f0ca4abb3b654c0
Red Ochsenbein <red.ochsenbein@frontify.com>
2025-11-03 12:52:44 +0100

    chore: remove import

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -1,7 +1,5 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import './icons.css';
-
 export { IconSize, type FondueIcon, type FondueIconProps, type IconNode } from './createFondueIcon';
 export * from './icons';
 export * as icons from './icons';
