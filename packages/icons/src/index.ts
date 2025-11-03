b7579810f6680180cfbb449e51e212bf077a0a08
Red Ochsenbein <red.ochsenbein@frontify.com>
2025-11-03 10:33:08 +0100

    fix: enforce currentColors in svg when forced-colors=active

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -1,5 +1,7 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
+import './icons.css';
+
 export { IconSize, type FondueIcon, type FondueIconProps, type IconNode } from './createFondueIcon';
 export * from './icons';
 export * as icons from './icons';
