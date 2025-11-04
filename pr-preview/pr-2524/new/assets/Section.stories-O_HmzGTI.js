0d6b2c4a62ba3f6369c4ac57dda7ad0d83f80aa1
syeo66 <syeo66@users.noreply.github.com>
2025-11-04 08:44:25 +0000

    Remove preview for PR 2524 🛬

--- pr-preview/pr-2524/new/assets/Section.stories-O_HmzGTI.js
+++
@@ -1,5 +0,0 @@
-import{S as a}from"./Section-CWjDufpR.js";import"./iframe-BGJ9BajC.js";import"./preload-helper-RiWMN6Y1.js";const l={title:"Layout/Section",component:a,tags:["autodocs"],parameters:{status:{type:"released"}},args:{}},e={args:{children:"Hello World"}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    children: 'Hello World'
-  }
-}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const d=["Default"];export{e as Default,d as __namedExportsOrder,l as default};
