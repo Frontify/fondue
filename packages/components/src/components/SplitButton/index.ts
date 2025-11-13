fca6bddd157bc53e90c1fb8c28ef35cf8114d2e7
Red Ochsenbein <red.ochsenbein@frontify.com>
2025-11-13 09:17:17 +0100

    chore: remove unused file

--- packages/components/src/components/SplitButton/index.ts
+++
@@ -1,4 +0,0 @@
-/* (c) Copyright Frontify Ltd., all rights reserved. */
-
-export { SplitButton, SplitButtonRoot, SplitButtonContent, SplitButtonAction } from './SplitButton';
-export type { SplitButtonProps, SplitButtonContentProps, SplitButtonActionProps } from './SplitButton';
