973cba22125fc6b2819e81a4d0d9d70ca8ad473c
noahwaldner <noahwaldner@users.noreply.github.com>
2025-11-21 15:25:38 +0000

    Deploy preview for PR 2543 🛫

---
+++ pr-preview/pr-2543/new/assets/ColorPickerInput.stories-Cg5g8yj9.js
@@ -0,0 +1,24 @@
+import{j as a}from"./iframe-CZCnSpCL.js";import{d as C,c as n}from"./ColorPicker-BBsfyGAM.js";import"./preload-helper-BPEylczw.js";import"./fondue-icons366-DM0n8-E_.js";import"./useTranslation-D-E1DKFB.js";import"./Button-DeKRqcx-.js";import"./styleUtilities-A7Ia-tuh.js";import"./focusStyle-CSp3JXta.js";import"./Select-B7AQ2ZsW.js";import"./index-BlKctXQg.js";import"./index-BMsU_Mn0.js";import"./index-2vm7WVBB.js";import"./index-Ctmyj9Jr.js";import"./index-D9KV2BPH.js";import"./index-DC3dy5B-.js";import"./index-Duv6v_Oa.js";import"./component-Jev65CEM.js";import"./LoadingCircle-ojeWM9MW.js";import"./TextInput-UZi8bbCZ.js";const A={title:"Components/ColorPicker/Input",component:C,tags:["autodocs"],parameters:{status:{type:"released"}},args:{}},e={args:{currentColor:{red:230,green:100,blue:50,alpha:.8,name:"custom-color"}},render:r=>a.jsx(n.Input,{...r,onClear:()=>{}})},o={args:{},render:r=>a.jsx(n.Input,{...r,onClear:()=>{}})},t={args:{},render:r=>a.jsx(n.Input,{...r,disabled:!0,onClear:()=>{}})};var s,p,m;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    currentColor: {
+      red: 230,
+      green: 100,
+      blue: 50,
+      alpha: 0.8,
+      name: 'custom-color'
+    }
+  },
+  render: args => {
+    return <ColorPicker.Input {...args} onClear={() => {}} />;
+  }
+}`,...(m=(p=e.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var c,i,u;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <ColorPicker.Input {...args} onClear={() => {}} />;
+  }
+}`,...(u=(i=o.parameters)==null?void 0:i.docs)==null?void 0:u.source}}};var l,d,g;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <ColorPicker.Input {...args} disabled onClear={() => {}} />;
+  }
+}`,...(g=(d=t.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};const B=["Default","Empty","Disabled"];export{e as Default,t as Disabled,o as Empty,B as __namedExportsOrder,A as default};
--- pr-preview/pr-2543/new/assets/index-CdIzLRbq.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./iframe-fEyRtEF0.js";var e=t.createContext(void 0);function i(r){const o=t.useContext(e);return r||o||"ltr"}export{i as u};
