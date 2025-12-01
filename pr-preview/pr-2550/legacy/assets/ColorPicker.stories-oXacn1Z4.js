2fc7c10592f65caf4a1d26285482ae6b7293aecb
syeo66 <syeo66@users.noreply.github.com>
2025-12-01 08:39:57 +0000

    Deploy preview for PR 2550 🛫

---
+++ pr-preview/pr-2550/legacy/assets/ColorPicker.stories-oXacn1Z4.js
@@ -0,0 +1,22 @@
+import{r as n,j as y}from"./iframe-BxkmZ_qS.js";import{C as f}from"./colors-cqDZVL9s.js";import{C as F,E as g}from"./example-palettes-B92wBCPh.js";import"./preload-helper-DGVESE2Y.js";import"./SegmentedControls-BfBCtjjx.js";import"./useFocusRing-3sDfkXlZ.js";import"./useEffectEvent-DC2V05mW.js";import"./useFocusable-CMqYS_wN.js";import"./chain-CVnaEK-u.js";import"./index-BHLQBrUq.js";import"./useFormValidationState-W-gQZp2T.js";import"./FocusScope-CjnLqfp2.js";import"./useField-D3FWcFBw.js";import"./useLabels-DlfpMOYu.js";import"./context-Bqa1Eh5k.js";import"./VisuallyHidden-CGNY0Pde.js";import"./useControlledState-D0YF3UCt.js";import"./useMemoizedId-BMYZCDTz.js";import"./focusStyle-Ch_WJ4aJ.js";import"./merge-DI-veSMP.js";import"./proxy-6Mqae5Ms.js";import"./TextInput-LXs-tQDI.js";import"./LoadingCircle-BXjrMe8D.js";import"./IconCheckMark-D276Qa_c.js";import"./IconSize-BcCrF_mi.js";import"./IconCross-Du4TXULc.js";import"./IconExclamationMarkTriangle-C2mrgeE6.js";import"./IconExclamationMarkTriangle24-D7QSRMTV.js";import"./validation-C9S5KgfE.js";import"./LegacyTooltip-NfvcJwD5.js";import"./useLink-C7JKiGPY.js";import"./usePopper-CfpTIbUB.js";import"./index-B1O1MNq5.js";import"./Button-B7-zHvnj.js";import"./useButton-DTKD_JJt.js";import"./Portal-C5SODFR_.js";import"./IconExclamationMarkCircle24-hm6ZOfLp.js";import"./IconMagnifier-DHg3Jdv6.js";import"./IconMagnifier16-DBLIDTrv.js";import"./index-DIuk4B_a.js";import"./Dropdown-rZLafnwN.js";import"./useOverlayTriggerState-CM5LBP8P.js";import"./useMenuTrigger-6RbXtrcu.js";import"./useLocalizedStringFormatter-2U36YzN4.js";import"./helper-C5TgkMkQ.js";import"./keyboard-DgolnSm_.js";import"./SelectionManager-Dh5zjJkk.js";import"./MenuItem-BgqPr_D_.js";import"./IconCaretRight-CKTXvcB3.js";import"./elements-BOYmskOL.js";import"./Trigger-CPFovDQw.js";import"./IconCaretDown-Bwq4qGRm.js";import"./IconCaretDown16-BSKaUddp.js";import"./IconTrashBin16-DbgFiwem.js";import"./IconTrashBin24-3t_bfVUY.js";import"./EnablePortalWrapper-DTqQDG5a.js";const _r={title:"Components/Color Picker",component:F,tags:["autodocs"],args:{currentColor:{red:85,green:102,blue:255}},argTypes:{onSelect:{action:"Select Color"}},parameters:{status:{type:"deprecated"},docs:{description:{component:"### This component is deprecated, please use the [new Color Picker component](/docs/current_components-colorpicker--documentation) instead."}}}},s=e=>{const[x,P]=n.useState(e.currentColor),[k,E]=n.useState(f.Hex);return y.jsx(F,{...e,currentColor:x,currentFormat:k,setFormat:E,onSelect:m=>{e.onSelect(m),P(m)}})},t=s.bind({}),r=s.bind({});r.args={palettes:g,allowCustomColor:!1};const o=s.bind({});o.args={palettes:g};var c,a,l;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`args => {
+  const [selectedColor, setSelectedColor] = useState(args.currentColor);
+  const [currentFormat, setCurrentFormat] = useState(ColorFormat.Hex);
+  return <ColorPicker {...args} currentColor={selectedColor} currentFormat={currentFormat} setFormat={setCurrentFormat} onSelect={color => {
+    args.onSelect(color);
+    setSelectedColor(color);
+  }} />;
+}`,...(l=(a=t.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};var p,i,u;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`args => {
+  const [selectedColor, setSelectedColor] = useState(args.currentColor);
+  const [currentFormat, setCurrentFormat] = useState(ColorFormat.Hex);
+  return <ColorPicker {...args} currentColor={selectedColor} currentFormat={currentFormat} setFormat={setCurrentFormat} onSelect={color => {
+    args.onSelect(color);
+    setSelectedColor(color);
+  }} />;
+}`,...(u=(i=r.parameters)==null?void 0:i.docs)==null?void 0:u.source}}};var C,d,S;o.parameters={...o.parameters,docs:{...(C=o.parameters)==null?void 0:C.docs,source:{originalSource:`args => {
+  const [selectedColor, setSelectedColor] = useState(args.currentColor);
+  const [currentFormat, setCurrentFormat] = useState(ColorFormat.Hex);
+  return <ColorPicker {...args} currentColor={selectedColor} currentFormat={currentFormat} setFormat={setCurrentFormat} onSelect={color => {
+    args.onSelect(color);
+    setSelectedColor(color);
+  }} />;
+}`,...(S=(d=o.parameters)==null?void 0:d.docs)==null?void 0:S.source}}};const br=["OnlyCustomColor","OnlyBrandColor","WithBrandColors"];export{r as OnlyBrandColor,t as OnlyCustomColor,o as WithBrandColors,br as __namedExportsOrder,_r as default};
---
+++ pr-preview/pr-2550/new/assets/index-B4xEGM-t.js
@@ -0,0 +1,1 @@
+import{r as t}from"./iframe-u58m5pFi.js";var e=t.createContext(void 0);function i(r){const o=t.useContext(e);return r||o||"ltr"}export{i as u};
