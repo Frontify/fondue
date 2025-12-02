8074ad84822751f87003845a8e63af8568f4f067
noahwaldner <noahwaldner@users.noreply.github.com>
2025-12-02 10:22:51 +0000

    Deploy preview for PR 2551 🛫

---
+++ pr-preview/pr-2551/new/assets/LoadingBar.stories-Crqt4nNy.js
@@ -0,0 +1,5 @@
+import{L as d}from"./LoadingBar-DxQY9SRT.js";import"./iframe-UpR0Ys68.js";import"./preload-helper-Dxe1e-6H.js";import"./index-CQHC8u3b.js";import"./index-D_HzTJg8.js";import"./styleUtilities-A7Ia-tuh.js";const f={title:"Components/Loading Bar",component:d,tags:["autodocs"],parameters:{status:{type:"released"}},args:{value:42,max:100,rounded:!0,"aria-label":"Fondue Loading Bar"}},e={},a={args:{value:null}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var s,n,m;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    value: null
+  }
+}`,...(m=(n=a.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const L=["WithDefinedValue","Indeterminate"];export{a as Indeterminate,e as WithDefinedValue,L as __namedExportsOrder,f as default};
