d58ec15c70e3d7b94d4b982005526502ba43fe11
noahwaldner <noahwaldner@users.noreply.github.com>
2025-12-03 16:52:04 +0000

    Remove preview for PR 2555 🛬

--- pr-preview/pr-2555/new/assets/Section.stories-CQQG_BSA.js
+++
@@ -1,5 +0,0 @@
-import{S as a}from"./Section-C9hs-xuf.js";import"./iframe-_YkFcI3W.js";import"./preload-helper-DavUUsB2.js";const l={title:"Layout/Section",component:a,tags:["autodocs"],parameters:{status:{type:"released"}},args:{}},e={args:{children:"Hello World"}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    children: 'Hello World'
-  }
-}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const d=["Default"];export{e as Default,d as __namedExportsOrder,l as default};
--- pr-preview/pr-2555/new/assets/useSyncRefs-Lga0M2Pk.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./iframe-_YkFcI3W.js";import{s as e}from"./domUtilities-DGDVwu3_.js";const c=(s,r)=>{t.useEffect(()=>{e(s,r)},[s])};export{c as u};
