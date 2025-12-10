b19d61a3cba3a3eae800c3d0e625895874923620
syeo66 <red@redochsenbein.ch>
2025-12-10 12:11:17 +0000

    Deploying to gh-pages from @ Frontify/fondue@cfc11424a43ecb18547f4c6035c4dd56cc351799 🚀

--- next/tokens/assets/client-CNAj9-Ii.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./iframe-BabAiq9q.js";var t,o=r;t=o.createRoot,o.hydrateRoot;export{t as c};
---
+++ next/tokens/assets/client-CiEQUdq6.js
@@ -0,0 +1,1 @@
+import{b as r}from"./iframe-Cg3KPH10.js";var t,o=r;t=o.createRoot,o.hydrateRoot;export{t as c};
