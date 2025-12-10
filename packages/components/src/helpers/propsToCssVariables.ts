35509408b64b6efa6cb06ca1d7c48269fe2f9177
Red Ochsenbein <red.ochsenbein@frontify.com>
2025-12-10 12:45:44 +0100

    fix(LayoutComponents): introduce explodable layout props (#2569)

--- packages/components/src/helpers/propsToCssVariables.ts
+++ packages/components/src/helpers/propsToCssVariables.ts
@@ -30,6 +30,10 @@ const abbreviationToCssProperty: Record<string, string> = {
     rows: 'grid-template-rows',
 };
 
+const explodeProps: Record<string, string[]> = {
+    overflow: ['overflow-x', 'overflow-y'],
+};
+
 const transformValueBasedOnKey = (key: string, value: string | number): string | number => {
     if (key === 'columns' || key === 'rows') {
         if (typeof value === 'number') {
@@ -50,32 +54,44 @@ export const propsToCssVariables = (
     props: Record<string, string | number | boolean | { [key in Breakpoint]?: string | number | boolean }>,
     extraAbbreviationToCssProperty: Record<string, string> = {},
 ): CSSProperties => {
-    return Object.entries(props).reduce((acc, [key, value]) => {
-        if (isAriaProp(key)) {
-            return acc;
-        }
-
-        const cssProperty =
-            key in extraAbbreviationToCssProperty
-                ? extraAbbreviationToCssProperty[key]
-                : key in abbreviationToCssProperty
-                  ? abbreviationToCssProperty[key]
-                  : key;
-        // The lookahead is necessary to split camelCase boundaries; disabling false-positive lint warning
-        const cssPropertyKebabCase = cssProperty?.replaceAll(/([\da-z]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase();
-
-        if (typeof value === 'object') {
-            for (const [breakpoint, breakpointValue] of Object.entries(value)) {
-                if (breakpointValue !== undefined) {
-                    // @ts-expect-error CSS variables are not typed into `CSSProperties`
-                    acc[`--${breakpoint}-${cssPropertyKebabCase}`] = transformValueBasedOnKey(key, breakpointValue);
-                }
+    return Object.entries(props)
+        .reduce<[keyof typeof props, (typeof props)[keyof typeof props]][]>((acc, [key, value]) => {
+            if (!(key in explodeProps)) {
+                return [...acc, [key, value]];
             }
-        } else {
-            // @ts-expect-error CSS variables are not typed into `CSSProperties`
-            acc[`--${cssPropertyKebabCase}`] = transformValueBasedOnKey(key, value);
-        }
 
-        return acc;
-    }, {} as CSSProperties);
+            acc.push([key, value]);
+            for (const newKey of explodeProps[key] ?? []) {
+                acc.push([newKey, value]);
+            }
+
+            return acc;
+        }, [])
+        .reduce<CSSProperties>((acc, [key, value]) => {
+            if (isAriaProp(key)) {
+                return acc;
+            }
+
+            const cssProperty =
+                key in extraAbbreviationToCssProperty
+                    ? extraAbbreviationToCssProperty[key]
+                    : key in abbreviationToCssProperty
+                      ? abbreviationToCssProperty[key]
+                      : key;
+            // The lookahead is necessary to split camelCase boundaries; disabling false-positive lint warning
+            const cssPropertyKebabCase = cssProperty?.replaceAll(/([\da-z]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase();
+
+            if (typeof value === 'object') {
+                for (const [breakpoint, breakpointValue] of Object.entries(value)) {
+                    if (breakpointValue !== undefined) {
+                        // @ts-expect-error CSS variables are not typed into `CSSProperties`
+                        acc[`--${breakpoint}-${cssPropertyKebabCase}`] = transformValueBasedOnKey(key, breakpointValue);
+                    }
+                }
+            } else {
+                // @ts-expect-error CSS variables are not typed into `CSSProperties`
+                acc[`--${cssPropertyKebabCase}`] = transformValueBasedOnKey(key, value);
+            }
+            return acc;
+        }, {});
 };
