4335b56822604e5ac8824c3890bf370a5c2ecb35
syeo66 <syeo66@users.noreply.github.com>
2025-12-15 09:22:02 +0000

    Deploy preview for PR 2572 🛫

---
+++ pr-preview/pr-2572/new/assets/ColorPickerInput.stories-p8sZ__oM.js
@@ -0,0 +1,24 @@
+import{j as a}from"./iframe-qSzOjRx8.js";import{d as C,c as n}from"./ColorPicker-dS1ojS6M.js";import"./preload-helper-5fg-36yc.js";import"./fondue-icons369-Bhn_6moN.js";import"./useTranslation-DaSoEDyg.js";import"./Button-BpOHxnJC.js";import"./Select-DGIfzfw0.js";import"./index-HBFF1xOS.js";import"./index-IqlIgUk0.js";import"./index-CiTxdX5Q.js";import"./index-tl5DSJtO.js";import"./index-D8trQ_Cz.js";import"./index-DM9ZCYHn.js";import"./index-CB92suUH.js";import"./component-Baic9kSc.js";import"./LoadingCircle-C1Sg6Scr.js";import"./TextInput-BLtvGyqd.js";import"./styleUtilities-A7Ia-tuh.js";const z={title:"Components/ColorPicker/Input",component:C,tags:["autodocs"],parameters:{status:{type:"released"}},args:{}},e={args:{currentColor:{red:230,green:100,blue:50,alpha:.8,name:"custom-color"}},render:r=>a.jsx(n.Input,{...r,onClear:()=>{}})},o={args:{},render:r=>a.jsx(n.Input,{...r,onClear:()=>{}})},t={args:{},render:r=>a.jsx(n.Input,{...r,disabled:!0,onClear:()=>{}})};var s,p,m;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    currentColor: {
+      red: 230,
+      green: 100,
+      blue: 50,
+      alpha: 0.8,
+      name: 'custom-color'
+    }
+  },
+  render: args => {
+    return <ColorPicker.Input {...args} onClear={() => {}} />;
+  }
+}`,...(m=(p=e.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var c,i,u;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <ColorPicker.Input {...args} onClear={() => {}} />;
+  }
+}`,...(u=(i=o.parameters)==null?void 0:i.docs)==null?void 0:u.source}}};var l,d,g;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <ColorPicker.Input {...args} disabled onClear={() => {}} />;
+  }
+}`,...(g=(d=t.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};const A=["Default","Empty","Disabled"];export{e as Default,t as Disabled,o as Empty,A as __namedExportsOrder,z as default};
