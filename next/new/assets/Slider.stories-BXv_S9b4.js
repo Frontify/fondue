bff5d3cbc9ecdb8ba8964f360a0126255f03fa5c
syeo66 <red@redochsenbein.ch>
2025-12-15 14:07:50 +0000

    Deploying to gh-pages from @ Frontify/fondue@e9f4ebd640f775c5813bf77d4fe038bd18dd674f 🚀

---
+++ next/new/assets/Slider.stories-BXv_S9b4.js
@@ -0,0 +1,90 @@
+import{r as b,j as a}from"./iframe-DO8Sp0Kx.js";import{L as J}from"./Label-BbldnU4h.js";import{T as K}from"./TextInput-BLfnERft.js";import{S as f}from"./Slider-Cd3hLtLQ.js";import"./preload-helper-mNEUNaaY.js";import"./index-CP-yyu_7.js";import"./styleUtilities-A7Ia-tuh.js";import"./fondue-icons369-D-31d5yw.js";import"./LoadingCircle--y8jLHTL.js";import"./index-BdQq_4o_.js";import"./index-Cw_iJPO_.js";import"./index-d0SD19xQ.js";import"./index-hFXZkLQg.js";import"./index-D5--3XIJ.js";import"./index-Bj138pl5.js";import"./index-yR1no-ln.js";const ce={title:"Components/Slider",component:f,tags:["autodocs"],parameters:{status:{type:"released"}},argTypes:{defaultValue:{control:"object",description:"The default value of the slider, used for uncontrolled components.",defaultValue:[0],table:{type:{summary:"number[]"},defaultValue:{summary:"[0]"}}},value:{control:"object",description:"The controlled value of the slider.",defaultValue:[0],table:{type:{summary:"number[]"},defaultValue:{summary:"[0]"}}},min:{control:"number",description:"Minimum value of the slider.",defaultValue:0,table:{type:{summary:"number"},defaultValue:{summary:"0"}}},max:{control:"number",description:"Maximum value of the slider.",defaultValue:100,table:{type:{summary:"number"},defaultValue:{summary:"100"}}},step:{control:"number",description:"The granularity with which the slider can step through values.",defaultValue:1,table:{type:{summary:"number"},defaultValue:{summary:"1"}}},minStepsBetweenThumbs:{control:"number",description:"The minimum steps between thumbs in a range slider.",defaultValue:0,table:{type:{summary:"number"},defaultValue:{summary:"0"}}},disabled:{control:"boolean",description:"Disable the slider.",defaultValue:!1,table:{type:{summary:"boolean"},defaultValue:{summary:"false"}}},onChange:{action:"onChange",description:"Function called when the value changes.",table:{type:{summary:"(value: number[]) => void"}}},onCommit:{action:"onCommit",description:"Function called when the value change is committed.",table:{type:{summary:"(value: number[]) => void"}}}},args:{min:0,max:100}},o={args:{name:"Simple slider"}},u={args:{disabled:!0,defaultValue:[50]}},i={args:{min:50,max:1e3}},m={args:{minStepsBetweenThumbs:25}},d={args:{step:25}},c={args:{defaultValue:[33,67]}},p={render:n=>{const[t,l]=b.useState([50]),e=b.useRef(!1);return a.jsx(f,{onChange:r=>{r[0]&&(r[0]>80&&!e.current&&(e.current=!0,alert("You're going a bit high there")),l(r))},value:t,...n})}},g={render:n=>{const[t,l]=b.useState([33,67]);return a.jsx(f,{onChange:e=>{if(!(!e[0]||!e[1])){if(e[1]-e[0]>80){alert("The range is too large");return}l(e)}},value:t,...n})}},h={decorators:n=>a.jsx("div",{style:{display:"flex",gap:16},children:a.jsx(n,{})}),render:()=>{const[n,t]=b.useState([250,450]),l=r=>{t(r)},e=r=>s=>{const y=parseInt(s.target.value,10);y>=200&&y<=500&&t(q=>{const S=[...q];return S[r]=y,S})};return a.jsxs(a.Fragment,{children:[a.jsx(J,{id:"new-slider-label",htmlFor:"new-slider",children:"Price range"}),a.jsx(f,{id:"new-slider","aria-labelledby":"new-slider-label",value:n,min:200,max:500,onChange:l}),n.map((r,s)=>a.jsx(K,{id:`new-slider-input-${s}`,value:r,type:"number","aria-label":`Price range ${s===0?"from":"to"}`,onChange:e(s)},s))]})}};var v,x,w;o.parameters={...o.parameters,docs:{...(v=o.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    name: 'Simple slider'
+  }
+}`,...(w=(x=o.parameters)==null?void 0:x.docs)==null?void 0:w.source}}};var R,C,V;u.parameters={...u.parameters,docs:{...(R=u.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    defaultValue: [50]
+  }
+}`,...(V=(C=u.parameters)==null?void 0:C.docs)==null?void 0:V.source}}};var T,I,M;i.parameters={...i.parameters,docs:{...(T=i.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    min: 50,
+    max: 1000
+  }
+}`,...(M=(I=i.parameters)==null?void 0:I.docs)==null?void 0:M.source}}};var j,E,W;m.parameters={...m.parameters,docs:{...(j=m.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    minStepsBetweenThumbs: 25
+  }
+}`,...(W=(E=m.parameters)==null?void 0:E.docs)==null?void 0:W.source}}};var F,L,P;d.parameters={...d.parameters,docs:{...(F=d.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    step: 25
+  }
+}`,...(P=(L=d.parameters)==null?void 0:L.docs)==null?void 0:P.source}}};var $,B,D;c.parameters={...c.parameters,docs:{...($=c.parameters)==null?void 0:$.docs,source:{originalSource:`{
+  args: {
+    defaultValue: [33, 67]
+  }
+}`,...(D=(B=c.parameters)==null?void 0:B.docs)==null?void 0:D.source}}};var z,A,G;p.parameters={...p.parameters,docs:{...(z=p.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  render: args => {
+    const [sliderRange, setSliderRange] = useState([50]);
+    const userIsWarned = useRef(false);
+    return <Slider onChange={value => {
+      if (!value[0]) {
+        return;
+      }
+      if (value[0] > 80 && !userIsWarned.current) {
+        userIsWarned.current = true;
+        alert("You're going a bit high there");
+      }
+      setSliderRange(value);
+    }} value={sliderRange} {...args} />;
+  }
+}`,...(G=(A=p.parameters)==null?void 0:A.docs)==null?void 0:G.source}}};var U,Y,_;g.parameters={...g.parameters,docs:{...(U=g.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  render: args => {
+    const [sliderRange, setSliderRange] = useState([33, 67]);
+    return <Slider onChange={value => {
+      if (!value[0] || !value[1]) {
+        return;
+      }
+      if (value[1] - value[0] > 80) {
+        alert('The range is too large');
+        return;
+      }
+      setSliderRange(value);
+    }} value={sliderRange} {...args} />;
+  }
+}`,...(_=(Y=g.parameters)==null?void 0:Y.docs)==null?void 0:_.source}}};var k,H,O;h.parameters={...h.parameters,docs:{...(k=h.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  decorators: Story => {
+    return <div style={{
+      display: 'flex',
+      gap: 16
+    }}>
+                <Story />
+            </div>;
+  },
+  render: () => {
+    const [range, setRange] = useState([250, 450]);
+    const onRangeChange = (value: number[]) => {
+      setRange(value);
+    };
+    const onInputChange = (index: number) => (event: ChangeEvent<HTMLInputElement>) => {
+      const newValue = parseInt(event.target.value, 10);
+      if (newValue >= 200 && newValue <= 500) {
+        setRange(prev => {
+          const newRange = [...prev];
+          newRange[index] = newValue;
+          return newRange;
+        });
+      }
+    };
+    return <>
+                <Label id="new-slider-label" htmlFor="new-slider">
+                    Price range
+                </Label>
+
+                <Slider id="new-slider" aria-labelledby="new-slider-label" value={range} min={200} max={500} onChange={onRangeChange} />
+
+                {range.map((value, index) => <TextInput key={index} id={\`new-slider-input-\${index}\`} value={value} type="number" aria-label={\`Price range \${index === 0 ? 'from' : 'to'}\`} onChange={onInputChange(index)} />)}
+            </>;
+  }
+}`,...(O=(H=h.parameters)==null?void 0:H.docs)==null?void 0:O.source}}};const pe=["SimpleSlider","Disabled","MinimumAndMaximum","MinimumGap","StepSize","MultipleThumbsUncontrolled","ExternallyControlled","MultipleThumbsExternallyControlled","WithInputs"];export{u as Disabled,p as ExternallyControlled,i as MinimumAndMaximum,m as MinimumGap,g as MultipleThumbsExternallyControlled,c as MultipleThumbsUncontrolled,o as SimpleSlider,d as StepSize,h as WithInputs,pe as __namedExportsOrder,ce as default};
---
+++ next/new/assets/Textarea.stories-B8DKXZ8U.js
@@ -0,0 +1,29 @@
+import{j as e}from"./iframe-DO8Sp0Kx.js";import{p as j,q as b}from"./fondue-icons369-D-31d5yw.js";import{T as R,a as C,b as n}from"./Textarea-D9l8iDmO.js";import"./preload-helper-mNEUNaaY.js";import"./useSyncRefs-BjzWeHJ2.js";import"./domUtilities-DGDVwu3_.js";import"./styleUtilities-A7Ia-tuh.js";const{action:r}=__STORYBOOK_MODULE_ACTIONS__,I={title:"Components/Textarea",component:C,subcomponents:{"Textarea.Slot":R},tags:["autodocs"],parameters:{status:{type:"released"}},args:{autocomplete:!1,autosize:!1,clearable:!1,defaultValue:"Hello from Frontify",disabled:!1,focusOnMount:!1,minRows:1,maxRows:void 0,onBlur:r("onBlur"),onChange:r("onChange"),onEnterPressed:r("onEnterPressed"),onFocus:r("onFocus"),placeholder:"Placeholder",readOnly:!1,resizable:!1,selectable:!0,value:void 0},render:l=>{const c=E=>e.jsx(n,{...E});return c.displayName="Textarea",e.jsx(c,{...l})}},o={},t={args:{autosize:!0,decorator:e.jsx(j,{size:16}),placeholder:"Enter some long form text here"}},a={args:{placeholder:"Enter some long form text here",required:!0}},s={args:{placeholder:"Enter some long form text here",autosize:!0},render:l=>e.jsxs(n.Root,{...l,children:[e.jsx(n.Slot,{name:"left",children:e.jsx(j,{size:16})}),e.jsx(n.Slot,{name:"right",children:e.jsx("button",{onClick:()=>alert("Action clicked!"),style:{cursor:"pointer"},children:e.jsx(b,{size:16})})})]})};var i,m,u;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:"{}",...(u=(m=o.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};var d,p,x;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    autosize: true,
+    decorator: <IconNook size={16} />,
+    placeholder: 'Enter some long form text here'
+  }
+}`,...(x=(p=t.parameters)==null?void 0:p.docs)==null?void 0:x.source}}};var h,f,g;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Enter some long form text here',
+    required: true
+  }
+}`,...(g=(f=a.parameters)==null?void 0:f.docs)==null?void 0:g.source}}};var S,T,z;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Enter some long form text here',
+    autosize: true
+  },
+  render: args => <Textarea.Root {...args}>
+            <Textarea.Slot name="left">
+                <IconNook size={16} />
+            </Textarea.Slot>
+            <Textarea.Slot name="right">
+                <button onClick={() => alert('Action clicked!')} style={{
+        cursor: 'pointer'
+      }}>
+                    <IconClipboard size={16} />
+                </button>
+            </Textarea.Slot>
+        </Textarea.Root>
+}`,...(z=(T=s.parameters)==null?void 0:T.docs)==null?void 0:z.source}}};const N=["Default","WithDecoratorAndAutosize","Required","WithSlots"];export{o as Default,a as Required,t as WithDecoratorAndAutosize,s as WithSlots,N as __namedExportsOrder,I as default};
