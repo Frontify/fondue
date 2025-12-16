12275f29a3b29adf7311760f11ed8de1d4fc995a
syeo66 <syeo66@users.noreply.github.com>
2025-12-16 09:56:33 +0000

    Deploy preview for PR 2577 🛫

---
+++ pr-preview/pr-2577/new/assets/Box.stories-DAgK5YhJ.js
@@ -0,0 +1,17 @@
+import{j as t}from"./iframe-CEbMwow_.js";import{D as m}from"./DecorativeContent-D-aQifII.js";import{B as a}from"./Box-B66_1jpZ.js";import"./preload-helper-BF13SaVy.js";const h={title:"Layout/Box",component:a,tags:["autodocs"],parameters:{status:{type:"released"}},args:{width:25,height:25}},e={render:s=>t.jsx(a,{...s,children:t.jsx(m,{})})},r={args:{width:"100px",height:"100px"},render:s=>t.jsx(a,{...s,children:t.jsx(m,{})})};var o,n,c;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: args => {
+    return <Box {...args}>
+                <DecorativeContent />
+            </Box>;
+  }
+}`,...(c=(n=e.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var i,p,d;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    width: '100px',
+    height: '100px'
+  },
+  render: args => {
+    return <Box {...args}>
+                <DecorativeContent />
+            </Box>;
+  }
+}`,...(d=(p=r.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};const B=["Default","PixelValues"];export{e as Default,r as PixelValues,B as __namedExportsOrder,h as default};
---
+++ pr-preview/pr-2577/tokens/assets/client-C6Z0GhDF.js
@@ -0,0 +1,1 @@
+import{b as r}from"./iframe-DsFgeDrg.js";var t,o=r;t=o.createRoot,o.hydrateRoot;export{t as c};
