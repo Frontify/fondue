a742984d4e444e9cccded4e605eab39f45ebeea9
syeo66 <syeo66@users.noreply.github.com>
2025-12-17 12:56:24 +0000

    Deploy preview for PR 2585 🛫

---
+++ pr-preview/pr-2585/new/assets/formatter-QJ4M4OGQ-CVqjyRWz.js
@@ -0,0 +1,1 @@
+import{e as i,f as a,h as m}from"./iframe-BlmJ5ied.js";import"./preload-helper-C3tmUAw0.js";var t=i(m(),1),p=(0,t.default)(2)(async(r,e)=>r===!1?e:a(e));export{p as formatter};
---
+++ pr-preview/pr-2585/tokens/assets/formatter-QJ4M4OGQ-C46l1-C3.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./DocsRenderer-GHJI37HO-11i89k3g.js";import{N as o}from"./iframe-BmlEux5_.js";import"./preload-helper-Bv-k8cDi.js";import"./react-18-XOwwWrEa.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
