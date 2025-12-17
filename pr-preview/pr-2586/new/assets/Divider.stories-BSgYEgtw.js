8838f872b955047c5c325a8588c7e927b1f21c38
syeo66 <syeo66@users.noreply.github.com>
2025-12-17 13:48:08 +0000

    Deploy preview for PR 2586 🛫

---
+++ pr-preview/pr-2586/new/assets/Divider.stories-BSgYEgtw.js
@@ -0,0 +1,59 @@
+import{j as t}from"./iframe-FgFRzHZg.js";import{D as A}from"./Divider-D84dFlZU.js";import"./preload-helper-BDtrOIoV.js";import"./index-B8fYahwT.js";const J={title:"Components/Divider",component:A,tags:["autodocs"],args:{color:"default",variant:"solid",padding:"medium",direction:"horizontal"},parameters:{status:{type:"released"}},decorators:[q=>t.jsx("div",{className:"tw-h-8",children:t.jsx(q,{})})]},r={args:{}},e={args:{color:"strong"}},a={args:{variant:"dashed"}},o={args:{padding:"large"}},s={args:{direction:"vertical"}};var n,i,d;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{code:`const Primary = () => (
+  <Divider
+    color="default"
+    variant="solid"
+    padding="medium"
+    direction="horizontal"
+  ></Divider>
+);
+`,...(d=(i=r.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var c,m,p;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{code:`const CustomColor = () => (
+  <Divider
+    color="strong"
+    variant="solid"
+    padding="medium"
+    direction="horizontal"
+  ></Divider>
+);
+`,...(p=(m=e.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var l,u,g;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{code:`const CustomStyle = () => (
+  <Divider
+    color="default"
+    variant="dashed"
+    padding="medium"
+    direction="horizontal"
+  ></Divider>
+);
+`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var v,h,C;o.parameters={...o.parameters,docs:{...(v=o.parameters)==null?void 0:v.docs,source:{code:`const CustomHeight = () => (
+  <Divider
+    color="default"
+    variant="solid"
+    padding="large"
+    direction="horizontal"
+  ></Divider>
+);
+`,...(C=(h=o.parameters)==null?void 0:h.docs)==null?void 0:C.source}}};var D,f,S;s.parameters={...s.parameters,docs:{...(D=s.parameters)==null?void 0:D.docs,source:{code:`const Vertical = () => (
+  <Divider
+    color="default"
+    variant="solid"
+    padding="medium"
+    direction="vertical"
+  ></Divider>
+);
+`,...(S=(f=s.parameters)==null?void 0:f.docs)==null?void 0:S.source}}};var y,x,z;r.parameters={...r.parameters,docs:{...(y=r.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {}
+}`,...(z=(x=r.parameters)==null?void 0:x.docs)==null?void 0:z.source}}};var j,H,P;e.parameters={...e.parameters,docs:{...(j=e.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    color: 'strong'
+  }
+}`,...(P=(H=e.parameters)==null?void 0:H.docs)==null?void 0:P.source}}};var V,E,_;a.parameters={...a.parameters,docs:{...(V=a.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    variant: 'dashed'
+  }
+}`,...(_=(E=a.parameters)==null?void 0:E.docs)==null?void 0:_.source}}};var w,N,O;o.parameters={...o.parameters,docs:{...(w=o.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    padding: 'large'
+  }
+}`,...(O=(N=o.parameters)==null?void 0:N.docs)==null?void 0:O.source}}};var R,b,k;s.parameters={...s.parameters,docs:{...(R=s.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    direction: 'vertical'
+  }
+}`,...(k=(b=s.parameters)==null?void 0:b.docs)==null?void 0:k.source}}};const K=["Primary","CustomColor","CustomStyle","CustomHeight","Vertical"];export{e as CustomColor,o as CustomHeight,a as CustomStyle,r as Primary,s as Vertical,K as __namedExportsOrder,J as default};
