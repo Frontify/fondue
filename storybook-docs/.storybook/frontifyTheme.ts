91c53e2dd998d8bf260c3e9c8eeec9f68b2c5cc7
Noah Waldner <noahwaldner@users.noreply.github.com>
2025-12-17 09:47:21 +0100

    feat: upgrade storybook (#2579)

--- storybook-docs/.storybook/frontifyTheme.ts
+++ storybook-docs/.storybook/frontifyTheme.ts
@@ -1,6 +1,6 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { create } from '@storybook/theming/create';
+import { create } from 'storybook/theming/create';
 
 export const dark = create({
     base: 'dark',
