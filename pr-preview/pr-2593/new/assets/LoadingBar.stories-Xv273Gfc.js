3367b28186ad4d446960f812c717a4164123d5d5
syeo66 <syeo66@users.noreply.github.com>
2026-01-07 07:27:26 +0000

    Deploy preview for PR 2593 🛫

---
+++ pr-preview/pr-2593/new/assets/LoadingBar.stories-Xv273Gfc.js
@@ -0,0 +1,21 @@
+import{L as g}from"./LoadingBar-CIoqun8e.js";import"./iframe-K5_82yzx.js";import"./preload-helper-BWcRnP3k.js";import"./index-C7DqOYZ_.js";const x={title:"Components/Loading Bar",component:g,tags:["autodocs"],parameters:{status:{type:"released"}},args:{value:42,max:100,rounded:!0,"aria-label":"Fondue Loading Bar"}},e={},a={args:{value:null}};var r,n,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{code:`const WithDefinedValue = () => (
+  <LoadingBar
+    value={42}
+    max={100}
+    rounded
+    aria-label="Fondue Loading Bar"
+  ></LoadingBar>
+);
+`,...(o=(n=e.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};var s,t,d;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{code:`const Indeterminate = () => (
+  <LoadingBar
+    value={null}
+    max={100}
+    rounded
+    aria-label="Fondue Loading Bar"
+  ></LoadingBar>
+);
+`,...(d=(t=a.parameters)==null?void 0:t.docs)==null?void 0:d.source}}};var c,m,u;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(u=(m=e.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};var i,p,l;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    value: null
+  }
+}`,...(l=(p=a.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};const b=["WithDefinedValue","Indeterminate"];export{a as Indeterminate,e as WithDefinedValue,b as __namedExportsOrder,x as default};
---
+++ pr-preview/pr-2593/new/assets/useSyncRefs-B8mYf5XH.js
@@ -0,0 +1,1 @@
+import{r as t}from"./iframe-K5_82yzx.js";import{s as e}from"./domUtilities-DGDVwu3_.js";const c=(s,r)=>{t.useEffect(()=>{e(s,r)},[s])};export{c as u};
