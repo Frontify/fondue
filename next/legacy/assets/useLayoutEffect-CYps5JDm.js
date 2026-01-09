d3986eb8050b45051e53173f8818a7fc4fdc7747
noahwaldner <noahwaldner@users.noreply.github.com>
2026-01-09 15:40:08 +0000

    Deploying to gh-pages from @ Frontify/fondue@f62de743188570ce7fe3cb1b9e3a00f2823e7354 🚀

---
+++ next/legacy/assets/useLayoutEffect-CYps5JDm.js
@@ -0,0 +1,1 @@
+import{a as e}from"./iframe--JUoSY6h.js";const a=typeof document<"u"?e.useLayoutEffect:()=>{};export{a as $};
--- next/legacy/assets/useLayoutEffect-DWlZvDTD.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./iframe-1Dzly0tf.js";const a=typeof document<"u"?e.useLayoutEffect:()=>{};export{a as $};
