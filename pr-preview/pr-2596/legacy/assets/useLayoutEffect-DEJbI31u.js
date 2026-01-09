07bc318d3243cfc6f851f7be1eeba411c7db8953
noahwaldner <noahwaldner@users.noreply.github.com>
2026-01-09 15:37:46 +0000

    Remove preview for PR 2596 🛬

--- pr-preview/pr-2596/legacy/assets/useLayoutEffect-DEJbI31u.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./iframe-B5u-MPmA.js";const a=typeof document<"u"?e.useLayoutEffect:()=>{};export{a as $};
--- pr-preview/pr-2596/rte/assets/formatter-QJ4M4OGQ-DvVKYMq8.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as o}from"./DocsRenderer-GHJI37HO-RvhyYuqD.js";import{J as t}from"./iframe-BUmsAK4z.js";import"./preload-helper-DCZ13Lzk.js";import"./react-18-DGgI6sOH.js";import"./index-C--JYG-m.js";import"./index-DD6OLoxJ.js";var e=t(o(),1),c=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{c as formatter};
