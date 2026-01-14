e2a26d5e1f7b01f1674de19b39c42b1cffc8cd94
noahwaldner <noahwaldner@users.noreply.github.com>
2026-01-14 12:33:57 +0000

    Deploy preview for PR 2601 🛫

---
+++ pr-preview/pr-2601/new/assets/Section.stories-BBAfaptF.js
@@ -0,0 +1,6 @@
+import{S as n}from"./Section-DhUWiYeA.js";import"./iframe-BRSZf0QU.js";import"./preload-helper-352D8iK5.js";const p={title:"Layout/Section",component:n,tags:["autodocs"],parameters:{status:{type:"released"}},args:{}},e={args:{children:"Hello World"}};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{code:`const Default = () => <Section>Hello World</Section>;
+`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};var a,s,c;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: 'Hello World'
+  }
+}`,...(c=(s=e.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};const i=["Default"];export{e as Default,i as __namedExportsOrder,p as default};
---
+++ pr-preview/pr-2601/tokens/assets/formatter-QJ4M4OGQ-DKspXCb8.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./DocsRenderer-GHJI37HO-aCC69BvI.js";import{N as o}from"./iframe-BXytnNAg.js";import"./preload-helper-fcmwSJAi.js";import"./react-18-0-zbkfc4.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
