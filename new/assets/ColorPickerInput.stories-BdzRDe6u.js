98abf74d7cfad6f2539dc981a6f23d26e0392509
syeo66 <red@redochsenbein.ch>
2026-02-10 20:12:36 +0000

    Deploying to gh-pages from @ Frontify/fondue@0cebed2479e80231d563763febc6757ec68e343f 🚀

---
+++ new/assets/ColorPickerInput.stories-BdzRDe6u.js
@@ -0,0 +1,44 @@
+import{j as n}from"./iframe-7Vp90Q64.js";import{C as E,a as s}from"./ColorPicker-B7fGkBGL.js";import"./preload-helper-C1FmrZbK.js";import"./fondue-icons372-POAVWW0X.js";import"./useTranslation-CGINz5YD.js";import"./Button-CnqO6I3L.js";import"./index-CUd9mNfL.js";import"./index-C2QNke-2.js";import"./index-CKZNW8I5.js";import"./index-DXVVoL9W.js";import"./index-CaSelV5N.js";import"./index-DuUpH-yH.js";import"./Combination-DWQD4C_F.js";import"./LoadingCircle-Dy-yCqGk.js";import"./Badge-CLpGeneT.js";import"./TextInput-CUVNY8Ce.js";const K={title:"Components/ColorPicker/Input",component:E,tags:["autodocs"],parameters:{status:{type:"released"}},args:{}},r={args:{currentColor:{red:230,green:100,blue:50,alpha:.8,name:"custom-color"}},render:a=>n.jsx(s.Input,{...a,onClear:()=>{}})},e={args:{},render:a=>n.jsx(s.Input,{...a,onClear:()=>{}})},o={args:{},render:a=>n.jsx(s.Input,{...a,disabled:!0,onClear:()=>{}})};var t,c,p;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{code:`const Default = () => {
+  return (
+    <ColorPicker.Input
+      currentColor={{
+        red: 230,
+        green: 100,
+        blue: 50,
+        alpha: 0.8,
+        name: "custom-color",
+      }}
+      onClear={() => {}}
+    />
+  );
+};
+`,...(p=(c=r.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var m,u,l;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{code:`const Empty = () => {
+  return <ColorPicker.Input onClear={() => {}} />;
+};
+`,...(l=(u=e.parameters)==null?void 0:u.docs)==null?void 0:l.source}}};var d,i,C;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{code:`const Disabled = () => {
+  return <ColorPicker.Input disabled onClear={() => {}} />;
+};
+`,...(C=(i=o.parameters)==null?void 0:i.docs)==null?void 0:C.source}}};var g,I,b;r.parameters={...r.parameters,docs:{...(g=r.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    currentColor: {
+      red: 230,
+      green: 100,
+      blue: 50,
+      alpha: 0.8,
+      name: 'custom-color'
+    }
+  },
+  render: args => {
+    return <ColorPicker.Input {...args} onClear={() => {}} />;
+  }
+}`,...(b=(I=r.parameters)==null?void 0:I.docs)==null?void 0:b.source}}};var k,P,x;e.parameters={...e.parameters,docs:{...(k=e.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <ColorPicker.Input {...args} onClear={() => {}} />;
+  }
+}`,...(x=(P=e.parameters)==null?void 0:P.docs)==null?void 0:x.source}}};var f,D,j;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <ColorPicker.Input {...args} disabled onClear={() => {}} />;
+  }
+}`,...(j=(D=o.parameters)==null?void 0:D.docs)==null?void 0:j.source}}};const L=["Default","Empty","Disabled"];export{r as Default,o as Disabled,e as Empty,L as __namedExportsOrder,K as default};
