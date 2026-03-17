4ce5e6f4096ffc50c5fd2f3cceae07524ade2eeb
Samuel Alev <samuel.alev@frontify.com>
2026-03-17 23:33:52 +0100

    fix: remove invalid class generation

--- packages/rte/tailwind.config.js
+++ packages/rte/tailwind.config.js
@@ -7,6 +7,9 @@ import frontifyTailwindConfig from '@frontify/fondue-tokens/tailwind';
 export default {
     presets: [frontifyTailwindConfig, legacyFondueTailwindConfig],
     darkMode: '',
-    content: ['./src/**/*.{js,ts,tsx}'],
+    content: [
+        './src/**/*.{js,ts,tsx}',
+        '!./src/components/RichTextEditor/serializer/markdown/__tests__/fixtures/mentions.ts',
+    ],
     prefix: 'tw-',
 };
