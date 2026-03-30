43316f646b93151d93291627ae449c11c43fe1f9
Samuel Alev <samuel.alev@frontify.com>
2026-03-30 10:37:54 +0200

    fix: ignore file during linting

--- packages/tokens/eslint.config.mjs
+++ packages/tokens/eslint.config.mjs
@@ -12,7 +12,7 @@ import tailwindcssPlugin from 'eslint-plugin-tailwindcss';
 
 export default defineConfig(
     {
-        ignores: ['tmp/', 'dist/', 'node_modules/'],
+        ignores: ['tmp/', 'dist/', 'node_modules/', 'availableTokens.json'],
     },
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     frontifyConfig,
