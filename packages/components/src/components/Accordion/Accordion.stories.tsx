e59ef7354b23b2a804eb269997187fedcdf6d894
Red Ochsenbein <red.ochsenbein@frontify.com>
2026-06-03 13:53:28 +0200

    fix(Accordion): fix stuck sticky styles

--- packages/components/src/components/Accordion/Accordion.stories.tsx
+++ packages/components/src/components/Accordion/Accordion.stories.tsx
@@ -104,7 +104,7 @@ export const Default: Story = {
                             Item with icon
                         </Flex>
                     </Accordion.Header>
-                    <Accordion.Content divider>
+                    <Accordion.Content>
                         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
                         ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
                         dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
