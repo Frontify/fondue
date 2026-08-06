72028b1545b9f9810f48b025033152ceaca55fac
Red Ochsenbein <red.ochsenbein@frontify.com>
2026-08-06 09:27:54 +0200

    fix(Tabs): allow Tabs to be disabled after init (#2818)

--- packages/components/src/components/Tabs/types.ts
+++ packages/components/src/components/Tabs/types.ts
@@ -5,7 +5,6 @@ import { type HTMLAttributes, type ReactNode } from 'react';
 export type TabTrigger = {
     ref?: { current: HTMLButtonElement | null };
     element: ReactNode;
-    previousElement?: ReactNode;
     props: HTMLAttributes<HTMLButtonElement>;
     disabled?: boolean;
     value: string;
